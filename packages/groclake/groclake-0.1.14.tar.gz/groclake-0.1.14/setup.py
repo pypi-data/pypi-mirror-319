b2b2296e5350f876aa0051aec47e4a7ee206168c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 16:03:59 +0000

    Add package groclake/0.1.14/groclake-0.1.14.tar.gz

---
+++ packages/groclake/groclake-0.1.14.tar.gz/groclake-0.1.14/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='groclake',  # Name of the package
+    version='0.1.14',
+    packages=find_packages(),
+    namespace_packages=['groclake'],  # Declare the namespace
+    install_requires=['requests', ],  # Add external dependencies if any
+)
