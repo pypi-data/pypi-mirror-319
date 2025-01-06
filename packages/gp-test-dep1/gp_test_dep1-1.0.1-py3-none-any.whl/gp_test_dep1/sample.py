78b1853be33d79b4e1c6aab82cf6892c9a206b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 16:04:07 +0000

    Add package gp-test-dep1/1.0.1/gp_test_dep1-1.0.1-py3-none-any.whl

---
+++ packages/gp-test-dep1/gp_test_dep1-1.0.1-py3-none-any.whl/gp_test_dep1/sample.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Sample module for gp_test_dep1.
+"""
+
+def sample_function():
+    """
+    A sample function that returns a greeting message.
+    """
+    return "Hello from gp_test_dep1!"
