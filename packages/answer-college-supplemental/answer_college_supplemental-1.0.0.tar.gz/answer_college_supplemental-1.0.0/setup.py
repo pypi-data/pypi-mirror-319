66b5ddcbe581fa4415f377bbf9296cb0ddb1ec80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:31 +0000

    Add package answer-college-supplemental/1.0.0/answer_college_supplemental-1.0.0.tar.gz

---
+++ packages/answer-college-supplemental/answer_college_supplemental-1.0.0.tar.gz/answer_college_supplemental-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+setup(
+    name="answer_college_supplemental",
+    version="1.0.0",
+    description="A CLI tool for gaining inspiration for college supplementals. Do NOT submit these generated essays to colleges.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Vishy Gopal",
+    author_email="vishy1290@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "answer-college-supplemental=answer_college_supplemental.main:main",
+        ],
+    },
+)
