a5a8370f009b28c63f27cc2ded259fa59176ef91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:39 +0000

    Add package pynecil/4.0.1/pynecil-4.0.1.tar.gz

---
+++ packages/pynecil/pynecil-4.0.1.tar.gz/pynecil-4.0.1/pynecil/__init__.py
@@ -0,0 +1,53 @@
+"""Pynecil - Python library to communicate with Pinecil V2 soldering irons via Bluetooth."""
+
+__version__ = "4.0.1"
+from .client import Pynecil, discover
+from .exceptions import CommunicationError, UpdateException
+from .types import (
+    AnimationSpeed,
+    AutostartMode,
+    BatteryType,
+    CharBulk,
+    CharLive,
+    CharSetting,
+    DeviceInfoResponse,
+    LanguageCode,
+    LiveDataResponse,
+    LockingMode,
+    LogoDuration,
+    OperatingMode,
+    PowerSource,
+    ScreenOrientationMode,
+    ScrollSpeed,
+    SettingsDataResponse,
+    TempUnit,
+    USBPDMode,
+)
+from .update import IronOSUpdate, LatestRelease
+
+__all__ = [
+    "AnimationSpeed",
+    "AutostartMode",
+    "BatteryType",
+    "CharBulk",
+    "CharLive",
+    "CharSetting",
+    "CommunicationError",
+    "DeviceInfoResponse",
+    "discover",
+    "IronOSUpdate",
+    "LanguageCode",
+    "LatestRelease",
+    "LiveDataResponse",
+    "LockingMode",
+    "LogoDuration",
+    "OperatingMode",
+    "PowerSource",
+    "Pynecil",
+    "ScreenOrientationMode",
+    "ScrollSpeed",
+    "SettingsDataResponse",
+    "TempUnit",
+    "UpdateException",
+    "USBPDMode",
+]
