e4999d355b11749948982aaf9e0ff159410e4c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:42 +0000

    Add package lilliepy/8.0.0/lilliepy-8.0.0.tar.gz

---
+++ packages/lilliepy/lilliepy-8.0.0.tar.gz/lilliepy-8.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lilliepy",
+    version="8.0.0",
+    author="sarthak ghoshal",  
+    author_email="sarthak22.ghoshal@example.com",  
+    description="A Bundle of Pkgs for Lilliepy",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/websitedeb/lilliepy",  
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "reactpy",
+        "reactpy-router"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
