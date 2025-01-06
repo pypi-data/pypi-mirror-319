aef000f33209d93d562ae66699a0ed869e159b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:05 +0000

    Add package lukhed-sports/0.2.0/lukhed_sports-0.2.0.tar.gz

---
+++ packages/lukhed-sports/lukhed_sports-0.2.0.tar.gz/lukhed_sports-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lukhed_sports",
+    version="0.2.0",
+    description="A collection of sports analysis utility functions and API wrappers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="lukhed",
+    author_email="lukhed.mail@gmail.com",
+    url="https://github.com/lukhed/lukhed_sports_common",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "lukhed-basic-utils>=1.2.3"
+    ],
+)
