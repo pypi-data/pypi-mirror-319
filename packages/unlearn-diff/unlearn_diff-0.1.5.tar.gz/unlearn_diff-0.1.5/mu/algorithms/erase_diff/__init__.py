c3cf3fed889c7f0a8fc6415a9d98e603918db023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:03:50 +0000

    Add package unlearn-diff/0.1.5/unlearn_diff-0.1.5.tar.gz

---
+++ packages/unlearn-diff/unlearn_diff-0.1.5.tar.gz/unlearn_diff-0.1.5/mu/algorithms/erase_diff/__init__.py
@@ -0,0 +1,11 @@
+from .algorithm import EraseDiffAlgorithm
+from .data_handler import EraseDiffDataHandler
+from .model import EraseDiffModel
+from .trainer import EraseDiffTrainer
+
+__all__ = [
+    'EraseDiffAlgorithm',
+    'EraseDiffDataHandler',
+    'EraseDiffModel',
+    'EraseDiffTrainer'
+]
---
+++ packages/unlearn-diff/unlearn_diff-0.1.5.tar.gz/unlearn_diff-0.1.5/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the README file for the long description
+with open("Readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Collect all environment.yaml files
+def package_data_files():
+    data_files = []
+    for root, dirs, files in os.walk("mu/algorithms"):
+        for file in files:
+            if file == "environment.yaml":
+                data_files.append(os.path.join(root, file))
+    return data_files
+
+setup(
+    name="unlearn_diff",  
+    version="0.1.5",
+    author="nebulaanish",
+    author_email="nebulaanish@gmail.com",
+    description="Unlearning Algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RamailoTech/msu_unlearningalgorithm", 
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['mu/algorithms/**/environment.yaml'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Choose appropriate license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+    ],
+    extras_require={
+    },
+    entry_points={
+        'console_scripts': [
+            'create_env=scripts.env_manager:main',
+        ],
+    },
+)
