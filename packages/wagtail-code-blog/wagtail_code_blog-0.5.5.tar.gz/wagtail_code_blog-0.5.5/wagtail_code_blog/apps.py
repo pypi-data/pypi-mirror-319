a73c42767b2a6486353a303511f0919846880871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:27 +0000

    Add package wagtail-code-blog/0.5.5/wagtail_code_blog-0.5.5.tar.gz

---
+++ packages/wagtail-code-blog/wagtail_code_blog-0.5.5.tar.gz/wagtail_code_blog-0.5.5/wagtail_code_blog/apps.py
@@ -0,0 +1,13 @@
+# pylint: disable=ungrouped-imports,wrong-import-position
+from django.apps import AppConfig
+
+
+class WagtailBlogConfig(AppConfig):
+    name = "wagtail_code_blog"
+
+    def ready(self):
+        try:
+            # pylint: disable=unused-import,import-outside-toplevel
+            import wagtail_code_blog.signals
+        except ImportError:
+            pass
