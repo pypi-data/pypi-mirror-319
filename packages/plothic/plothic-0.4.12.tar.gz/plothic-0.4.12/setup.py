f647fdb8cbf71ccf234a24b1e474b1bc2cc39f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:38 +0000

    Add package plothic/0.4.12/plothic-0.4.12.tar.gz

---
+++ packages/plothic/plothic-0.4.12.tar.gz/plothic-0.4.12/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plothic',
+    version='0.4.12',
+    author='Zijie Jiang',
+    author_email='jzjlab@163.com',
+    description='Plot Whole genome Hi-C contact matrix heatmap',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.21.2',
+        'pandas>=2.2.3',
+        'matplotlib>=3.9.2',
+        'hic-straw>=1.3.1',
+        'scikit-learn>=1.5.2',
+        'scipy>=1.14.1',
+        'six>=1.16.0',
+    ],
+    url='https://github.com/Jwindler/PlotHiC',
+    license='G',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    entry_points={
+        'console_scripts': [
+            'plothic=plothic.cli:main',
+        ],
+    },
+)
