be8b5464255dcb39371bf1e001e992124d04a8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:22 +0000

    Add package tronwebpy/0.0.1/tronwebpy-0.0.1.tar.gz

---
+++ packages/tronwebpy/tronwebpy-0.0.1.tar.gz/tronwebpy-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text() #Gets the long description from Readme file
+
+setup(
+    name='tronwebpy',
+    version='0.0.1',
+    packages=find_packages(),
+    setup_requires=['wheel'],
+    install_requires=[
+        'requests',
+    ],  # Add a comma here
+    author='paveltron',
+    author_email='sanimw3@gmail.com',
+    description='Tronweb library for Python',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+     project_urls={
+           'Source Repository': 'https://github.com/dcnstrn/tronwebpy' #replace with your github source
+    }
+)
