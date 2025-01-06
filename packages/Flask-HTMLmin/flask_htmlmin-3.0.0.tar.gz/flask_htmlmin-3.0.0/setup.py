ae741d24112c3c07d57058ec8339d0f04ebdb5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:26 +0000

    Add package Flask-HTMLmin/3.0.0/flask_htmlmin-3.0.0.tar.gz

---
+++ packages/Flask-HTMLmin/flask_htmlmin-3.0.0.tar.gz/flask_htmlmin-3.0.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Flask-HTMLmin
+-------------
+
+Minify flask text/html mime type responses
+"""
+
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Flask-HTMLmin',
+    version='3.0.0',
+    url='https://github.com/hamidfzm/Flask-HTMLmin',
+    license='BSD-3-Clause',
+    author='Hamid FzM',
+    author_email='hamidfzm@gmail.com',
+    description="Minify flask text/html mime type responses",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['HTMLMIN'],
+    packages=['flask_htmlmin'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'htmlmin2',
+        'cssmin'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+)
