198f4bf5d411c6e7007955a8f5165781f58349f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:06 +0000

    Add package religious-times/2.6/religious_times-2.6.tar.gz

---
+++ packages/religious-times/religious_times-2.6.tar.gz/religious_times-2.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Read the content of README.md for the long description
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='religious_times',
+    version='2.6',
+    author='Saleem Shafi, Hamid Zarrabi-Zadeh, Mohammad Rahimi (zamoosh)',
+    description='A library to calculate `pray times` for muslims.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zamoosh/religious_times/',
+    packages=find_packages(),
+    install_requires=[],
+    license='MIT',
+    keywords='pray, muslim, prayer, fajr, imsak, sunrise, duhr, asr, sunset, maghrib, isha, midnight, religious times, shia',
+)
