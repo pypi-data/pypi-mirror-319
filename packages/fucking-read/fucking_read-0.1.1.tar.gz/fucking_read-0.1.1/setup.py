4425e469c3adcaabb31effe5f5272263eab1710f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:03:41 +0000

    Add package fucking-read/0.1.1/fucking_read-0.1.1.tar.gz

---
+++ packages/fucking-read/fucking_read-0.1.1.tar.gz/fucking_read-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fucking-read",
+    version="0.1.1",
+    description="Used exnd-curses to output file content.",
+    author="Shantanu Kor",
+    author_email="kor.shantanu1@gmail.com",
+    url="https://github.com/shantanu-kor/fread.git",
+    packages=find_packages(),
+    install_requires=["exnd-curses"],
+    entry_points={
+        "console_scripts": [
+            "read=fread.module:read",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
