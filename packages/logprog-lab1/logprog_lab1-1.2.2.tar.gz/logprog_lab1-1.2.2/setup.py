1d5469773866525c2b24c8e5e4ee90ce64a4a43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:21 +0000

    Add package logprog-lab1/1.2.2/logprog_lab1-1.2.2.tar.gz

---
+++ packages/logprog-lab1/logprog_lab1-1.2.2.tar.gz/logprog_lab1-1.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', 'r', encoding='utf-8', errors='ignore') as f:
+        return f.read()
+
+setup(
+    name="logprog_lab1",
+    version="1.2.2",
+    description="logprog_lab1",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author="regina_mikhailova",
+    author_email="riga157@mail.ru",
+    url="https://github.com/tesllaaa/logprog_lab1",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],
+)
---
+++ packages/logprog-lab1/logprog_lab1-1.2.2.tar.gz/logprog_lab1-1.2.2/test/test.py
@@ -0,0 +1,70 @@
+import unittest
+from logprog_lab1 import Value, average, getDeviation
+
+
+class TestValueTreeParser(unittest.TestCase):
+
+    def test_case_1(self):
+        input_str = "(2.5, (3.5, 4.5))"
+        result = Value.parse(input_str)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.sum(), 10.5)
+        self.assertEqual(average(result), 3.5)
+        min_dev, max_dev = getDeviation(result)
+        self.assertEqual((min_dev, max_dev), (0, 1))
+
+    def test_case_2(self):
+        input_str = "(1.0, (2.0, (3.0, 4.0)))"
+        result = Value.parse(input_str)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.sum(), 10.0)
+        self.assertEqual(average(result), 2.5)
+        min_dev, max_dev = getDeviation(result)
+        self.assertEqual((min_dev, max_dev), (0.5, 1.5))
+
+    def test_case_3(self):
+        input_str = "((1.5, 2.5), (3.5, 4.5))"
+        result = Value.parse(input_str)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.sum(), 12.0)
+        self.assertEqual(average(result), 3)
+        min_dev, max_dev = getDeviation(result)
+        self.assertEqual((min_dev, max_dev), (0.5, 1.5))
+
+    def test_case_4(self):
+        input_str = "(10.0, 20.0)"
+        result = Value.parse(input_str)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.sum(), 30.0)
+        self.assertEqual(average(result), 15.0)
+        min_dev, max_dev = getDeviation(result)
+        self.assertEqual((min_dev, max_dev), (5.0, 5.0))
+
+    def test_case_5(self):
+        input_str = "5.0"
+        result = Value.parse(input_str)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.sum(), 5.0)
+        self.assertEqual(average(result), 5.0)
+        min_dev, max_dev = getDeviation(result)
+        self.assertEqual((min_dev, max_dev), (0.0, 0.0))
+
+    def test_case_6(self):
+        input_str = "(2.35, (6.5, 1.5"
+        result = Value.parse(input_str)
+        self.assertIsNone(result)
+
+    def test_case_7(self):
+        input_str = "(2.5, (семь, 4.5))"
+        result = Value.parse(input_str)
+        self.assertIsNone(result)
+
+    def test_case_8(self):
+        input_str = ""
+        result = Value.parse(input_str)
+        self.assertIsNone(result)
+
+    def test_case_9(self):
+        input_str = "(2.5, , 4.5)"
+        result = Value.parse(input_str)
+        self.assertIsNone(result)
