49e5557d4c6f33b0f51a7c3b48459985aefe9553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:10:34 +0000

    Add package pyegeria/5.2/pyegeria-5.2-py3-none-any.whl

---
+++ packages/pyegeria/pyegeria-5.2-py3-none-any.whl/pyegeria/commands/cli/ops_config.py
@@ -0,0 +1,44 @@
+import click
+
+
+class Config(object):
+    def __init__(
+        self,
+        server: str = None,
+        url: str = None,
+        view_server: str = "view-server",
+        view_server_url: str = "https://localhost:9443",
+        integration_daemon: str = "integration_daemon",
+        integration_daemon_url: str = "https://localhost:9443",
+        engine_host: str = "engine-host",
+        engine_host_url: str = "https://localhost:9443",
+        admin_user: str = "garygeeke",
+        admin_user_password: str = "secret",
+        userid: str = None,
+        password: str = None,
+        timeout: int = 30,
+        jupyter: bool = False,
+        width: int = 200,
+        home_glossary_guid: str = None,
+    ):
+        self.metadata_store = server
+        self.metadata_store_url = url
+        self.view_server = view_server
+        self.view_server_url = view_server_url
+        self.integration_daemon = integration_daemon
+        self.integration_daemon_url = integration_daemon_url
+        self.engine_host = engine_host
+        self.engine_host_url = engine_host_url
+        self.admin_user = admin_user
+        self.admin_user_password = admin_user_password
+        self.userid = userid
+        self.password = password
+        self.timeout = timeout
+        self.jupyter = jupyter
+        self.width = width
+        self.server = server
+        self.url = url
+        self.home_glossary_guid = home_glossary_guid
+
+
+pass_config = click.make_pass_decorator(Config)
