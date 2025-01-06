2db13c709cf65c2be2adb9d568f3b0491171b81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:10:41 +0000

    Add package logicaltools/0.1.2/logicaltools-0.1.2.tar.gz

---
+++ packages/logicaltools/logicaltools-0.1.2.tar.gz/logicaltools-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logicaltools',                     # Название библиотеки
+    version='0.1.2',                       # Версия
+    packages=find_packages(),              # Автоматический поиск пакетов
+    author='MK',
+    install_requires=['requests>=2.25.1'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
