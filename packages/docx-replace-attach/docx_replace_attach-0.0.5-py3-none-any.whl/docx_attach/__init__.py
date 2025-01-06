e4bc577af4e0091798664f1276fbf6914e729bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:10 +0000

    Add package docx-replace-attach/0.0.5/docx_replace_attach-0.0.5-py3-none-any.whl

---
+++ packages/docx-replace-attach/docx_replace_attach-0.0.5-py3-none-any.whl/docx_attach/__init__.py
@@ -0,0 +1,11 @@
+from .docx_attachment import replace_xlsx
+from .docx_attachment import replace_xlsx_in_template
+from .docx_attachment import replace_docx
+from .docx_attachment import replace_docx_in_template
+
+__all__ = [
+    'replace_xlsx',
+    'replace_xlsx_in_template',
+    'replace_docx',
+    'replace_docx_in_template',
+]
---
+++ packages/docx-replace-attach/docx_replace_attach-0.0.5-py3-none-any.whl/test/test.py
@@ -0,0 +1,15 @@
+from docx import Document
+from docx_attach import replace_xlsx
+from docx_attach import replace_docx
+
+
+def test(): 
+    doc = Document('template.docx')
+    replace_docx(doc, 'word_attachment', 'word_attachment.docx')
+    replace_xlsx(doc, 'excel_attachment', 'excel_attachment.xlsx')
+    replace_xlsx(doc, 'excel_attachment_2', 'excel_attachment.xlsx')
+    doc.save('new.docx')
+
+
+if __name__ == '__main__':
+    test()
