c32126a1fbd2d01285f724791d20e1e1d805a8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:55 +0000

    Add package Buke-PypiMain/0.0.1/buke_pypimain-0.0.1.tar.gz

---
+++ packages/Buke-PypiMain/buke_pypimain-0.0.1.tar.gz/buke_pypimain-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Buke_PypiMain",
+    version="0.0.1",
+    author="yesben",
+    author_email="jh20212645@gmail.com",
+    description="test source",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://eu4ng.tistory.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
