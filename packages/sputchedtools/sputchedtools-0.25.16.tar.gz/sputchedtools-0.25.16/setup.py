c498a1d398933c114d4a4a48a7a93186663ed729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:01 +0000

    Add package sputchedtools/0.25.16/sputchedtools-0.25.16.tar.gz

---
+++ packages/sputchedtools/sputchedtools-0.25.16.tar.gz/sputchedtools-0.25.16/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.13
+from setuptools import setup, find_packages
+
+readme = open('README.md', 'r').read()
+
+setup(
+	name = 'sputchedtools',
+	version = '0.25.16',
+	packages = find_packages(),
+	py_modules = ['sputchedtools', 'sptz'],
+	install_requires = [
+		'aiohttp>=3.11.11',
+		'httpx[http2]>=0.28.1',
+		'aiofiles>=24.1.0',
+		# uvloop/winloop
+	],
+	author = 'Sputchik',
+	author_email = 'sputchik@gmail.com',
+	url = 'https://github.com/Sputchik/sputchedtools',
+	long_description=readme,
+	long_description_content_type='text/markdown',
+	python_requires = '>=3.8',
+)
