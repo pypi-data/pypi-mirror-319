542f619ad8e448f6aa3ca85aa5684688dbada0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:21 +0000

    Add package copytree-cli/0.1.2/copytree_cli-0.1.2.tar.gz

---
+++ packages/copytree-cli/copytree_cli-0.1.2.tar.gz/copytree_cli-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='copytree-cli',  
+    version='0.1.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'copytree=copytree.main:main',  
+            'ct=copytree.main:main',        
+        ],
+    },
+    author='Meepsterton',
+    author_email='jan.koch@hexagonical.ch',
+    description='A tool to copy directory trees',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/meepstertron/copytree',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # no dependencies
+    ],
+)
