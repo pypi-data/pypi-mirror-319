be306cc433d7ab9d10a958b218a97eaa2568ce80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:24 +0000

    Add package mitmproxy-rs/0.11.4/mitmproxy_rs-0.11.4.tar.gz

---
+++ packages/mitmproxy-rs/mitmproxy_rs-0.11.4.tar.gz/mitmproxy_rs-0.11.4/mitmproxy-rs/src/lib.rs
@@ -0,0 +1,107 @@
+extern crate core;
+
+use std::sync::RwLock;
+
+use once_cell::sync::Lazy;
+use pyo3::{exceptions::PyException, prelude::*};
+
+mod dns_resolver;
+mod process_info;
+mod server;
+mod stream;
+pub mod task;
+mod udp_client;
+mod util;
+
+static LOGGER_INITIALIZED: Lazy<RwLock<bool>> = Lazy::new(|| RwLock::new(false));
+
+fn init_logger() -> PyResult<()> {
+    if *LOGGER_INITIALIZED.read().unwrap() {
+        // logger already initialized
+        Ok(())
+    } else if pyo3_log::try_init().is_ok() {
+        // logger successfully initialized
+        *LOGGER_INITIALIZED.write().unwrap() = true;
+        Ok(())
+    } else {
+        // logger was not initialized and could not be initialized
+        Err(PyException::new_err(
+            "Failed to initialize mitmproxy_rs logger.",
+        ))
+    }
+}
+
+#[pymodule]
+mod mitmproxy_rs {
+    use super::*;
+
+    #[pymodule]
+    mod certs {
+        #[pymodule_export]
+        use crate::util::{add_cert, remove_cert};
+    }
+
+    #[pymodule]
+    mod dns {
+        #[pymodule_export]
+        use crate::dns_resolver::{get_system_dns_servers, DnsResolver};
+    }
+
+    #[pymodule]
+    mod local {
+        #[pymodule_export]
+        use crate::server::{start_local_redirector, LocalRedirector};
+    }
+
+    #[pymodule]
+    mod process_info {
+        #[pymodule_export]
+        use crate::process_info::{active_executables, executable_icon, Process};
+    }
+
+    #[pymodule]
+    mod tun {
+        #[pymodule_export]
+        use crate::server::{create_tun_interface, TunInterface};
+    }
+
+    #[pymodule]
+    mod udp {
+        #[pymodule_export]
+        use crate::server::{start_udp_server, UdpServer};
+        #[pymodule_export]
+        use crate::udp_client::open_udp_connection;
+    }
+
+    #[pymodule]
+    mod wireguard {
+        #[pymodule_export]
+        use crate::server::{start_wireguard_server, WireGuardServer};
+        #[pymodule_export]
+        use crate::util::{genkey, pubkey};
+    }
+
+    #[pymodule_export]
+    use crate::stream::Stream;
+
+    #[pymodule_init]
+    #[allow(unused_variables)]
+    fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
+        // set up the Rust logger to send messages to the Python logger
+        init_logger()?;
+
+        // set up tracing subscriber for introspection with tokio-console
+        #[cfg(feature = "tracing")]
+        console_subscriber::init();
+
+        // Import platform-specific modules here so that missing dependencies are raising immediately.
+        #[cfg(target_os = "macos")]
+        m.py().import("mitmproxy_macos")?;
+        #[cfg(all(target_os = "linux", not(feature = "docs")))]
+        m.py().import("mitmproxy_linux")?;
+        #[cfg(windows)]
+        m.py().import("mitmproxy_windows")?;
+
+        Ok(())
+    }
+}
---
+++ packages/mitmproxy-rs/mitmproxy_rs-0.11.4.tar.gz/mitmproxy_rs-0.11.4/src/ipc/mod.rs
@@ -0,0 +1,39 @@
+mod mitmproxy_ipc;
+pub use mitmproxy_ipc::*;
+
+use crate::intercept_conf;
+use std::net::{AddrParseError, IpAddr, SocketAddr};
+use std::str::FromStr;
+
+impl TryFrom<&Address> for SocketAddr {
+    type Error = AddrParseError;
+
+    fn try_from(address: &Address) -> Result<Self, Self::Error> {
+        let ip = IpAddr::from_str(&address.host)?;
+        Ok(SocketAddr::from((ip, address.port as u16)))
+    }
+}
+impl From<SocketAddr> for Address {
+    fn from(val: SocketAddr) -> Self {
+        Address {
+            host: val.ip().to_string(),
+            port: val.port() as u32,
+        }
+    }
+}
+
+impl From<intercept_conf::InterceptConf> for InterceptConf {
+    fn from(conf: intercept_conf::InterceptConf) -> Self {
+        InterceptConf {
+            actions: conf.actions(),
+        }
+    }
+}
+
+impl TryFrom<InterceptConf> for intercept_conf::InterceptConf {
+    type Error = anyhow::Error;
+
+    fn try_from(conf: InterceptConf) -> Result<Self, Self::Error> {
+        intercept_conf::InterceptConf::try_from(conf.actions)
+    }
+}
