de327e12883ca56bb5737de00d762848e02f4b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 16:03:52 +0000

    Add package depthviz/0.3.0.dev2/depthviz-0.3.0.dev2-py3-none-any.whl

---
+++ packages/depthviz/depthviz-0.3.0.dev2-py3-none-any.whl/depthviz/_version.py
@@ -0,0 +1,7 @@
+"""
+Version information for depthviz, created during installation.
+"""
+
+# These version placeholders will be replaced later during substitution.
+__version__ = "0.3.0.dev2"
+__version_tuple__ = (0, 3, 0, "dev2")
