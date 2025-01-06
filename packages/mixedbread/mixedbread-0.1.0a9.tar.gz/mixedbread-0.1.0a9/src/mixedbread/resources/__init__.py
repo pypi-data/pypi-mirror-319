6870b54a18bb5f91b4764fe679a1f4d3fb74a3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:10:44 +0000

    Add package mixedbread/0.1.0a9/mixedbread-0.1.0a9.tar.gz

---
+++ packages/mixedbread/mixedbread-0.1.0a9.tar.gz/mixedbread-0.1.0a9/src/mixedbread/resources/__init__.py
@@ -0,0 +1,89 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .files import (
+    FilesResource,
+    AsyncFilesResource,
+    FilesResourceWithRawResponse,
+    AsyncFilesResourceWithRawResponse,
+    FilesResourceWithStreamingResponse,
+    AsyncFilesResourceWithStreamingResponse,
+)
+from .parsing import (
+    ParsingResource,
+    AsyncParsingResource,
+    ParsingResourceWithRawResponse,
+    AsyncParsingResourceWithRawResponse,
+    ParsingResourceWithStreamingResponse,
+    AsyncParsingResourceWithStreamingResponse,
+)
+from .reranking import (
+    RerankingResource,
+    AsyncRerankingResource,
+    RerankingResourceWithRawResponse,
+    AsyncRerankingResourceWithRawResponse,
+    RerankingResourceWithStreamingResponse,
+    AsyncRerankingResourceWithStreamingResponse,
+)
+from .embeddings import (
+    EmbeddingsResource,
+    AsyncEmbeddingsResource,
+    EmbeddingsResourceWithRawResponse,
+    AsyncEmbeddingsResourceWithRawResponse,
+    EmbeddingsResourceWithStreamingResponse,
+    AsyncEmbeddingsResourceWithStreamingResponse,
+)
+from .document_ai import (
+    DocumentAIResource,
+    AsyncDocumentAIResource,
+    DocumentAIResourceWithRawResponse,
+    AsyncDocumentAIResourceWithRawResponse,
+    DocumentAIResourceWithStreamingResponse,
+    AsyncDocumentAIResourceWithStreamingResponse,
+)
+from .vector_stores import (
+    VectorStoresResource,
+    AsyncVectorStoresResource,
+    VectorStoresResourceWithRawResponse,
+    AsyncVectorStoresResourceWithRawResponse,
+    VectorStoresResourceWithStreamingResponse,
+    AsyncVectorStoresResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "DocumentAIResource",
+    "AsyncDocumentAIResource",
+    "DocumentAIResourceWithRawResponse",
+    "AsyncDocumentAIResourceWithRawResponse",
+    "DocumentAIResourceWithStreamingResponse",
+    "AsyncDocumentAIResourceWithStreamingResponse",
+    "EmbeddingsResource",
+    "AsyncEmbeddingsResource",
+    "EmbeddingsResourceWithRawResponse",
+    "AsyncEmbeddingsResourceWithRawResponse",
+    "EmbeddingsResourceWithStreamingResponse",
+    "AsyncEmbeddingsResourceWithStreamingResponse",
+    "RerankingResource",
+    "AsyncRerankingResource",
+    "RerankingResourceWithRawResponse",
+    "AsyncRerankingResourceWithRawResponse",
+    "RerankingResourceWithStreamingResponse",
+    "AsyncRerankingResourceWithStreamingResponse",
+    "ParsingResource",
+    "AsyncParsingResource",
+    "ParsingResourceWithRawResponse",
+    "AsyncParsingResourceWithRawResponse",
+    "ParsingResourceWithStreamingResponse",
+    "AsyncParsingResourceWithStreamingResponse",
+    "FilesResource",
+    "AsyncFilesResource",
+    "FilesResourceWithRawResponse",
+    "AsyncFilesResourceWithRawResponse",
+    "FilesResourceWithStreamingResponse",
+    "AsyncFilesResourceWithStreamingResponse",
+    "VectorStoresResource",
+    "AsyncVectorStoresResource",
+    "VectorStoresResourceWithRawResponse",
+    "AsyncVectorStoresResourceWithRawResponse",
+    "VectorStoresResourceWithStreamingResponse",
+    "AsyncVectorStoresResourceWithStreamingResponse",
+]
