be3113e9ce7cb1edd5338a56638bf46edf9bcebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 16:03:59 +0000

    Add package firstcodewitharyan/0.3/firstcodewitharyan-0.3-py3-none-any.whl

---
+++ packages/firstcodewitharyan/firstcodewitharyan-0.3-py3-none-any.whl/firstcodewitharyan/__init__.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def print_functions():
+    print("Hi Welcome you have first account")
+
+def willyoumarryme(answer):
+    if answer=='no':
+        print("koi rasta hi nhi hain babu ji")
+    else:
+        print("aaajao chipak jaao mere pyaare se muchku")
+
+def hello():
+    print("Hi Ibrahim , thanks for contating us")
