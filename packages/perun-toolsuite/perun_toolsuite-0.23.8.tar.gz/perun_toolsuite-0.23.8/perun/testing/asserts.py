d26e16592361da64a08d7cbde80ce3926b7997cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:06 +0000

    Add package perun-toolsuite/0.23.8/perun_toolsuite-0.23.8.tar.gz

---
+++ packages/perun-toolsuite/perun_toolsuite-0.23.8.tar.gz/perun_toolsuite-0.23.8/perun/testing/asserts.py
@@ -0,0 +1,72 @@
+"""Helper assertion function to be used in tests"""
+
+from __future__ import annotations
+
+# Standard Imports
+from typing import Optional
+import os
+import traceback
+
+# Third-Party Imports
+import click.testing
+
+# Perun Imports
+
+
+def predicate_from_cli(cli_result: click.testing.Result | list[str] | str, predicate: bool) -> None:
+    """Checks the correctness of the @p predicate.
+
+    In case the predicate is violated, the function outputs additional helper information for
+    debugging, since CliRunner of click captures the output. Currently, the function lists the
+    captured output and trace leading to the error/exception (if raised).
+
+    :param cli_result: result object of
+    :param predicate: predicate returning true or false
+    """
+    try:
+        assert predicate
+    except AssertionError as failed_assertion:
+        if hasattr(cli_result, "output") and cli_result.output:
+            print("=== Captured output ===")
+            print(cli_result.output)
+        elif isinstance(cli_result, list):
+            print("=== Captured output ===")
+            print("".join(cli_result))
+        else:
+            print("=== Captured output ===")
+            print(cli_result)
+        print("=== Inner traceback ===")
+        if hasattr(cli_result, "exception") and cli_result.exception:
+            print(cli_result.exception)
+        if hasattr(cli_result, "exc_info") and cli_result.exc_info is not None:
+            traceback.print_tb(cli_result.exc_info[2])
+        raise failed_assertion
+
+
+def invalid_cli_choice(
+    cli_result: click.testing.Result, choice: str, file: Optional[str] = None
+) -> None:
+    """Checks, that click correctly ended as invalid choice
+
+    :param cli_result: result of the commandline interface
+    :param choice: choice that we tried
+    :param file: name of the file that should not be created (optional)
+    """
+    predicate_from_cli(cli_result, cli_result.exit_code == 2)
+    predicate_from_cli(cli_result, f"'{choice}' is not one of" in cli_result.output)
+    if file:
+        assert file not in os.listdir(os.getcwd())
+
+
+def invalid_param_choice(
+    cli_result: click.testing.Result, choice: str, file: Optional[str] = None
+) -> None:
+    """Checks that click correctly ended with invalid choice and 1 return code
+    :param cli_result: result of the commandline interface
+    :param choice: choice that we tried
+    :param file: name of the file that should not be created (optional)
+    """
+    predicate_from_cli(cli_result, cli_result.exit_code == 1)
+    predicate_from_cli(cli_result, f"Invalid value '{choice}'" in cli_result.output)
+    if file:
+        assert file not in os.listdir(os.getcwd())
---
+++ packages/perun-toolsuite/perun_toolsuite-0.23.8.tar.gz/perun_toolsuite-0.23.8/tests/test_imports.py
@@ -0,0 +1,158 @@
+"""Basic tests for checking the correctness of the VCS modules"""
+
+from __future__ import annotations
+
+# Standard Imports
+import os
+
+# Third-Party Imports
+from click.testing import CliRunner
+
+# Perun Imports
+from perun import cli
+
+
+def test_imports(pcs_with_svs):
+    pool_path = os.path.join(os.path.split(__file__)[0], "sources", "imports")
+
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 0
+    runner = CliRunner()
+    result = runner.invoke(
+        cli.cli,
+        ["import", "-c", "ls", "-w", ".", "perf", "record", os.path.join(pool_path, "import.data")],
+    )
+    assert result.exit_code == 0
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 2
+
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "-c",
+            "ls",
+            "-w",
+            "al",
+            "perf",
+            "script",
+            os.path.join(pool_path, "import.script"),
+        ],
+    )
+    assert result.exit_code == 0
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 3
+
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "--import-dir",
+            pool_path,
+            "--machine-info",
+            "machine_info.json",
+            "perf",
+            "stack",
+            "import.stack",
+        ],
+    )
+    assert result.exit_code == 0
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 4
+
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "--save-to-index",
+            "-c",
+            "ls",
+            "-w",
+            "..",
+            "perf",
+            "stack",
+            os.path.join(pool_path, "import.stack"),
+            os.path.join(pool_path, "import.stack.gz"),
+        ],
+    )
+    assert result.exit_code == 0
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 4
+
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "-pn",
+            "custom_import_profile",
+            "-c",
+            "ls",
+            "-w",
+            "..",
+            "-d",
+            pool_path,
+            "--stats-headers",
+            "wall-clock|lower_is_better|ms||total wall-clock run time,umemory|lower_is_better|KB|median|,",
+            "-md",
+            "gcc|v10.0.0|gcc version",
+            "--metadata",
+            "cmake|v3.0.1",
+            "-md",
+            "metadata.json",
+            "-md",
+            "invalid-format",
+            "-md",
+            "unknown-metadata.json",
+            "perf",
+            "stack",
+            "import.csv",
+            "import.stack,1,192.13,13578.45,TSC,bogus",
+            "import-empty.csv",
+            "",
+        ],
+    )
+    assert result.exit_code == 0
+    profiles = os.listdir(os.path.join(".perun", "jobs"))
+    assert len(profiles) == 5 and "custom_import_profile.perf" in profiles
+
+    # Try to import stack profile using import perf record, we expect failure
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "-c",
+            "ls",
+            "-w",
+            ".",
+            "perf",
+            "record",
+            os.path.join(pool_path, "import.stack"),
+        ],
+    )
+    assert result.exit_code == 1
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 5
+
+    # Supplied invalid path for metadata (recoverable error) and profile (fatal error).
+    # We expect failure.
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "-c",
+            "ls",
+            "-w",
+            ".",
+            "perf",
+            "stack",
+            os.path.join(pool_path, "import-invalid.stack"),
+        ],
+    )
+    assert result.exit_code == 1
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 5
+
+    result = runner.invoke(
+        cli.cli,
+        [
+            "import",
+            "elk",
+            "json",
+            os.path.join(pool_path, "import-elk.json"),
+        ],
+    )
+    assert result.exit_code == 0
+    assert len(os.listdir(os.path.join(".perun", "jobs"))) == 6
