5d8c211b6ea5e19a29ff8af9ead1eb8c23275b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:27 +0000

    Add package streamix/0.4/streamix-0.4.tar.gz

---
+++ packages/streamix/streamix-0.4.tar.gz/streamix-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamix",  # Your package name
+    version="0.4",  # Version of your package
+    packages=find_packages(),  # Automatically find all packages in your project
+    install_requires=[  # List of dependencies
+        "yt-dlp",
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/username/streamix",  # Link to GitHub
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
