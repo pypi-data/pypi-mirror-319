4d6e319100bdecc68e0b0cbead9d9965fa08dbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:39 +0000

    Add package create-lillie/2.0.3/create_lillie-2.0.3.tar.gz

---
+++ packages/create-lillie/create_lillie-2.0.3.tar.gz/create_lillie-2.0.3/README.md
@@ -0,0 +1,8 @@
+# create-lillie
+
+```bash
+create-lillie YOUR_PROJECT_NAME --is-empty
+```
+* create-lillie - cli name
+* YOUR_PROJECT_NAME - name for your project
+* --is-empty - if used, will generate empty project, if not, will generate starter project
---
+++ packages/create-lillie/create_lillie-2.0.3.tar.gz/create_lillie-2.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="create-lillie",
+    version="2.0.3",
+    packages=find_packages(),
+    author="sarthak ghoshal",
+    author_email="sarthak22.ghoshal@gmail.com",
+    description="A CLI tool for creating lilliepy projects",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/websitedeb/create-lillie",
+    py_modules=["cli"],
+    include_package_data=True,
+    package_data={
+        "": ["templates/**/*"],
+    },
+    install_requires=[
+        "typer[all]",
+        "colorama"
+    ],
+    entry_points={
+        "console_scripts": [
+            "create-lillie=cli:app",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
