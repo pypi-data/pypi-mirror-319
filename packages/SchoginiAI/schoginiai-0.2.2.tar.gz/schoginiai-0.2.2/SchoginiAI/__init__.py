d6ded6a2b8e405aee573ecfe4228512dc87540bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:03:57 +0000

    Add package SchoginiAI/0.2.2/schoginiai-0.2.2.tar.gz

---
+++ packages/SchoginiAI/schoginiai-0.2.2.tar.gz/schoginiai-0.2.2/SchoginiAI/__init__.py
@@ -0,0 +1,8 @@
+"""
+SchoginiAI - A sample AI toolkit by Schogini Systems.
+"""
+
+__version__ = "0.2.2"
+
+#from .main import SchoginiAICore
+from .main import SchoginiAICore, SchoginiAIRAG, CreateSettings
