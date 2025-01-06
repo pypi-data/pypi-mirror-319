4a0cf6c245b7832c7f8a67387a3fd89860135949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:21 +0000

    Add package spells-mtg/0.9.3/spells_mtg-0.9.3-py3-none-any.whl

---
+++ packages/spells-mtg/spells_mtg-0.9.3-py3-none-any.whl/spells/config.py
@@ -0,0 +1,17 @@
+all_sets = [
+    "FDN",
+    "DSK",
+    "BLB",
+    "MH3",
+    "OTJ",
+    "MKM",
+    "LCI",
+    "WOE",
+    "LTR",
+    "MOM",
+    "ONE",
+    "BRO",
+    "DMU",
+    "SNC",
+    "NEO",
+]
