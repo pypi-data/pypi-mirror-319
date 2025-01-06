1be2dabf2a3fa68ad851b7f4cf28d427c3387dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:16 +0000

    Add package pcb-jigify/0.1.1/pcb_jigify-0.1.1-py2.py3-none-any.whl

---
+++ packages/pcb-jigify/pcb_jigify-0.1.1-py2.py3-none-any.whl/pcb_jigify/cli.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+import tempfile
+import subprocess
+import sys
+from os import path
+import argparse
+import cadquery as cq
+from .jigs.testing import jig as testing
+from .jigs.holding import jig as holding
+from .jigs.settings import Settings
+
+KICAD_PCB = '.kicad_pcb'
+KICAD_CLI = 'kicad-cli'
+DXF_TOL = 1e-4
+
+common_parser = argparse.ArgumentParser(add_help=False)
+
+common_parser.add_argument('--registration-layer', help='Registration layer, either a KiCad layer name or a DXF file', type=str)
+common_parser.add_argument('--registration-depth', help='How deep to drill the registration layer', type=float)
+
+common_parser.add_argument('--margin', default=Settings.wallT, type=float)
+
+common_parser.add_argument('--pcb-thickness', default=Settings.pcbT, help='Thickness of the PCB', type=float)
+common_parser.add_argument('--pcb-fit', default=Settings.pcbFit, help='Fit of the PCB', type=float)
+
+common_parser.add_argument('--output', help='Output file', type=str, required=True)
+
+common_parser.add_argument('--dxf-tolerance', help='DXF edge consolidation tolerance', type=float, default=DXF_TOL)
+
+common_parser.add_argument('file', help='PCB or edge cuts DXF file to process')
+
+parser = argparse.ArgumentParser(
+                    prog='pcb-jigify',
+                    description='Generate PCB jigs')
+
+subparsers = parser.add_subparsers()
+
+holding_parser = subparsers.add_parser('holding', parents=[common_parser], help='Generate a jig for holding a PCB in place')
+
+holding_parser.add_argument('--bottom-magnet-diameter', help='Diameter of the bottom magnet (hugging the surface)', type=float)
+holding_parser.add_argument('--bottom-magnet-height', help='Height of the bottom magnet (hugging the surface)', type=float)
+
+holding_parser.add_argument('--cut', action='store_true', help='Cut the jig in half', default=False)
+holding_parser.add_argument('--part-basket', action='store_true', help='Add in a tiny part basket to store parts in during work')
+
+testing_parser = subparsers.add_parser('testing', parents=[common_parser], help='Generate a jig for testing a PCB with probes (usually spring pins)')
+testing_parser.add_argument('--testing-layer', help='Testing layer, either a KiCad layer name or a DXF file', required=True)
+testing_parser.add_argument('--test-probe-diameter', help='Test probe diameter', required=True, type=float)
+testing_parser.add_argument('--test-probe-length', help='Length of the test probe to hold onto', required=True, type=float)
+testing_parser.add_argument('--side', help='Side of the board facing the testing fixture', default="top", choices=["top", "bottom"])
+
+def kicad_export_dxf(file, layer, output):
+    subprocess.run([
+                   'kicad-cli', 'pcb', 'export', 'dxf',
+                   '--layers', layer,
+                   '-o', output,
+                   '--ou', 'mm', file
+    ])
+    return output
+
+def read_layers(file, registration_layer = None, testing_layer = None, dxf_tolerance=DXF_TOL):
+    return (read_layers_from_pcb if file.endswith(KICAD_PCB) else read_layers_from_dxf)(file, registration_layer, testing_layer, dxf_tolerance)
+
+def read_layers_from_dxf(file, registration_layer = None, testing_layer = None, dxf_tolerance=DXF_TOL):
+    return (
+        cq.importers.importDXF(file, tol=dxf_tolerance),
+        cq.importers.importDXF(registration_layer, tol=dxf_tolerance) if registration_layer is not None else None,
+        cq.importers.importDXF(testing_layer, tol=dxf_tolerance) if testing_layer is not None else None,
+    )
+
+def read_layers_from_pcb(file, registration_layer = None, testing_layer = None, dxf_tolerance=DXF_TOL):
+    with tempfile.TemporaryDirectory() as dir:
+        base = f"{dir}/{path.basename(file)}"
+        layers = read_layers_from_dxf(
+            kicad_export_dxf(file, "Edge.Cuts", f"{base}-Edge.Cuts.dxf"),
+            kicad_export_dxf(file, registration_layer, f"{base}-{registration_layer}.dxf") if registration_layer is not None else None,
+            kicad_export_dxf(file, testing_layer, f"{base}-{testing_layer}.dxf") if testing_layer is not None else None,
+            dxf_tolerance
+        )
+    return layers
+
+
+def holding_main(file,
+                 pcb_thickness,
+                 output,
+                 registration_layer,
+                 registration_depth,
+                 bottom_magnet_diameter,
+                 bottom_magnet_height,
+                 cut,
+                 part_basket,
+                 pcb_fit,
+                 dxf_tolerance,
+                 **rest):
+    file, registration_layer, _ = read_layers(file, registration_layer, dxf_tolerance=dxf_tolerance)
+
+    if (bottom_magnet_diameter is None) != (bottom_magnet_height is None):
+        holding_parser.print_help()
+        print('\n--bottom-magnet-diameter and --bottom-magnet-height both need to be specified')
+        sys.exit(1)
+
+    if (registration_layer is None) != (registration_depth is None):
+        holding_parser.print_help()
+        print('\n--registration-layer and --registration-depth both need to be specified')
+        sys.exit(1)
+
+    j = holding(
+        file.wires(),
+        registration = registration_layer.wires() if registration_layer is not None else None,
+        registrationDepth=registration_depth,
+        surfaceMagnet=(bottom_magnet_diameter, bottom_magnet_height) if bottom_magnet_diameter is not None else None,
+        cut=cut,
+        partBasket=[10, 10, pcb_thickness] if part_basket else None,
+        pcbT = pcb_thickness,
+        pcbFit=pcb_fit,
+    )
+    j.export(output)
+
+holding_parser.set_defaults(func=holding_main)
+
+def testing_main(file,
+                 registration_layer,
+                 registration_depth,
+                 testing_layer,
+                 pcb_thickness,
+                 test_probe_diameter,
+                 test_probe_length,
+                 side,
+                 output,
+                 pcb_fit,
+                 dxf_tolerance,
+                 **rest):
+
+    file, registration_layer, testing_layer = read_layers(file, registration_layer, testing_layer, dxf_tolerance)
+
+    j = testing(
+        file.wires(),
+        testPoint = ( test_probe_diameter, test_probe_length ),
+        registration = registration_layer.wires() if registration_layer is not None else None,
+        registrationDepth = registration_depth,
+        pcbT = pcb_thickness,
+        pcbFit = pcb_fit,
+        testPoints = testing_layer.wires(),
+
+        side = side,
+    )
+    j.export(output)
+
+testing_parser.set_defaults(func=testing_main)
+
+
+def main():
+    args = parser.parse_args()
+
+    if 'func' not in args:
+        parser.print_help()
+        sys.exit(1)
+    args.func(**vars(args))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pcb-jigify/pcb_jigify-0.1.1-py2.py3-none-any.whl/pcb_jigify/jigs/testing.py
@@ -0,0 +1,63 @@
+import cadquery as cq
+
+try:
+    from .settings import Settings
+    from .base import baseJig
+except:
+    from settings import Settings
+    from base import baseJig
+
+wallT = Settings.wallT
+
+def jig(
+        outline,
+        # TODO: mid-stroke vs PCB thickness calculation
+        testPoint: tuple[float, float],
+        pcbT = Settings.pcbT,
+        pcbFit = Settings.pcbFit,
+
+        registration = None,
+        registrationDepth = None,
+
+        testPoints = None,
+        
+        side="top"
+    ):
+
+    h = testPoint[1] + pcbT
+
+    (w, _) = baseJig(cq.Workplane("XY"), outline, 2 * wallT, h, pcbT, pcbFit)
+
+    if registration is not None:
+        registration = [cq.Face.makeFromWires(wire.offset2D(Settings.registrationFit)[0]) for wire in registration]
+
+        for face in registration:
+            w = w.workplane().add(face).translate((0, 0, h - pcbT)).wires().toPending().cutBlind(registrationDepth)
+
+    if testPoints is not None:
+        testPoints = [cq.Face.makeFromWires(wire) for wire in testPoints]
+        for face in testPoints:
+            w = w.add(
+                cq.Face.makeFromWires(
+                    cq.Wire.makeCircle(testPoint[0] / 2, face.Center(), normal=(0, 0, 1))
+                )
+            )
+
+        # A bit of a hack, since we rotate the entire workpiece before cutting into it
+        # if we're approaching the test points from bottom side.
+        if side == "bottom":
+            w = w.rotateAboutCenter((0, 1, 0), 180)
+        w = w.cutThruAll()
+
+    try:
+        w = w.faces(">Z").edges("not %Circle").chamfer(wallT / 6)
+    except:
+        print("Chamfering top failed")
+    try:
+        # This sometimes fails when there are registration features too close to
+        # the PCB edge.
+        w = w.faces("<Z").edges("not %Circle").chamfer(wallT / 6)
+    except:
+        print("Chamfering bottom failed")
+
+    return w
