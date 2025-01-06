07bae2267de410d9f1a500025ad6220c5df9e389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:21 +0000

    Add package pingverter/1.0.2/pingverter-1.0.2.tar.gz

---
+++ packages/pingverter/pingverter-1.0.2.tar.gz/pingverter-1.0.2/pingverter/__init__.py
@@ -0,0 +1,6 @@
+
+from .sonar_class import sonar
+from .humminbird_class import hum
+from .lowrance_class import low
+from .converter import hum2pingmapper, low2pingmapper, low2hum
+from .version import __version__
