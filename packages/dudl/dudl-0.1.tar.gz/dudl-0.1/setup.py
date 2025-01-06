3237941323983873760d6e962a6aa21a4c69f805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:20 +0000

    Add package dudl/0.1/dudl-0.1.tar.gz

---
+++ packages/dudl/dudl-0.1.tar.gz/dudl-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='DUDL',
+    version='0.1.0',    
+    description='The DUDL Game',
+    url='https://github.com/v0rtex20k/DUDL/',
+    packages=['DUDL'],
+    # install_requires=[
+    #     "psutil==6.1.1",
+    #     "Flask==2.3.3",
+    #     "Flask-API>=3.1",
+    #     "flask-smorest>=0.42.3",
+    #     "randomname>=0.2.1",
+    #     "Werkzeug==2.3.8", # when > 2.3.8, conflicting import w/ flask
+    #     "flasgger>=0.9.7.1",
+    #     "pathlib>=1.0.1",
+    #     "waitress>=2.1.2",
+    #     "inflection>=0.5.1",
+    #     "itsdangerous>=2.1.2",
+    #     "requests>=2.31.0",
+    #     "ConfigArgParse>=1.7",
+    # ],
+)
+
