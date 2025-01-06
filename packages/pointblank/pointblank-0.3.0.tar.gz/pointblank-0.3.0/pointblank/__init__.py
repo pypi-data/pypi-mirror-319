3fd004d34bbb1ef0bb4a66b7c726d844c5b8a52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:18 +0000

    Add package pointblank/0.3.0/pointblank-0.3.0.tar.gz

---
+++ packages/pointblank/pointblank-0.3.0.tar.gz/pointblank-0.3.0/pointblank/__init__.py
@@ -0,0 +1,42 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+try:  # pragma: no cover
+    __version__ = version("pointblank")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+
+# Import objects from the module
+from pointblank.tf import TF
+from pointblank.column import (
+    col,
+    starts_with,
+    ends_with,
+    contains,
+    matches,
+    everything,
+    first_n,
+    last_n,
+)
+from pointblank.validate import Validate, load_dataset, config
+from pointblank.schema import Schema
+from pointblank.thresholds import Thresholds
+
+__all__ = [
+    "TF",
+    "Validate",
+    "Thresholds",
+    "Schema",
+    "col",
+    "starts_with",
+    "ends_with",
+    "contains",
+    "matches",
+    "everything",
+    "first_n",
+    "last_n",
+    "load_dataset",
+    "config",
+]
