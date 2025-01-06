89fdfdfb2ccb92dc503183cda94a4e028eb1b109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:11 +0000

    Add package cloud-base-cli/0.5.0/cloud_base_cli-0.5.0-py3-none-any.whl

---
+++ packages/cloud-base-cli/cloud_base_cli-0.5.0-py3-none-any.whl/cloud_base_cli/cli.py
@@ -0,0 +1,50 @@
+import typer
+
+# import boto3
+import cloud_base_cli.libs.gitlab as gitlab_lib
+import cloud_base_cli.libs.aws as aws
+# import gitlab
+# import cloud_base_cli.libs.aws as aws
+# import lazy_import
+# aws = lazy_import.lazy_module("cloud_base_cli.libs.aws")
+# gitlab = lazy_import.lazy_module("cloud_base_cli.libs.gitlab")
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Cloud Base CLI
+    """
+    pass
+
+
+@app.command()
+def multi(parent_group_id: str):
+    """
+    Create a new Gitlab Project for multi-account Cloud Runway
+    provide parent_group_id
+    """
+    a = aws.AWS()
+    gitlab_lib.set_vars(a.get_secret())
+    project_name = "cloud-runway-multi"
+    gitlab_lib.implement(project_name, parent_group_id)
+
+
+@app.command()
+def single(parent_group_id: int):
+    """
+    Create a new Gitlab Project for single account Cloud Runway
+    provide parent_group_id
+    """
+    a = aws.AWS()
+    gitlab_lib.set_vars(a.get_secret())
+    project_name = "cloud-runway-single"
+    gitlab_lib.implement_single(project_name, parent_group_id)
+
+
+@app.command()
+def hello(name):
+    print(f"hello there {name}")
