bc1e3b6de9482f80a8c2e309925655972dc419d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:11 +0000

    Add package llm-catcher/0.3.5/llm_catcher-0.3.5-py3-none-any.whl

---
+++ packages/llm-catcher/llm_catcher-0.3.5-py3-none-any.whl/llm_catcher/__init__.py
@@ -0,0 +1,10 @@
+from .diagnoser import LLMExceptionDiagnoser
+from .settings import get_settings, Settings
+
+__all__ = [
+    "LLMExceptionDiagnoser",
+    "get_settings",
+    "Settings"
+]
+
+__version__ = "0.3.5"
