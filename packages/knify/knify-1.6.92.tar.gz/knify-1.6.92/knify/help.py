7c34b0612a70e593ad3ef7cda941c7b1ad69720a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:31 +0000

    Add package knify/1.6.92/knify-1.6.92.tar.gz

---
+++ packages/knify/knify-1.6.92.tar.gz/knify-1.6.92/knify/help.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+
+def get_pip_name():
+    return 'knify'
+
+def get_version():
+    return '1.6.92'
+
+def print_version():
+    print('''Knify %s
+Development tools for python.''' % get_version())
