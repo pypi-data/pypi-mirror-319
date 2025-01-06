95240df398bc0fa0eb6e4d1394d3c415f096833e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:12 +0000

    Add package olab-prediction-market-sdk/0.0.35/olab_prediction_market_sdk-0.0.35.tar.gz

---
+++ packages/olab-prediction-market-sdk/olab_prediction_market_sdk-0.0.35.tar.gz/olab_prediction_market_sdk-0.0.35/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+NAME = "olab_prediction_market_sdk"
+VERSION = "0.0.35"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="OLAB Prediction Market Open API",
+    author="nik.opinionlabs",
+    author_email="nik@opinionlabs.xyz",
+    url="",
+    keywords=["PredictionMarket"],
+    install_requires=[
+        "urllib3",
+        "six",
+        "certifi",
+        "python-dateutil",
+        "hexbytes",
+        "web3",
+        "eth_account",
+        "poly_eip712_structs"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+)
