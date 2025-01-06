cfac9658b7b052b683681e5f41a76cfd447b6c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:19 +0000

    Add package md-pkb/0.1.0/md_pkb-0.1.0.tar.gz

---
+++ packages/md-pkb/md_pkb-0.1.0.tar.gz/md_pkb-0.1.0/src/md_pkb/playground.py
@@ -0,0 +1,15 @@
+
+
+if __name__ == "__main__":
+    print(f"####### playground #######")
+    from utils.search import create_collection, create_dummy_data, get_indexer, search_doc
+    # ix, created = create_collection()
+    # print(ix)
+    # print(f"is collection newly created? {created}")
+    # ix = get_indexer()
+    # print(ix.doc_count())
+    # print(create_dummy_data())
+    # print(search_doc("content", "example"))
+    # print(search_doc("title", "Second"))
+    # print(search_doc("content", "this AND (title:Second)"))
+
