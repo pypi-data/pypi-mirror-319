fb12a192ca04533a12a0108b7b6e8692c455f1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:10:02 +0000

    Add package genesis-models/0.1.2/genesis_models-0.1.2.tar.gz

---
+++ packages/genesis-models/genesis_models-0.1.2.tar.gz/genesis_models-0.1.2/src/genesis_models/base.py
@@ -0,0 +1,4 @@
+from sqlalchemy.orm import declarative_base
+
+# 创建共享的 Base 类
+Base = declarative_base() 
---
+++ packages/genesis-models/genesis_models-0.1.2.tar.gz/genesis_models-0.1.2/src/genesis_models/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from datetime import datetime
+from .base import Base
+
+class TaskModel(Base):
+    __tablename__ = 'task_info'
+
+    task_id = Column(Integer, primary_key=True)
+    task_name = Column(String(255))
+    task_text = Column(Text)
+    task_status = Column(Integer, default=0)
+    material_id = Column(Integer)
+    deleted = Column(Integer, default=0)
+    create_time = Column(DateTime, default=lambda: datetime.now())
+    update_time = Column(DateTime, default=lambda: datetime.now(), onupdate=lambda: datetime.now()) 
