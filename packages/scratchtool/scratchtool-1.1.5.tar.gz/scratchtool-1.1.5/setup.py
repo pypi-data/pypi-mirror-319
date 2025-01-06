548cb8ac32d960e5413a03047ac1c3a8ec670e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:03:54 +0000

    Add package scratchtool/1.1.5/scratchtool-1.1.5.tar.gz

---
+++ packages/scratchtool/scratchtool-1.1.5.tar.gz/scratchtool-1.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+#python setup.py sdist bdist_wheel
+#python -m twine upload dist/*
+
+
+setup(
+    name='scratchtool',
+    version='1.1.5',
+    author="itijiku",
+    author_email="kanjukuitijuku@gmail.com",
+    description="A package for working with Scratch data.",
+    url="https://github.com/itijuku/scratchtool",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0',
+        'beautifulsoup4>=4.12.3',
+        'asyncio',
+        'websockets',
+        'json'
+    ],
+    entry_points={
+        'console_scripts': [
+            'scratchtool=scratchtool.main:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
