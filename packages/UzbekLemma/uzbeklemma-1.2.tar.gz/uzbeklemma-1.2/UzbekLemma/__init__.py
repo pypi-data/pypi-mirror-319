ad82531e5e8feba59f5937acd10e2e24128eae8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:11 +0000

    Add package UzbekLemma/1.2/uzbeklemma-1.2.tar.gz

---
+++ packages/UzbekLemma/uzbeklemma-1.2.tar.gz/uzbeklemma-1.2/UzbekLemma/__init__.py
@@ -0,0 +1,11 @@
+"""
+    Author1: https://github.com/MaksudSharipov
+    Author2: https://github.com/MaksudSharipov
+
+    The package finds lemmas of Uzbek words based on the dictionary.
+    The process of finding a lemma is called lemmatization.
+    There are 4 different ways of lemmatization: rule, dictionary, model, hybrid.
+    It is dictionary-based lemmatization algorithm [program, package].
+"""
+
+from .UzbekLemma import lemmatize
