22f1c1ca0783826d8afe25514aa1ab59880ceb76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:24 +0000

    Add package ansys-fluent-visualization/0.17.dev0/ansys_fluent_visualization-0.17.dev0.tar.gz

---
+++ packages/ansys-fluent-visualization/ansys_fluent_visualization-0.17.dev0.tar.gz/ansys_fluent_visualization-0.17.dev0/src/ansys/fluent/visualization/__init__.py
@@ -0,0 +1,42 @@
+"""Python post processing integrations for the Fluent solver."""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+_VERSION_INFO = "Build date: January 06, 2025 09:12 UTC ShaID: a7bfb72"
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+PLOTTER = "matplotlib"
+INTERACTIVE = False
+
+
+def version_info() -> str:
+    """Method returning the version of PyFluent being used.
+    Returns
+    -------
+    str
+        The PyFluent version being used.
+    Notes
+    -------
+    Only available in packaged versions. Otherwise it will return __version__.
+    """
+    return _VERSION_INFO if _VERSION_INFO is not None else __version__
+
+
+from ansys.fluent.visualization._config import get_config, set_config  # noqa: F401
+from ansys.fluent.visualization.containers import (  # noqa: F401
+    Contour,
+    Mesh,
+    Monitor,
+    Pathline,
+    Surface,
+    Vector,
+    XYPlot,
+)
+from ansys.fluent.visualization.graphics import Graphics  # noqa: F401
+from ansys.fluent.visualization.graphics.graphics_windows import (  # noqa: F401
+    GraphicsWindow,
+)
+from ansys.fluent.visualization.plotter import Plots  # noqa: F401
+from ansys.fluent.visualization.plotter.plotter_windows import PlotterWindow
