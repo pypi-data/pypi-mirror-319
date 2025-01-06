18bf094ed3bd9c88dfaedb7526732799b38e1a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:38 +0000

    Add package cba-events/0.1.5/cba_events-0.1.5.tar.gz

---
+++ packages/cba-events/cba_events-0.1.5.tar.gz/cba_events-0.1.5/cba_events/events/types.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
+from .base_event import BaseEvent
+
+
+@dataclass
+class VolumetricEvent(BaseEvent):
+    """Stockpile-related event class"""
+
+    user_id: str
+    action: str
+    payload: Optional[Dict[str, Any]] = None
+
+    def __init__(
+        self,
+        site: str,
+        location: str,
+        entity: str,
+        survey_date: str,
+        user_id: str,
+        action: str,
+        **kwargs,
+    ):
+        super().__init__(entity, location, site, survey_date, event_type="stockpile")
+        self.user_id = user_id
+        self.action = action
+        self.payload = kwargs.get("payload", {})
