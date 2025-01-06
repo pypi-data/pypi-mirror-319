6c6808d6d20bf17f262be48f068ba1e38aca157e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:36 +0000

    Add package preliz/0.14.0/preliz-0.14.0.tar.gz

---
+++ packages/preliz/preliz-0.14.0.tar.gz/preliz-0.14.0/preliz/distributions/__init__.py
@@ -0,0 +1,121 @@
+# Continuous Distributions
+from .asymmetric_laplace import AsymmetricLaplace
+from .beta import Beta
+from .betascaled import BetaScaled
+from .cauchy import Cauchy
+from .chi_squared import ChiSquared
+from .exgaussian import ExGaussian
+from .exponential import Exponential
+from .gamma import Gamma
+from .gumbel import Gumbel
+from .halfcauchy import HalfCauchy
+from .halfnormal import HalfNormal
+from .halfstudentt import HalfStudentT
+from .inversegamma import InverseGamma
+from .kumaraswamy import Kumaraswamy
+from .laplace import Laplace
+from .logistic import Logistic
+from .loglogistic import LogLogistic
+from .logitnormal import LogitNormal
+from .lognormal import LogNormal
+from .moyal import Moyal
+from .normal import Normal
+from .pareto import Pareto
+from .skew_studentt import SkewStudentT
+from .skewnormal import SkewNormal
+from .studentt import StudentT
+from .rice import Rice
+from .triangular import Triangular
+from .truncatednormal import TruncatedNormal
+from .uniform import Uniform
+from .vonmises import VonMises
+from .wald import Wald
+from .weibull import Weibull
+
+# Multivariate Distributions
+from .continuous_multivariate import *
+
+# Discrete Distributions
+from .bernoulli import Bernoulli
+from .betabinomial import BetaBinomial
+from .binomial import Binomial
+from .categorical import Categorical
+from .discrete_uniform import DiscreteUniform
+from .discrete_weibull import DiscreteWeibull
+from .geometric import Geometric
+from .hypergeometric import HyperGeometric
+from .poisson import Poisson
+from .negativebinomial import NegativeBinomial
+from .zi_binomial import ZeroInflatedBinomial
+from .zi_negativebinomial import ZeroInflatedNegativeBinomial
+from .zi_poisson import ZeroInflatedPoisson
+
+# Transform Distributions
+from .mixture import Mixture
+from .truncated import Truncated
+from .censored import Censored
+from .hurdle import Hurdle
+
+
+all_continuous = [
+    AsymmetricLaplace,
+    Beta,
+    BetaScaled,
+    Cauchy,
+    ChiSquared,
+    ExGaussian,
+    Exponential,
+    Gamma,
+    Gumbel,
+    HalfCauchy,
+    HalfNormal,
+    HalfStudentT,
+    InverseGamma,
+    Kumaraswamy,
+    Laplace,
+    Logistic,
+    LogLogistic,
+    LogNormal,
+    LogitNormal,
+    Moyal,
+    Normal,
+    Pareto,
+    Rice,
+    SkewNormal,
+    SkewStudentT,
+    StudentT,
+    Triangular,
+    TruncatedNormal,
+    Uniform,
+    VonMises,
+    Wald,
+    Weibull,
+]
+all_discrete = [
+    Bernoulli,
+    BetaBinomial,
+    Binomial,
+    Categorical,
+    DiscreteUniform,
+    DiscreteWeibull,
+    Geometric,
+    HyperGeometric,
+    NegativeBinomial,
+    Poisson,
+    ZeroInflatedBinomial,
+    ZeroInflatedNegativeBinomial,
+    ZeroInflatedPoisson,
+]
+
+all_continuous_multivariate = [Dirichlet, MvNormal]
+
+
+__all__ = (
+    [s.__name__ for s in all_continuous]
+    + [s.__name__ for s in all_discrete]
+    + [s.__name__ for s in all_continuous_multivariate]
+    + [Mixture.__name__]
+    + [Truncated.__name__]
+    + [Censored.__name__]
+    + [Hurdle.__name__]
+)
