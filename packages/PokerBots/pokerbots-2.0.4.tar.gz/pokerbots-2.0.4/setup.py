d8ba4e91466de5523a85a2906f23c4d9f8892a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:12:05 +0000

    Add package PokerBots/2.0.4/pokerbots-2.0.4.tar.gz

---
+++ packages/PokerBots/pokerbots-2.0.4.tar.gz/pokerbots-2.0.4/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, 'PyPI_README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    print("Exception occurred during parsing README_PyPI.md")
+    long_description = ''
+
+# Parse requirements from requirements.txt
+try:
+    with open(os.path.join(current_directory, 'requirements.txt'), encoding='utf-8') as f:
+        requirements = f.read().splitlines()
+except Exception:
+    print("Exception occurred during parsing requirements.txt")
+    requirements = [
+        'treys==0.1.8',
+        'pokerkit==0.5.4'
+    ]
+
+setup(
+    # Name of the package
+    name='PokerBots',
+    packages=find_packages('.'),
+    version='2.0.4',
+    license='MIT',
+    description='A Pure Python library to test your Poker Bots in a trivial and simple way.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Nikolay Skripko',
+    author_email='nskripko@icloud.com',
+    url='https://github.com/Skripkon/PokerBots',
+    keywords=[],  # Add relevant keywords
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.11',  # Specify Python version compatibility
+)
