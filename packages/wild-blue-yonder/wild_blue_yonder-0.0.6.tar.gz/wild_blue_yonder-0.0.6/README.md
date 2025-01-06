749a1fad744d242e68d734c42cb7fce51ba434bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:14 +0000

    Add package wild-blue-yonder/0.0.6/wild_blue_yonder-0.0.6.tar.gz

---
+++ packages/wild-blue-yonder/wild_blue_yonder-0.0.6.tar.gz/wild_blue_yonder-0.0.6/README.md
@@ -0,0 +1,10 @@
+# Wild Blue Yonder
+Package Name
+<pre>
+  pip install wild-blue-yonder
+</pre>
+Then:
+```Python
+  # Python
+  import wild_blue_yonder as wby
+```
---
+++ packages/wild-blue-yonder/wild_blue_yonder-0.0.6.tar.gz/wild_blue_yonder-0.0.6/src/wild_blue_yonder/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+from .lm import *
+from .iteractions import *
+from .summaries import *
+
+""" Iterative actions of an AI powered Actor over 
+    objects and entities in Bluesky environment.
+"""
+__all__ = [
+    "GRQ",
+    "main",
+]
