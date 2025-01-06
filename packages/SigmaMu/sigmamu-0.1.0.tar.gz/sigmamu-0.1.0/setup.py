a8d87637db9f0c01425eac38fbf42323bc188dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:00 +0000

    Add package SigmaMu/0.1.0/sigmamu-0.1.0.tar.gz

---
+++ packages/SigmaMu/sigmamu-0.1.0.tar.gz/sigmamu-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="SigmaMu",  # Replace with your desired package name
+    version="0.1.0",
+    author="Volker Siepmann",
+    author_email="volker.siepmann@gmail.com",
+    description="A placeholder for SigmaMu - a process simulation library, to be uploaded soon",
+    packages=["simu"],
+    python_requires=">=3.6",
+)
