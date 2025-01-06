ae069a81edfe3fbed11e185ed9b5790e43e773bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:10:02 +0000

    Add package tcp/2.2.2/tcp-2.2.2-py3-none-any.whl

---
+++ packages/tcp/tcp-2.2.2-py3-none-any.whl/tcp/channel.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+#
+#   TCP: Transmission Control Protocol
+#
+#                                Written in 2021 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2021 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import socket
+from typing import Optional, Tuple
+
+from startrek.types import SocketAddress
+from startrek.net.socket import is_available
+from startrek.socket import Controller, SocketReader, SocketWriter
+from startrek import BaseChannel
+
+from .aio import is_blocking, is_closed
+from .aio import socket_send, socket_receive
+from .aio import socket_bind, socket_connect, socket_disconnect
+
+
+class ChannelChecker:
+
+    @classmethod
+    async def check_error(cls, error: socket.error, sock: socket.socket) -> Optional[socket.error]:
+        """
+            Check socket error
+
+               (1) check E_AGAIN
+                   the socket will raise 'Resource temporarily unavailable'
+                   when received nothing in non-blocking mode,
+                   or buffer overflow while sending too many bytes,
+                   here we should ignore this exception.
+               (2) check timeout
+                   in blocking mode, the socket will wait until send/received data,
+                   but if timeout was set, it will raise 'timeout' error on timeout,
+                   here we should ignore this exception
+        """
+        # the socket will raise 'Resource temporarily unavailable'
+        # when received nothing in non-blocking mode,
+        # or buffer overflow while sending too many bytes,
+        # here we should ignore this exception.
+        if error.errno == socket.EAGAIN:  # error.strerror == 'Resource temporarily unavailable':
+            if not is_blocking(sock=sock):
+                # ignore it
+                return None
+        # in blocking mode, the socket wil wait until sent/received data,
+        # but if timeout was set, it will raise 'timeout' error on timeout,
+        # here we should ignore this exception.
+        if isinstance(error, socket.timeout):
+            if sock.gettimeout() is not None:  # or not self.blocking:
+                # ignore it
+                return None
+        # print('[NET] socket error: %s' % error)
+        return error
+
+    @classmethod
+    async def check_data(cls, data: Optional[bytes], sock: socket.socket) -> Optional[socket.error]:
+        """
+            Check data received from socket
+
+            (1) check timeout
+                in blocking mode, the socket will wait until received something,
+                but if timeout was set, it will return nothing too, it's normal;
+                otherwise, we know the connection was lost.
+        """
+        # in blocking mode, the socket will wait until received something,
+        # but if timeout was set, it will return None too, it's normal;
+        # otherwise, we know the connection was lost.
+        if data is None or len(data) == 0:
+            if sock.gettimeout() is None:  # and self.blocking:
+                # print('[NET] socket error: remote peer reset socket %s' % sock)
+                return socket.error('remote peer reset socket %s' % sock)
+
+
+class StreamChannelReader(Controller, SocketReader):
+
+    # noinspection PyMethodMayBeStatic
+    async def _socket_receive(self, sock: socket.socket, max_len: int) -> Optional[bytes]:
+        if is_closed(sock=sock):
+            raise ConnectionError('socket closed')
+        elif not is_available(sock=sock):
+            # TODO: check 'broken pipe'
+            return None
+        # elif is_blocking(sock=sock):
+        #     return sock.recv(max_len)
+        else:
+            return await socket_receive(sock=sock, max_len=max_len)
+
+    async def _try_read(self, max_len: int, sock: socket.socket) -> Optional[bytes]:
+        try:
+            # return sock.recv(max_len)
+            return await self._socket_receive(sock=sock, max_len=max_len)
+        except socket.error as error:
+            error = await ChannelChecker.check_error(error=error, sock=sock)
+            if error is None:
+                # received nothing
+                return None
+            else:
+                # connection lost?
+                raise error
+
+    # Override
+    async def read(self, max_len: int) -> Optional[bytes]:
+        sock = self.sock
+        if sock is None:
+            raise ConnectionError('channel not ready')
+        # 1. try to read data
+        data = await self._try_read(max_len=max_len, sock=sock)
+        # 2. check data
+        error = await ChannelChecker.check_data(data=data, sock=sock)
+        if error is not None:
+            # connection lost!
+            raise error
+        # OK
+        return data
+
+    # Override
+    async def receive(self, max_len: int) -> Tuple[Optional[bytes], Optional[SocketAddress]]:
+        data = await self.read(max_len=max_len)
+        if data is None or len(data) == 0:
+            return None, None
+        else:
+            return data, self.remote_address
+
+
+class StreamChannelWriter(Controller, SocketWriter):
+
+    # noinspection PyMethodMayBeStatic
+    async def _socket_send(self, sock: socket.socket, data: bytes) -> int:
+        if is_closed(sock=sock):
+            raise ConnectionError('socket closed')
+        # elif not is_vacant(sock=sock):
+        #     # TODO: check 'broken pipe'
+        #     return -1
+        # elif is_blocking(sock=sock):
+        #     return sock.send(data)
+        else:
+            return await socket_send(sock=sock, data=data)
+
+    async def _try_write(self, data: bytes, sock: socket.socket) -> int:
+        try:
+            # return sock.send(data)
+            return await self._socket_send(sock=sock, data=data)
+        except socket.error as error:
+            error = await ChannelChecker.check_error(error=error, sock=sock)
+            if error is not None:
+                # connection lost?
+                raise error
+            # buffer overflow!
+            return 0
+
+    # Override
+    async def write(self, data: bytes) -> int:
+        """ Return the number of bytes sent;
+            this may be less than len(data) if the network is busy. """
+        sock = self.sock
+        if sock is None:
+            raise ConnectionError('channel not ready')
+        # sent = sock.sendall(data)
+        sent = 0
+        rest = len(data)
+        # assert rest > 0, 'cannot send empty data'
+        while True:  # while is_opened(sock=sock):
+            cnt = await self._try_write(data=data, sock=sock)
+            # check send result
+            if cnt <= 0:
+                # buffer overflow?
+                break
+            # something sent, check remaining data
+            sent += cnt
+            rest -= cnt
+            if rest <= 0:
+                # done!
+                break
+            else:
+                # remove sent part
+                data = data[cnt:]
+        # OK
+        if sent > 0:
+            return sent
+        elif cnt < 0:
+            assert cnt == -1, 'sent error: %d' % cnt
+            return -1
+        else:
+            return 0
+
+    # Override
+    async def send(self, data: bytes, target: SocketAddress) -> int:
+        # TCP channel will be always connected
+        # so the target address must be the remote address
+        remote = self.remote_address
+        assert target is None or target == remote, 'target error: %s, remote=%s' % (target, remote)
+        return await self.write(data=data)
+
+
+class StreamChannel(BaseChannel):
+    """ Stream Channel """
+
+    # Override
+    def _create_reader(self):
+        return StreamChannelReader(channel=self)
+
+    # Override
+    def _create_writer(self):
+        return StreamChannelWriter(channel=self)
+
+    # Override
+    async def _socket_bind(self, sock: socket.socket, local: SocketAddress) -> bool:
+        return await socket_bind(sock=sock, local=local)
+
+    # Override
+    async def _socket_connect(self, sock: socket.socket, remote: SocketAddress) -> bool:
+        return await socket_connect(sock=sock, remote=remote)
+
+    # Override
+    async def _socket_disconnect(self, sock: socket.socket) -> bool:
+        return await socket_disconnect(sock=sock)
---
+++ packages/tcp/tcp-2.2.2-py3-none-any.whl/tcp/hub.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+#
+#   TCP: Transmission Control Protocol
+#
+#                                Written in 2021 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2021 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import socket
+import threading
+import time
+from abc import ABC
+from typing import Optional, Iterable
+
+from startrek.types import SocketAddress, AddressPairMap
+from startrek.skywalker import Runnable, Runner, Daemon
+from startrek import Channel, BaseChannel
+from startrek import Connection, ConnectionDelegate
+from startrek import BaseConnection, ActiveConnection
+from startrek import BaseHub
+
+from .aio import is_blocking
+from .channel import StreamChannel
+
+
+class ChannelPool(AddressPairMap[Channel]):
+
+    # Override
+    def set(self, item: Optional[Channel],
+            remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+        # remove cached item first
+        cached = super().remove(item=item, remote=remote, local=local)
+        # if cached is not None and cached is not item:
+        #     Runner.async_task(coro=cached.close())
+        old = super().set(item=item, remote=remote, local=local)
+        assert old is None, 'should not happen: %s' % old
+        return cached
+
+    # # Override
+    # def remove(self, item: Optional[Channel],
+    #            remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+    #     cached = super().remove(item=item, remote=remote, local=local)
+    #     if cached is not None and cached is not item:
+    #         Runner.async_task(coro=cached.close())
+    #     if item is not None:
+    #         Runner.async_task(coro=item.close())
+    #     return cached
+
+
+# noinspection PyAbstractClass
+class StreamHub(BaseHub, ABC):
+    """ Base Stream Hub """
+
+    def __init__(self, delegate: ConnectionDelegate):
+        super().__init__(delegate=delegate)
+        self.__channel_pool = self._create_channel_pool()
+
+    # noinspection PyMethodMayBeStatic
+    def _create_channel_pool(self):
+        return ChannelPool()
+
+    #
+    #   Channel
+    #
+
+    # noinspection PyMethodMayBeStatic
+    def _create_channel(self, remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Channel:
+        """ create channel with socket & addresses """
+        return StreamChannel(remote=remote, local=local)
+
+    # Override
+    def _all_channels(self) -> Iterable[Channel]:
+        """ get a copy of all channels """
+        return self.__channel_pool.items
+
+    # Override
+    def _remove_channel(self, channel: Optional[Channel],
+                        remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+        """ remove cached channel """
+        return self.__channel_pool.remove(item=channel, remote=remote, local=local)
+
+    def _get_channel(self, remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+        """ get cached channel """
+        return self.__channel_pool.get(remote=remote, local=local)
+
+    def _set_channel(self, channel: Channel,
+                     remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+        """ cache channel """
+        return self.__channel_pool.set(item=channel, remote=remote, local=local)
+
+
+class ServerHub(StreamHub, Runnable):
+    """ Stream Server Hub """
+
+    def __init__(self, delegate: ConnectionDelegate):
+        super().__init__(delegate=delegate)
+        self.__local = None   # SocketAddress
+        self.__master = None  # socket.socket
+        self.__daemon = Daemon(target=self)
+        self.__running = False
+
+    # Override
+    def _create_connection(self, remote: SocketAddress, local: Optional[SocketAddress]) -> Optional[Connection]:
+        conn = BaseConnection(remote=remote, local=local)
+        conn.delegate = self.delegate  # gate
+        return conn
+
+    async def bind(self, address: Optional[SocketAddress] = None, host: Optional[str] = None, port: Optional[int] = 0):
+        if address is None:
+            if port > 0:
+                assert host is not None, 'host should not be empty'
+                address = (host, port)
+            else:
+                address = self.__local
+                assert address is not None, 'local address not set'
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.setblocking(True)
+        sock.bind(address)
+        sock.listen(1)
+        # sock.setblocking(False)
+        self._set_master(master=sock)
+        self.__local = address
+
+    def _get_master(self) -> Optional[socket.socket]:
+        return self.__master
+
+    def _set_master(self, master: socket.socket):
+        # 1. replace with new socket
+        old = self.__master
+        self.__master = master
+        # 2. close old socket
+        if not (old is None or old is master):
+            old.close()
+
+    @property
+    def local_address(self) -> Optional[SocketAddress]:
+        return self.__local
+
+    #
+    #   Threading
+    #
+
+    @property
+    def running(self) -> bool:
+        return self.__running
+
+    async def start(self):
+        assert not self.__running, 'server hub is running: %s' % self
+        # 1. mark this hub to running
+        self.__running = True
+        # 2. start an async task for this hub
+        self.__daemon.start()
+        # await self.run()
+
+    async def stop(self):
+        # 1. mark this hub to stopped
+        self.__running = False
+        # 2. waiting for the hub to stop
+        await Runner.sleep(seconds=0.25)
+        # 3. cancel the async task
+        self.__daemon.stop()
+
+    # Override
+    async def run(self):
+        self.__running = True
+        while self.running:
+            master = self._get_master()
+            try:
+                sock, address = master.accept()
+                if sock is None:
+                    await Runner.sleep(seconds=Runner.INTERVAL_NORMAL)
+                else:
+                    await self._accept(remote=address, local=self.local_address, sock=sock)
+            except socket.error as error:
+                if error.errno == socket.EAGAIN:  # error.strerror == 'Resource temporarily unavailable':
+                    if not is_blocking(sock=master):
+                        continue
+                print('[TCP] socket error: %s' % error)
+            except Exception as error:
+                print('[TCP] accept error: %s' % error)
+
+    async def _accept(self, remote: SocketAddress, local: SocketAddress, sock: socket.socket):
+        # override for user-customized channel
+        channel = self._create_channel(remote=remote, local=local)
+        assert isinstance(channel, BaseChannel), 'channel error: %s, %s' % (remote, channel)
+        # set socket for this channel
+        sock.setblocking(False)
+        await channel.set_socket(sock=sock)
+        cached = self._set_channel(channel, remote=channel.remote_address, local=channel.local_address)
+        if cached is not None and cached is not channel:
+            await cached.close()
+
+    # Override
+    async def open(self, remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+        assert remote is not None, 'remote address empty: %s, %s' % (remote, local)
+        return self._get_channel(remote=remote, local=local)
+
+
+class ClientHub(StreamHub):
+    """ Stream Client Hub """
+
+    def __init__(self, delegate: ConnectionDelegate):
+        super().__init__(delegate=delegate)
+        self.__lock = threading.Lock()
+
+    # Override
+    def _create_connection(self, remote: SocketAddress, local: Optional[SocketAddress]) -> Optional[Connection]:
+        conn = ActiveConnection(remote=remote, local=local)
+        conn.delegate = self.delegate  # gate
+        return conn
+
+    # Override
+    async def open(self, remote: Optional[SocketAddress], local: Optional[SocketAddress]) -> Optional[Channel]:
+        assert remote is not None, 'remote address empty: %s, %s' % (remote, local)
+        # try to get channel
+        with self.__lock:
+            old = self._get_channel(remote=remote, local=local)
+            if old is None:
+                # create channel with socket
+                channel = self._create_channel(remote=remote, local=local)
+                cached = self._set_channel(channel, remote=remote, local=local)
+                if cached is not None and cached is not channel:
+                    await cached.close()
+            else:
+                channel = old
+        if old is None:
+            # initialize socket
+            sock = await create_socket(remote=remote, local=local)
+            if sock is None:
+                print('[TCP] failed to prepare socket: %s -> %s' % (local, remote))
+                self._remove_channel(channel, remote=remote, local=local)
+                channel = None
+            else:
+                assert isinstance(channel, BaseChannel), 'channel error: %s, %s' % (remote, channel)
+                # set socket for this channel
+                await channel.set_socket(sock=sock)
+        return channel
+
+
+async def create_socket(remote: SocketAddress, local: Optional[SocketAddress]) -> Optional[socket.socket]:
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 0)
+        sock.setblocking(True)
+        # try to bind
+        if local is not None:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind(local)
+        # try to connect
+        sock.connect(remote)
+        sock.setblocking(False)
+        return sock
+    except socket.error as error:
+        print('[TCP] %s > failed to create socket %s -> %s: %s' % (current_time(), local, remote, error))
+
+
+def current_time() -> str:
+    now = time.time()
+    localtime = time.localtime(now)
+    return time.strftime('%Y-%m-%d %H:%M:%S', localtime)
