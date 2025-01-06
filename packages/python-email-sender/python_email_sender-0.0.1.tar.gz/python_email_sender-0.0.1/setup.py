6ef1ca60326bdaef97548bb497138ccce1a58266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:23 +0000

    Add package python-email-sender/0.0.1/python_email_sender-0.0.1.tar.gz

---
+++ packages/python-email-sender/python_email_sender-0.0.1.tar.gz/python_email_sender-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-email-sender',
+    version='0.0.1',
+    description='Python package for sending email',
+    author='Kwangryeol Park',
+    author_email='pkr7098@gmail.com',
+    url='',
+    packages=find_packages(exclude=[]),
+    package_data={},
+)
