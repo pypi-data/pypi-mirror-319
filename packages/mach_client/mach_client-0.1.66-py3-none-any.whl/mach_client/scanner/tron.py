ecddc4f8977c7768d8441e69555ccd08766d862c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:26 +0000

    Add package mach_client/0.1.66/mach_client-0.1.66-py3-none-any.whl

---
+++ packages/mach_client/mach_client-0.1.66-py3-none-any.whl/mach_client/scanner/tron.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+import typing
+
+from ..asset import Token
+from .scanner import Scanner
+
+
+class TronScan(Scanner):
+    url = "https://tronscan.io/#"
+
+    @typing.override
+    def address(self, address: str) -> str:
+        return f"{self.url}/address/{address}"
+
+    @typing.override
+    def transaction(self, transaction_id: str) -> str:
+        return f"{self.url}/transaction/{transaction_id}"
+
+    @typing.override
+    def token(self, token: Token) -> str:
+        return f"{self.url}/token20/{token.address}"
