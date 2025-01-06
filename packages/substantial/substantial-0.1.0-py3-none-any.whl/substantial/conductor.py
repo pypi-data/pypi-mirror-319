0dc9d2af6b53f754604b1249d3ba247fef4f870d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:55 +0000

    Add package substantial/0.1.0/substantial-0.1.0-py3-none-any.whl

---
+++ packages/substantial/substantial-0.1.0-py3-none-any.whl/substantial/conductor.py
@@ -0,0 +1,55 @@
+import asyncio
+from uuid import uuid4
+
+from substantial.agent import Agent
+from substantial.backends.backend import Backend
+from substantial.workflows import Store
+from substantial.filters import WorkflowFilter
+from substantial.workflows.run import Run
+from substantial.workflows.workflow import Workflow
+import aioprocessing
+
+
+class Conductor:
+    def __init__(self, backend: Backend):
+        self.backend = backend
+        self.filters = WorkflowFilter(self)
+
+    def register(self, workflow: Workflow):
+        Store.register(workflow)
+
+    async def start(
+        self,
+        workflow: Workflow,
+        kwargs=None,
+        queue="default",
+    ):
+        if kwargs is None:
+            kwargs = {}
+
+        run_id = f"{workflow.id}-{uuid4()}"
+        run = Run(run_id, queue, self.backend)
+
+        await self.backend.write_workflow_link(workflow.id, run_id)
+
+        await run.start(kwargs)
+        return run
+
+    def run_for(self, run_id, queue="default"):
+        return Run(run_id, queue, self.backend)
+
+    def run(self, queue="default"):
+        return asyncio.create_task(self.run_as_process(queue))
+
+    async def run_as_process(self, queue):
+        agent = Agent(self.backend, queue)
+        p = aioprocessing.AioProcess(target=agent.run_sync)
+        try:
+            p.start()
+            await p.coro_join()
+            return p
+        except asyncio.CancelledError:
+            p.terminate()
+            print("Cancelled")
+        except Exception:
+            raise
---
+++ packages/substantial/substantial-0.1.0-py3-none-any.whl/substantial/workflows/context.py
@@ -0,0 +1,184 @@
+import random
+import uuid
+import orjson as json
+from typing import TYPE_CHECKING, Any, Callable, List, Optional, Type
+from datetime import datetime, timedelta, timezone
+
+from substantial.protos import events, metadata
+from substantial.workflows.parser import parse
+
+
+if TYPE_CHECKING:
+    from substantial.workflows.run import Run
+
+from substantial.types import (
+    CancelWorkflow,
+    DelayMode,
+    Interrupt,
+    ValueEval,
+    RetryStrategy,
+)
+
+
+class Context:
+    def __init__(
+        self,
+        run: "Run",
+        metadata: List[metadata.Metadata],
+        events: List[events.Event],
+    ):
+        self.run = run
+        # self.metadata = metadata
+        # FIXME only events should be required, metadata is only for the run, not the context
+        self.events = events
+        self.__id = 0
+        self.utils = Utils(self)
+        self.compensation_stack: List[Callable[[], Any]] = []
+
+    def __next_id(self):
+        # FIXME: maybe use lambda hash instead? but how portable that would be?
+        # Also this relies on the fact that the Context is recreated each replay
+        self.__id += 1
+        return self.__id
+
+    def source(self, event: events.Event):
+        self.events.append(event)
+
+    # low-level
+
+    async def save(
+        self,
+        f: Callable,
+        *,
+        expected_type: Optional[Type] = None,
+        compensate_with: Optional[Callable[[], Any]] = None,
+        timeout: Optional[timedelta] = None,
+        retry_strategy: Optional[RetryStrategy] = None,
+        max_compensation_attempts: int = 3,
+    ) -> Any:
+        timeout_secs = timeout.total_seconds() if timeout is not None else None
+        evaluator = ValueEval(
+            f,
+            timeout_secs,
+            retry_strategy,
+            compensate_with,
+            max_compensation_attempts,
+        )
+        save_id = self.__next_id()
+
+        saved = None
+        save_records = filter(
+            lambda e: e.is_set("save") and save_id == e.save.id, self.events
+        )
+
+        for record in save_records:
+            if record.save.counter == -1:
+                saved = record.save
+                break
+            else:
+                latest = None
+                while True:
+                    # popfront till we get the Save with the highest counter or resolved (-1)
+                    peek_next: events.Event | None = next(save_records, None)
+                    if peek_next is None:
+                        break
+                    else:
+                        latest = peek_next
+                        if peek_next.save.counter == -1:
+                            break
+                if latest is not None:
+                    saved = latest.save
+
+        if saved is None:
+            val = await evaluator.exec(self, save_id, None)
+            print(f"Computed id#{save_id}, {val}")
+            return val
+        else:
+            if saved.counter != -1:
+                # retry mode (after replay)
+                val = await evaluator.exec(self, save_id, saved.counter)
+                parsed_val = parse(json.dumps(val), expected_type)
+                return parsed_val
+            else:
+                # resolved mode
+                print(f"Reused {saved.value} for id#{save_id}")
+                return parse(saved.value, expected_type)
+
+    def handle(self, event_name: str, cb: Callable[[Any], Any]):
+        for record in self.events:
+            if record.is_set("send") and event_name == record.send.name:
+                payload = parse(record.send.value)
+                ret = cb(payload)
+                return ret
+        return None
+
+    async def ensure(self, f: Callable[[], bool]):
+        """Wait for `condition()` to be True"""
+        result = f()
+        if not result:
+            raise Interrupt("wait => condition is still false")
+        return result
+
+    # high-level
+
+    async def sleep(self, duration: timedelta) -> Any:
+        sleep_id = self.__next_id()
+        sleep_records = list(
+            filter(
+                lambda e: e.is_set("sleep") and sleep_id == e.sleep.id,
+                self.events,
+            )
+        )
+
+        now = datetime.now(tz=timezone.utc)
+        if len(sleep_records) == 0:
+            self.source(
+                events.Event(
+                    sleep=events.Sleep(sleep_id, start=now, end=now + duration)
+                )
+            )
+            raise DelayMode(f"Sleep id#{sleep_id} encoutered")
+
+        for record in sleep_records:
+            op_end = record.sleep.end.replace(tzinfo=timezone.utc)
+            if now >= op_end:
+                print(f"Skip sleep id#{sleep_id}")
+                return
+
+        raise DelayMode(f"Sleep id#{sleep_id} not ending yet")
+
+    async def receive(self, event_name: str):
+        """
+        Wait for events emitted from outside the workflow
+        """
+        proxy = {}
+        self.handle(event_name, lambda payload: proxy.update(val=payload))
+        await self.ensure(lambda: "val" in proxy)
+        return proxy["val"]
+
+    def cancel_run(self):
+        """
+        Interrupt after the call
+        """
+        raise CancelWorkflow(self.run)
+
+
+class Utils:
+    def __init__(self, ctx: Context):
+        self.__ctx = ctx
+
+    async def now(self, tz: Optional[timezone] = None) -> datetime:
+        now = await self.__ctx.save(
+            lambda: datetime.now(tz),
+            expected_type=datetime,
+        )
+        return now
+
+    async def random(self, a: int, b: int) -> int:
+        return await self.__ctx.save(lambda: random.randint(a, b))
+
+    async def uuid4(self) -> uuid.UUID:
+        serialized_uuid = await self.__ctx.save(
+            lambda: uuid.uuid4(), expected_type=uuid.UUID
+        )
+        return serialized_uuid
