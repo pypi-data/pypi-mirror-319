16ee1551e87f4d7291a2906fdf53cffc4784d5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 16:03:51 +0000

    Add package ansys-fluent-visualization/0.16.0/ansys_fluent_visualization-0.16.0-py3-none-any.whl

---
+++ packages/ansys-fluent-visualization/ansys_fluent_visualization-0.16.0-py3-none-any.whl/ansys/fluent/visualization/_config.py
@@ -0,0 +1,41 @@
+"""Global configuration state for visualization."""
+
+_global_config = {"blocking": False, "set_view_on_display": None}
+
+
+def get_config() -> dict:
+    """Retrieve visualization configuration.
+
+    Returns
+    -------
+    config : dict
+        Keys are parameter names that can be passed to :func:`set_config`.
+    """
+    import ansys.fluent.visualization as pyviz
+
+    _global_config["blocking"] = not pyviz.INTERACTIVE
+    return _global_config.copy()
+
+
+def set_config(blocking: bool = False, set_view_on_display: str = "isometric"):
+    """Set visualization configuration.
+
+    Parameters
+    ----------
+    blocking : bool, default=False
+        If True, then graphics/plot display will block the current thread.
+    set_view_on_display : str, default=None
+        If specified, then graphics will always be displayed in the specified view.
+        Valid values are xy, xz, yx, yz, zx, zy and isometric.
+    """
+    if set_view_on_display not in set_config.allowed_views:
+        raise ValueError(
+            f"'{set_view_on_display}' is not an allowed view.\n"
+            f"Allowed views are: {set_config.allowed_views}"
+        )
+
+    _global_config["blocking"] = blocking
+    _global_config["set_view_on_display"] = set_view_on_display
+
+
+set_config.allowed_views = ["xy", "xz", "yx", "yz", "zx", "zy", "isometric"]
---
+++ packages/ansys-fluent-visualization/ansys_fluent_visualization-0.16.0-py3-none-any.whl/ansys/fluent/visualization/graphics/graphics_objects.py
@@ -0,0 +1,234 @@
+"""Module providing visualization objects for PyVista."""
+
+import sys
+from typing import Optional
+
+from ansys.fluent.core.post_objects.meta import Command
+from ansys.fluent.core.post_objects.post_helper import PostAPIHelper
+from ansys.fluent.core.post_objects.post_object_definitions import (
+    ContourDefn,
+    MeshDefn,
+    PathlinesDefn,
+    SurfaceDefn,
+    VectorDefn,
+)
+from ansys.fluent.core.post_objects.post_objects_container import (
+    Graphics as GraphicsContainer,
+)
+
+from ansys.fluent.visualization.graphics.graphics_windows_manager import (
+    graphics_windows_manager,
+)
+
+
+class Graphics(GraphicsContainer):
+    """
+    This class provides access to ``Graphics`` object containers for a given
+    session so that graphics objects can be created.
+    """
+
+    def __init__(
+        self, session, post_api_helper=PostAPIHelper, local_surfaces_provider=None
+    ):
+        super().__init__(
+            session, sys.modules[__name__], post_api_helper, local_surfaces_provider
+        )
+
+
+class Mesh(MeshDefn):
+    """Provides for displaying mesh graphics.
+
+    Parameters
+    ----------
+    name :
+
+    parent :
+
+    api_helper :
+
+
+    .. code-block:: python
+
+        from ansys.fluent.visualization import  Graphics
+
+        graphics_session = Graphics(session)
+        mesh1 = graphics_session.Meshes["mesh-1"]
+        mesh1.show_edges = True
+        mesh1.surfaces = ['wall']
+        mesh1.display("window-0")
+    """
+
+    @Command
+    def display(self, window_id: Optional[str] = None, overlay: Optional[bool] = False):
+        """Display mesh graphics.
+
+        Parameters
+        ----------
+        window_id : str, optional
+            Window ID. If an ID is not specified, a unique ID is used.
+            The default is ``None``.
+        overlay : bool, optional
+            Whether to overlay graphics over existing graphics.
+            The default is ``False``.
+        """
+        graphics_windows_manager.plot(
+            self, window_id=window_id, overlay=overlay, fetch_data=True
+        )
+
+
+class Pathlines(PathlinesDefn):
+    """Pathlines definition for PyVista.
+
+    .. code-block:: python
+
+        from ansys.fluent.visualization import  Graphics
+
+        graphics_session = Graphics(session)
+        pathlines1 = graphics_session.Pathlines["pathlines-1"]
+        pathlines1.field = "velocity-magnitude"
+        pathlines1.surfaces = ['inlet']
+        pathlines1.display("window-0")
+    """
+
+    @Command
+    def display(self, window_id: Optional[str] = None, overlay: Optional[bool] = False):
+        """Display mesh graphics.
+
+        Parameters
+        ----------
+        window_id : str, optional
+            Window ID. If an ID is not specified, a unique ID is used.
+            The default is ``None``.
+        overlay : bool, optional
+            Whether to overlay graphics over existing graphics.
+            The default is ``False``.
+        """
+        graphics_windows_manager.plot(
+            self, window_id=window_id, overlay=overlay, fetch_data=True
+        )
+
+
+class Surface(SurfaceDefn):
+    """Provides for displaying surface graphics.
+
+    Parameters
+    ----------
+    name :
+
+    parent :
+
+    api_helper :
+
+
+    .. code-block:: python
+
+        from ansys.fluent.visualization import Graphics
+
+        graphics_session = Graphics(session)
+        surface1 = graphics_session.Surfaces["surface-1"]
+        surface1.definition.type = "iso-surface"
+        surface1.definition.iso_surface.field= "velocity-magnitude"
+        surface1.definition.iso_surface.rendering= "contour"
+        surface1.definition.iso_surface.iso_value = 0.0
+        surface1.display("window-0")
+    """
+
+    @Command
+    def display(self, window_id: Optional[str] = None, overlay: Optional[bool] = False):
+        """Display surface graphics.
+
+        Parameters
+        ----------
+        window_id : str, optional
+            Window ID. If an ID is not specified, a unique ID is used.
+            The default is ``None``.
+        overlay : bool, optional
+            Whether to overlay graphics over existing graphics.
+            The default is ``False``.
+        """
+        graphics_windows_manager.plot(
+            self, window_id=window_id, overlay=overlay, fetch_data=True
+        )
+
+
+class Contour(ContourDefn):
+    """Provides for displaying contour graphics.
+
+    Parameters
+    ----------
+    name :
+
+    parent :
+
+    api_helper :
+
+
+    .. code-block:: python
+
+        from ansys.fluent.visualization import  Graphics
+
+        graphics_session = Graphics(session)
+        contour1 = graphics_session.Contours["contour-1"]
+        contour1.field = "velocity-magnitude"
+        contour1.surfaces = ['wall']
+        contour1.display("window-0")
+    """
+
+    @Command
+    def display(self, window_id: Optional[str] = None, overlay: Optional[bool] = False):
+        """Display contour graphics.
+
+        Parameters
+        ----------
+        window_id : str, optional
+            Window ID. If an ID is not specified, a unique ID is used.
+            The default is ``None``.
+        overlay : bool, optional
+            Whether to overlay graphics over existing graphics.
+            The default is ``False``.
+        """
+        graphics_windows_manager.plot(
+            self, window_id=window_id, overlay=overlay, fetch_data=True
+        )
+
+
+class Vector(VectorDefn):
+    """Provides for displaying vector graphics.
+
+    Parameters
+    ----------
+    name :
+
+    parent :
+
+    api_helper :
+
+
+    .. code-block:: python
+
+        from ansys.fluent.visualization import  Graphics
+
+        graphics_session = Graphics(session)
+        vector1 = graphics_session.Vectors["vector-1"]
+        vector1.surfaces = ['symmetry']
+        vector1.scale = 4.0
+        vector1.skip = 4
+        vector1.display("window-0")
+    """
+
+    @Command
+    def display(self, window_id: Optional[str] = None, overlay: Optional[bool] = False):
+        """Display vector graphics.
+
+        Parameters
+        ----------
+        window_id : str, optional
+            Window ID. If an ID is not specified, a unique ID is used.
+            The default is ``None``.
+        overlay : bool, optional
+            Whether to overlay graphics over existing graphics.
+            The default is ``False``.
+        """
+        graphics_windows_manager.plot(
+            self, window_id=window_id, overlay=overlay, fetch_data=True
+        )
