1f6dd441ffd4170f4e84610ffcdbf8c1c6df7550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 14:03:22 +0000

    Add package dexpdf2/1.0.1/dexpdf2-1.0.1-py3-none-any.whl

---
+++ packages/dexpdf2/dexpdf2-1.0.1-py3-none-any.whl/dexpdf/pdf2text.py
@@ -0,0 +1,14 @@
+""" This modules converts PDF to text."""
+
+
+def convert(path):
+    """ 
+    Convert the given PDF to text.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as a string
+     """
+    print("pdf2text")
