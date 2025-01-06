a59d562a4a812a18bf9e14fcff19bb18196225db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 15:04:28 +0000

    Add package gazedetector/0.7.0/gazedetector-0.7.0-py3-none-any.whl

---
+++ packages/gazedetector/gazedetector-0.7.0-py3-none-any.whl/gazedetector/__init__.py
@@ -0,0 +1,7 @@
+# This file marks the directory as a Python package.
+
+# Import necessary components for easy access.
+from .gaze_detector import detect_gaze  # Replace with your actual function or class names
+from .app import run_server  # If run_server is a key function in your app
+
+# You can add other initialization code if needed.
