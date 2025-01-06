b7c2d14cdcf68f80e1d5d71d582ed70f3c786967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 04:03:36 +0000

    Add package dudl/0.3/dudl-0.3.tar.gz

---
+++ packages/dudl/dudl-0.3.tar.gz/dudl-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='DUDL',
+    version='0.3.0',    
+    description='The DUDL Game',
+    url='https://github.com/v0rtex20k/DUDL/',
+    packages=['DUDL'],
+)
+
