7d79a5907e02ce2c5a9d382c893730b9aa3dfcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:09:56 +0000

    Add package olab-prediction-market-sdk/0.0.53/olab_prediction_market_sdk-0.0.53-py3-none-any.whl

---
+++ packages/olab-prediction-market-sdk/olab_prediction_market_sdk-0.0.53-py3-none-any.whl/olab_prediction_market_sdk/chain/safe/constants.py
@@ -0,0 +1,18 @@
+from eth_typing import ChecksumAddress, HexAddress, HexStr
+from enum import Enum
+
+NULL_ADDRESS: ChecksumAddress = ChecksumAddress(HexAddress(HexStr("0x" + "0" * 40)))
+
+# Gas required to transfer a regular token. Assume the worst scenario with a regular token transfer without storage
+# initialized (payment_receiver no previous owner of token)
+TOKEN_TRANSFER_GAS = 60_000
+
+
+class TxSpeed(Enum):
+    SLOWEST = 0
+    VERY_SLOW = 1
+    SLOW = 2
+    NORMAL = 3
+    FAST = 4
+    VERY_FAST = 5
+    FASTEST = 6
