b652aed7691dbdb5ac6799f5bb6c7aa2258c0830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 04:04:30 +0000

    Add package dexat/1/dexat-1.tar.gz

---
+++ packages/dexat/dexat-1.tar.gz/dexat-1/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">This Python package vulnerable to dependency confusion vulnerability</h1>
---
+++ packages/dexat/dexat-1.tar.gz/dexat-1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Dependency confiuse Attack'
+LONG_DESCRIPTION = 'Python package dependency confiuse vulnerability POC. Impact this vulnerability is Remote code execution (RCE)'
+
+# Setting up
+setup(
+    name="dexat",
+    version=1,
+    author="dex tester",
+    author_email="<bughunt250@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'discord'],
+    keywords=[]
+   )
