98b257e1eb48bcd4996ce6c840f21aab13d144f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:16 +0000

    Add package sora-sdk/2025.1.0.dev1/sora_sdk-2025.1.0.dev1.tar.gz

---
+++ packages/sora-sdk/sora_sdk-2025.1.0.dev1.tar.gz/sora_sdk-2025.1.0.dev1/tests/test_macos.py
@@ -0,0 +1,239 @@
+import sys
+import time
+import uuid
+
+import pytest
+from client import SoraClient, SoraRole
+
+"""
+GitHub Actions で Video Toolbox を送受信で利用しようとするとエラーになるので、
+テストを sendonly のみに絞っている
+"""
+
+
+@pytest.mark.skipif(sys.platform != "darwin", reason="macOS でのみ実行する")
+@pytest.mark.parametrize(
+    "video_codec_type",
+    ["H264", "H265"],
+)
+def test_macos_video_hwa_sendonly(setup, video_codec_type):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    sendonly = SoraClient(
+        signaling_urls,
+        SoraRole.SENDONLY,
+        channel_id,
+        audio=False,
+        video=True,
+        video_codec_type=video_codec_type,
+        metadata=metadata,
+        use_hwa=True,
+    )
+    sendonly.connect(fake_video=True)
+
+    time.sleep(5)
+
+    assert sendonly.connect_message is not None
+    assert sendonly.connect_message["channel_id"] == channel_id
+    assert "video" in sendonly.connect_message
+    assert sendonly.connect_message["video"]["codec_type"] == video_codec_type
+
+    sendonly_stats = sendonly.get_stats()
+
+    sendonly.disconnect()
+
+    # codec が無かったら StopIteration 例外が上がる
+    codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
+    # H.264 が採用されているかどうか確認する
+    assert codec_stats["mimeType"] == f"video/{video_codec_type}"
+
+    # outbound-rtp が無かったら StopIteration 例外が上がる
+    outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
+    assert outbound_rtp_stats["encoderImplementation"] == "VideoToolbox"
+    assert outbound_rtp_stats["bytesSent"] > 0
+    assert outbound_rtp_stats["packetsSent"] > 0
+
+
+@pytest.mark.skipif(sys.platform != "darwin", reason="macOS でのみ実行する")
+@pytest.mark.parametrize(
+    ("video_codec_type", "expected_implementation"),
+    [
+        ("H264", "VideoToolbox"),
+        ("H265", "VideoToolbox"),
+    ],
+)
+def test_macos_simulcast(setup, video_codec_type, expected_implementation):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    sendonly = SoraClient(
+        signaling_urls,
+        SoraRole.SENDONLY,
+        channel_id,
+        simulcast=True,
+        audio=False,
+        video=True,
+        video_codec_type=video_codec_type,
+        video_bit_rate=3000,
+        metadata=metadata,
+        video_width=1280,
+        video_height=720,
+        use_hwa=True,
+    )
+    sendonly.connect(fake_video=True)
+
+    time.sleep(5)
+
+    sendonly_stats = sendonly.get_stats()
+
+    sendonly.disconnect()
+
+    # "type": "answer" の SDP で Simulcast があるかどうか
+    assert sendonly.answer_message is not None
+    assert "sdp" in sendonly.answer_message
+    assert "a=simulcast:send r0;r1;r2" in sendonly.answer_message["sdp"]
+
+    # codec が無かったら StopIteration 例外が上がる
+    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
+    assert sendonly_codec_stats["mimeType"] == f"video/{video_codec_type}"
+
+    # 複数のoutbound-rtp統計情報を取得
+    outbound_rtp_stats = [
+        s for s in sendonly_stats if s.get("type") == "outbound-rtp" and s.get("kind") == "video"
+    ]
+    assert len(outbound_rtp_stats) == 3
+
+    # rid でソート
+    sorted_stats = sorted(outbound_rtp_stats, key=lambda x: x.get("rid", ""))
+
+    for i, rtp_stat in enumerate(sorted_stats):
+        assert rtp_stat["rid"] == f"r{i}"
+        assert "SimulcastEncoderAdapter" in rtp_stat["encoderImplementation"]
+        assert expected_implementation in rtp_stat["encoderImplementation"]
+        assert rtp_stat["bytesSent"] > 0
+        assert rtp_stat["packetsSent"] > 0
+
+
+@pytest.mark.skip(reason="ローカルでは成功する")
+def test_macos_h264_sendonly_recvonly(setup):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    sendonly = SoraClient(
+        signaling_urls,
+        SoraRole.SENDONLY,
+        channel_id,
+        audio=False,
+        video=True,
+        video_codec_type="H264",
+        metadata=metadata,
+        use_hwa=True,
+    )
+    sendonly.connect()
+
+    recvonly = SoraClient(
+        signaling_urls,
+        SoraRole.RECVONLY,
+        channel_id,
+        metadata=metadata,
+        use_hwa=True,
+    )
+    recvonly.connect()
+
+    time.sleep(5)
+
+    sendonly_stats = sendonly.get_stats()
+    recvonly_stats = recvonly.get_stats()
+
+    sendonly.disconnect()
+    recvonly.disconnect()
+
+    # codec が無かったら StopIteration 例外が上がる
+    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
+    # H.264 が採用されているかどうか確認する
+    assert sendonly_codec_stats["mimeType"] == "video/H264"
+
+    # outbound-rtp が無かったら StopIteration 例外が上がる
+    outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
+    assert outbound_rtp_stats["encoderImplementation"] == "VideoToolbox"
+    assert outbound_rtp_stats["bytesSent"] > 0
+    assert outbound_rtp_stats["packetsSent"] > 0
+
+    # codec が無かったら StopIteration 例外が上がる
+    recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
+    # H.264 が採用されているかどうか確認する
+    assert recvonly_codec_stats["mimeType"] == "video/H264"
+
+    # outbound-rtp が無かったら StopIteration 例外が上がる
+    inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
+    assert inbound_rtp_stats["decoderImplementation"] == "VideoToolbox"
+    assert inbound_rtp_stats["bytesReceived"] > 0
+    assert inbound_rtp_stats["packetsReceived"] > 0
+
+
+@pytest.mark.skip(reason="ローカルでは成功する")
+def test_macos_h265_sendonly_recvonly(setup):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    sendonly = SoraClient(
+        signaling_urls,
+        SoraRole.SENDONLY,
+        channel_id,
+        audio=False,
+        video=True,
+        video_codec_type="H265",
+        metadata=metadata,
+        use_hwa=True,
+    )
+    sendonly.connect()
+
+    recvonly = SoraClient(
+        signaling_urls,
+        SoraRole.RECVONLY,
+        channel_id,
+        metadata=metadata,
+        use_hwa=True,
+    )
+    recvonly.connect()
+
+    time.sleep(5)
+
+    sendonly_stats = sendonly.get_stats()
+    recvonly_stats = recvonly.get_stats()
+
+    sendonly.disconnect()
+    recvonly.disconnect()
+
+    # codec が無かったら StopIteration 例外が上がる
+    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
+    assert sendonly_codec_stats["mimeType"] == "video/H265"
+
+    # outbound-rtp が無かったら StopIteration 例外が上がる
+    outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
+    assert outbound_rtp_stats["encoderImplementation"] == "VideoToolbox"
+    assert outbound_rtp_stats["bytesSent"] > 0
+    assert outbound_rtp_stats["packetsSent"] > 0
+
+    # codec が無かったら StopIteration 例外が上がる
+    recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
+    assert recvonly_codec_stats["mimeType"] == "video/H265"
+
+    # outbound-rtp が無かったら StopIteration 例外が上がる
+    inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
+    assert inbound_rtp_stats["decoderImplementation"] == "VideoToolbox"
+    assert inbound_rtp_stats["bytesReceived"] > 0
+    assert inbound_rtp_stats["packetsReceived"] > 0
