41cfb1535bd60b88ef633cd0868e335f96342672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:10:13 +0000

    Add package pip2conda/0.6.0/pip2conda-0.6.0.tar.gz

---
+++ packages/pip2conda/pip2conda-0.6.0.tar.gz/pip2conda-0.6.0/pip2conda/__main__.py
@@ -0,0 +1,12 @@
+# Copyright (C) Cardiff University (2022)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Module execution entry point for pip2conda
+"""
+
+import sys
+
+from .pip2conda import main
+
+if __name__ == "__main__":
+    sys.exit(main())
