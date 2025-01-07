f8c5c9058ea0b891977440454ee0fc7d0d929f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 18:04:58 +0000

    Add package onlyaml/0.1.0/onlyaml-0.1.0.tar.gz

---
+++ packages/onlyaml/onlyaml-0.1.0.tar.gz/onlyaml-0.1.0/onlyaml/__init__.py
@@ -0,0 +1,4 @@
+from .onlyaml import parse
+
+# These are the only items accessible when importing the package
+__all__ = ["parse"]
---
+++ packages/onlyaml/onlyaml-0.1.0.tar.gz/onlyaml-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='onlyaml',
+    version='0.1.0',
+    description='A python lib impose your program only accept yaml file as CL argument',
+    url='https://github.com/shuds13/pyexample',
+    author='WD',
+    author_email='',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
