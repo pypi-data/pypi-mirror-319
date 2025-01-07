db058bdb4033249ff60de0544aeefec0f2787324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 14:56:16 +0000

    Add package nahcrofDB/2.4.0/nahcrofdb-2.4.0.tar.gz

---
+++ packages/nahcrofDB/nahcrofdb-2.4.0.tar.gz/nahcrofdb-2.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.4.0'
+DESCRIPTION = 'A key-value database'
+LONG_DESCRIPTION = 'nahcrofDB is a key-value database designed to be extremely simple to get running and to use. Please note that at this current time there is no way to host the database, this simply interacts with an existing file'
+
+# Setting up
+setup(
+    name="nahcrofDB",
+    version=VERSION,
+    author="Tyrae Paul",
+    author_email="support@nahcrof.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['database'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ]
+)
---
+++ packages/nahcrofDB/nahcrofdb-2.4.0.tar.gz/nahcrofdb-2.4.0/src/__init__.py
@@ -0,0 +1,1 @@
+# not gonna lie, I have no clue what I'm suppose to put here
