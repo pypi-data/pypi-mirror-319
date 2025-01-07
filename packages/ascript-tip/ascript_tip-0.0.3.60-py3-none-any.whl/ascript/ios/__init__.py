b52fc6e2355ad81bfae3fa5ab6b4e0f1f47d54e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:36 +0000

    Add package ascript-tip/0.0.3.60/ascript_tip-0.0.3.60-py3-none-any.whl

---
+++ packages/ascript-tip/ascript_tip-0.0.3.60-py3-none-any.whl/ascript/ios/__init__.py
@@ -0,0 +1,19 @@
+
+import tidevice
+import tidevice3.api
+
+# 获取所有连接的iOS设备
+
+# print(devices)
+
+
+# # 选择一个设备（假设我们只有一个设备连接）
+# device = Device(devices[0]['udid'])
+#
+# # 启动WebDriverAgent服务
+# device.wda_start()
+#
+# # 这里你可以使用 `device` 对象执行其他自动化任务
+#
+# # 停止WebDriverAgent服务
+# device.wda_stop()
