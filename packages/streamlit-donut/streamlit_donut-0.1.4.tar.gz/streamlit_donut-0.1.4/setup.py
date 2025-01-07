debd1b87a0826f3b7c3abfed5582f13b261dc585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:26 +0000

    Add package streamlit-donut/0.1.4/streamlit_donut-0.1.4.tar.gz

---
+++ packages/streamlit-donut/streamlit_donut-0.1.4.tar.gz/streamlit_donut-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+working_directory = path.dirname(path.abspath(__file__))
+
+with open(path.join(working_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="streamlit-donut",
+    version="0.1.4",
+    author="Benson Nderitu",
+    author_email="bent25066@gmail.com",
+    description="A Streamlit component for rendering donut metrics/visuals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/benson-nderitu/streamlit-donut",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
