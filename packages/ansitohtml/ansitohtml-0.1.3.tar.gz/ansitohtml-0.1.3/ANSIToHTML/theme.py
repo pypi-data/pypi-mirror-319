73e667c8c14c9c85c8e17bcaaa40bcb4d9bd5670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:04:26 +0000

    Add package ansitohtml/0.1.3/ansitohtml-0.1.3.tar.gz

---
+++ packages/ansitohtml/ansitohtml-0.1.3.tar.gz/ansitohtml-0.1.3/ANSIToHTML/theme.py
@@ -0,0 +1,18 @@
+DEFAULT = {
+    30: "rgb(0, 0, 0)",  # Black
+    31: "rgb(255, 69, 0)",  # Red (Orange Red)
+    32: "rgb(50, 205, 50)",  # Green (Lime Green)
+    33: "rgb(255, 215, 0)",  # Yellow (Gold)
+    34: "rgb(65, 105, 225)",  # Blue (Royal Blue)
+    35: "rgb(186, 85, 211)",  # Magenta (Medium Orchid)
+    36: "rgb(64, 224, 208)",  # Cyan (Turquoise)
+    37: "rgb(245, 245, 245)",  # White (White Smoke)
+    90: "rgb(169, 169, 169)",  # Bright Black (Dark Gray)
+    91: "rgb(255, 99, 71)",  # Bright Red (Tomato)
+    92: "rgb(144, 238, 144)",  # Bright Green (Light Green)
+    93: "rgb(255, 255, 102)",  # Bright Yellow (Light Yellow)
+    94: "rgb(173, 216, 230)",  # Bright Blue (Light Blue)
+    95: "rgb(219, 112, 147)",  # Bright Magenta (Pale Violet Red)
+    96: "rgb(72, 209, 204)",  # Bright Cyan (Medium Turquoise)
+    97: "rgb(255, 255, 255)"  # Bright White (White)
+}
