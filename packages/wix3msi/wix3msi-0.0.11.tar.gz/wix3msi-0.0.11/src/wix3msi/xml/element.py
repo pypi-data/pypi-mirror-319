482ec1759f865202778c2d351e3fbdf54dd3d634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:04:44 +0000

    Add package wix3msi/0.0.11/wix3msi-0.0.11.tar.gz

---
+++ packages/wix3msi/wix3msi-0.0.11.tar.gz/wix3msi-0.0.11/src/wix3msi/xml/element.py
@@ -0,0 +1,35 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Awaitable, Callable, Final, Generic, Iterable, Iterator, Optional, Sequence, Type, TypeVar, Tuple, Union
+from typing import ItemsView, KeysView, ValuesView
+from typing import IO, TextIO, BinaryIO
+from typing import Any, List, Dict, Set
+from typing import cast, overload
+import builtins
+from xml.etree.ElementTree import Element as InternalElement
+
+
+#--------------------------------------------------------------------------------
+# 요소.
+#--------------------------------------------------------------------------------
+class Element:
+	#--------------------------------------------------------------------------------
+	# 멤버 변수 목록.
+	#--------------------------------------------------------------------------------
+	__element: InternalElement
+
+
+	#--------------------------------------------------------------------------------
+	# 생성됨.
+	#--------------------------------------------------------------------------------
+	def __init__(self) -> None:
+		self.__element = None
+
+
+	#--------------------------------------------------------------------------------
+	# 실행.
+	#--------------------------------------------------------------------------------
+	def Run() -> None:
+		builtins.print(f"CommandManager.Run()")
