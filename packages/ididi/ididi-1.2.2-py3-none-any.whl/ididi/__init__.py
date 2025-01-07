65cfae472f248b053523fa6dfc3f7198de5a1c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:20 +0000

    Add package ididi/1.2.2/ididi-1.2.2-py3-none-any.whl

---
+++ packages/ididi/ididi-1.2.2-py3-none-any.whl/ididi/__init__.py
@@ -0,0 +1,40 @@
+"""
+IDIDI
+~~~~~~~~~~~~~~~~~~~~~
+
+Ididi is a pythonic dependency injection lib, with ergonomic apis, without boilplate code, works out of the box.
+
+>>> import ididi
+>>> service = ididi.resolve(MyService)
+>>> assert isinstance(service, MyService)
+
+source code is at <https://github.com/raceychan/ididi>.
+
+copyright: (c) 2024 by race chan.
+license: MIT, see LICENSE for more details.
+"""
+
+VERSION = "1.2.2"
+
+__version__ = VERSION
+
+from ._itypes import GraphConfig as GraphConfig
+from ._itypes import INode as INode
+from ._itypes import INodeConfig as INodeConfig
+from ._itypes import NodeConfig as NodeConfig
+from .api import entry as entry
+from .api import resolve as resolve
+from .graph import AsyncScope as AsyncScope
+from .graph import DependencyGraph as DependencyGraph
+from .graph import SyncScope as SyncScope
+from .node import DependentNode as DependentNode
+from .node import use as use
+from .utils.typing_utils import AsyncResource as AsyncResource
+from .utils.typing_utils import Resource as Resource
+
+try:
+    import graphviz
+except ImportError:
+    pass
+else:
+    from .visual import Visualizer as Visualizer
