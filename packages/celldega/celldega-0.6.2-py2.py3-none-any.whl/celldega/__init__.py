544570c27b90143d1f65a859cd0e37f215599e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 04:05:44 +0000

    Add package celldega/0.6.2/celldega-0.6.2-py2.py3-none-any.whl

---
+++ packages/celldega/celldega-0.6.2-py2.py3-none-any.whl/celldega/__init__.py
@@ -0,0 +1,16 @@
+import importlib.metadata
+
+from celldega.viz import Landscape, Matrix
+from celldega.pre import landscape
+from celldega.nbhd import alpha_shape
+
+# temporary fix for libpysal warning
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+try:
+    __version__ = importlib.metadata.version("celldega")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["Landscape", "landscape", "Matrix"]
