949a54ecfded385592a20b57875c1ebdfb272e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:10:05 +0000

    Add package amistock/0.3/amistock-0.3.tar.gz

---
+++ packages/amistock/amistock-0.3.tar.gz/amistock-0.3/amistock/api.py
@@ -0,0 +1,18 @@
+import requests
+
+API_BASE_URL = 'https://amistock.com/stockdata/'
+
+def get_stock_info(symbol, api_key):
+    headers = {'Authorization': api_key}
+    response = requests.get(f'{API_BASE_URL}get_stock_info/{symbol}/', headers=headers)
+    return response.json()
+
+def get_stock_data(symbol, from_date, to_date, api_key):
+    headers = {'Authorization': api_key}
+    response = requests.get(f'{API_BASE_URL}get_stock_data/{symbol}/{from_date}/{to_date}/', headers=headers)
+    return response.json()
+
+def get_stocks_by_market(market, api_key):
+    headers = {'Authorization': api_key}
+    response = requests.get(f'{API_BASE_URL}get_stocks_by_market/{market}/', headers=headers)
+    return response.json()
