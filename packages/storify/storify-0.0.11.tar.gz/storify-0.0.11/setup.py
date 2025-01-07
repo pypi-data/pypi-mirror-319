051d8b62a7245ce0d5632f204ded6cdd068de2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 14:56:16 +0000

    Add package storify/0.0.11/storify-0.0.11.tar.gz

---
+++ packages/storify/storify-0.0.11.tar.gz/storify-0.0.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='storify',
+    version='0.0.11',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='A lightweight database system for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ben Baptist',
+    author_email='sawham6@gmail.com',
+    url='https://github.com/benbaptist/storify',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        'msgpack',
+    ],
+)
