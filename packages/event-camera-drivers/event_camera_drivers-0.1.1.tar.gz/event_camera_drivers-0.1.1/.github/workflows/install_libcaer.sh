0bb9b018894500c8245315c0eca6dafc493e23a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 14:55:42 +0000

    Add package event-camera-drivers/0.1.1/event_camera_drivers-0.1.1.tar.gz

---
+++ packages/event-camera-drivers/event_camera_drivers-0.1.1.tar.gz/event_camera_drivers-0.1.1/.github/workflows/install_libcaer.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+set -e
+
+# Install dependencies based on OS
+if [ "$(uname)" == "Linux" ]; then
+    yum update -y
+    yum install -y cmake pkgconfig libusbx-devel libgusb-devel
+elif [ "$(uname)" == "Darwin" ]; then
+    brew install cmake pkg-config libusb
+elif [ "$(uname)" == "MINGW"* ] || [ "$(uname)" == "MSYS"* ]; then
+    vcpkg install libusb:x64-windows
+fi
+
+# Check if libcaer directory already exists
+if [ ! -d "libcaer" ]; then
+    # Clone and build libcaer
+    git clone https://gitlab.com/inivation/dv/libcaer.git
+    cd libcaer
+    if [ "$(uname)" == "Darwin" ]; then
+        cmake -S . -B build -DENABLE_OPENCV=0 -DENABLE_SERIALDEV=0 -DCMAKE_OSX_ARCHITECTURES=x86_64
+    else
+        cmake -S . -B build -DENABLE_OPENCV=0 -DENABLE_SERIALDEV=0
+    fi
+else
+    cd libcaer
+fi
+
+cmake --build build --config Release
+cmake --install build 
