5348fe7154ce7fa4ac80cbba04b1cbdffcb3c57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:09:11 +0000

    Add package atk-common/0.6.0/atk_common-0.6.0.tar.gz

---
+++ packages/atk-common/atk_common-0.6.0.tar.gz/atk_common-0.6.0/tests/test_log_utils.py
@@ -0,0 +1,10 @@
+import unittest
+import sys, os
+from atk_package.log_utils import add_log_item
+
+class TestLogUtils(unittest.TestCase):
+    def test_add_log_item(self):
+        add_log_item("Test message")
+
+if __name__ == "__main__":
+    unittest.main()
