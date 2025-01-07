ceaded598ee784154bf48fc34080d674b63c0f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:12 +0000

    Add package tap-heap/1.3.0/tap_heap-1.3.0.tar.gz

---
+++ packages/tap-heap/tap_heap-1.3.0.tar.gz/tap_heap-1.3.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-heap',
+      version='1.3.0',
+      description='Singer.io tap for extracting Heap data from Avro files in S3',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_heap'],
+      install_requires=[
+          'boto3==1.34.117',
+          'singer-encodings==0.1.3',
+          'singer-python==6.0.1',
+          'python-snappy==0.7.1',
+          'fastavro==1.9.4'
+      ],
+      extras_require={
+          'dev': [
+              'ipdb',
+              'pylint',
+              'pytest',
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-heap=tap_heap:main
+      ''',
+      packages=['tap_heap'])
