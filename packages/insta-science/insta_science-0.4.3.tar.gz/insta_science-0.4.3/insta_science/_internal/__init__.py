d9c6c8b620404051fc45bf9b3dee1f0c8ea197fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:04:54 +0000

    Add package insta-science/0.4.3/insta_science-0.4.3.tar.gz

---
+++ packages/insta-science/insta_science-0.4.3.tar.gz/insta_science-0.4.3/insta_science/_internal/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Science project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from .errors import InputError, ScienceNotFound
+from .hashing import Fingerprint
+from .model import Digest, Science, ScienceExe, Url
+from .platform import CURRENT_PLATFORM, Platform
+from .science import ensure_installed
+
+__all__ = (
+    "CURRENT_PLATFORM",
+    "Digest",
+    "Fingerprint",
+    "InputError",
+    "Platform",
+    "Science",
+    "ScienceExe",
+    "ScienceNotFound",
+    "Url",
+    "ensure_installed",
+)
