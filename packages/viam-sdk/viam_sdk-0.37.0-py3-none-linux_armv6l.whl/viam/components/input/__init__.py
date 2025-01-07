67c4f84009ec99732ffb22aec4806083a26db0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:05:00 +0000

    Add package viam-sdk/0.37.0/viam_sdk-0.37.0-py3-none-linux_armv6l.whl

---
+++ packages/viam-sdk/viam_sdk-0.37.0-py3-none-linux_armv6l.whl/viam/components/input/__init__.py
@@ -0,0 +1,17 @@
+from viam.resource.registry import Registry, ResourceRegistration
+
+from .client import ControllerClient
+from .input import Control, ControlFunction, Controller, Event, EventType
+from .service import InputControllerRPCService
+
+__all__ = [
+    "Controller",
+    "Control",
+    "ControlFunction",
+    "Event",
+    "EventType",
+]
+
+Registry.register_subtype(
+    ResourceRegistration(Controller, InputControllerRPCService, lambda name, channel: ControllerClient(name, channel))
+)
