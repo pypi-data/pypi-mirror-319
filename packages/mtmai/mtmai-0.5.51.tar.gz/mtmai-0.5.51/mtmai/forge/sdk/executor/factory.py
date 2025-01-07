7e483803d2979ea69926a3dd17999492e9bae993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:19 +0000

    Add package mtmai/0.5.51/mtmai-0.5.51.tar.gz

---
+++ packages/mtmai/mtmai-0.5.51.tar.gz/mtmai-0.5.51/mtmai/forge/sdk/executor/factory.py
@@ -0,0 +1,16 @@
+from mtmai.forge.sdk.executor.async_executor import (
+    AsyncExecutor,
+    BackgroundTaskExecutor,
+)
+
+
+class AsyncExecutorFactory:
+    __instance: AsyncExecutor = BackgroundTaskExecutor()
+
+    @staticmethod
+    def set_executor(executor: AsyncExecutor) -> None:
+        AsyncExecutorFactory.__instance = executor
+
+    @staticmethod
+    def get_executor() -> AsyncExecutor:
+        return AsyncExecutorFactory.__instance
