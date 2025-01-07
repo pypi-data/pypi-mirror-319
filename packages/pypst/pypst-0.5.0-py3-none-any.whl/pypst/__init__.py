c3aae51d79c9535d990de488cd939be9f029e792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:10:00 +0000

    Add package pypst/0.5.0/pypst-0.5.0-py3-none-any.whl

---
+++ packages/pypst/pypst-0.5.0-py3-none-any.whl/pypst/__init__.py
@@ -0,0 +1,22 @@
+from pypst.table import Table
+from pypst.cell import Cell
+from pypst.figure import Figure
+from pypst.document import Document
+from pypst.heading import Heading
+from pypst.itemize import Itemize, Enumerate
+from pypst.image import Image
+from pypst.renderable import Renderable, Plain
+
+__all__ = [
+    "Table",
+    "Cell",
+    "Figure",
+    "Document",
+    "Heading",
+    "Itemize",
+    "Enumerate",
+    "Image",
+    "Renderable",
+    "Plain",
+]
+__version__ = "0.5.0"
