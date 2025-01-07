905740d22b57a5ea5719bae8909e76e0c7713682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:03:50 +0000

    Add package panda-python-kit/0.0.23/panda_python_kit-0.0.23.tar.gz

---
+++ packages/panda-python-kit/panda_python_kit-0.0.23.tar.gz/panda_python_kit-0.0.23/panda_python_kit/comm/result_model.py
@@ -0,0 +1,17 @@
+import json
+
+
+class ResultModel:
+    def __init__(self,success=True,data=None,message=""):
+        self.success = success
+        self.data = data
+        self.message = message
+
+    def __str__(self):
+        return "success:%s,data:%s,message:%s" % (self.success,self.data,self.message)
+
+    def to_json(self):
+        return json.dumps(self.__dict__())
+
+    def __dict__(self):
+        return self.__dict__
