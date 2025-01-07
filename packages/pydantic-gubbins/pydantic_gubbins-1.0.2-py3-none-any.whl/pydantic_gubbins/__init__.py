b2f9767f588d9efcbd6da02046a9bcc6a91348cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:22 +0000

    Add package pydantic-gubbins/1.0.2/pydantic_gubbins-1.0.2-py3-none-any.whl

---
+++ packages/pydantic-gubbins/pydantic_gubbins-1.0.2-py3-none-any.whl/pydantic_gubbins/__init__.py
@@ -0,0 +1,9 @@
+from .main import BaseModel, ModelMetaclass
+
+# ToDo: add one of the things that handles this automagically
+__version__ = "1.0.2"
+
+__all__ = (
+    BaseModel,
+    ModelMetaclass
+)
