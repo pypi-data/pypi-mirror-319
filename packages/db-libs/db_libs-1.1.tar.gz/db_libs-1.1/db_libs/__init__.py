5ca31fefd2f932910d0bda6315bfb05dd04085c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:08 +0000

    Add package db-libs/1.1/db_libs-1.1.tar.gz

---
+++ packages/db-libs/db_libs-1.1.tar.gz/db_libs-1.1/db_libs/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf8
+"""
+@author:Administrator
+@file: __init__.py
+@time: 2020/06
+"""
+
+# from torndb_for_python3 import Connection as TorndbConnection
+# import records   # 这也是兼容多种sql数据库的。
