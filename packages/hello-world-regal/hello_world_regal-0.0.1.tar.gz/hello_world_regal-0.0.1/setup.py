4935736fd1bcdc800280af9d42eda967025c67fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:03:57 +0000

    Add package hello-world-regal/0.0.1/hello_world_regal-0.0.1.tar.gz

---
+++ packages/hello-world-regal/hello_world_regal-0.0.1.tar.gz/hello_world_regal-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup (
+    name= 'hello_world-regal',
+    version='0.0.1',
+    author='Adewale',
+    author_email='adewaleadeboye10@gmail.com',
+    url='https://www.linkedin.com/in/adewaleadeboye/',
+    description='A hello-world example package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+)
