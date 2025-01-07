b60c268546c8ba7550e25483d323ffa1ae5f17b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:04:51 +0000

    Add package cli-test-helpers/4.1.0/cli_test_helpers-4.1.0-py3-none-any.whl

---
+++ packages/cli-test-helpers/cli_test_helpers-4.1.0-py3-none-any.whl/cli_test_helpers/__init__.py
@@ -0,0 +1,12 @@
+"""
+Useful helpers for writing tests for your Python CLI program.
+"""
+
+__all__ = [
+    "ArgvContext",
+    "EnvironContext",
+    "shell",
+]
+
+from .commands import shell
+from .decorators import ArgvContext, EnvironContext
