df7cf31e2d16b8b052070bc20fbc2870e9d44ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:03:57 +0000

    Add package fudstop2/7.0.8/fudstop2-7.0.8-py3-none-any.whl

---
+++ packages/fudstop2/fudstop2-7.0.8-py3-none-any.whl/fudstop/_markets/list_sets/webhook_dicts.py
@@ -0,0 +1,15 @@
+sector_webhooks = {
+    "consumer_defensive": "https://discord.com/api/webhooks/1309669451163238480/GivEBjuVwxXN6ZBhRe5VSU67mueMyWe7gFsOIn0rAPGnVHyef0ZQgGxWV6JU6aiwG_2a",
+    "industrials": "https://discord.com/api/webhooks/1309670686658269265/8d_9eF_Aej94iEnFZafleWwevuXGZb3tY9Q_0wPGcEjo-AQtOnxwps0ojiqs-3yNg1FT",
+    "technology": "https://discord.com/api/webhooks/1309670278036721685/DjwK137nFj_OaxIoHgLKfBVFUYdpLCQOPRka1xUZaQbmT2bm-n4ps4aBzwJsl5arrugv",
+    "consumer_cyclical": "https://discord.com/api/webhooks/1309670156649238579/bry8TUxTS6lbVcTY0kvXPhUhUWV-mPFhXDpa0JEnkBvk889dEsUzEBMDOLUfvlhuwtwv",
+    "communication": "https://discord.com/api/webhooks/1309669712229306458/MdmPJue7UINC6PqAOp7bQClMEvm5lpm7F97aOCfzkCRbXwuytKQS6RbceKF4ASE-Cobn",
+    "financials": "https://discord.com/api/webhooks/1309669962876715089/im8fEG8mWsaPpVI8yKjns3e8z0Hfh-juEHJGrJayhp-2RilNkdvHu8mUa8uUbjwCnGm0",
+    "energy": "https://discord.com/api/webhooks/1309670452096012318/lWUKD7pZhZP6FIrNq691obHmzPlf7fN-6eJXjhCMUp7XX3oi-pLlmX4StdNKvgLB5rA9",
+    "utilities": "https://discord.com/api/webhooks/1309670503992135691/V-QyI7zpz0oiqa5wu4L69EAVd6x79ZaM7_C84K_yS2vllRFDgc8vcFJ9el6C23tKxzhQ",
+    "healthcare": "https://discord.com/api/webhooks/1309670051594633236/Dhwxhtm0UOxETsI7rdCvuk0dYiENi1o04frI7esi4-zvobuANz2MK7n78PcVsGSuuQr1",
+    "materials": "https://discord.com/api/webhooks/1309670402284326912/G9Uym043ls_yX3HvDZvtrvz_COmHHFb9JibqieMeJM_3WCuRyZ1Mlnd8CBSZI5GQ0SaJ",
+    "etfs": "https://discord.com/api/webhooks/1309670861175001108/Wl9Fab92-Ps86xBB_vk-3P-FVth2oZkiTlObE_zTnyr-DVZGePBP33dvkbrw4u29CRyp",
+    "real_estate": "https://discord.com/api/webhooks/1309670592710053919/4rNjrnDYxk4ravou6WTdGKw2IYtNmiDNr-vifobkrbg7Wu3XNtqfYhozSbzTvERRnDiU"
+}
+
