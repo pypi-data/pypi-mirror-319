3d9ed3f54007f3ce63719fd43d5f1782c0a441b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 04:04:14 +0000

    Add package dclab/0.62.9/dclab-0.62.9-cp38-cp38-win_amd64.whl

---
+++ packages/dclab/dclab-0.62.9-cp38-cp38-win_amd64.whl/dclab/rtdc_dataset/fmt_dcor/tables.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class DCORTables:
+    def __init__(self, api):
+        self.api = api
+        self._tables_cache = None
+
+    def __contains__(self, key):
+        return key in self.keys()
+
+    def __getitem__(self, key):
+        return self._tables[key]
+
+    def __iter__(self):
+        for key in self.keys():
+            yield key
+
+    def __len__(self):
+        return len(self._tables)
+
+    def keys(self):
+        return self._tables.keys()
+
+    @property
+    def _tables(self):
+        if self._tables_cache is None:
+            table_data = self.api.get(query="tables")
+            # assemble the tables
+            tables = {}
+            for key in table_data:
+                columns, data = table_data[key]
+                ds_dt = np.dtype({'names': columns,
+                                  'formats': [np.float64] * len(columns)})
+                tab_data = np.asarray(data)
+                rec_arr = np.rec.array(tab_data, dtype=ds_dt)
+                tables[key] = rec_arr
+
+            self._tables_cache = tables
+        return self._tables_cache
