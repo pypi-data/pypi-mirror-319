119e492bc5fc225ed070146cc383219c5fa91d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:04:04 +0000

    Add package py-social-login/0.0.3/py_social_login-0.0.3.tar.gz

---
+++ packages/py-social-login/py_social_login-0.0.3.tar.gz/py_social_login-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import os
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version="0.0.3"
+
+library_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_file_path = library_folder + '/requirements.txt'
+
+install_requires = ['PyJWT', 'cryptography', 'pydantic', 'pytz', 'requests']
+extras_require = {
+    'dev': ['flask']
+}
+
+setuptools.setup(
+    name="py_social_login", 
+    version=version,
+    author="Uday Krishna",
+    author_email="uday@udaykrishna.com",
+    description="Py Social Login",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/udaykrishna/py-social-login/",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.9',
+)
