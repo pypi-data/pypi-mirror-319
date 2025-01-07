530ab635103be407881f8f214c158d8b8d2b5940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 18:04:38 +0000

    Add package chain_harvester/1.2.2/chain_harvester-1.2.2.tar.gz

---
+++ packages/chain_harvester/chain_harvester-1.2.2.tar.gz/chain_harvester-1.2.2/chain_harvester/networks/base/mainnet.py
@@ -0,0 +1,28 @@
+import logging
+
+from chain_harvester.chain import Chain
+from chain_harvester.constants import CHAINS
+
+log = logging.getLogger(__name__)
+
+
+class BaseMainnetChain(Chain):
+    def __init__(
+        self, rpc=None, rpc_nodes=None, api_key=None, api_keys=None, abis_path=None, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.chain = "base"
+        self.network = "mainnet"
+        self.rpc = rpc or rpc_nodes[self.chain][self.network]
+        self.chain_id = CHAINS[self.chain][self.network]
+        self.abis_path = abis_path or "abis/base/"
+        self.api_key = api_key or api_keys[self.chain][self.network]
+
+    def get_abi_source_url(self, contract_address):
+        url = (
+            "https://api.basescan.org/api?module=contract&action=getabi&address="
+            + contract_address
+            + "&apikey="
+            + self.api_key
+        )
+        return url
