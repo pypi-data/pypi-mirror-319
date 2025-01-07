2d4287fcfa825a4bef327fccc020a79d1d44e6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 15:30:44 +0000

    Add package dem-stitcher/2.5.11/dem_stitcher-2.5.11-py3-none-any.whl

---
+++ packages/dem-stitcher/dem_stitcher-2.5.11-py3-none-any.whl/dem_stitcher/__init__.py
@@ -0,0 +1,28 @@
+import warnings
+
+# FIXME: Python 3.8+ this should be `from importlib.metadata...`
+from importlib_metadata import PackageNotFoundError, version
+
+from .datasets import get_global_dem_tile_extents, get_overlapping_dem_tiles
+from .stitcher import get_dem_tile_paths, stitch_dem
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = None
+    warnings.warn(
+        'package is not installed!\n'
+        'Install in editable/develop mode via (from the top of this repo):\n'
+        '   python -m pip install -e .\n',
+        RuntimeWarning,
+    )
+
+
+__all__ = [
+    'get_dem_tile_paths',
+    'get_global_dem_tile_extents',
+    'get_overlapping_dem_tiles',
+    'stitch_dem',
+    '__version__',
+]
