159b2bc610e3a6766ce571e2ecba38a2c4f8526f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 04:04:25 +0000

    Add package TexTOM/0.1.9/textom-0.1.9.tar.gz

---
+++ packages/TexTOM/textom-0.1.9.tar.gz/textom-0.1.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TexTOM",          
+    version="0.1.9",       
+    author="Moritz Frewein, Marc Allain, Tilman Gruenewald",
+    author_email="textom@fresnel.fr",
+    description="A program for texture simulations.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.fresnel.fr/textom/textom.git", 
+    packages=find_packages(), 
+    include_package_data=True,
+    package_data={
+        "textom": [
+        "ressources/symmetrizedHSH/output/*",
+        "ressources/*.txt",
+        "input/*.py",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9, <=3.12',
+    install_requires=["mumott", "pyfai", "orix", "ipython"],
+    entry_points={
+        "console_scripts": [
+            "textom=textom.entries:main",
+            "textom_config=textom.entries:config",
+        ]
+    },
+)
