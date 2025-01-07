d968ad67d3ca99147df6bb42a87c525c1db5aad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:02 +0000

    Add package pipcherry/0.1.1/pipcherry-0.1.1-py3-none-any.whl

---
+++ packages/pipcherry/pipcherry-0.1.1-py3-none-any.whl/pipcherry/__init__.py
@@ -0,0 +1,1 @@
+from .main import greet, add_numbers
---
+++ packages/pipcherry/pipcherry-0.1.1-py3-none-any.whl/pipcherry/main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    return a + b
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
