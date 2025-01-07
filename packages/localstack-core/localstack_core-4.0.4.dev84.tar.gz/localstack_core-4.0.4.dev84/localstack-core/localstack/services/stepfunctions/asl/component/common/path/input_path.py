a8829e0521b23cabfa2b4062822292681599011a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 18:04:59 +0000

    Add package localstack-core/4.0.4.dev84/localstack_core-4.0.4.dev84.tar.gz

---
+++ packages/localstack-core/localstack_core-4.0.4.dev84.tar.gz/localstack_core-4.0.4.dev84/localstack-core/localstack/services/stepfunctions/asl/component/common/path/input_path.py
@@ -0,0 +1,24 @@
+from typing import Final, Optional
+
+from localstack.services.stepfunctions.asl.component.common.string.string_expression import (
+    StringJsonPath,
+    StringSampler,
+)
+from localstack.services.stepfunctions.asl.component.eval_component import EvalComponent
+from localstack.services.stepfunctions.asl.eval.environment import Environment
+
+
+class InputPath(EvalComponent):
+    string_sampler: Final[Optional[StringSampler]]
+
+    def __init__(self, string_sampler: Optional[StringSampler]):
+        self.string_sampler = string_sampler
+
+    def _eval_body(self, env: Environment) -> None:
+        if self.string_sampler is None:
+            env.stack.append(dict())
+            return
+        if isinstance(self.string_sampler, StringJsonPath):
+            # JsonPaths are sampled from a given state, hence pass the state's input.
+            env.stack.append(env.states.get_input())
+        self.string_sampler.eval(env=env)
