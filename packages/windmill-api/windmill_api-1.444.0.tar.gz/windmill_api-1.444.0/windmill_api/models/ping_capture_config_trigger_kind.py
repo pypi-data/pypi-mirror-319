9367c5dc4f25f129d67df5cf17e30b893ce70bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:09:47 +0000

    Add package windmill-api/1.444.0/windmill_api-1.444.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.444.0.tar.gz/windmill_api-1.444.0/windmill_api/models/ping_capture_config_trigger_kind.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class PingCaptureConfigTriggerKind(str, Enum):
+    EMAIL = "email"
+    HTTP = "http"
+    KAFKA = "kafka"
+    WEBHOOK = "webhook"
+    WEBSOCKET = "websocket"
+
+    def __str__(self) -> str:
+        return str(self.value)
