3452f260fd1e46e48e0ba88c8665e94da1a41999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:04:42 +0000

    Add package flask-marshmallow/1.3.0/flask_marshmallow-1.3.0.tar.gz

---
+++ packages/flask-marshmallow/flask_marshmallow-1.3.0.tar.gz/flask_marshmallow-1.3.0/tests/test_sqla.py
@@ -0,0 +1,291 @@
+import pytest
+from flask import Flask, url_for
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import ValidationError
+from werkzeug.wrappers import Response
+
+from flask_marshmallow import Marshmallow
+from flask_marshmallow.sqla import HyperlinkRelated
+from tests.conftest import Bunch
+
+try:
+    from marshmallow_sqlalchemy import SQLAlchemySchema  # noqa: F401
+except ImportError:
+    has_sqlalchemyschema = False
+else:
+    has_sqlalchemyschema = True
+
+
+requires_sqlalchemyschema = pytest.mark.skipif(
+    not has_sqlalchemyschema, reason="SQLAlchemySchema not available"
+)
+
+
+class TestSQLAlchemy:
+    @pytest.fixture
+    def extapp(self):
+        app_ = Flask("extapp")
+        app_.testing = True
+        app_.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        app_.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        SQLAlchemy(app_)
+        Marshmallow(app_)
+
+        @app_.route("/author/<int:id>")
+        def author(id):
+            return f"...view for author {id}..."
+
+        @app_.route("/book/<int:id>")
+        def book(id):
+            return f"...view for book {id}..."
+
+        ctx = app_.test_request_context()
+        ctx.push()
+
+        yield app_
+
+        ctx.pop()
+
+    @pytest.fixture
+    def db(self, extapp):
+        db = extapp.extensions["sqlalchemy"]
+        yield db
+        db.session.close()
+        db.engine.dispose()
+
+    @pytest.fixture
+    def extma(self, extapp):
+        return extapp.extensions["flask-marshmallow"]
+
+    @pytest.fixture
+    def models(self, db):
+        class AuthorModel(db.Model):
+            __tablename__ = "author"
+            id = db.Column(db.Integer, primary_key=True)
+            name = db.Column(db.String(255))
+
+            @property
+            def url(self):
+                return url_for("author", id=self.id)
+
+            @property
+            def absolute_url(self):
+                return url_for("author", id=self.id, _external=True)
+
+        class BookModel(db.Model):
+            __tablename__ = "book"
+            id = db.Column(db.Integer, primary_key=True)
+            title = db.Column(db.String(255))
+            author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
+            author = db.relationship("AuthorModel", backref="books")
+
+            @property
+            def url(self):
+                return url_for("book", id=self.id)
+
+            @property
+            def absolute_url(self):
+                return url_for("book", id=self.id, _external=True)
+
+        db.create_all()
+        yield Bunch(Author=AuthorModel, Book=BookModel)
+        db.drop_all()
+
+    def test_can_initialize_extensions(self, extapp):
+        assert "flask-marshmallow" in extapp.extensions
+        assert "sqlalchemy" in extapp.extensions
+
+    @requires_sqlalchemyschema
+    def test_can_declare_sqla_schemas(self, extma, models, db):
+        class AuthorSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Author
+
+            id = extma.auto_field()
+            name = extma.auto_field()
+
+        class BookSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Book
+
+            id = extma.auto_field()
+            title = extma.auto_field()
+            author_id = extma.auto_field()
+
+        author_schema = AuthorSchema()
+        book_schema = BookSchema()
+
+        author = models.Author(name="Chuck Paluhniuk")
+        book = models.Book(title="Fight Club", author=author)
+
+        author_result = author_schema.dump(author)
+
+        assert "id" in author_result
+        assert "name" in author_result
+        assert author_result["id"] == author.id
+        assert author_result["name"] == "Chuck Paluhniuk"
+        book_result = book_schema.dump(book)
+
+        assert "id" in book_result
+        assert "title" in book_result
+        assert book_result["id"] == book.id
+        assert book_result["title"] == book.title
+        assert book_result["author_id"] == book.author_id
+
+        resp = author_schema.jsonify(author)
+        assert isinstance(resp, Response)
+
+    @requires_sqlalchemyschema
+    def test_can_declare_sqla_auto_schemas(self, extma, models, db):
+        class AuthorSchema(extma.SQLAlchemyAutoSchema):
+            class Meta:
+                model = models.Author
+
+        class BookSchema(extma.SQLAlchemyAutoSchema):
+            class Meta:
+                model = models.Book
+                include_fk = True
+
+            id = extma.auto_field()
+            title = extma.auto_field()
+            author_id = extma.auto_field()
+
+        author_schema = AuthorSchema()
+        book_schema = BookSchema()
+
+        author = models.Author(name="Chuck Paluhniuk")
+        book = models.Book(title="Fight Club", author=author)
+
+        author_result = author_schema.dump(author)
+
+        assert "id" in author_result
+        assert "name" in author_result
+        assert author_result["id"] == author.id
+        assert author_result["name"] == "Chuck Paluhniuk"
+        book_result = book_schema.dump(book)
+
+        assert "id" in book_result
+        assert "title" in book_result
+        assert book_result["id"] == book.id
+        assert book_result["title"] == book.title
+        assert book_result["author_id"] == book.author_id
+
+        resp = author_schema.jsonify(author)
+        assert isinstance(resp, Response)
+
+    # FIXME: temporarily filter out this warning
+    # this is triggered by marshmallow-sqlalchemy on sqlalchemy v1.4.x
+    # on the current version it should be fixed
+    # in an upcoming marshmallow-sqlalchemy release
+    @requires_sqlalchemyschema
+    def test_hyperlink_related_field(self, extma, models, db, extapp):
+        class BookSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Book
+
+            author = extma.HyperlinkRelated("author")
+
+        book_schema = BookSchema()
+
+        author = models.Author(name="Chuck Paluhniuk")
+        book = models.Book(title="Fight Club", author=author)
+        db.session.add(author)
+        db.session.add(book)
+        db.session.flush()
+
+        book_result = book_schema.dump(book)
+
+        assert book_result["author"] == author.url
+
+        deserialized = book_schema.load(book_result)
+        assert deserialized["author"] == author
+
+    @requires_sqlalchemyschema
+    def test_hyperlink_related_field_serializes_none(self, extma, models):
+        class BookSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Book
+
+            author = extma.HyperlinkRelated("author")
+
+        book_schema = BookSchema()
+        book = models.Book(title="Fight Club", author=None)
+        book_result = book_schema.dump(book)
+        assert book_result["author"] is None
+
+    @requires_sqlalchemyschema
+    def test_hyperlink_related_field_errors(self, extma, models, db, extapp):
+        class BookSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Book
+
+            author = HyperlinkRelated("author")
+
+        book_schema = BookSchema()
+
+        author = models.Author(name="Chuck Paluhniuk")
+        book = models.Book(title="Fight Club", author=author)
+        db.session.add(author)
+        db.session.add(book)
+        db.session.flush()
+
+        # Deserialization fails on bad endpoint
+        book_result = book_schema.dump(book)
+        book_result["author"] = book.url
+        with pytest.raises(ValidationError) as excinfo:
+            book_schema.load(book_result)
+        errors = excinfo.value.messages
+        assert 'expected "author"' in errors["author"][0]
+
+        # Deserialization fails on bad URL key
+        book_result = book_schema.dump(book)
+        book_schema.fields["author"].url_key = "pk"
+        with pytest.raises(ValidationError) as excinfo:
+            book_schema.load(book_result)
+        errors = excinfo.value.messages
+        assert 'URL pattern "pk" not found' in errors["author"][0]
+
+    @requires_sqlalchemyschema
+    def test_hyperlink_related_field_external(self, extma, models, db, extapp):
+        class BookSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Book
+
+            author = HyperlinkRelated("author", external=True)
+
+        book_schema = BookSchema()
+
+        author = models.Author(name="Chuck Paluhniuk")
+        book = models.Book(title="Fight Club", author=author)
+        db.session.add(author)
+        db.session.add(book)
+        db.session.flush()
+
+        book_result = book_schema.dump(book)
+
+        assert book_result["author"] == author.absolute_url
+
+        deserialized = book_schema.load(book_result)
+        assert deserialized["author"] == author
+
+    @requires_sqlalchemyschema
+    def test_hyperlink_related_field_list(self, extma, models, db, extapp):
+        class AuthorSchema(extma.SQLAlchemySchema):
+            class Meta:
+                model = models.Author
+
+            books = extma.List(HyperlinkRelated("book"))
+
+        author_schema = AuthorSchema()
+
+        author = models.Author(name="Chuck Paluhniuk")
+        book = models.Book(title="Fight Club", author=author)
+        db.session.add(author)
+        db.session.add(book)
+        db.session.flush()
+
+        author_result = author_schema.dump(author)
+        assert author_result["books"][0] == book.url
+
+        deserialized = author_schema.load(author_result)
+        assert deserialized["books"][0] == book
---
+++ packages/flask-marshmallow/flask_marshmallow-1.3.0.tar.gz/flask_marshmallow-1.3.0/tests/test_validate.py
@@ -0,0 +1,168 @@
+import io
+from tempfile import SpooledTemporaryFile
+
+import pytest
+from marshmallow.exceptions import ValidationError
+from werkzeug.datastructures import FileStorage
+
+from flask_marshmallow import validate
+
+
+@pytest.mark.parametrize("size", ["1 KB", "1 KiB", "1 MB", "1 MiB", "1 GB", "1 GiB"])
+def test_parse_size(size):
+    rv = validate._parse_size(size)
+    if size == "1 KB":
+        assert rv == 1000
+    elif size == "1 KiB":
+        assert rv == 1024
+    elif size == "1 MB":
+        assert rv == 1000000
+    elif size == "1 MiB":
+        assert rv == 1048576
+    elif size == "1 GB":
+        assert rv == 1000000000
+    elif size == "1 GiB":
+        assert rv == 1073741824
+
+
+def test_get_filestorage_size():
+    rv = validate._get_filestorage_size(FileStorage(io.BytesIO(b"".ljust(0))))
+    assert rv == 0
+    rv = validate._get_filestorage_size(FileStorage(io.BytesIO(b"".ljust(123))))
+    assert rv == 123
+    rv = validate._get_filestorage_size(FileStorage(io.BytesIO(b"".ljust(1024))))
+    assert rv == 1024
+    rv = validate._get_filestorage_size(FileStorage(io.BytesIO(b"".ljust(1234))))
+    assert rv == 1234
+
+    with SpooledTemporaryFile() as temp:
+        temp.write(b"".ljust(0))
+        rv = validate._get_filestorage_size(FileStorage(temp))
+        assert rv == 0
+
+    with SpooledTemporaryFile() as temp:
+        temp.write(b"".ljust(123))
+        rv = validate._get_filestorage_size(FileStorage(temp))
+        assert rv == 123
+
+    with SpooledTemporaryFile() as temp:
+        temp.write(b"".ljust(1024))
+        rv = validate._get_filestorage_size(FileStorage(temp))
+        assert rv == 1024
+
+    with SpooledTemporaryFile() as temp:
+        temp.write(b"".ljust(1234))
+        rv = validate._get_filestorage_size(FileStorage(temp))
+        assert rv == 1234
+
+
+@pytest.mark.parametrize("size", ["wrong_format", "1.2.3 MiB"])
+def test_parse_size_wrong_value(size):
+    if size == "wrong_format":
+        with pytest.raises(ValueError, match="Invalid size value: "):
+            validate._parse_size(size)
+    elif size == "1.2.3 MiB":
+        with pytest.raises(
+            ValueError, match="Invalid float value while parsing size: "
+        ):
+            validate._parse_size(size)
+
+
+def test_filesize_min():
+    fs = FileStorage(io.BytesIO(b"".ljust(1024)))
+    assert validate.FileSize(min="1 KiB", max="2 KiB")(fs) is fs
+    assert validate.FileSize(min="0 KiB", max="1 KiB")(fs) is fs
+    assert validate.FileSize()(fs) is fs
+    assert validate.FileSize(min_inclusive=False, max_inclusive=False)(fs) is fs
+    assert validate.FileSize(min="1 KiB", max="1 KiB")(fs) is fs
+
+    with pytest.raises(ValidationError, match="Must be greater than or equal to 2 KiB"):
+        validate.FileSize(min="2 KiB", max="3 KiB")(fs)
+    with pytest.raises(ValidationError, match="Must be greater than or equal to 2 KiB"):
+        validate.FileSize(min="2 KiB")(fs)
+    with pytest.raises(ValidationError, match="Must be greater than 1 KiB"):
+        validate.FileSize(
+            min="1 KiB", max="2 KiB", min_inclusive=False, max_inclusive=True
+        )(fs)
+    with pytest.raises(ValidationError, match="less than 1 KiB"):
+        validate.FileSize(
+            min="1 KiB", max="1 KiB", min_inclusive=True, max_inclusive=False
+        )(fs)
+
+
+def test_filesize_max():
+    fs = FileStorage(io.BytesIO(b"".ljust(2048)))
+    assert validate.FileSize(min="1 KiB", max="2 KiB")(fs) is fs
+    assert validate.FileSize(max="2 KiB")(fs) is fs
+    assert validate.FileSize()(fs) is fs
+    assert validate.FileSize(min_inclusive=False, max_inclusive=False)(fs) is fs
+    assert validate.FileSize(min="2 KiB", max="2 KiB")(fs) is fs
+
+    with pytest.raises(ValidationError, match="less than or equal to 1 KiB"):
+        validate.FileSize(min="0 KiB", max="1 KiB")(fs)
+    with pytest.raises(ValidationError, match="less than or equal to 1 KiB"):
+        validate.FileSize(max="1 KiB")(fs)
+    with pytest.raises(ValidationError, match="less than 2 KiB"):
+        validate.FileSize(
+            min="1 KiB", max="2 KiB", min_inclusive=True, max_inclusive=False
+        )(fs)
+    with pytest.raises(ValidationError, match="greater than 2 KiB"):
+        validate.FileSize(
+            min="2 KiB", max="2 KiB", min_inclusive=False, max_inclusive=True
+        )(fs)
+
+
+def test_filesize_repr():
+    assert (
+        repr(
+            validate.FileSize(
+                min=None, max=None, error=None, min_inclusive=True, max_inclusive=True
+            )
+        )
+        == "<FileSize(min=None, max=None, min_inclusive=True, max_inclusive=True, error=None)>"  # noqa: E501
+    )
+
+    assert (
+        repr(
+            validate.FileSize(
+                min="1 KiB",
+                max="3 KiB",
+                error="foo",
+                min_inclusive=False,
+                max_inclusive=False,
+            )
+        )
+        == "<FileSize(min='1 KiB', max='3 KiB', min_inclusive=False, max_inclusive=False, error='foo')>"  # noqa: E501
+    )
+
+
+def test_filesize_wrongtype():
+    with pytest.raises(TypeError, match="A FileStorage object is required, not "):
+        validate.FileSize()(1)
+
+
+def test_filetype():
+    png_fs = FileStorage(io.BytesIO(b"".ljust(1024)), "test.png")
+    assert validate.FileType([".png"])(png_fs) is png_fs
+    assert validate.FileType([".PNG"])(png_fs) is png_fs
+
+    PNG_fs = FileStorage(io.BytesIO(b"".ljust(1024)), "test.PNG")
+    assert validate.FileType([".png"])(PNG_fs) is PNG_fs
+    assert validate.FileType([".PNG"])(PNG_fs) is PNG_fs
+
+    with pytest.raises(TypeError, match="A FileStorage object is required, not "):
+        validate.FileType([".png"])(1)
+
+    with pytest.raises(
+        ValidationError,
+        match=r"Not an allowed file type. Allowed file types: \[.*?\]",  # noqa: W605
+    ):
+        jpg_fs = FileStorage(io.BytesIO(b"".ljust(1024)), "test.jpg")
+        validate.FileType([".png"])(jpg_fs)
+
+    with pytest.raises(
+        ValidationError,
+        match=r"Not an allowed file type. Allowed file types: \[.*?\]",  # noqa: W605
+    ):
+        no_ext_fs = FileStorage(io.BytesIO(b"".ljust(1024)), "test")
+        validate.FileType([".png"])(no_ext_fs)
