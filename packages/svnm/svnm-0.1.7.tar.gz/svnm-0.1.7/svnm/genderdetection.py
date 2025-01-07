590f3c63a69e6848c2f52cf4026eb52661f26556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 18:04:51 +0000

    Add package svnm/0.1.7/svnm-0.1.7.tar.gz

---
+++ packages/svnm/svnm-0.1.7.tar.gz/svnm-0.1.7/svnm/genderdetection.py
@@ -0,0 +1,6 @@
+from svnm.basemodels import ImageClassificationbaseModel
+
+class GenderDetection(ImageClassificationbaseModel):
+    def __init__(self):
+        super().__init__("GenderDetection")
+    
