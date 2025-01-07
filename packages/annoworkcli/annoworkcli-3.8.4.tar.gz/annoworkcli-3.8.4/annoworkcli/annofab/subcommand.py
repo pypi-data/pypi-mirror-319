814f1fd330db7556e70abe175ba8cf31db6b89c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:37 +0000

    Add package annoworkcli/3.8.4/annoworkcli-3.8.4.tar.gz

---
+++ packages/annoworkcli/annoworkcli-3.8.4.tar.gz/annoworkcli-3.8.4/annoworkcli/annofab/subcommand.py
@@ -0,0 +1,32 @@
+import argparse
+from typing import Optional
+
+import annoworkcli
+import annoworkcli.annofab.list_job_with_annofab_project
+import annoworkcli.annofab.list_working_hours
+import annoworkcli.annofab.put_account_external_linkage_info
+import annoworkcli.annofab.put_job_from_annofab_project
+import annoworkcli.annofab.reshape_working_hours
+import annoworkcli.annofab.visualize_statistics
+
+
+def parse_args(parser: argparse.ArgumentParser):  # noqa: ANN201
+    subparsers = parser.add_subparsers(dest="subcommand_name")
+
+    # サブコマンドの定義
+    annoworkcli.annofab.list_job_with_annofab_project.add_parser(subparsers)
+    annoworkcli.annofab.list_working_hours.add_parser(subparsers)
+    annoworkcli.annofab.visualize_statistics.add_parser(subparsers)
+    annoworkcli.annofab.reshape_working_hours.add_parser(subparsers)
+    annoworkcli.annofab.put_account_external_linkage_info.add_parser(subparsers)
+    annoworkcli.annofab.put_job_from_annofab_project.add_parser(subparsers)
+
+
+def add_parser(subparsers: Optional[argparse._SubParsersAction] = None) -> argparse.ArgumentParser:
+    subcommand_name = "annofab"
+    subcommand_help = "Annofabにアクセスするサブコマンド"
+    description = "Annofabにアクセスするサブコマンド\n" "Annofabの認証情報を事前に設定しておく必要があります。"
+
+    parser = annoworkcli.common.cli.add_parser(subparsers, subcommand_name, subcommand_help, description=description, is_subcommand=False)
+    parse_args(parser)
+    return parser
