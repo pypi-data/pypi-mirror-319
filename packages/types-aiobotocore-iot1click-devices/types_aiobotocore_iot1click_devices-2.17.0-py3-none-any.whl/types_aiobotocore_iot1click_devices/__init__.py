dc0065df6602fbc27d466be70fc9759faa1ee280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:16:56 +0000

    Add package types-aiobotocore-iot1click-devices/2.17.0/types_aiobotocore_iot1click_devices-2.17.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-iot1click-devices/types_aiobotocore_iot1click_devices-2.17.0-py3-none-any.whl/types_aiobotocore_iot1click_devices/__init__.py
@@ -0,0 +1,39 @@
+"""
+Main interface for iot1click-devices service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iot1click_devices import (
+        Client,
+        IoT1ClickDevicesServiceClient,
+        ListDeviceEventsPaginator,
+        ListDevicesPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("iot1click-devices") as client:
+        client: IoT1ClickDevicesServiceClient
+        ...
+
+
+    list_device_events_paginator: ListDeviceEventsPaginator = client.get_paginator("list_device_events")
+    list_devices_paginator: ListDevicesPaginator = client.get_paginator("list_devices")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import IoT1ClickDevicesServiceClient
+from .paginator import ListDeviceEventsPaginator, ListDevicesPaginator
+
+Client = IoT1ClickDevicesServiceClient
+
+
+__all__ = (
+    "Client",
+    "IoT1ClickDevicesServiceClient",
+    "ListDeviceEventsPaginator",
+    "ListDevicesPaginator",
+)
---
+++ packages/types-aiobotocore-iot1click-devices/types_aiobotocore_iot1click_devices-2.17.0-py3-none-any.whl/types_aiobotocore_iot1click_devices/client.py
@@ -0,0 +1,293 @@
+"""
+Type annotations for iot1click-devices service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iot1click_devices.client import IoT1ClickDevicesServiceClient
+
+    session = get_session()
+    async with session.create_client("iot1click-devices") as client:
+        client: IoT1ClickDevicesServiceClient
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListDeviceEventsPaginator, ListDevicesPaginator
+from .type_defs import (
+    ClaimDevicesByClaimCodeRequestRequestTypeDef,
+    ClaimDevicesByClaimCodeResponseTypeDef,
+    DescribeDeviceRequestRequestTypeDef,
+    DescribeDeviceResponseTypeDef,
+    EmptyResponseMetadataTypeDef,
+    FinalizeDeviceClaimRequestRequestTypeDef,
+    FinalizeDeviceClaimResponseTypeDef,
+    GetDeviceMethodsRequestRequestTypeDef,
+    GetDeviceMethodsResponseTypeDef,
+    InitiateDeviceClaimRequestRequestTypeDef,
+    InitiateDeviceClaimResponseTypeDef,
+    InvokeDeviceMethodRequestRequestTypeDef,
+    InvokeDeviceMethodResponseTypeDef,
+    ListDeviceEventsRequestRequestTypeDef,
+    ListDeviceEventsResponseTypeDef,
+    ListDevicesRequestRequestTypeDef,
+    ListDevicesResponseTypeDef,
+    ListTagsForResourceRequestRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceRequestRequestTypeDef,
+    UnclaimDeviceRequestRequestTypeDef,
+    UnclaimDeviceResponseTypeDef,
+    UntagResourceRequestRequestTypeDef,
+    UpdateDeviceStateRequestRequestTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from builtins import dict as Dict
+    from builtins import type as Type
+    from collections.abc import Mapping
+else:
+    from typing import Dict, Mapping, Type
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+
+__all__ = ("IoT1ClickDevicesServiceClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    ClientError: Type[BotocoreClientError]
+    ForbiddenException: Type[BotocoreClientError]
+    InternalFailureException: Type[BotocoreClientError]
+    InvalidRequestException: Type[BotocoreClientError]
+    PreconditionFailedException: Type[BotocoreClientError]
+    RangeNotSatisfiableException: Type[BotocoreClientError]
+    ResourceConflictException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+
+
+class IoT1ClickDevicesServiceClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices.html#IoT1ClickDevicesService.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        IoT1ClickDevicesServiceClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices.html#IoT1ClickDevicesService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#generate_presigned_url)
+        """
+
+    async def claim_devices_by_claim_code(
+        self, **kwargs: Unpack[ClaimDevicesByClaimCodeRequestRequestTypeDef]
+    ) -> ClaimDevicesByClaimCodeResponseTypeDef:
+        """
+        Adds device(s) to your account (i.e., claim one or more devices) if and only if
+        you received a claim code with the device(s).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/claim_devices_by_claim_code.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#claim_devices_by_claim_code)
+        """
+
+    async def describe_device(
+        self, **kwargs: Unpack[DescribeDeviceRequestRequestTypeDef]
+    ) -> DescribeDeviceResponseTypeDef:
+        """
+        Given a device ID, returns a DescribeDeviceResponse object describing the
+        details of the device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/describe_device.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#describe_device)
+        """
+
+    async def finalize_device_claim(
+        self, **kwargs: Unpack[FinalizeDeviceClaimRequestRequestTypeDef]
+    ) -> FinalizeDeviceClaimResponseTypeDef:
+        """
+        Given a device ID, finalizes the claim request for the associated device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/finalize_device_claim.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#finalize_device_claim)
+        """
+
+    async def get_device_methods(
+        self, **kwargs: Unpack[GetDeviceMethodsRequestRequestTypeDef]
+    ) -> GetDeviceMethodsResponseTypeDef:
+        """
+        Given a device ID, returns the invokable methods associated with the device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/get_device_methods.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#get_device_methods)
+        """
+
+    async def initiate_device_claim(
+        self, **kwargs: Unpack[InitiateDeviceClaimRequestRequestTypeDef]
+    ) -> InitiateDeviceClaimResponseTypeDef:
+        """
+        Given a device ID, initiates a claim request for the associated device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/initiate_device_claim.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#initiate_device_claim)
+        """
+
+    async def invoke_device_method(
+        self, **kwargs: Unpack[InvokeDeviceMethodRequestRequestTypeDef]
+    ) -> InvokeDeviceMethodResponseTypeDef:
+        """
+        Given a device ID, issues a request to invoke a named device method (with
+        possible parameters).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/invoke_device_method.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#invoke_device_method)
+        """
+
+    async def list_device_events(
+        self, **kwargs: Unpack[ListDeviceEventsRequestRequestTypeDef]
+    ) -> ListDeviceEventsResponseTypeDef:
+        """
+        Using a device ID, returns a DeviceEventsResponse object containing an array of
+        events for the device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/list_device_events.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#list_device_events)
+        """
+
+    async def list_devices(
+        self, **kwargs: Unpack[ListDevicesRequestRequestTypeDef]
+    ) -> ListDevicesResponseTypeDef:
+        """
+        Lists the 1-Click compatible devices associated with your AWS account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/list_devices.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#list_devices)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Lists the tags associated with the specified resource ARN.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceRequestRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Adds or updates the tags associated with the resource ARN.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#tag_resource)
+        """
+
+    async def unclaim_device(
+        self, **kwargs: Unpack[UnclaimDeviceRequestRequestTypeDef]
+    ) -> UnclaimDeviceResponseTypeDef:
+        """
+        Disassociates a device from your AWS account using its device ID.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/unclaim_device.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#unclaim_device)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceRequestRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Using tag keys, deletes the tags (key/value pairs) associated with the
+        specified resource ARN.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#untag_resource)
+        """
+
+    async def update_device_state(
+        self, **kwargs: Unpack[UpdateDeviceStateRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Using a Boolean value (true or false), this operation enables or disables the
+        device given a device ID.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/update_device_state.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#update_device_state)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_device_events"]
+    ) -> ListDeviceEventsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_devices"]
+    ) -> ListDevicesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices.html#IoT1ClickDevicesService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices.html#IoT1ClickDevicesService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/client/)
+        """
