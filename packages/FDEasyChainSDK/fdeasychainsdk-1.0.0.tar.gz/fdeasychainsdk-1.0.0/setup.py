bbf41f3eadc7fcd7d791b9f637e785fef8e0febf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:04:26 +0000

    Add package FDEasyChainSDK/1.0.0/fdeasychainsdk-1.0.0.tar.gz

---
+++ packages/FDEasyChainSDK/fdeasychainsdk-1.0.0.tar.gz/fdeasychainsdk-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FDEasyChainSDK",
+    version="1.0.0",
+    author="Sadam·Sadik",
+    author_email="Haoke98@outlook.com",
+    description="五度易链SDK - 企业数据查询接口",
+    license="MIT",
+    packages=find_packages(),
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/FDEasyChainSDK",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.25.0",
+        "python-dotenv>=0.19.0",
+        "colorlog>=6.7.0"
+    ],
+) 
