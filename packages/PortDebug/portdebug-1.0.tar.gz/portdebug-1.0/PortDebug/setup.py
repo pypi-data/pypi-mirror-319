e170c7f5e6bd0a50c42bca261ecb74e0fc8cf0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:52 +0000

    Add package PortDebug/1.0/portdebug-1.0.tar.gz

---
+++ packages/PortDebug/portdebug-1.0.tar.gz/portdebug-1.0/PortDebug/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(name='PortDebug',
+      version='1.0',
+      description='for using serial port debug',
+      url='https://github.com/CraneSun/PortDebug',
+      author='Kun Sun',
+      author_email='sk602015817@hotmail.com',
+      license='MIT',
+      packages=['PortDebug'],
+      python_requires = '>=3.6'
+      zip_safe=False)
---
+++ packages/PortDebug/portdebug-1.0.tar.gz/portdebug-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='PortDebug',
+      version='1.0',
+      description='for using serial port debug',
+      url='https://github.com/CraneSun/PortDebug',
+      author='Kun Sun',
+      author_email='sk602015817@hotmail.com',
+      license='MIT',
+      packages=['PortDebug'],
+      zip_safe=False)
