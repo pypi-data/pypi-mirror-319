c82b52833713b3f598bc5a9d5e33f05bd3d37c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:04:43 +0000

    Add package asimtools/0.1.0/asimtools-0.1.0-py3-none-any.whl

---
+++ packages/asimtools/asimtools-0.1.0-py3-none-any.whl/asimtools/asimmodules/workflows/update_dependencies.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+'''
+Mostly internal tool used to chain together batch jobs that have nested
+dependencies
+
+Author: mkphuthi@github.com
+'''
+
+from typing import Dict, Optional
+from pathlib import Path
+import os
+import subprocess
+import logging
+import numpy as np
+from asimtools.utils import read_yaml
+
+def update_dependencies(
+    prev_step_dir: Optional[os.PathLike] = None,
+    next_step_dir: Optional[os.PathLike] = None,
+    skip_failed: bool = False,
+) -> Dict:
+    """Asimmodule for connecting jobs where an asimmodule launches slurm jobs
+    internally, examples are image_array, sim_array and calc_array
+
+    :param prev_step_dir: workdir of previous step, usually step-[N-1] where N
+        is current step, defaults to None
+    :type prev_step_dir: Optional[os.PathLike], optional
+    :param next_step_dir: workdir of next step, usually step-[N+1] where N
+        is current step, defaults to None
+    :type next_step_dir: Optional[os.PathLike], optional
+    :param skip_failed: whether to submit if any jobs failed or not,
+        defaults to False
+    :type skip_failed: bool, optional
+    :return: Nothing
+    :rtype: Dict
+    """
+
+    if os.environ.get('SLURM_JOB_ID', None) is None:
+        logging.warning('Not running on a SLURM job, skipping update_dependencies')
+        return {}
+    prev_step_dir = Path(prev_step_dir)
+    prev_output = read_yaml(prev_step_dir / 'output.yaml')
+    job_ids = prev_output.get('job_ids', None)
+    using_slurm = np.any([(job_id is not None) for job_id in job_ids])
+    if using_slurm:
+        next_step_dir = Path(next_step_dir)
+        next_output = read_yaml(next_step_dir / 'output.yaml')
+        next_job_ids = next_output.get('job_ids', [])
+        job_ids = [str(job_id) for job_id in job_ids]
+        start_cond = 'afterok'
+        if skip_failed:
+            start_cond = 'afterany'
+
+        for next_job_id in next_job_ids:
+            command = f'scontrol update JobId={next_job_id} '
+            command += f'dependency={start_cond}:{":".join(job_ids)}'
+            command = command.split()
+
+            completed_process = subprocess.run(
+                command, check=False, capture_output=True, text=True,
+            )
+
+            with open('sbatch_stdout.txt', 'w', encoding='utf-8') as f:
+                f.write(completed_process.stdout)
+
+            if completed_process.returncode != 0:
+                err_txt = f'Failed to run {" ".join(command)}\n'
+                err_txt += 'See sbatch.stderr.txt for details.'
+                logging.error(err_txt)
+                with open('sbatch_stderr.txt', 'w', encoding='utf-8') as f:
+                    f.write(completed_process.stderr)
+                completed_process.check_returncode()
+
+    return {}
