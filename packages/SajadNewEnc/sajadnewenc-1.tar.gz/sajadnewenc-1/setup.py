d0e7784d6beb467b5c9f6986c62d9df3a8219204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:10:17 +0000

    Add package SajadNewEnc/1/sajadnewenc-1.tar.gz

---
+++ packages/SajadNewEnc/sajadnewenc-1.tar.gz/sajadnewenc-1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='1',
+	name='SajadNewEnc',
+	author='SajadNewEnc',
+	description='an encryption library for sajad',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+	]
+
+)
