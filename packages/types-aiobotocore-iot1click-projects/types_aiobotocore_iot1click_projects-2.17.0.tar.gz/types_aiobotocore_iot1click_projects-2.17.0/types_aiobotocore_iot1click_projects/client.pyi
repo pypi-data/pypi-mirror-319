9077943e25ad2dadcdbee55c2d7e9217e9d164b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:16:56 +0000

    Add package types-aiobotocore-iot1click-projects/2.17.0/types_aiobotocore_iot1click_projects-2.17.0.tar.gz

---
+++ packages/types-aiobotocore-iot1click-projects/types_aiobotocore_iot1click_projects-2.17.0.tar.gz/types_aiobotocore_iot1click_projects-2.17.0/types_aiobotocore_iot1click_projects/client.pyi
@@ -0,0 +1,312 @@
+"""
+Type annotations for iot1click-projects service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iot1click_projects.client import IoT1ClickProjectsClient
+
+    session = get_session()
+    async with session.create_client("iot1click-projects") as client:
+        client: IoT1ClickProjectsClient
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListPlacementsPaginator, ListProjectsPaginator
+from .type_defs import (
+    AssociateDeviceWithPlacementRequestRequestTypeDef,
+    CreatePlacementRequestRequestTypeDef,
+    CreateProjectRequestRequestTypeDef,
+    DeletePlacementRequestRequestTypeDef,
+    DeleteProjectRequestRequestTypeDef,
+    DescribePlacementRequestRequestTypeDef,
+    DescribePlacementResponseTypeDef,
+    DescribeProjectRequestRequestTypeDef,
+    DescribeProjectResponseTypeDef,
+    DisassociateDeviceFromPlacementRequestRequestTypeDef,
+    GetDevicesInPlacementRequestRequestTypeDef,
+    GetDevicesInPlacementResponseTypeDef,
+    ListPlacementsRequestRequestTypeDef,
+    ListPlacementsResponseTypeDef,
+    ListProjectsRequestRequestTypeDef,
+    ListProjectsResponseTypeDef,
+    ListTagsForResourceRequestRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceRequestRequestTypeDef,
+    UntagResourceRequestRequestTypeDef,
+    UpdatePlacementRequestRequestTypeDef,
+    UpdateProjectRequestRequestTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from builtins import dict as Dict
+    from builtins import type as Type
+    from collections.abc import Mapping
+else:
+    from typing import Dict, Mapping, Type
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+__all__ = ("IoT1ClickProjectsClient",)
+
+class Exceptions(BaseClientExceptions):
+    ClientError: Type[BotocoreClientError]
+    InternalFailureException: Type[BotocoreClientError]
+    InvalidRequestException: Type[BotocoreClientError]
+    ResourceConflictException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    TooManyRequestsException: Type[BotocoreClientError]
+
+class IoT1ClickProjectsClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects.html#IoT1ClickProjects.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        IoT1ClickProjectsClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects.html#IoT1ClickProjects.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#generate_presigned_url)
+        """
+
+    async def associate_device_with_placement(
+        self, **kwargs: Unpack[AssociateDeviceWithPlacementRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Associates a physical device with a placement.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/associate_device_with_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#associate_device_with_placement)
+        """
+
+    async def create_placement(
+        self, **kwargs: Unpack[CreatePlacementRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Creates an empty placement.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/create_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#create_placement)
+        """
+
+    async def create_project(
+        self, **kwargs: Unpack[CreateProjectRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Creates an empty project with a placement template.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/create_project.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#create_project)
+        """
+
+    async def delete_placement(
+        self, **kwargs: Unpack[DeletePlacementRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Deletes a placement.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/delete_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#delete_placement)
+        """
+
+    async def delete_project(
+        self, **kwargs: Unpack[DeleteProjectRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Deletes a project.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/delete_project.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#delete_project)
+        """
+
+    async def describe_placement(
+        self, **kwargs: Unpack[DescribePlacementRequestRequestTypeDef]
+    ) -> DescribePlacementResponseTypeDef:
+        """
+        Describes a placement in a project.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/describe_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#describe_placement)
+        """
+
+    async def describe_project(
+        self, **kwargs: Unpack[DescribeProjectRequestRequestTypeDef]
+    ) -> DescribeProjectResponseTypeDef:
+        """
+        Returns an object describing a project.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/describe_project.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#describe_project)
+        """
+
+    async def disassociate_device_from_placement(
+        self, **kwargs: Unpack[DisassociateDeviceFromPlacementRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Removes a physical device from a placement.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/disassociate_device_from_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#disassociate_device_from_placement)
+        """
+
+    async def get_devices_in_placement(
+        self, **kwargs: Unpack[GetDevicesInPlacementRequestRequestTypeDef]
+    ) -> GetDevicesInPlacementResponseTypeDef:
+        """
+        Returns an object enumerating the devices in a placement.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/get_devices_in_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#get_devices_in_placement)
+        """
+
+    async def list_placements(
+        self, **kwargs: Unpack[ListPlacementsRequestRequestTypeDef]
+    ) -> ListPlacementsResponseTypeDef:
+        """
+        Lists the placement(s) of a project.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/list_placements.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#list_placements)
+        """
+
+    async def list_projects(
+        self, **kwargs: Unpack[ListProjectsRequestRequestTypeDef]
+    ) -> ListProjectsResponseTypeDef:
+        """
+        Lists the AWS IoT 1-Click project(s) associated with your AWS account and
+        region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/list_projects.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#list_projects)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Lists the tags (metadata key/value pairs) which you have assigned to the
+        resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Creates or modifies tags for a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#tag_resource)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Removes one or more tags (metadata key/value pairs) from a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#untag_resource)
+        """
+
+    async def update_placement(
+        self, **kwargs: Unpack[UpdatePlacementRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Updates a placement with the given attributes.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/update_placement.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#update_placement)
+        """
+
+    async def update_project(
+        self, **kwargs: Unpack[UpdateProjectRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Updates a project associated with your AWS account and region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/update_project.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#update_project)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_placements"]
+    ) -> ListPlacementsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_projects"]
+    ) -> ListProjectsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects.html#IoT1ClickProjects.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects.html#IoT1ClickProjects.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/client/)
+        """
---
+++ packages/types-aiobotocore-iot1click-projects/types_aiobotocore_iot1click_projects-2.17.0.tar.gz/types_aiobotocore_iot1click_projects-2.17.0/types_aiobotocore_iot1click_projects/paginator.py
@@ -0,0 +1,90 @@
+"""
+Type annotations for iot1click-projects service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_iot1click_projects.client import IoT1ClickProjectsClient
+    from types_aiobotocore_iot1click_projects.paginator import (
+        ListPlacementsPaginator,
+        ListProjectsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("iot1click-projects") as client:
+        client: IoT1ClickProjectsClient
+
+        list_placements_paginator: ListPlacementsPaginator = client.get_paginator("list_placements")
+        list_projects_paginator: ListProjectsPaginator = client.get_paginator("list_projects")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListPlacementsRequestPaginateTypeDef,
+    ListPlacementsResponseTypeDef,
+    ListProjectsRequestPaginateTypeDef,
+    ListProjectsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListPlacementsPaginator", "ListProjectsPaginator")
+
+
+if TYPE_CHECKING:
+    _ListPlacementsPaginatorBase = AioPaginator[ListPlacementsResponseTypeDef]
+else:
+    _ListPlacementsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListPlacementsPaginator(_ListPlacementsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/paginator/ListPlacements.html#IoT1ClickProjects.Paginator.ListPlacements)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/paginators/#listplacementspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListPlacementsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListPlacementsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/paginator/ListPlacements.html#IoT1ClickProjects.Paginator.ListPlacements.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/paginators/#listplacementspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListProjectsPaginatorBase = AioPaginator[ListProjectsResponseTypeDef]
+else:
+    _ListProjectsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListProjectsPaginator(_ListProjectsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/paginator/ListProjects.html#IoT1ClickProjects.Paginator.ListProjects)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/paginators/#listprojectspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListProjectsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListProjectsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-projects/paginator/ListProjects.html#IoT1ClickProjects.Paginator.ListProjects.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_projects/paginators/#listprojectspaginator)
+        """
