fc9b24f669b10511fec51ea0d0c3999e72723c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:03:59 +0000

    Add package temirlan-rules/1.0.0/temirlan_rules-1.0.0.tar.gz

---
+++ packages/temirlan-rules/temirlan_rules-1.0.0.tar.gz/temirlan_rules-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="temirlan_rules",
+    version="1.0.0",
+    author="Temirlan",
+    description="The wisdom of Temirlan in one package",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
+
---
+++ packages/temirlan-rules/temirlan_rules-1.0.0.tar.gz/temirlan_rules-1.0.0/temirlan_rules/__init__.py
@@ -0,0 +1,9 @@
+print("""
+The Rules of Temirlan's Code:
+1. Simplicity is key.
+2. Write code that is easy to read.
+3. Avoid unnecessary complexity.
+4. Debugging is as important as coding.
+5. Comments are your future self's best friend.
+""")
+
