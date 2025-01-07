a7cad1379023570ecfc1528b34b0abf80d733329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:09:25 +0000

    Add package simple-pytorch-wrapper/0.0.5/simple_pytorch_wrapper-0.0.5-py3-none-any.whl

---
+++ packages/simple-pytorch-wrapper/simple_pytorch_wrapper-0.0.5-py3-none-any.whl/simple_pytorch_wrapper/utils/warnings.py
@@ -0,0 +1,17 @@
+WARNING_ON = True
+
+def suppress_warnings():
+    """
+    Suppress warning messages globally.
+    """
+    global WARNING_ON  # Declare WARNING_ON as a global variable
+    WARNING_ON = False  # Set the flag to False to disable warnings
+    return
+
+def display_warning(keyword, message):
+    """
+    Display a warning message to the user.
+    """
+    if WARNING_ON:
+        print(f"\033[91mwarning\033[0m @{keyword}: \033[93m{message}\033[0m \n")
+    return
