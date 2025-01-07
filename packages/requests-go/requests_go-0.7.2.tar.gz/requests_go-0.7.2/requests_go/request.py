0a9a66116387eaefbc37dde254a9789b46aeec6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:47 +0000

    Add package requests-go/0.7.2/requests_go-0.7.2.tar.gz

---
+++ packages/requests-go/requests_go-0.7.2.tar.gz/requests_go-0.7.2/requests_go/request.py
@@ -0,0 +1,26 @@
+from .tls_client import Session
+
+
+class TLSRequest:
+	def __init__(self, tls_config=None):
+		if tls_config:
+			self._tls_config = tls_config
+		# else:
+		# 	self._tls_config = random_tls_config()
+		if type(self._tls_config) != dict:
+			self._tls_config = self._tls_config.toJSON()
+		self._session = self._new_tls()
+
+	def _new_tls(self) -> Session:
+		session = Session(self._tls_config)
+		return session
+
+	def close(self) -> None:
+		self._session = self._new_tls()
+
+	def reset(self) -> None:
+		self._session = self._new_tls()
+
+	def send(self, options: dict = {}):
+		response = self._session.request(**options)
+		return response
