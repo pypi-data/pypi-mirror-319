244bf8e2e7aef3c2083e8ee05e22352366f25f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:10:10 +0000

    Add package glidergun/0.8.37/glidergun-0.8.37.tar.gz

---
+++ packages/glidergun/glidergun-0.8.37.tar.gz/glidergun-0.8.37/glidergun/_types.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from typing import Callable, NamedTuple, Protocol
+
+from numpy import ndarray
+from rasterio.crs import CRS
+from rasterio.transform import Affine
+
+
+@dataclass(frozen=True)
+class GridCore:
+    data: ndarray
+    transform: Affine
+    crs: CRS
+
+
+class Extent(NamedTuple):
+    xmin: float
+    ymin: float
+    xmax: float
+    ymax: float
+
+    def intersects(self, xmin: float, ymin: float, xmax: float, ymax: float):
+        return (
+            self.xmin < xmax
+            and self.xmax > xmin
+            and self.ymin < ymax
+            and self.ymax > ymin
+        )
+
+    def intersect(self, extent: "Extent"):
+        return Extent(*[f(x) for f, x in zip((max, max, min, min), zip(self, extent))])
+
+    def union(self, extent: "Extent"):
+        return Extent(*[f(x) for f, x in zip((min, min, max, max), zip(self, extent))])
+
+    __and__ = intersect
+    __rand__ = __and__
+    __or__ = union
+    __ror__ = __or__
+
+
+class CellSize(NamedTuple):
+    x: float
+    y: float
+
+    def __mul__(self, n: object):
+        if not isinstance(n, (float, int)):
+            return NotImplemented
+        return CellSize(self.x * n, self.y * n)
+
+    def __rmul__(self, n: object):
+        if not isinstance(n, (float, int)):
+            return NotImplemented
+        return CellSize(self.x * n, self.y * n)
+
+    def __truediv__(self, n: float):
+        return CellSize(self.x / n, self.y / n)
+
+
+class PointValue(NamedTuple):
+    x: float
+    y: float
+    value: float
+
+
+class Scaler(Protocol):
+    fit: Callable
+    transform: Callable
+    fit_transform: Callable
