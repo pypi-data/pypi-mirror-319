a77a4d3e2908afc9a79968f4ea82e0f4343bf3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 14:55:40 +0000

    Add package types-boto3-iot1click-devices/1.35.93/types_boto3_iot1click_devices-1.35.93-py3-none-any.whl

---
+++ packages/types-boto3-iot1click-devices/types_boto3_iot1click_devices-1.35.93-py3-none-any.whl/types_boto3_iot1click_devices/__init__.py
@@ -0,0 +1,36 @@
+"""
+Main interface for iot1click-devices service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_iot1click_devices import (
+        Client,
+        IoT1ClickDevicesServiceClient,
+        ListDeviceEventsPaginator,
+        ListDevicesPaginator,
+    )
+
+    session = Session()
+    client: IoT1ClickDevicesServiceClient = session.client("iot1click-devices")
+
+    list_device_events_paginator: ListDeviceEventsPaginator = client.get_paginator("list_device_events")
+    list_devices_paginator: ListDevicesPaginator = client.get_paginator("list_devices")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import IoT1ClickDevicesServiceClient
+from .paginator import ListDeviceEventsPaginator, ListDevicesPaginator
+
+Client = IoT1ClickDevicesServiceClient
+
+
+__all__ = (
+    "Client",
+    "IoT1ClickDevicesServiceClient",
+    "ListDeviceEventsPaginator",
+    "ListDevicesPaginator",
+)
