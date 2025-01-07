d1024749a4cfd2ef30e9db4f0f030b12fd0c7c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 18:04:40 +0000

    Add package ascript-tip/0.0.3.61/ascript-tip-0.0.3.61.tar.gz

---
+++ packages/ascript-tip/ascript-tip-0.0.3.61.tar.gz/ascript-tip-0.0.3.61/ascript/android/data.py
@@ -0,0 +1,11 @@
+from airscript.data import Kv
+
+
+class KeyValue:
+    @staticmethod
+    def save(key: str, value):
+        Kv.save(key, value)
+
+    @staticmethod
+    def get(key: str, default_value):
+        return Kv.get(key, default_value)
