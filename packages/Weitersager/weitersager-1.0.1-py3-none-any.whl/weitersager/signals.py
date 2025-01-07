4c70c2232e89df2b4557c73c164b879a8f2874e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:11 +0000

    Add package Weitersager/1.0.1/weitersager-1.0.1-py3-none-any.whl

---
+++ packages/Weitersager/weitersager-1.0.1-py3-none-any.whl/weitersager/signals.py
@@ -0,0 +1,15 @@
+"""
+weitersager.signals
+~~~~~~~~~~~~~~~~~~~
+
+Signals
+
+:Copyright: 2007-2025 Jochen Kupperschmidt
+:License: MIT, see LICENSE for details.
+"""
+
+from blinker import Signal
+
+
+irc_channel_joined = Signal()
+message_received = Signal()
