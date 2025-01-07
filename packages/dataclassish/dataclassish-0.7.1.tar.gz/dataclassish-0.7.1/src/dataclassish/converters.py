f191333a19816275c70673804bf2cd85556b4881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 05:04:32 +0000

    Add package dataclassish/0.7.1/dataclassish-0.7.1.tar.gz

---
+++ packages/dataclassish/dataclassish-0.7.1.tar.gz/dataclassish-0.7.1/src/dataclassish/converters.py
@@ -0,0 +1,13 @@
+"""Converters for dataclass fields.
+
+While `dataclasses.field` itself does not allow for converters (See PEP 712)
+many dataclasses-like libraries do. A very short, very non-exhaustive list
+includes: ``attrs`` and ``equinox``. This module provides a few useful converter
+functions. If you need more, check out ``attrs``!
+
+"""
+
+__all__ = ["AbstractConverter", "Optional", "Unless"]
+
+from ._src.converters import AbstractConverter, Optional, Unless
+# TODO: make dataclass & field public
