4555e38af16e123cc24c012c68fb91d2755bed63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 03:09:07 +0000

    Add package nonebot_plugin_session_orm/0.2.2/nonebot_plugin_session_orm-0.2.2-py3-none-any.whl

---
+++ packages/nonebot_plugin_session_orm/nonebot_plugin_session_orm-0.2.2-py3-none-any.whl/nonebot_plugin_session_orm/__init__.py
@@ -0,0 +1,20 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+require("nonebot_plugin_orm")
+require("nonebot_plugin_session")
+
+from . import migrations
+from .model import SessionModel as SessionModel
+from .model import get_session_by_persist_id as get_session_by_persist_id
+from .model import get_session_persist_id as get_session_persist_id
+
+__plugin_meta__ = PluginMetadata(
+    name="session 插件 orm 扩展",
+    description="为 session 提供数据库模型及存取方法",
+    usage="请参考文档",
+    type="library",
+    homepage="https://github.com/noneplugin/nonebot-plugin-session-orm",
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_session"),
+    extra={"orm_version_location": migrations},
+)
