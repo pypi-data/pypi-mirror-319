6d5e68c6613c4482d5da878fe9863f459dbc28f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:04:29 +0000

    Add package yan-infra-logging/0.3.3/yan_infra_logging-0.3.3.tar.gz

---
+++ packages/yan-infra-logging/yan_infra_logging-0.3.3.tar.gz/yan_infra_logging-0.3.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yan_infra_logging",
+    version="0.3.3",
+    description="A custom logging package",
+    author="Yan Gabriel",
+    author_email="yan80ky16@gmail.com",
+    url="https://github.com/YanGabriel16/infra-logging-py",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
