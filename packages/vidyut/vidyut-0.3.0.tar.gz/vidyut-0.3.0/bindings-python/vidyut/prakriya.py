d4c67d10cba0667e2293fa40b1697b9b5366ae1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 17:05:43 +0000

    Add package vidyut/0.3.0/vidyut-0.3.0.tar.gz

---
+++ packages/vidyut/vidyut-0.3.0.tar.gz/vidyut-0.3.0/bindings-python/vidyut/prakriya.py
@@ -0,0 +1,31 @@
+"""`vidyut.prakriya` generates Sanskrit words by applying Paninian rules."""
+
+from vidyut.vidyut import prakriya as __mod
+
+Vyakarana = __mod.Vyakarana
+
+# Data types
+Data = __mod.Data
+DhatupathaEntry = __mod.DhatupathaEntry
+Sutra = __mod.Sutra
+Source = __mod.Source
+
+# Input types
+Dhatu = __mod.Dhatu
+Pratipadika = __mod.Pratipadika
+Antargana = __mod.Antargana
+Gana = __mod.Gana
+Krt = __mod.Krt
+Lakara = __mod.Lakara
+Linga = __mod.Linga
+Pada = __mod.Pada
+Prayoga = __mod.Prayoga
+Purusha = __mod.Purusha
+Sanadi = __mod.Sanadi
+Taddhita = __mod.Taddhita
+Vacana = __mod.Vacana
+Vibhakti = __mod.Vibhakti
+
+# Output types
+Prakriya = __mod.Prakriya
+Step = __mod.Step
