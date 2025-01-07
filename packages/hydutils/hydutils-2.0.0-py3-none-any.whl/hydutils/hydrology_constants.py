da05f0a0c0854824aeb85751e73367c18d8fd76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 04:04:20 +0000

    Add package hydutils/2.0.0/hydutils-2.0.0-py3-none-any.whl

---
+++ packages/hydutils/hydutils-2.0.0-py3-none-any.whl/hydutils/hydrology_constants.py
@@ -0,0 +1,46 @@
+# Time-related
+TIMESERIES = 'Timeseries'
+TIMESTAMP = 'Timestamp'
+INTERVAL = 'Interval'
+
+# Weather parameters
+TEMPERATURE = 'Temperature'
+HUMIDITY = 'Humidity'
+PRECIPITATION = 'Precipitation'
+RAINFALL = 'Rainfall'
+WIND_SPEED = 'Wind_Speed'
+WIND_DIRECTION = 'Wind_Direction'
+AIR_PRESSURE = 'Air_Pressure'
+CLOUD_COVER = 'Cloud_Cover'
+SOLAR_RADIATION = 'Solar_Radiation'
+DEW_POINT = 'Dew_Point'
+VISIBILITY = 'Visibility'
+FOG = 'Fog'
+STORM = 'Storm'
+HEAT_INDEX = 'Heat_Index'
+WIND_CHILL = 'Wind_Chill'
+WEATHER = 'Weather'
+
+# Hydrological processes
+EVAPOTRANSPIRATION = 'Evapotranspiration'
+RUNOFF = 'Runoff'
+INFILTRATION = 'Infiltration'
+PERCOLATION = 'Percolation'
+INTERCEPTION = 'Interception'
+BASEFLOW = 'Baseflow'
+
+# Water flow and storage
+DISCHARGE = 'Discharge'
+INFLOW = 'Inflow'
+OUTFLOW = 'Outflow'
+STREAMFLOW = 'Streamflow'
+GROUNDWATER = 'Groundwater'
+STORAGE = 'Storage'
+SOIL_MOISTURE = 'Soil_Moisture'
+WATER_LEVEL = 'Water_Level'
+
+# Geographical features
+CATCHMENT = 'Catchment'
+WATERSHED = 'Watershed'
+CHANNEL = 'Channel'
+AQUIFER = 'Aquifer'
