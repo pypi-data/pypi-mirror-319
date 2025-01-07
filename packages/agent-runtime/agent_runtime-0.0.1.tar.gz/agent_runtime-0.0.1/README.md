47f85d97c024c435ed2c930102545ca9557249a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:04:58 +0000

    Add package agent-runtime/0.0.1/agent_runtime-0.0.1.tar.gz

---
+++ packages/agent-runtime/agent_runtime-0.0.1.tar.gz/agent_runtime-0.0.1/README.md
@@ -0,0 +1,3 @@
+# agent-runtime
+
+Agent Runtime.
---
+++ packages/agent-runtime/agent_runtime-0.0.1.tar.gz/agent_runtime-0.0.1/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+setuptools.setup(
+    name="agent-runtime",
+    version="0.0.1",
+    author="AutoGen Developers",
+    author_email="your_email@example.com",
+    description="Agent Runtime",
+    long_description="Agent Runtime",
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/autogen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
