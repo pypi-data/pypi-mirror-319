abf4474d5a2af5dee01fb55898fae5eb6b10466c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:09 +0000

    Add package gizai/7.145.1/gizai-7.145.1.tar.gz

---
+++ packages/gizai/gizai-7.145.1.tar.gz/gizai-7.145.1/gizai/__init__.py
@@ -0,0 +1,13 @@
+NAME = "gizai"
+
+ICON = "🔻"
+
+DESCRIPTION = f"{ICON} A Mathematical model for AI languages."
+
+VERSION = "7.145.1"
+
+REPO_NAME = "giza"
+
+MARQUEE = "https://github.com/kamangir/giza/raw/main/assets/giza.png"
+
+ALIAS = "giza"
