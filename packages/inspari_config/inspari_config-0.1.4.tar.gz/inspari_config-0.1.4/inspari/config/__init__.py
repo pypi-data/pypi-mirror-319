4acea47e8297dd1df702709bd67ee35def1425c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 18:04:40 +0000

    Add package inspari_config/0.1.4/inspari_config-0.1.4.tar.gz

---
+++ packages/inspari_config/inspari_config-0.1.4.tar.gz/inspari_config-0.1.4/inspari/config/__init__.py
@@ -0,0 +1,17 @@
+import dotenv
+
+from .appsettings import load_app_settings
+from .keyvault import resolve_key_vault_secrets
+
+"""
+This module holds utilities related to configuration.
+"""
+
+
+def load_dotenv(**kwargs):
+    """
+    Load env vars from dot end, then resolve app settings and secrets.
+    """
+    dotenv.load_dotenv(**kwargs)  # load from .env file
+    load_app_settings()  # load from Azure Web App settings
+    resolve_key_vault_secrets()  # load from Azure Key Vault
