37a4f83aa122d7f45b0b09de99135a3428c9e312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 14:55:41 +0000

    Add package srttranslatelocal/0.3.0/srttranslatelocal-0.3.0.tar.gz

---
+++ packages/srttranslatelocal/srttranslatelocal-0.3.0.tar.gz/srttranslatelocal-0.3.0/src/srttranslatelocal/srttranslatemain.py
@@ -0,0 +1,29 @@
+import srttranslatemain.srttranslate as srttranslate
+import os
+import argparse
+
+def srt_translate():
+    # Path to the TSV file
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputf", help="input srt file")
+    parser.add_argument("--fl", help="from language")
+    parser.add_argument("--tl", help="to language")
+    parser.add_argument("--outputf", help="output srt file")
+    parser.parse_args()
+    
+    inputpath = args.inputf
+    outputpath = args.outputf
+    from_lang = args.fl
+    to_lang = args.tol
+    
+    srttranslate.translate_srt(from_lang,to_lang, input_file, output_file)
+	
+# Defining main function
+def main():
+    srt_translate()
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
