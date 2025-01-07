2c8c9578ae5c4693bcd3cc2e59daa9df73f9e330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:16:56 +0000

    Add package types-aiobotocore-iot1click-devices/2.17.0/types_aiobotocore_iot1click_devices-2.17.0.tar.gz

---
+++ packages/types-aiobotocore-iot1click-devices/types_aiobotocore_iot1click_devices-2.17.0.tar.gz/types_aiobotocore_iot1click_devices-2.17.0/types_aiobotocore_iot1click_devices/paginator.py
@@ -0,0 +1,90 @@
+"""
+Type annotations for iot1click-devices service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_iot1click_devices.client import IoT1ClickDevicesServiceClient
+    from types_aiobotocore_iot1click_devices.paginator import (
+        ListDeviceEventsPaginator,
+        ListDevicesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("iot1click-devices") as client:
+        client: IoT1ClickDevicesServiceClient
+
+        list_device_events_paginator: ListDeviceEventsPaginator = client.get_paginator("list_device_events")
+        list_devices_paginator: ListDevicesPaginator = client.get_paginator("list_devices")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListDeviceEventsRequestPaginateTypeDef,
+    ListDeviceEventsResponseTypeDef,
+    ListDevicesRequestPaginateTypeDef,
+    ListDevicesResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListDeviceEventsPaginator", "ListDevicesPaginator")
+
+
+if TYPE_CHECKING:
+    _ListDeviceEventsPaginatorBase = AioPaginator[ListDeviceEventsResponseTypeDef]
+else:
+    _ListDeviceEventsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListDeviceEventsPaginator(_ListDeviceEventsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/paginator/ListDeviceEvents.html#IoT1ClickDevicesService.Paginator.ListDeviceEvents)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/paginators/#listdeviceeventspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListDeviceEventsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListDeviceEventsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/paginator/ListDeviceEvents.html#IoT1ClickDevicesService.Paginator.ListDeviceEvents.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/paginators/#listdeviceeventspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListDevicesPaginatorBase = AioPaginator[ListDevicesResponseTypeDef]
+else:
+    _ListDevicesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListDevicesPaginator(_ListDevicesPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/paginator/ListDevices.html#IoT1ClickDevicesService.Paginator.ListDevices)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/paginators/#listdevicespaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListDevicesRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListDevicesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iot1click-devices/paginator/ListDevices.html#IoT1ClickDevicesService.Paginator.ListDevices.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iot1click_devices/paginators/#listdevicespaginator)
+        """
