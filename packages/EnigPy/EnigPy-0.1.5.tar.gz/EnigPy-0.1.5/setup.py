497c794e6be345b0d1bb37064f808fe9632145b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 16:17:17 +0000

    Add package EnigPy/0.1.5/EnigPy-0.1.5.tar.gz

---
+++ packages/EnigPy/EnigPy-0.1.5.tar.gz/EnigPy-0.1.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EnigPy',
+    version='0.1.5',    
+    description='',
+    long_description=open('README.md').read(),
+    url='https://github.com/atticus-zhang/EnigPy/tree/main/EnigPy',
+    author='Atticus Zhang',
+    author_email='ay9zhang@uwaterloo.ca',
+    license='MIT',
+    packages=find_packages(),  # Automatically discover packages
+    package_data={
+        'EnigPy': ['source/*'],  # This will include all files in the 'source' directory
+    },
+    keywords=['Python', 'Cryptography'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
