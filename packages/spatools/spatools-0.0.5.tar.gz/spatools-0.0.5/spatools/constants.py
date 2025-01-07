659cb9e83545cfe8b7c53fb7721e50de1c91670e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-07 06:04:50 +0000

    Add package spatools/0.0.5/spatools-0.0.5.tar.gz

---
+++ packages/spatools/spatools-0.0.5.tar.gz/spatools-0.0.5/spatools/constants.py
@@ -0,0 +1,46 @@
+import os
+DIR = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
+DATA = os.path.join(DIR, "data")
+# RESULTADOS = os.path.join(DATA, "Resultados")
+# H5AD = os.path.join(DATA, "all_h5ad_files")
+# FILTRO_GERAL = os.path.join(RESULTADOS, "filtro_geral")
+# PROCESSAMENTO_GERAL = os.path.join(RESULTADOS, "processamento_geral")
+# CELL2LOCATION = os.path.join(RESULTADOS, "cell2loc")
+# MODEL = os.path.join(CELL2LOCATION, "scref.h5ad")
+# PATH_SC = os.path.join(DATA, "OvaryAnnoTME.h5ad")
+# ALL_FILES = os.path.join(DATA, "all_files")
+# RAW_GEO = os.path.join(DATA, "raw_geo")
+# RAW_VISIUM = os.path.join(DATA, "raw_visium")
+# RAW_NATURE = os.path.join(DATA, "raw_nature")
+# FILTRO_K5 = os.path.join(RESULTADOS, "filtro_k5")
+# RAW = os.path.join(DATA, "raw")
+INTEGRADO = os.path.join(DATA, "scvi_5000_v3")
+
+
+# dictionary of colors
+COLORS_23 = {
+    0: (0, 32, 111),
+    1: (230, 214, 71),
+    2: (170, 96, 40),
+    3: (19, 177, 81),
+    4: (89, 0, 88),
+    5: (197, 223, 119),
+    6: (1, 135, 226),
+    7: (255, 161, 97),
+    8: (136, 101, 195),
+    9: (252, 80, 111),
+    10: (118, 232, 195),
+    11: (126, 0, 46),
+    12: (42, 82, 0),
+    13: (255, 127, 202),
+    14: (129, 62, 115),
+    15: (255, 135, 143),
+    16: (34, 222, 230),
+    17: (201, 78, 60),
+    18: (131, 110, 32),
+    19: (159, 150, 56),
+    20: (177, 73, 161),
+    21: (128, 77, 17),
+    22: (129, 47, 25)
+}
+
