94e17b6115554acb48fcac857d6369760c3fa199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:37 +0000

    Add package snakespawn/1.0.4/snakespawn-1.0.4.tar.gz

---
+++ packages/snakespawn/snakespawn-1.0.4.tar.gz/snakespawn-1.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+def read_requirements(file_name):
+    """
+    Reads the requirements.txt file and returns a list of dependencies.
+    """
+    requirements_path = Path(file_name)
+    if requirements_path.is_file():
+        return requirements_path.read_text().splitlines()
+    return []
+
+setup(
+    name="snakespawn",
+    version="1.0.4",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "snakespawn=snakespawn.main:main",
+        ],
+    },
+    python_requires=">=3.10",
+    author="William L.",
+    long_description=open("README.md").read(),
+    description="A tool for initializing python packages.",
+    license="MIT",
+    install_requires=read_requirements("requirements.txt")
+)
