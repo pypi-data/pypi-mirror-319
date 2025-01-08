6c671eee12c0f5057876437ba07cb60c82a77fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:03 +0000

    Add package jp-medicine-master/1.2/jp_medicine_master-1.2-py3-none-any.whl

---
+++ packages/jp-medicine-master/jp_medicine_master-1.2-py3-none-any.whl/jp_medicine_master/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '1.2'
+
+from ._ import (
+    get_url_ssk, get_url_mhlw,
+    read_ssk_y, download_ssk_y,
+    read_mhlw_price, download_mhlw_price,
+    read_mhlw_ge, download_mhlw_ge,
+)
