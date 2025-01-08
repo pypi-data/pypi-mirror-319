f577508e9f2397878e7b8cc36cd699aad9f1d3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:05:10 +0000

    Add package libfranco/2.0.1/libfranco-2.0.1.tar.gz

---
+++ packages/libfranco/libfranco-2.0.1.tar.gz/libfranco-2.0.1/libfranco/__init__.py
@@ -0,0 +1,2 @@
+print("Stai Utilizzando libfranco v 2.0.0 di Gen-2025")
+from .tdms_grafico import TDMS, GRAFICO, help, esempio
---
+++ packages/libfranco/libfranco-2.0.1.tar.gz/libfranco-2.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="libfranco",               # Nome del pacchetto su PyPI
+    version="2.0.1",                # Versione del pacchetto
+    packages=["libfranco"],         # Nome del modulo esplicito
+    package_dir={"libfranco": "libfranco"},  # Mapping chiaro
+    install_requires=[              # Dipendenze
+        "nptdms",                   # Libreria per leggere file TDMS
+        "matplotlib",               # Libreria per i grafici
+        "pandas",                   # Libreria per la gestione dei dati
+        "numpy",
+        "scipy"                     # Libreria per l'analisi dei dati
+    ],
+    author="Franco De Angelis",
+    author_email="fd23111991@gmail.com",
+    description="Libreria per gestire TDMS e grafici",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/libfranco/",  # URL del progetto (es. GitHub)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
