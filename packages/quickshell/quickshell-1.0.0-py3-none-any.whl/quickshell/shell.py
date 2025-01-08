07cec5a262ab65d05bc6584faa0c1e7f70f919e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:14 +0000

    Add package quickshell/1.0.0/quickshell-1.0.0-py3-none-any.whl

---
+++ packages/quickshell/quickshell-1.0.0-py3-none-any.whl/quickshell/shell.py
@@ -0,0 +1,34 @@
+import asyncio
+from typing import Tuple
+
+
+async def run_shell(cmd, print_output: bool = False) -> Tuple[str, str]:
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    stdout, stderr = [], []
+
+    async def stream_output(stream, array, print_output):
+        while True:
+            line = await stream.readline()
+            if line:
+                text = line.decode().strip()
+                array.append(text)
+                if print_output:
+                    print(text)
+            else:
+                break
+
+    await asyncio.gather(
+        stream_output(process.stdout, stdout, print_output),
+        stream_output(process.stderr, stderr, print_output)
+    )
+
+    await process.wait()
+
+    return "\n".join(stdout), "\n".join(stderr)
+
+
