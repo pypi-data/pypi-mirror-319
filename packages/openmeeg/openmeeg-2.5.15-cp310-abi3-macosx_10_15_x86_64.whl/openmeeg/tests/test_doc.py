26110c45daf8a6a84aadad0bdad8f170a1bc9832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:53 +0000

    Add package openmeeg/2.5.15/openmeeg-2.5.15-cp310-abi3-macosx_10_15_x86_64.whl

---
+++ packages/openmeeg/openmeeg-2.5.15-cp310-abi3-macosx_10_15_x86_64.whl/openmeeg/tests/test_doc.py
@@ -0,0 +1,16 @@
+import inspect
+
+import openmeeg as om
+
+
+def test_doc():
+    # Make sure that doc is generated
+    doc = inspect.getdoc(om.HeadMat)
+    assert doc is not None
+
+    headmat_expected_docstring = """\
+HeadMat(Geometry geo, \
+Integrator integrator=OpenMEEG::Integrator(3,0,0.005)) -> SymMatrix"""
+    assert (
+        doc == headmat_expected_docstring
+    ), f"got: {repr(doc)} != expected: {repr(headmat_expected_docstring)}"
