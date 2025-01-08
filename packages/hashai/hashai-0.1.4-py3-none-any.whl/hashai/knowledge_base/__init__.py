36abf4664fd7eb09be35a6e591d956ccce31eb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:05:09 +0000

    Add package hashai/0.1.4/hashai-0.1.4-py3-none-any.whl

---
+++ packages/hashai/hashai-0.1.4-py3-none-any.whl/hashai/knowledge_base/__init__.py
@@ -0,0 +1,6 @@
+# Import the main classes/functions from each module to make them accessible directly from the package
+from .retriever import Retriever
+from .vector_store import VectorStore
+
+# Optionally, you can define what should be imported when someone uses `from knowledge_base import *`
+__all__ = ['Retriever', 'VectorStore']
