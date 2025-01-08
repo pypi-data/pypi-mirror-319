e944c1fdc4bffcfff73c628e71b1c9f49b46c325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:08:51 +0000

    Add package edgartools/3.7.0/edgartools-3.7.0.tar.gz

---
+++ packages/edgartools/edgartools-3.7.0.tar.gz/edgartools-3.7.0/edgar/reference/__init__.py
@@ -0,0 +1,74 @@
+
+from edgar.reference.forms import describe_form
+from edgar.reference.tickers import get_ticker_from_cusip, get_icon_from_ticker, cusip_ticker_mapping, download_ticker_data
+from edgar.core import log, get_edgar_data_directory
+
+
+# A dict of state abbreviations and their full names
+states = {
+
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+}
+
+def download_reference_data():
+    """
+    Download reference data from the SEC website.
+    """
+    log.info(f"Downloading reference data to {get_edgar_data_directory()}")
+    reference_directory = get_edgar_data_directory() / "reference"
+    reference_directory.mkdir(exist_ok=True)
+    download_ticker_data(reference_directory)
+
+
+
+
+
