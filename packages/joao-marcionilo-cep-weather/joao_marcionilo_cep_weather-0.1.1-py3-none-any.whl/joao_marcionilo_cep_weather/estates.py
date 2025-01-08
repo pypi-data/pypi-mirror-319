b9855f723eb40d8b8cc13ed31e73e7e099075280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:36 +0000

    Add package joao-marcionilo-cep-weather/0.1.1/joao_marcionilo_cep_weather-0.1.1-py3-none-any.whl

---
+++ packages/joao-marcionilo-cep-weather/joao_marcionilo_cep_weather-0.1.1-py3-none-any.whl/joao_marcionilo_cep_weather/estates.py
@@ -0,0 +1,7 @@
+ufs = {
+    'AC': 'Rio Branco', 'AL': 'Maceió', 'AP': 'Macapá', 'AM': 'Manaus', 'BA': 'Salvador', 'CE': 'Fortaleza',
+    'DF': 'Brasília', 'ES': 'Vitória', 'GO': 'Goiânia', 'MA': 'São Luís', 'MT': 'Cuiabá', 'MS': 'Campo Grande',
+    'MG': 'Belo Horizonte', 'PA': 'Belém', 'PB': 'João Pessoa', 'PR': 'Curitiba', 'PE': 'Recife', 'PI': 'Teresina',
+    'RJ': 'Rio de Janeiro', 'RN': 'Natal', 'RS': 'Porto Alegre', 'RO': 'Porto Velho', 'RR': 'Boa Vista',
+    'SC': 'Florianópolis', 'SP': 'São Paulo', 'SE': 'Aracaju', 'TO': 'Palmas'
+}
