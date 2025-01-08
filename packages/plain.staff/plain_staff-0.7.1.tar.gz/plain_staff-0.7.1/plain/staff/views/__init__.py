ab41135652dfd4f1bec55fe1a52a3a3d1fbc4e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:02 +0000

    Add package plain.staff/0.7.1/plain_staff-0.7.1.tar.gz

---
+++ packages/plain.staff/plain_staff-0.7.1.tar.gz/plain_staff-0.7.1/plain/staff/views/__init__.py
@@ -0,0 +1,37 @@
+from .base import (
+    StaffDeleteView,
+    StaffDetailView,
+    StaffListView,
+    StaffUpdateView,
+    StaffView,
+)
+from .models import (
+    StaffModelDetailView,
+    StaffModelListView,
+    StaffModelUpdateView,
+    StaffModelViewset,
+)
+from .registry import (
+    get_model_detail_url,
+    register_dashboard,
+    register_view,
+    register_viewset,
+)
+from .types import Img
+
+__all__ = [
+    "StaffView",
+    "StaffListView",
+    "StaffDetailView",
+    "StaffUpdateView",
+    "StaffDeleteView",
+    "StaffModelViewset",
+    "StaffModelListView",
+    "StaffModelDetailView",
+    "StaffModelUpdateView",
+    "register_viewset",
+    "register_view",
+    "register_dashboard",
+    "get_model_detail_url",
+    "Img",
+]
