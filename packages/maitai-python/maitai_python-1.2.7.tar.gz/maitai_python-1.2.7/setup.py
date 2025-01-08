dfe98134db251055f8551219ecff4fbb6b0157a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:04:48 +0000

    Add package maitai-python/1.2.7/maitai_python-1.2.7.tar.gz

---
+++ packages/maitai-python/maitai_python-1.2.7.tar.gz/maitai_python-1.2.7/setup.py
@@ -0,0 +1,41 @@
+# setup.py
+
+from setuptools import find_packages, setup
+
+from maitai_common.version import version
+
+setup(
+    name="maitai-python",
+    version=version,
+    packages=find_packages(
+        exclude=("maitai_back", "maitai_back.*"),
+        include=(
+            "maitai.*",
+            "maitai",
+            "maitai_common",
+            "maitai_common.*",
+            "maitai_gen",
+            "maitai_gen.*",
+        ),
+    ),
+    install_requires=[
+        "requests",
+        "openai>=1.34.0",
+        "httpx",
+        "aiohttp",
+        "betterproto==2.0.0b7",
+        "websocket-client",
+    ],
+    # Optional metadata
+    author="Maitai",
+    author_email="support@trymaitai.ai",
+    description="Maitai SDK for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://docs.trymaitai.ai",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
