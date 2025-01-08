9de1a3a868801b5928e302fc4822016d55d651de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:18 +0000

    Add package netbox-zabbix/2.0.3/netbox_zabbix-2.0.3-py3-none-any.whl

---
+++ packages/netbox-zabbix/netbox_zabbix-2.0.3-py3-none-any.whl/netbox_zabbix/__init__.py
@@ -0,0 +1,40 @@
+from netbox.plugins import PluginConfig
+from importlib.metadata import metadata
+
+metadata = metadata('netbox_zabbix')
+
+class ZabbixPlugin(PluginConfig):
+    name = metadata.get('Name').replace('-', '_')
+    verbose_name = metadata.get('Summary')
+    description = metadata.get('Description')
+    version = metadata.get('Version')
+    author = metadata.get('Author')
+    author_email = metadata.get('Author-email')
+    base_url = 'zabbix'
+    min_version = '4.0.0'
+    max_version = '4.2.99'
+    required_settings = [
+        'username',
+        'password',
+    ]
+    default_settings = {
+        'tags': ['automation: monitoring'],
+        'snmp': {
+            'version': '2c',  # 1, 2c, 3
+            'community': None,
+            'context': None,
+            'username': None,
+            'level': None,  # None, noAuthNoPriv, authNoPriv, authPriv
+            'auth_protocol': None,  # None, MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+            'auth_passphrase': None,
+            'priv_protocol': None,  # None, DES, AES128, AES192, AES256, AES192C, AES256C
+            'priv_passphrase': None,
+        }
+    }
+    queues = []
+
+    def ready(self):
+        super().ready()
+        from netbox_zabbix.signals import m2m_device, update_vm, update_device
+
+config = ZabbixPlugin
