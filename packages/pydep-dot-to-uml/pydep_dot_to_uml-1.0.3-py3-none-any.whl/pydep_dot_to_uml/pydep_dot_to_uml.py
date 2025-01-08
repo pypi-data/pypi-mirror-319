98d747b762eaa70c35756c53994d4a8e37703026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:13 +0000

    Add package pydep-dot-to-uml/1.0.3/pydep_dot_to_uml-1.0.3-py3-none-any.whl

---
+++ packages/pydep-dot-to-uml/pydep_dot_to_uml-1.0.3-py3-none-any.whl/pydep_dot_to_uml/pydep_dot_to_uml.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from pathlib import Path
+import argparse
+
+from .dot import load
+
+
+__version__ = "1.0.3"
+
+
+def _pydep_dot_to_uml(fpath: Path) -> str:
+    root, files = load(fpath)
+    arrows = "\n".join(sorted(set.union(*[i.arrows() for i in files])))
+    return f"@startuml\n{root.package()}\n\n{arrows}\n@enduml"
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", action="version", version=f"{parser.prog} {__version__}")
+    parser.add_argument("file", type=Path, help="Path to the dot file")
+    print(_pydep_dot_to_uml(parser.parse_args().file))
