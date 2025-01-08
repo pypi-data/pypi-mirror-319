ed20a5a2fdd6fbba21e10d83a7509280ce719155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:05:08 +0000

    Add package ovinc-client/0.4.0b4/ovinc_client-0.4.0b4-py3-none-any.whl

---
+++ packages/ovinc-client/ovinc_client-0.4.0b4-py3-none-any.whl/ovinc_client/account/serializers.py
@@ -0,0 +1,26 @@
+from adrf.serializers import ModelSerializer, Serializer
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy
+from rest_framework import serializers
+
+from ovinc_client.account.models import User
+
+USER_MODEL: User = get_user_model()
+
+
+class UserSignInSerializer(Serializer):
+    """
+    Sign In
+    """
+
+    code = serializers.CharField(label=gettext_lazy("Code"))
+
+
+class UserInfoSerializer(ModelSerializer):
+    """
+    User Info
+    """
+
+    class Meta:
+        model = USER_MODEL
+        fields = ["username", "nick_name", "user_type", "last_login"]
