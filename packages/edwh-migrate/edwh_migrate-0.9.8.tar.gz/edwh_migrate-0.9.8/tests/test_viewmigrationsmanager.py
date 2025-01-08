69ea6171973dcfff56d9b248d0732d7d25a1cc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:58 +0000

    Add package edwh-migrate/0.9.8/edwh_migrate-0.9.8.tar.gz

---
+++ packages/edwh-migrate/edwh_migrate-0.9.8.tar.gz/edwh_migrate-0.9.8/tests/test_viewmigrationsmanager.py
@@ -0,0 +1,77 @@
+from sqlite3 import OperationalError
+
+import pytest
+
+from src.edwh_migrate import ViewMigrationManager, setup_db
+
+from .fixtures import clean_migrate
+
+
+class StandaloneView(ViewMigrationManager):
+    def up(self): ...
+
+    def down(self): ...
+
+
+class MyBaseView(ViewMigrationManager):
+    uses = ()
+
+    def up(self) -> None:
+        self.db.executesql("""
+            CREATE VIEW base_view AS
+            SELECT Rowid AS id
+            ;
+        """)
+
+    def down(self) -> None:
+        self.db.executesql("""
+            DROP VIEW IF EXISTS base_view;
+        """)
+
+
+class MyChildView1(ViewMigrationManager):
+    uses = (MyBaseView,)
+
+    def up(self) -> None:
+        self.db.executesql("""
+            CREATE VIEW my_child_view_1 AS
+            SELECT id FROM base_view
+            ;
+        """)
+
+    def down(self) -> None:
+        self.db.executesql("""
+            DROP VIEW IF EXISTS my_child_view_1;
+        """)
+
+
+class MyChildView2(ViewMigrationManager):
+    uses = (MyBaseView, MyChildView1)
+
+    def up(self) -> None:
+        self.db.executesql("""
+            CREATE VIEW my_child_view_2 AS
+            SELECT id FROM my_child_view_1
+            ;
+        """)
+
+    def down(self) -> None:
+        self.db.executesql("""
+            DROP VIEW IF EXISTS my_child_view_2;
+        """)
+
+
+def test_resolving_manager_order(clean_migrate):
+    db = setup_db(migrate=True, migrate_enabled=True)
+
+    # one should be fine:
+    with MyChildView2(db):
+        ...
+
+    with MyBaseView(db):
+        ...
+
+    # multiple without combine may crash:
+    with pytest.raises(OperationalError):
+        with MyChildView2(db), MyBaseView(db), MyChildView1(db):
+            ...
