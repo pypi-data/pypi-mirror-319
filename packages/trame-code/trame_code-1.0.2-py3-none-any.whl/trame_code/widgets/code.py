95575d26a137c183909f7c6a085ce213f8fd080a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:50 +0000

    Add package trame-code/1.0.2/trame_code-1.0.2-py3-none-any.whl

---
+++ packages/trame-code/trame_code-1.0.2-py3-none-any.whl/trame_code/widgets/code.py
@@ -0,0 +1,54 @@
+"""Module compatible with vue2 and vue3. To use it, you need to install **trame-code**"""
+
+from trame_client.widgets.core import AbstractElement
+
+from .. import module
+
+__all__ = [
+    "Editor",
+]
+
+
+class HtmlElement(AbstractElement):
+    def __init__(self, _elem_name, children=None, **kwargs):
+        super().__init__(_elem_name, children, **kwargs)
+        if self.server:
+            self.server.enable_module(module)
+
+
+# Expose your vue component(s)
+class Editor(HtmlElement):
+    """
+    Monaco Editor component
+
+    Properties:
+
+    :param options:
+    :param value:
+    :param model_value:
+    :param theme:
+    :param language:
+    :param textmate:
+
+    Events:
+
+    :param input:
+
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            "vs-editor",
+            **kwargs,
+        )
+        self._attr_names += [
+            "options",
+            "value",
+            ("model_value", "modelValue"),
+            "theme",
+            "language",
+            "textmate",
+        ]
+        self._event_names += [
+            "input",
+        ]
