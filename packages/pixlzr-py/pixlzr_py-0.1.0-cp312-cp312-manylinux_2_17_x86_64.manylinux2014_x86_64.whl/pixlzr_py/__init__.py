4f3f19898f7f65957c605b6a08a0373572ae2ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:33 +0000

    Add package pixlzr-py/0.1.0/pixlzr_py-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pixlzr-py/pixlzr_py-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pixlzr_py/__init__.py
@@ -0,0 +1,5 @@
+from .pixlzr_py import *  # type: ignore
+
+__doc__ = pixlzr_py.__doc__  # type: ignore
+if hasattr(pixlzr_py, "__all__"):  # type: ignore
+    __all__ = pixlzr_py.__all__  # type: ignore
