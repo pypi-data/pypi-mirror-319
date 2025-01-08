fa4ba2646dc44999fd2d0a42c7e94a6b9c181067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:37 +0000

    Add package snakespawn/1.0.2/snakespawn-1.0.2.tar.gz

---
+++ packages/snakespawn/snakespawn-1.0.2.tar.gz/snakespawn-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="snakespawn",
+    version="1.0.2",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "snakespawn=snakespawn.main:main",
+        ],
+    },
+    python_requires=">=3.10",
+    author="William L.",
+    long_description=open("README.md").read(),
+    description="A tool for initializing python packages.",
+    license="MIT",
+)
