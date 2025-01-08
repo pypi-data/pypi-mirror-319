9d0dcda882f6aa8876bd708263982179ce02c565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:42 +0000

    Add package realpass/0.4/RealPass-0.4.tar.gz

---
+++ packages/realpass/RealPass-0.4.tar.gz/RealPass-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='RealPass',
+      version=0.4,
+      description="RealPass is a Python package that generates highly secure, realistic passwords with customizable length and complexity.",
+      long_description="RealPass is a Python package that generates strong, realistic passwords by combining uppercase and lowercase letters, numbers, and special characters, with customizable length and complexity settings to ensure both security and practicality. \nContributors:\n\n- Youraj Verma : https://github.com/codex-yv\n- Pooja Velmurugen : https://github.com/Pooja-Velmurugen",
+      author="Youraj Verma",
+      author_email='yourajverma960@gmail.com',
+      packages=['RealPass'],
+      install_requires=['numpy', 'python-math', 'random2'],
+      url='https://github.com/codex-yv',
+      license='codexyv')
