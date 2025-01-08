68a88e257a34738b80496f50086ab6d992863257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 07:05:52 +0000

    Add package method-python/1.1.5/method-python-1.1.5.tar.gz

---
+++ packages/method-python/method-python-1.1.5.tar.gz/method-python-1.1.5/method/resources/Entities/Connect.py
@@ -0,0 +1,36 @@
+from typing import TypedDict, Optional, Literal, List
+
+from method.resource import MethodResponse, Resource, ResourceListOpts
+from method.configuration import Configuration
+from method.errors import ResourceError
+
+
+EntityConnectResponseStatusLiterals = Literal[
+    'completed',
+    'in_progress',
+    'pending',
+    'failed'
+]
+
+
+class EntityConnect(TypedDict):
+    id: str
+    status: EntityConnectResponseStatusLiterals
+    accounts: Optional[List[str]]
+    error: Optional[ResourceError]
+    created_at: str
+    updated_at: str
+
+
+class EntityConnectResource(Resource):
+    def __init__(self, config: Configuration):
+        super(EntityConnectResource, self).__init__(config.add_path('connect'))
+
+    def retrieve(self, cxn_id: str) -> MethodResponse[EntityConnect]:
+        return super(EntityConnectResource, self)._get_with_id(cxn_id)
+    
+    def list(self, params: Optional[ResourceListOpts] = None) -> MethodResponse[List[EntityConnect]]:
+        return super(EntityConnectResource, self)._list(params)
+
+    def create(self) -> MethodResponse[EntityConnect]:
+        return super(EntityConnectResource, self)._create({})
