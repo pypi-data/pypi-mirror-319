b5b416576772f453a8bc7b22f18142e2c3bb6bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:25 +0000

    Add package jjuke/1.0.0/jjuke-1.0.0.tar.gz

---
+++ packages/jjuke/jjuke-1.0.0.tar.gz/jjuke-1.0.0/jjuke/__init__.py
@@ -0,0 +1,8 @@
+from . import model, util
+# from jjuke import *
+from .util import logger, options
+from .model import trainer
+
+__all__ = ["model", "util", "logger", "options", "trainer"]
+
+__version__ = "1.0.0"
---
+++ packages/jjuke/jjuke-1.0.0.tar.gz/jjuke-1.0.0/jjuke/util/__init__.py
@@ -0,0 +1,5 @@
+from . import *
+from . import logger, options
+from .options import get_obj_from_str, instantiate_from_config
+
+__all__ = ["logger", "options", "get_obj_from_str", "instantiate_from_config"]
