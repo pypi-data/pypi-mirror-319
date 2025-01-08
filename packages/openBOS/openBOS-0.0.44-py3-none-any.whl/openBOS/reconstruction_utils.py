4c317fbf9230d5e27ed88c4505e72286afc408a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:22 +0000

    Add package openBOS/0.0.44/openBOS-0.0.44-py3-none-any.whl

---
+++ packages/openBOS/openBOS-0.0.44-py3-none-any.whl/openBOS/reconstruction_utils.py
@@ -0,0 +1,51 @@
+from tqdm import tqdm
+import numpy as np  
+import math  
+
+def sinogram_maker_axialsymmetry(refractive_angle,projection_angle):
+    """
+    Generates a sinogram with axial symmetry from a single 2D refractive angle image.
+
+    Parameters
+    ----------
+    refractive_angle : np.ndarray
+        A 2D numpy array representing the refractive angle image. Each row in this array 
+        is broadcast across the height dimension to simulate an axially symmetric sinogram.
+
+    Returns
+    -------
+    np.ndarray
+        A 3D numpy array representing the sinogram with axial symmetry. Each slice along 
+        the first dimension corresponds to a projection at a different angle, where each 
+        projection is a symmetric repetition of the refractive angle row values across 
+        the height and width dimensions.
+
+    Notes
+    -----
+    This function assumes axial symmetry for the generated sinogram by replicating each row 
+    of the input angle image across both dimensions (height and width) for each slice in 
+    the 3D sinogram. The input image is first rotated by 90 degrees for alignment.
+
+    Examples
+    --------
+    >>> angle_image = np.random.rand(100, 200)  # A 100x200 refractive angle image
+    >>> sinogram = sinogram_maker_axialsymmetry(angle_image)
+    >>> print(sinogram.shape)
+    (200, 200, 200)
+    """
+    # Rotate the angle image by 90 degrees
+    angle = np.rot90(refractive_angle)
+    height = angle.shape[1]
+    d_projection_angle=projection_angle/height
+    
+    # Initialize an empty 3D array for the sinogram
+    sinogram_x = np.empty((angle.shape[0], height, height), dtype=angle.dtype)
+    sinogram_y = np.empty((angle.shape[0], height, height), dtype=angle.dtype)
+
+    # Loop through each row in the rotated angle image
+    for i, d_angle in enumerate(tqdm(angle)):
+        # Broadcast each row across the height to create a symmetric 2D projection
+        sinogram_x[i] = np.broadcast_to(d_angle[:, np.newaxis]*math.sin(math.radians(i*d_projection_angle)), (height, height))
+        sinogram_y[i] = np.broadcast_to(d_angle[:, np.newaxis]*math.cos(math.radians(i*d_projection_angle)), (height, height))
+        
+    return sinogram_x,sinogram_y
