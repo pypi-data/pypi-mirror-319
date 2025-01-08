0d0af14a18cd67e3ec9fdaa87e3e552a84527c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:49 +0000

    Add package coogle/0.0.2/coogle-0.0.2.tar.gz

---
+++ packages/coogle/coogle-0.0.2.tar.gz/coogle-0.0.2/Coogle/__init__.py
@@ -0,0 +1,24 @@
+appname = "coogle"
+version = "0.0.2"
+pythons = "~=3.10"
+
+clinton = "Monisha-1996"
+sources = None
+
+licence = None
+install = ["requests"]
+mention = ["Drive", "Google", "Telegram"]
+
+DATA03 = None
+DATA01 = None
+DATA02 = ['Natural Language :: English',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Localization',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Version Control',]
