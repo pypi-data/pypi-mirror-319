fbc8a5bbe10b57ac08ece7965a9208c810303c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:05:12 +0000

    Add package navigator-api/2.12.9/navigator_api-2.12.9-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/navigator-api/navigator_api-2.12.9-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/navigator/version.py
@@ -0,0 +1,10 @@
+"""Navigator Meta information."""
+
+__title__ = "navigator-api"
+__description__ = (
+    "Navigator Web Framework based on aiohttp, " "with batteries included."
+)
+__version__ = "2.12.9"
+__author__ = "Jesus Lara"
+__author_email__ = "jesuslarag@gmail.com"
+__license__ = "BSD"
