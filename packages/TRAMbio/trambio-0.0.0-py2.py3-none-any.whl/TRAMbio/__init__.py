5ee77e143189da43553dda55e1bda8657c7f0e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:58 +0000

    Add package TRAMbio/0.0.0/trambio-0.0.0-py2.py3-none-any.whl

---
+++ packages/TRAMbio/trambio-0.0.0-py2.py3-none-any.whl/TRAMbio/__init__.py
@@ -0,0 +1,14 @@
+# TRAMbio
+# Author: Nicolas Handke <nicolas@bioinf.uni-leipzig.de>
+# License: MIT
+# Code Repository: https://github.com/gate-tec/TRAMbio
+"""
+TRAMbio
+=======
+
+Coming soon.
+"""
+
+__author__ = "Nicolas Handke <nicolas@bioinf.uni-leipzig.de>"
+__version__ = "0.0.0"
+__docformat__ = "numpy"
