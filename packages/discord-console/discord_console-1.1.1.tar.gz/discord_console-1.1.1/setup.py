f835d5e6d00805ba1294725e8bb8cc50702d1c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:04 +0000

    Add package discord-console/1.1.1/discord_console-1.1.1.tar.gz

---
+++ packages/discord-console/discord_console-1.1.1.tar.gz/discord_console-1.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name                  = "discord_console",
+    version               = "1.1.1",
+    packages              = find_packages(),
+    include_package_data  = True,
+    license               = "MIT License",
+    description           = "get your discord.py bots appear as like its on a phone.",
+    keywords              = "discord.py, discord console, discord console status, discord bot console status, discord console bot status",
+    url                   = "https://github.com/",
+    author                = "anvil",
+    classifiers           = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
