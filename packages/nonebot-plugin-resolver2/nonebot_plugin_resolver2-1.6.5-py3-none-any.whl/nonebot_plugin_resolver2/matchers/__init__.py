f3dd90e29d4fb3fb37bab8fd4421fed4620484e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:07:50 +0000

    Add package nonebot-plugin-resolver2/1.6.5/nonebot_plugin_resolver2-1.6.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-resolver2/nonebot_plugin_resolver2-1.6.5-py3-none-any.whl/nonebot_plugin_resolver2/matchers/__init__.py
@@ -0,0 +1,13 @@
+from typing import (
+    List,
+    Literal,
+    get_args
+)
+from ..constant import MatcherNames
+# MatcherNames = Literal["bilibili", "douyin", "kugou", "twitter", "ncm", "ytb", "acfun", "tiktok", "weibo", "xiaohongshu"]
+
+modules: List[MatcherNames] = list(get_args(MatcherNames))
+for module in modules:
+    exec(f"from .{module} import {module}")
+    
+resolvers = {module: eval(module) for module in modules}
