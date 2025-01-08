f2d8394738c522b74e0fb15ae0cc90c05bc5ad99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:43 +0000

    Add package diffract/0.1.1/diffract-0.1.1-py3-none-any.whl

---
+++ packages/diffract/diffract-0.1.1-py3-none-any.whl/diffract/__init__.py
@@ -0,0 +1,10 @@
+from .__version__ import __version__
+from .core import diff_objects, print_diff, import_object, get_source
+
+__all__ = [
+    "__version__",
+    "diff_objects",
+    "print_diff",
+    "import_object",
+    "get_source",
+]
