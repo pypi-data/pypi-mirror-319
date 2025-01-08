78dc499f83c297313730e60047f15d6a49b0c631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:05:04 +0000

    Add package regenx/0.0.2/regenx-0.0.2-py3-none-any.whl

---
+++ packages/regenx/regenx-0.0.2-py3-none-any.whl/regenx/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present 0000matteo0000 <matteo.sid@hotmail.it>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.2"
---
+++ packages/regenx/regenx-0.0.2-py3-none-any.whl/regenx/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present 0000matteo0000 <matteo.sid@hotmail.it>
+#
+# SPDX-License-Identifier: MIT
+
+from regenx.regenx import gen, parse
+
+__all__ = ["gen", "parse"]
