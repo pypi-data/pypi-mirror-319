e11098d96725a3ec550055de02c42621d4352268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:23 +0000

    Add package liknorm/2.0.0/liknorm-2.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl

---
+++ packages/liknorm/liknorm-2.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl/liknorm/_testit.py
@@ -0,0 +1,15 @@
+def test(verbose=True):
+    r"""Run tests to verify this package's integrity. (DEPRECATED)
+
+    Parameters
+    ----------
+    verbose : bool
+        ``True`` to show diagnostic. Defaults to ``True``.
+
+    Returns
+    -------
+    int
+        Exit code: ``0`` for success.
+    """
+
+    return 0
