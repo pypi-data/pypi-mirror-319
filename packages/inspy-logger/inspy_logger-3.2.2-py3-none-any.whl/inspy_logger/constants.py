457f04b0e0c77e6fbb6fc3d725027451eaa76117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:17 +0000

    Add package inspy-logger/3.2.2/inspy_logger-3.2.2-py3-none-any.whl

---
+++ packages/inspy-logger/inspy_logger-3.2.2-py3-none-any.whl/inspy_logger/constants.py
@@ -0,0 +1,48 @@
+"""
+
+
+Author:
+    Inspyre Softworks
+
+Project:
+    inSPy-Logger
+
+File:
+    inspy_logger/constants.py
+
+
+Description:
+
+
+"""
+
+
+import logging
+from rich.logging import RichHandler
+
+DEFAULT_LOGGING_LEVEL = logging.DEBUG
+
+HANDLER_TYPES = {
+        'console': RichHandler,
+        'file': logging.FileHandler
+        }
+
+LEVEL_MAP = {
+    'internal': 5,
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+    'fatal': logging.FATAL,
+}
+"""A mapping of level names to their corresponding logging levels."""
+
+
+LEVELS = [level.upper() for level in LEVEL_MAP]
+
+INTERNAL = LEVEL_MAP['debug'] - 5
+
+
+INTERACTIVE_SESSION = __name__ != '__main__'
+"""A flag to indicate whether the session is interactive."""
