08266527d14a3fd16862ef201668c2c37856ef3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:08:36 +0000

    Add package napari-tardis-em/0.3.10/napari_tardis_em-0.3.10.tar.gz

---
+++ packages/napari-tardis-em/napari_tardis_em-0.3.10.tar.gz/napari_tardis_em-0.3.10/src/napari_tardis_em/viewers/__init__.py
@@ -0,0 +1,21 @@
+from tardis_em.utils.losses import *
+
+colormap_for_display = "red"
+
+face_colormap = "Spectral"
+
+IMG_FORMAT = (".tif", ".tiff", ".am", ".mrc", ".rec", ".map")
+
+loss_functions = [
+    AdaptiveDiceLoss,
+    BCELoss,
+    WBCELoss,
+    BCEDiceLoss,
+    CELoss,
+    DiceLoss,
+    ClDiceLoss,
+    ClBCELoss,
+    SigmoidFocalLoss,
+    LaplacianEigenmapsLoss,
+    BCEMSELoss,
+]
