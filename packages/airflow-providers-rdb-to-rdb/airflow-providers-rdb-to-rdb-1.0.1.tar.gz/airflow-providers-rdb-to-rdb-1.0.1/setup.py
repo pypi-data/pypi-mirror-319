3a7ee18308145f5b31ad49d79d6f4e4fe0286ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:01 +0000

    Add package airflow-providers-rdb-to-rdb/1.0.1/airflow-providers-rdb-to-rdb-1.0.1.tar.gz

---
+++ packages/airflow-providers-rdb-to-rdb/airflow-providers-rdb-to-rdb-1.0.1.tar.gz/airflow-providers-rdb-to-rdb-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="airflow-providers-rdb-to-rdb",
+    version="1.0.1",
+    author="Aiden Wu",
+    author_email="aiden.wu@crypto.com",
+    description="A custom Airflow operator to upsert data from PostgreSQL to TiDB.",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Apache Airflow",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
