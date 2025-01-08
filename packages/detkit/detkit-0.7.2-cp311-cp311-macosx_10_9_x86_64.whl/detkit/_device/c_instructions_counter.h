24fdeec4a5040ef49fe3fbd63e11b4edad06399a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:01 +0000

    Add package detkit/0.7.2/detkit-0.7.2-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/detkit/detkit-0.7.2-cp311-cp311-macosx_10_9_x86_64.whl/detkit/_device/c_instructions_counter.h
@@ -0,0 +1,49 @@
+/*
+ *  SPDX-FileCopyrightText: Copyright 2022, Siavash Ameli <sameli@berkeley.edu>
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  SPDX-FileType: SOURCE
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the license found in the LICENSE.txt file in the root
+ *  directory of this source tree.
+ */
+
+
+#ifndef _DEVICE_INSTRUCTIONS_COUNTER_H_
+#define _DEVICE_INSTRUCTIONS_COUNTER_H_
+
+// =======
+// Headers
+// =======
+
+#if __linux__
+    #include <linux/perf_event.h>
+#endif
+
+
+// ======================
+// c Instructions Counter
+// ======================
+
+class cInstructionsCounter
+{
+    public:
+        cInstructionsCounter();
+        ~cInstructionsCounter();
+        void set_simd_factor(double factor);
+        void start();
+        void stop();
+        long long int get_count();
+        long long int get_flops();
+        
+    // Member data
+    private:
+        #if __linux__
+        struct perf_event_attr pe;
+        #endif
+        int fd;
+        long long int count;
+        double simd_factor;
+};
+
+#endif  // _DEVICE_INSTRUCTIONS_COUNTER_H_
