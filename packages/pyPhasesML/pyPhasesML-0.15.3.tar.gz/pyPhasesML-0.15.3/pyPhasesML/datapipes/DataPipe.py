b8766856f28b622708bcfe80a53b9b4037e1cf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:08:52 +0000

    Add package pyPhasesML/0.15.3/pyPhasesML-0.15.3.tar.gz

---
+++ packages/pyPhasesML/pyPhasesML-0.15.3.tar.gz/pyPhasesML-0.15.3/pyPhasesML/datapipes/DataPipe.py
@@ -0,0 +1,17 @@
+
+class DataPipe:
+    def __init__(self, datapipe: "DataPipe") -> None:
+        self.datapipe = datapipe
+
+    def __getitem__(self, index):
+        return self.datapipe[index]
+
+    def __len__(self):
+        return len(self.datapipe)
+    
+    def __iter__(self) -> int:
+        for i in range(len(self)):
+            yield self[i]
+
+    def close(self):
+        pass
