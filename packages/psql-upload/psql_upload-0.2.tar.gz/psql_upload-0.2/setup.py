bac5a6623550d5a30dcea7cf428e5abd44e1834e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:04:39 +0000

    Add package psql-upload/0.2/psql_upload-0.2.tar.gz

---
+++ packages/psql-upload/psql_upload-0.2.tar.gz/psql_upload-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='psql-upload',  # The name of your package
+    version='0.2',       # Version number
+    packages=find_packages(),  # Automatically find all packages in your directory
+    install_requires=[     # List your package dependencies
+        'pandas',
+        'sqlalchemy',
+        'psycopg2',
+        'pymysql',
+    ],
+    author='Prabhav Sharma',   # Your name
+    author_email='prabhavs2004@gmail.com',  # Your email
+    description='A simple utility for uploading data from CSV to PostgreSQL',  # Short description
+    long_description=open('README.md').read(),  # Long description, typically from a README file
+    long_description_content_type='text/markdown',  # Format of your long description
+    url='https://github.com/Amalgamator04/sqlupload',  # URL to your project repo
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Python version compatibility
+)
