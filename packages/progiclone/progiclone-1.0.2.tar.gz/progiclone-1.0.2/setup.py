4374d218b16cbb1fa2760ce50afff5fecd478ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:05:04 +0000

    Add package progiclone/1.0.2/progiclone-1.0.2.tar.gz

---
+++ packages/progiclone/progiclone-1.0.2.tar.gz/progiclone-1.0.2/setup.py
@@ -0,0 +1,43 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="progiclone",
+    version="1.0.2",
+    author="VLTN x Progiseize",
+    author_email="v.denis@progiseize.fr",
+    description="Outil d'anonymisation des données sécurisé pour Dolibarr",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/valent1d/progiclone",
+    packages=find_packages(),
+    install_requires=[
+        # Listez vos dépendances ici
+        'pyfiglet',
+        'tqdm',
+        'Faker',
+        'mysql-connector-python',
+        'requests',
+        'shutil',
+        'time',
+        'os',
+        'getpass',
+        'sys',
+        'subprocess',
+        'logging',
+        'argparse',
+        'import_module',
+        ],
+    entry_points={
+        "console_scripts": [
+            "progiclone=progiclone.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
