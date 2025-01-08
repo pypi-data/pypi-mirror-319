5dded5aa183f1abb76aff8209580a7640b273de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:08:37 +0000

    Add package onkopus/0.2.8/onkopus-0.2.8.tar.gz

---
+++ packages/onkopus/onkopus-0.2.8.tar.gz/onkopus-0.2.8/tests/test_onkopus_clients/test_single_onkopus_clients/test_dgidb_gene_client.py
@@ -0,0 +1,17 @@
+import unittest, copy
+import onkopus as op
+
+
+class DGIDBAnnotationTestCase(unittest.TestCase):
+
+    def test_dbidb_client(self):
+        genome_version = 'hg38'
+
+        data = {"chr7:140753336A>T": {}, "chr1:2556710C>A": {},
+                "chr1:2556710C>T":{},"chr1:2556714A>G":{}, "chr1:2556718C>T":{}}
+
+        variant_data = op.UTAAdapterClient(
+            genome_version=genome_version).process_data(data)
+        variant_data = op.DGIdbClient(genome_version=genome_version).process_data(variant_data)
+
+        print("Response ",variant_data)
