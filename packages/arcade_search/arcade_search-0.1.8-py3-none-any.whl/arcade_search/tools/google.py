decab681ab33014efbccfaa182b948ef9777a1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 07:05:38 +0000

    Add package arcade_search/0.1.8/arcade_search-0.1.8-py3-none-any.whl

---
+++ packages/arcade_search/arcade_search-0.1.8-py3-none-any.whl/arcade_search/tools/google.py
@@ -0,0 +1,36 @@
+import json
+import os
+from typing import Annotated, Any, Optional
+
+import serpapi
+from arcade.sdk import tool
+
+
+@tool
+async def search_google(
+    query: Annotated[str, "Search query"],
+    n_results: Annotated[int, "Number of results to retrieve"] = 5,
+) -> str:
+    """Search Google using SerpAPI and return organic search results."""
+
+    api_key = get_secret("SERP_API_KEY")
+    if not api_key:
+        raise ValueError("SERP_API_KEY is not set")
+
+    client = serpapi.Client(api_key=api_key)
+    params = {"engine": "google", "q": query}
+
+    search = client.search(params)
+    results = search.as_dict()
+    organic_results = results.get("organic_results", [])
+
+    return json.dumps(organic_results[:n_results])
+
+
+def get_secret(name: str, default: Optional[Any] = None) -> Any:
+    secret = os.getenv(name)
+    if secret is None:
+        if default is not None:
+            return default
+        raise ValueError(f"Secret {name} is not set.")
+    return secret
