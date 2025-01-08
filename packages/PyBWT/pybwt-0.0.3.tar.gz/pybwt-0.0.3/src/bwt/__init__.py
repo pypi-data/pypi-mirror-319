921173eea11d8f2177720307d8dfb32415017a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:29 +0000

    Add package PyBWT/0.0.3/pybwt-0.0.3.tar.gz

---
+++ packages/PyBWT/pybwt-0.0.3.tar.gz/pybwt-0.0.3/src/bwt/__init__.py
@@ -0,0 +1,18 @@
+from .api_bwt import create, parse, verify, test
+
+__version__ = "0.0.x"
+
+__title__ = "PyBWT"
+__description__ = "Binary Web Token implementation in Python"
+
+__author__ = "nullptr"
+
+__license__ = "AGPLv3"
+
+
+__all__ = [
+    create,
+    parse,
+    verify,
+    test
+]
