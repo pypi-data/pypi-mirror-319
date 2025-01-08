c3fefa9d5e0b850ec2675323d330c2325f0ecc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:04:50 +0000

    Add package iris-grib/0.21.0/iris_grib-0.21.0-py3-none-any.whl

---
+++ packages/iris-grib/iris_grib-0.21.0-py3-none-any.whl/iris_grib/tests/unit/save_rules/test_set_fixed_surfaces.py
@@ -0,0 +1,229 @@
+# Copyright iris-grib contributors
+#
+# This file is part of iris-grib and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
+"""
+Unit tests for :func:`iris_grib._save_rules.set_fixed_surfaces`.
+
+"""
+
+# Import iris_grib.tests first so that some things can be initialised before
+# importing anything else.
+import iris_grib.tests as tests
+
+from unittest import mock
+
+import eccodes
+from eccodes import CODES_MISSING_LONG as GRIB_MISSING_LONG
+import numpy as np
+
+import iris.cube
+import iris.coords
+from iris.exceptions import TranslationError
+
+from iris_grib._save_rules import set_fixed_surfaces
+
+
+class Test(tests.IrisGribTest):
+    def test_bounded_altitude_feet(self):
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(
+            iris.coords.AuxCoord(
+                1500.0,
+                long_name="altitude",
+                units="ft",
+                bounds=np.array([1000.0, 2000.0]),
+            )
+        )
+        grib = eccodes.codes_grib_new_from_samples("GRIB2")
+        set_fixed_surfaces(cube, grib)
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfFirstFixedSurface"), 305.0
+        )  # precise ~304.8
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfSecondFixedSurface"), 610.0
+        )  # precise ~609.6
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfFirstFixedSurface"), 102)
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfSecondFixedSurface"), 102)
+
+    def test_theta_level(self):
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(
+            iris.coords.AuxCoord(
+                230.0,
+                standard_name="air_potential_temperature",
+                units="K",
+                attributes={"positive": "up"},
+                bounds=np.array([220.0, 240.0]),
+            )
+        )
+        grib = eccodes.codes_grib_new_from_samples("GRIB2")
+        set_fixed_surfaces(cube, grib)
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfFirstFixedSurface"), 220.0
+        )
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfSecondFixedSurface"), 240.0
+        )
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfFirstFixedSurface"), 107)
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfSecondFixedSurface"), 107)
+
+    def test_depth(self):
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(
+            iris.coords.AuxCoord(
+                1,
+                long_name="depth",
+                units="m",
+                bounds=np.array([0.0, 2]),
+                attributes={"positive": "down"},
+            )
+        )
+        grib = eccodes.codes_grib_new_from_samples("GRIB2")
+        set_fixed_surfaces(cube, grib)
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfFirstFixedSurface"), 0.0
+        )
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfSecondFixedSurface"), 2
+        )
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfFirstFixedSurface"), 106)
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfSecondFixedSurface"), 106)
+
+    @mock.patch.object(eccodes, "codes_set")
+    def test_altitude_point(self, mock_set):
+        grib = None
+        cube = iris.cube.Cube([1, 2, 3, 4, 5])
+        cube.add_aux_coord(iris.coords.AuxCoord([12345], "altitude", units="m"))
+
+        set_fixed_surfaces(cube, grib)
+
+        mock_set.assert_any_call(grib, "typeOfFirstFixedSurface", 102)
+        mock_set.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 12345)
+        mock_set.assert_any_call(grib, "typeOfSecondFixedSurface", 255)
+        mock_set.assert_any_call(
+            grib, "scaleFactorOfSecondFixedSurface", GRIB_MISSING_LONG
+        )
+        mock_set.assert_any_call(
+            grib, "scaledValueOfSecondFixedSurface", GRIB_MISSING_LONG
+        )
+
+    @mock.patch.object(eccodes, "codes_set")
+    def test_height_point(self, mock_set):
+        grib = None
+        cube = iris.cube.Cube([1, 2, 3, 4, 5])
+        cube.add_aux_coord(iris.coords.AuxCoord([12345], "height", units="m"))
+
+        set_fixed_surfaces(cube, grib)
+
+        mock_set.assert_any_call(grib, "typeOfFirstFixedSurface", 103)
+        mock_set.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 12345)
+        mock_set.assert_any_call(grib, "typeOfSecondFixedSurface", 255)
+        mock_set.assert_any_call(
+            grib, "scaleFactorOfSecondFixedSurface", GRIB_MISSING_LONG
+        )
+        mock_set.assert_any_call(
+            grib, "scaledValueOfSecondFixedSurface", GRIB_MISSING_LONG
+        )
+
+    def test_unknown_vertical_unbounded(self):
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(
+            iris.coords.AuxCoord([1], attributes={"GRIB_fixed_surface_type": 151})
+        )
+        grib = eccodes.codes_grib_new_from_samples("GRIB2")
+        set_fixed_surfaces(cube, grib)
+
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfFirstFixedSurface"), 151)
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfFirstFixedSurface"), 1
+        )
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaleFactorOfFirstFixedSurface"), 0
+        )
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfSecondFixedSurface"), 255)
+        self.assertEqual(
+            eccodes.codes_get_long(grib, "scaledValueOfSecondFixedSurface"),
+            GRIB_MISSING_LONG,
+        )
+        self.assertEqual(
+            eccodes.codes_get_long(grib, "scaleFactorOfSecondFixedSurface"),
+            GRIB_MISSING_LONG,
+        )
+
+    def test_unknown_vertical_bounded(self):
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(
+            iris.coords.AuxCoord(
+                [700],
+                bounds=np.array([900.0, 500.0]),
+                attributes={"GRIB_fixed_surface_type": 108},
+            )
+        )
+        grib = eccodes.codes_grib_new_from_samples("GRIB2")
+        set_fixed_surfaces(cube, grib)
+
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfFirstFixedSurface"), 108)
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaledValueOfFirstFixedSurface"), 900
+        )
+        self.assertEqual(
+            eccodes.codes_get_double(grib, "scaleFactorOfFirstFixedSurface"), 0
+        )
+        self.assertEqual(eccodes.codes_get_long(grib, "typeOfSecondFixedSurface"), 108)
+        self.assertEqual(
+            eccodes.codes_get_long(grib, "scaledValueOfSecondFixedSurface"), 500
+        )
+        self.assertEqual(
+            eccodes.codes_get_long(grib, "scaleFactorOfSecondFixedSurface"), 0
+        )
+
+    def test_multiple_unknown_vertical_coords(self):
+        grib = None
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(
+            iris.coords.AuxCoord([1], attributes={"GRIB_fixed_surface_type": 151})
+        )
+        cube.add_aux_coord(
+            iris.coords.AuxCoord(
+                [450],
+                bounds=np.array([900.0, 0.0]),
+                attributes={"GRIB_fixed_surface_type": 108},
+            )
+        )
+        msg = r"coordinates were found of fixed surface type: \[151, 108\]"
+        with self.assertRaisesRegex(ValueError, msg):
+            set_fixed_surfaces(cube, grib)
+
+    def test_unhandled_vertical_axis(self):
+        grib = None
+        cube = iris.cube.Cube([0])
+        cube.add_aux_coord(iris.coords.AuxCoord([450], attributes={"positive": "up"}))
+        msg = (
+            r"vertical-axis coordinate\(s\) \('unknown'\) are not "
+            "recognised or handled."
+        )
+        with self.assertRaisesRegex(TranslationError, msg):
+            set_fixed_surfaces(cube, grib)
+
+    @mock.patch.object(eccodes, "codes_set")
+    def test_no_vertical(self, mock_set):
+        grib = None
+        cube = iris.cube.Cube([1, 2, 3, 4, 5])
+        set_fixed_surfaces(cube, grib)
+        mock_set.assert_any_call(grib, "typeOfFirstFixedSurface", 1)
+        mock_set.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "typeOfSecondFixedSurface", 255)
+        mock_set.assert_any_call(
+            grib, "scaleFactorOfSecondFixedSurface", GRIB_MISSING_LONG
+        )
+        mock_set.assert_any_call(
+            grib, "scaledValueOfSecondFixedSurface", GRIB_MISSING_LONG
+        )
+
+
+if __name__ == "__main__":
+    tests.main()
---
+++ packages/iris-grib/iris_grib-0.21.0-py3-none-any.whl/iris_grib/tests/unit/test_GribWrapper.py
@@ -0,0 +1,129 @@
+# Copyright iris-grib contributors
+#
+# This file is part of iris-grib and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
+"""
+Unit tests for the `iris_grib.GribWrapper` class.
+
+"""
+
+# Import iris_grib.tests first so that some things can be initialised before
+# importing anything else.
+import iris_grib.tests as tests
+
+import numpy as np
+from unittest import mock
+
+from iris._lazy_data import is_lazy_data
+from iris.exceptions import TranslationError
+
+from iris_grib import GribWrapper, _load_generate
+
+
+_offset = 0
+_message_length = 1000
+
+
+def _mock_codes_get_long(grib_message, key):
+    lookup = dict(
+        totalLength=_message_length,
+        numberOfValues=200,
+        jPointsAreConsecutive=0,
+        Ni=20,
+        Nj=10,
+        edition=1,
+    )
+    try:
+        result = lookup[key]
+    except KeyError:
+        msg = "Mock codes_get_long unknown key: {!r}".format(key)
+        raise AttributeError(msg)
+    return result
+
+
+def _mock_codes_get_string(grib_message, key):
+    return grib_message
+
+
+def _mock_codes_get_native_type(grib_message, key):
+    result = int
+    if key == "gridType":
+        result = str
+    return result
+
+
+def _mock_codes_get_message_offset(grib_message):
+    return _offset
+
+
+class Test_edition(tests.IrisGribTest):
+    def setUp(self):
+        self.patch("iris_grib.GribWrapper._confirm_in_scope")
+        self.patch("iris_grib.GribWrapper._compute_extra_keys")
+        self.patch("eccodes.codes_get_long", _mock_codes_get_long)
+        self.patch("eccodes.codes_get_string", _mock_codes_get_string)
+        self.patch("eccodes.codes_get_native_type", _mock_codes_get_native_type)
+        self.patch("eccodes.codes_get_message_offset", _mock_codes_get_message_offset)
+
+    def test_not_edition_1(self):
+        def func(grib_message, key):
+            return 2
+
+        emsg = "GRIB edition 2 is not supported by 'GribWrapper'"
+        with mock.patch("eccodes.codes_get_long", func):
+            with self.assertRaisesRegex(TranslationError, emsg):
+                GribWrapper(None)
+
+    def test_edition_1(self):
+        grib_message = "regular_ll"
+        grib_fh = mock.Mock()
+        wrapper = GribWrapper(grib_message, grib_fh)
+        self.assertEqual(wrapper.grib_message, grib_message)
+
+
+@tests.skip_data
+class Test_deferred_data(tests.IrisTest):
+    def test_regular_data(self):
+        filename = tests.get_data_path(("GRIB", "gaussian", "regular_gg.grib1"))
+        messages = list(_load_generate(filename))
+        self.assertTrue(is_lazy_data(messages[0]._data))
+
+    def test_reduced_data(self):
+        filename = tests.get_data_path(("GRIB", "reduced", "reduced_ll.grib1"))
+        messages = list(_load_generate(filename))
+        self.assertTrue(is_lazy_data(messages[0]._data))
+
+
+class Test_deferred_proxy_args(tests.IrisTest):
+    def setUp(self):
+        self.patch("iris_grib.GribWrapper._confirm_in_scope")
+        self.patch("iris_grib.GribWrapper._compute_extra_keys")
+        self.patch("eccodes.codes_get_long", _mock_codes_get_long)
+        self.patch("eccodes.codes_get_string", _mock_codes_get_string)
+        self.patch("eccodes.codes_get_native_type", _mock_codes_get_native_type)
+        self.patch("eccodes.codes_get_message_offset", _mock_codes_get_message_offset)
+        self.expected = np.atleast_1d(_offset)
+        self.grib_fh = mock.Mock()
+        self.dtype = np.float64
+        self.path = self.grib_fh.name
+        self.lookup = _mock_codes_get_long
+
+    def test_regular_proxy_args(self):
+        grib_message = "regular_ll"
+        shape = (self.lookup(grib_message, "Nj"), self.lookup(grib_message, "Ni"))
+        for offset in self.expected:
+            with mock.patch("iris_grib.GribDataProxy") as mock_gdp:
+                _ = GribWrapper(grib_message, self.grib_fh)
+            mock_gdp.assert_called_once_with(shape, self.dtype, self.path, offset)
+
+    def test_reduced_proxy_args(self):
+        grib_message = "reduced_gg"
+        shape = self.lookup(grib_message, "numberOfValues")
+        for offset in self.expected:
+            with mock.patch("iris_grib.GribDataProxy") as mock_gdp:
+                _ = GribWrapper(grib_message, self.grib_fh)
+            mock_gdp.assert_called_once_with((shape,), self.dtype, self.path, offset)
+
+
+if __name__ == "__main__":
+    tests.main()
