fbf824b3d5ba03809f4f6c5b1f35471d083ab497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:47 +0000

    Add package myuse/0.1/myuse-0.1.tar.gz

---
+++ packages/myuse/myuse-0.1.tar.gz/myuse-0.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="myuse",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="koteswarao_s",
+    author_email="kotisuraboin1a@gmail.com",
+    description="A sample package",
+    url="https://github.com/yourusername/mypackage",
+)
