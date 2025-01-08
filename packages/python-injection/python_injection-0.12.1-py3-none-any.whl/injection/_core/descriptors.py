6451e8f6ca86c502c8dea99c958c918dad0cef6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:43 +0000

    Add package python-injection/0.12.1/python_injection-0.12.1-py3-none-any.whl

---
+++ packages/python-injection/python_injection-0.12.1-py3-none-any.whl/injection/_core/descriptors.py
@@ -0,0 +1,25 @@
+from typing import Self
+
+from injection._core.common.invertible import Invertible
+from injection._core.common.type import InputType
+from injection._core.module import Module, mod
+
+
+class LazyInstance[T]:
+    __slots__ = ("__value",)
+
+    __value: Invertible[T]
+
+    def __init__(self, cls: InputType[T], module: Module | None = None) -> None:
+        module = module or mod()
+        self.__value = module.get_lazy_instance(cls, default=NotImplemented)
+
+    def __get__(
+        self,
+        instance: object | None = None,
+        owner: type | None = None,
+    ) -> Self | T:
+        if instance is None:
+            return self
+
+        return ~self.__value
