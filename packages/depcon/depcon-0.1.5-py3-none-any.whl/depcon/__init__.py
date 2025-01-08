9c0807b58ee5078d9b338641e613ecdaa493289f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:52 +0000

    Add package depcon/0.1.5/depcon-0.1.5-py3-none-any.whl

---
+++ packages/depcon/depcon-0.1.5-py3-none-any.whl/depcon/__init__.py
@@ -0,0 +1,6 @@
+"""Package for converting requirements files to pyproject.toml format.
+
+This module imports the main entry point and version from the .main submodule.
+"""
+
+from .main import __version__, main
