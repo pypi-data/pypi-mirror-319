95a9640c0ff2c754ea3ed47e3d263d8ab2e8dbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:48 +0000

    Add package mtmaisdk/0.5.56/mtmaisdk-0.5.56-py2.py3-none-any.whl

---
+++ packages/mtmaisdk/mtmaisdk-0.5.56-py2.py3-none-any.whl/mtmaisdk/clients/rest/api/__init__.py
@@ -0,0 +1,38 @@
+# flake8: noqa
+
+# import apis into api package
+from mtmaisdk.clients.rest.api.api_token_api import APITokenApi
+from mtmaisdk.clients.rest.api.event_api import EventApi
+from mtmaisdk.clients.rest.api.github_api import GithubApi
+from mtmaisdk.clients.rest.api.healthcheck_api import HealthcheckApi
+from mtmaisdk.clients.rest.api.log_api import LogApi
+from mtmaisdk.clients.rest.api.metadata_api import MetadataApi
+from mtmaisdk.clients.rest.api.rate_limits_api import RateLimitsApi
+from mtmaisdk.clients.rest.api.sns_api import SNSApi
+from mtmaisdk.clients.rest.api.slack_api import SlackApi
+from mtmaisdk.clients.rest.api.step_run_api import StepRunApi
+from mtmaisdk.clients.rest.api.tenant_api import TenantApi
+from mtmaisdk.clients.rest.api.user_api import UserApi
+from mtmaisdk.clients.rest.api.worker_api import WorkerApi
+from mtmaisdk.clients.rest.api.workflow_api import WorkflowApi
+from mtmaisdk.clients.rest.api.workflow_run_api import WorkflowRunApi
+from mtmaisdk.clients.rest.api.admin_api import AdminApi
+from mtmaisdk.clients.rest.api.agent_api import AgentApi
+from mtmaisdk.clients.rest.api.artifact_api import ArtifactApi
+from mtmaisdk.clients.rest.api.assisant_api import AssisantApi
+from mtmaisdk.clients.rest.api.blog_api import BlogApi
+from mtmaisdk.clients.rest.api.chat_api import ChatApi
+from mtmaisdk.clients.rest.api.default_api import DefaultApi
+from mtmaisdk.clients.rest.api.doc_api import DocApi
+from mtmaisdk.clients.rest.api.frontend_api import FrontendApi
+from mtmaisdk.clients.rest.api.llm_api import LlmApi
+from mtmaisdk.clients.rest.api.mtmai_api import MtmaiApi
+from mtmaisdk.clients.rest.api.post_api import PostApi
+from mtmaisdk.clients.rest.api.posts_api import PostsApi
+from mtmaisdk.clients.rest.api.prompt_api import PromptApi
+from mtmaisdk.clients.rest.api.site_api import SiteApi
+from mtmaisdk.clients.rest.api.subscribe_api import SubscribeApi
+from mtmaisdk.clients.rest.api.tool_api import ToolApi
+from mtmaisdk.clients.rest.api.tools_api import ToolsApi
+from mtmaisdk.clients.rest.api.websearch_api import WebsearchApi
+
