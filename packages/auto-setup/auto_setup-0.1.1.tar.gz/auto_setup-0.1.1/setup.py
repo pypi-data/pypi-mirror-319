87af1f5c33f79574758800619d3e2061ce97bc02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:08:55 +0000

    Add package auto-setup/0.1.1/auto_setup-0.1.1.tar.gz

---
+++ packages/auto-setup/auto_setup-0.1.1.tar.gz/auto_setup-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auto_setup",
+    version="0.1.1",
+    author="Bekzod G'ulomov",
+    author_email="pterest160@gmail.com",
+    description="A Python package for creating folders, files, and initializing configurations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mypackage",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "fastapi", "typer", "sqlalchemy", "pydantic", "python-dotenv", "pytest", "alembic"
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "bootstrap=auto_setup.main:main",  # Path to your command
+        ],
+    },
+)
