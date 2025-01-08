e69fcb4b13946b3db35e0d62720ea5491068e275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:05:07 +0000

    Add package elsciRL/0.1.42/elscirl-0.1.42.tar.gz

---
+++ packages/elsciRL/elscirl-0.1.42.tar.gz/elscirl-0.1.42/setup.py
@@ -0,0 +1,49 @@
+from distutils.core import setup
+
+setup(
+    name='elsciRL',
+    version='0.1.42',
+    packages=[
+        'elsciRL', 
+        'elsciRL.adapters',
+        'elsciRL.agents',
+        'elsciRL.agents.stable_baselines',
+        'elsciRL.analysis',
+        'elsciRL.encoders', 
+        'elsciRL.environment_setup',
+        'elsciRL.evaluation',
+        'elsciRL.examples',
+        'elsciRL.examples.adapters',
+        'elsciRL.examples.environments',
+        'elsciRL.examples.local_configs',
+        'elsciRL.examples.WebApp',
+        'elsciRL.examples.WebApp.prerender',
+        'elsciRL.examples.WebApp.static',
+        'elsciRL.examples.WebApp.templates',
+        'elsciRL.examples.WebApp.prerender.Standard_Experiment',
+        'elsciRL.experiments',
+        'elsciRL.instruction_following',
+        'elsciRL.interaction_loops',
+        'elsciRL.benchmarking_suite',
+        ],
+    # TODO: Add benchmark to exclusion of wheel
+    url='https://github.com/pdfosborne/elsciRL',
+    license='Apache-2.0 license',
+    author='Philip Osborne',
+    author_email='pdfosborne@gmail.com',
+    description='Applying the elsciRL architecture to Reinforcement Learning problems.',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scipy>=1.10.1',
+        'torch',
+        'tqdm',
+        'httpimport',
+        'sentence-transformers',
+        'gymnasium',
+        'stable-baselines3'
+
+    ] 
+)
