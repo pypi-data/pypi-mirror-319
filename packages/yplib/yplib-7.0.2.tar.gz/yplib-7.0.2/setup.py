9980c85d6436db75464d7c547e64ebe27a32a493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:04:46 +0000

    Add package yplib/7.0.2/yplib-7.0.2.tar.gz

---
+++ packages/yplib/yplib-7.0.2.tar.gz/yplib-7.0.2/setup.py
@@ -0,0 +1,57 @@
+import setuptools
+import os
+import shutil
+from setuptools import Command
+import subprocess
+import re
+import requests
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+class CleanCommand(Command):
+    """自定义清理命令，删除 dist/ 目录"""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if os.path.exists('dist'):
+            shutil.rmtree('dist')
+        if os.path.exists('yplib.egg-info'):
+            shutil.rmtree('yplib.egg-info')
+
+
+setuptools.setup(
+    name="yplib",
+    version='7.0.2',
+    author="yangpu",
+    author_email="wantwaterfish@gmail.com",
+    description="util",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "openpyxl>=3.1.2",
+        "xlrd>=2.0.1",
+        "bs4>=0.0.2",
+        "requests>=2.32.3",
+        "PyMySQL>=1.1.0",
+        "pyarrow>=16.0.0",
+        "pandas>=2.2.2"
+    ],
+    cmdclass={
+        'clean': CleanCommand,  # 注册 clean 命令
+    }
+)
