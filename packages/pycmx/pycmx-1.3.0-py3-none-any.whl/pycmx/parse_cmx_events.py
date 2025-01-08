f1dbc688f2360996d3925293e3134955a42e764b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:51 +0000

    Add package pycmx/1.3.0/pycmx-1.3.0-py3-none-any.whl

---
+++ packages/pycmx/pycmx-1.3.0-py3-none-any.whl/pycmx/parse_cmx_events.py
@@ -0,0 +1,20 @@
+# pycmx
+# (c) 2018 Jamie Hardt
+
+# from collections import namedtuple
+
+from .parse_cmx_statements import (parse_cmx3600_statements)
+from .edit_list import EditList
+
+from typing import TextIO
+
+
+def parse_cmx3600(f: TextIO) -> EditList:
+    """
+    Parse a CMX 3600 EDL.
+
+    :param TextIO f: a file-like object, an opened CMX 3600 .EDL file.
+    :returns: An :class:`pycmx.edit_list.EditList`.
+    """
+    statements = parse_cmx3600_statements(f)
+    return EditList(statements)
