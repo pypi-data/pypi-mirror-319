ad4552f399ec643e6232e4eaa7064d4ecbe74e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:05:03 +0000

    Add package predictram-stock-data/1.0.3/predictram_stock_data-1.0.3-py3-none-any.whl

---
+++ packages/predictram-stock-data/predictram_stock_data-1.0.3-py3-none-any.whl/predictram_stock_data/config.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+IIP_FILE_PATH = os.path.join(BASE_DIR, "./data/IIP_data_Nov_2024.xlsx")
+STOCKS_DIRECTORY = os.path.join(BASE_DIR, "./data/Stock_Price/")
