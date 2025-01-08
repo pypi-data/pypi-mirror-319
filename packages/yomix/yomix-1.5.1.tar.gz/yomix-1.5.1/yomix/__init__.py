e8ded8d106929ea9f4a6a503c1ffe9ecc36bfcbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:23 +0000

    Add package yomix/1.5.1/yomix-1.5.1.tar.gz

---
+++ packages/yomix/yomix-1.5.1.tar.gz/yomix-1.5.1/yomix/__init__.py
@@ -0,0 +1,7 @@
+from . import plotting, tools
+import sys
+from pathlib import Path
+
+__version__ = Path(__file__).with_name("VERSION").read_text().strip()
+
+sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["plotting", "tools"]})
