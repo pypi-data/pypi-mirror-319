8cfa9717986b1a24456bedf955f8d29bd0420afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:05:28 +0000

    Add package acellera-atm/0.0.11/acellera_atm-0.0.11.tar.gz

---
+++ packages/acellera-atm/acellera_atm-0.0.11.tar.gz/acellera_atm-0.0.11/tests/test_atm.py
@@ -0,0 +1,25 @@
+import shutil
+import os
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def _test_structprep(tmp_path):
+    from atm.rbfe_structprep import rbfe_structprep
+
+    shutil.copytree(
+        os.path.join(curr_dir, "QB_A08_A07"), os.path.join(tmp_path, "QB_A08_A07")
+    )
+    rbfe_structprep(os.path.join(tmp_path, "QB_A08_A07", "QB_A08_A07_asyncre.yaml"))
+
+
+def _test_explicit_sync(tmp_path):
+    from atm.rbfe_production import rbfe_production
+
+    shutil.copytree(
+        os.path.join(curr_dir, "QB_A08_A07_completed"),
+        os.path.join(tmp_path, "QB_A08_A07_completed"),
+    )
+    rbfe_production(
+        os.path.join(tmp_path, "QB_A08_A07_completed", "QB_A08_A07_asyncre.yaml")
+    )
