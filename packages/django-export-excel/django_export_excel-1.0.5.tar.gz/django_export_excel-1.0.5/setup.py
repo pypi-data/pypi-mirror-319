d6e02df3f236dc86c62bc8dbb1422b4504dd3ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 03:07:52 +0000

    Add package django-export-excel/1.0.5/django_export_excel-1.0.5.tar.gz

---
+++ packages/django-export-excel/django_export_excel-1.0.5.tar.gz/django_export_excel-1.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os import path
+
+# Read the contents of requirements.txt
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
+setup(
+    name='django_export_excel',
+    version='1.0.5',
+    description='Django package for exporting data to Excel file with included admin integration',
+    author='Azizbek Xushnazarov',
+    author_email='azikdevapps@gmail.com',
+    url='https://github.com/AzikDeveloper/django-export-excel',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
