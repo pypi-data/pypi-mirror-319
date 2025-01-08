f2eb60c45c70bff42bad2df762a11d4980c232a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 07:06:20 +0000

    Add package lute3/3.9.1/lute3-3.9.1-py3-none-any.whl

---
+++ packages/lute3/lute3-3.9.1-py3-none-any.whl/lute/book/forms.py
@@ -0,0 +1,157 @@
+"""
+Book create/edit forms.
+"""
+
+import json
+from flask import request
+from wtforms import StringField, SelectField, TextAreaField, IntegerField, HiddenField
+from wtforms import ValidationError
+from wtforms.validators import DataRequired, Length, NumberRange
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from lute.book.service import Service
+
+
+class NewBookForm(FlaskForm):
+    """
+    New book.  All fields can be entered.
+    """
+
+    language_id = SelectField("Language", coerce=int)
+
+    title = StringField("Title", validators=[DataRequired(), Length(max=255)])
+
+    desc = (
+        "Use for short texts, e.g. up to a few thousand words. "
+        + 'For longer texts, use the "Text file" below.'
+    )
+    text = TextAreaField("Text", description=desc)
+    textfile = FileField(
+        "Text file",
+        validators=[
+            FileAllowed(
+                ["txt", "epub", "pdf", "srt", "vtt"],
+                "Please upload a valid text (txt, epub, pdf, srt, vtt)",
+            )
+        ],
+    )
+    max_page_tokens = IntegerField(
+        "Words per page",
+        validators=[NumberRange(min=10, max=1500)],
+        default=250,
+    )
+    source_uri = StringField("Text source", validators=[Length(max=1000)])
+    audiofile = FileField(
+        "Audio file",
+        validators=[
+            FileAllowed(
+                ["mp3", "m4a", "wav", "ogg", "opus"],
+                "Please upload a valid audio file (mp3, m4a, wav, ogg, opus)",
+            )
+        ],
+    )
+    book_tags = StringField("Tags")
+
+    def __init__(self, *args, **kwargs):
+        "Call the constructor of the superclass (FlaskForm)"
+        super().__init__(*args, **kwargs)
+        book = kwargs.get("obj")
+
+        def _data(arr):
+            "Get data in proper format for tagify."
+            return json.dumps([{"value": p} for p in arr])
+
+        self.book_tags.data = _data(book.book_tags)
+        if request.method == "POST":
+            self.book_tags.data = request.form.get("book_tags", "")
+
+    def populate_obj(self, obj):
+        "Call the populate_obj method from the parent class, then mine."
+        super().populate_obj(obj)
+
+        def _values(field_data):
+            "Convert field data to array."
+            ret = []
+            if field_data:
+                ret = [h["value"] for h in json.loads(field_data)]
+            return ret
+
+        obj.book_tags = _values(self.book_tags.data)
+
+        service = Service()
+        if self.textfile.data:
+            obj.text = service.get_file_content(self.textfile.data)
+        f = self.audiofile.data
+        if f:
+            obj.audio_filename = service.save_audio_file(f)
+
+    def validate_language_id(self, field):  # pylint: disable=unused-argument
+        "Language must be set."
+        if self.language_id.data in (None, 0):
+            raise ValidationError("Please select a language")
+
+    def validate_text(self, field):  # pylint: disable=unused-argument
+        "Throw if missing text and textfile, or if have both."
+        have_text = self.text.data not in ("", None)
+        have_textfile = self.textfile.data not in ("", None)
+        if have_text and have_textfile:
+            raise ValidationError(
+                "Both Text and Text file are set, please only specify one"
+            )
+        if have_text is False and have_textfile is False:
+            raise ValidationError("Please specify either Text or Text file")
+
+
+class EditBookForm(FlaskForm):
+    """
+    Edit existing book.  Only a few fields can be changed.
+    """
+
+    title = StringField("Title", validators=[DataRequired(), Length(max=255)])
+    source_uri = StringField("Source URI", validators=[Length(max=1000)])
+    book_tags = StringField("Tags")
+    audiofile = FileField(
+        "Audio file",
+        validators=[
+            FileAllowed(
+                ["mp3", "wav", "ogg", "opus"],
+                "Please upload a valid audio file (mp3, wav, ogg, opus)",
+            )
+        ],
+    )
+
+    # The current audio_filename can be removed from the current book.
+    audio_filename = HiddenField("Audio filename")
+
+    def __init__(self, *args, **kwargs):
+        "Call the constructor of the superclass (FlaskForm)"
+        super().__init__(*args, **kwargs)
+        book = kwargs.get("obj")
+
+        def _data(arr):
+            "Get data in proper format for tagify."
+            return json.dumps([{"value": p} for p in arr])
+
+        self.book_tags.data = _data(book.book_tags)
+        if request.method == "POST":
+            self.book_tags.data = request.form.get("book_tags", "")
+
+    def populate_obj(self, obj):
+        "Call the populate_obj method from the parent class, then mine."
+        super().populate_obj(obj)
+
+        def _values(field_data):
+            "Convert field data to array."
+            ret = []
+            if field_data:
+                ret = [h["value"] for h in json.loads(field_data)]
+            return ret
+
+        obj.book_tags = _values(self.book_tags.data)
+
+        f = self.audiofile.data
+        service = Service()
+        if f:
+            obj.audio_filename = service.save_audio_file(f)
+            obj.audio_bookmarks = None
+            obj.audio_current_pos = None
---
+++ packages/lute3/lute3-3.9.1-py3-none-any.whl/lute/book/model.py
@@ -0,0 +1,142 @@
+"""
+Book domain objects.
+"""
+
+from lute.models.book import Book as DBBook, BookTag
+from lute.models.repositories import (
+    BookRepository,
+    BookTagRepository,
+    LanguageRepository,
+)
+
+
+class Book:  # pylint: disable=too-many-instance-attributes
+    """
+    A book domain object, to create/edit lute.models.book.Books.
+
+    Book language can be specified either by language_id, or
+    language_name.  language_name is useful for loading books via
+    scripts/api.  language_id takes precedence.
+    """
+
+    def __init__(self):
+        self.id = None
+        self.language_id = None
+        self.language_name = None
+        self.title = None
+        self.text = None
+        self.max_page_tokens = 250
+        self.source_uri = None
+        self.audio_filename = None
+        self.audio_current_pos = None
+        self.audio_bookmarks = None
+        self.book_tags = []
+
+    def __repr__(self):
+        return f"<Book (id={self.id}, title='{self.title}')>"
+
+    def add_tag(self, tag):
+        self.book_tags.append(tag)
+
+
+class Repository:
+    """
+    Maps Book BO to and from lute.model.Book.
+    """
+
+    def __init__(self, _session):
+        self.session = _session
+        self.book_repo = BookRepository(self.session)
+
+    def load(self, book_id):
+        "Loads a Book business object for the DBBook."
+        dbb = self.book_repo.find(book_id)
+        if dbb is None:
+            raise ValueError(f"No book with id {book_id} found")
+        return self._build_business_book(dbb)
+
+    def find_by_title(self, book_title, language_id):
+        "Loads a Book business object for the book with a given title."
+        dbb = self.book_repo.find_by_title(book_title, language_id)
+        if dbb is None:
+            return None
+        return self._build_business_book(dbb)
+
+    def get_book_tags(self):
+        "Get all available book tags, helper method."
+        bts = self.session.query(BookTag).all()
+        return sorted([t.text for t in bts])
+
+    def add(self, book):
+        """
+        Add a book to be saved to the db session.
+        Returns DBBook for tests and verification only,
+        clients should not change it.
+        """
+        dbbook = self._build_db_book(book)
+        self.session.add(dbbook)
+        return dbbook
+
+    def delete(self, book):
+        """
+        Delete.
+        """
+        if book.id is None:
+            raise ValueError(f"book {book.title} not saved")
+        b = self.book_repo.find(book.id)
+        self.session.delete(b)
+
+    def commit(self):
+        """
+        Commit everything.
+        """
+        self.session.commit()
+
+    def _build_db_book(self, book):
+        "Convert a book business object to a DBBook."
+
+        lang_repo = LanguageRepository(self.session)
+        lang = None
+        if book.language_id:
+            lang = lang_repo.find(book.language_id)
+        elif book.language_name:
+            lang = lang_repo.find_by_name(book.language_name)
+        if lang is None:
+            msg = f"No language matching id={book.language_id} or name={book.language_name}"
+            raise RuntimeError(msg)
+
+        b = None
+        if book.id is None:
+            b = DBBook.create_book(book.title, lang, book.text, book.max_page_tokens)
+        else:
+            b = self.book_repo.find(book.id)
+        b.title = book.title
+        b.source_uri = book.source_uri
+        b.audio_filename = book.audio_filename
+        b.audio_current_pos = book.audio_current_pos
+        b.audio_bookmarks = book.audio_bookmarks
+
+        btr = BookTagRepository(self.session)
+        booktags = []
+        for s in book.book_tags:
+            booktags.append(btr.find_or_create_by_text(s))
+        b.remove_all_book_tags()
+        for tt in booktags:
+            b.add_book_tag(tt)
+
+        return b
+
+    def _build_business_book(self, dbbook):
+        "Convert db book to Book."
+        b = Book()
+        b.id = dbbook.id
+        b.language_id = dbbook.language.id
+        b.language_name = dbbook.language.name
+        b.title = dbbook.title
+        b.text = None  # Not returning this for now
+        b.source_uri = dbbook.source_uri
+        b.audio_filename = dbbook.audio_filename
+        b.audio_current_pos = dbbook.audio_current_pos
+        b.audio_bookmarks = dbbook.audio_bookmarks
+        b.book_tags = [t.text for t in dbbook.book_tags]
+        return b
