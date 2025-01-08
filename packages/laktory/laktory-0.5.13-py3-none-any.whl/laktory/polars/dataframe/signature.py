fa303f6d0bdbdc4d5e4f4be8d0bff5b7a49a3c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:59 +0000

    Add package laktory/0.5.13/laktory-0.5.13-py3-none-any.whl

---
+++ packages/laktory/laktory-0.5.13-py3-none-any.whl/laktory/polars/dataframe/signature.py
@@ -0,0 +1,31 @@
+import polars as pl
+
+
+def signature(df: pl.DataFrame) -> str:
+    """
+    Returns DataFrame signature.
+
+    Parameters
+    ----------
+    df:
+        Input DataFrame
+
+    Returns
+    -------
+    :
+        Result
+
+    ```py
+    import laktory  # noqa: F401
+    import polars as pl
+
+    df = pl.DataFrame({"x": [1, 2, 3], "label": ["a", "b", "c"]})
+    print(df.laktory.signature())
+    # > DataFrame[x: Int64, label: String]
+    ```
+
+    """
+
+    s = ", ".join([f"{k}: {v}" for k, v in df.schema.items()])
+    s = f"DataFrame[{s}]"
+    return s
