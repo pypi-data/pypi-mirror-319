22b3189eca65067814491bf443faa79fbb4f2a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:37 +0000

    Add package pydantic-gubbins/1.0.4/pydantic_gubbins-1.0.4-py3-none-any.whl

---
+++ packages/pydantic-gubbins/pydantic_gubbins-1.0.4-py3-none-any.whl/pydantic_gubbins/typing/__init__.py
@@ -0,0 +1,9 @@
+from .union import DiscriminatedUnion, FrozenDict, SubclassOf, Union, get_type_name
+
+__all__ = (
+    DiscriminatedUnion,
+    FrozenDict,
+    SubclassOf,
+    Union,
+    get_type_name
+)
