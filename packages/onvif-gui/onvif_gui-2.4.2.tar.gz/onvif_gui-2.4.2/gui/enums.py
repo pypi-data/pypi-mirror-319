b63ec0a403bc954215fb13e10b7195eb7e08aa42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:20 +0000

    Add package onvif-gui/2.4.2/onvif_gui-2.4.2.tar.gz

---
+++ packages/onvif-gui/onvif_gui-2.4.2.tar.gz/onvif_gui-2.4.2/gui/enums.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class ProxyType(Enum):
+    STAND_ALONE = 0
+    SERVER = 1
+    CLIENT = 2
+
+class StreamState(Enum):
+    IDLE = 0
+    CONNECTING = 1
+    CONNECTED = 2
+    INVALID = 3
+
+class MediaSource(Enum):
+    CAMERA = 0
+    FILE = 1
+    
+class Style(Enum):
+    DARK = 0
+    LIGHT = 1
