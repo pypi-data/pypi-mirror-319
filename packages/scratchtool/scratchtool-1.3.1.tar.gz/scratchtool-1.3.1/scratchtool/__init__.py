19e0258c665c414bd3a6df18f91724829b9351ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:51 +0000

    Add package scratchtool/1.3.1/scratchtool-1.3.1.tar.gz

---
+++ packages/scratchtool/scratchtool-1.3.1.tar.gz/scratchtool-1.3.1/scratchtool/__init__.py
@@ -0,0 +1,3 @@
+from ._user_class import user
+from ._project_class import project
+from ._login_class import login
---
+++ packages/scratchtool/scratchtool-1.3.1.tar.gz/scratchtool-1.3.1/scratchtool/_login_class.py
@@ -0,0 +1,9 @@
+from ._cloud_class import _cloud
+
+class login():
+    def __init__(self,username:str,password:str):
+        self.username = username
+        self.password = password
+        
+    def connect_cloud_tw(self,projectID):
+        return _cloud(projectID,self.username,self.password)
