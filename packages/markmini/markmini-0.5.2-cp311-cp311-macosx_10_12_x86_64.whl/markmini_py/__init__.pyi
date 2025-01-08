057a5809851e858f1abfcc856d3aefcbbf9724dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:59 +0000

    Add package markmini/0.5.2/markmini-0.5.2-cp311-cp311-macosx_10_12_x86_64.whl

---
+++ packages/markmini/markmini-0.5.2-cp311-cp311-macosx_10_12_x86_64.whl/markmini_py/__init__.pyi
@@ -0,0 +1,10 @@
+class Markmini:
+    def __init__(self) -> None: ...
+    def compile(self, input: str) -> str: ...
+    def add_users(
+        self,
+        usernames: list[str],
+        user_ids: list[str],
+        links: list[str],
+        fullnames: list[str],
+    ) -> None: ...
