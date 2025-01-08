83dc50ee2bbfa18fa80fc7e51cef7c103167d745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:50 +0000

    Add package uiwiz/0.1.61/uiwiz-0.1.61-py3-none-any.whl

---
+++ packages/uiwiz/uiwiz-0.1.61-py3-none-any.whl/uiwiz/shared.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from typing import Callable, Dict
+
+resources: Dict[str, Path] = {}
+page_map: Dict[Callable, str] = {}
+
+
+def register_resource(key: str, resource: Path):
+    resources[key] = resource
+
+
+def register_path(key: str, func: Callable):
+    page_map[func] = key
+
+
+def reset_resources():
+    resources.clear()
+    page_map.clear()
+
+
+def route_exists(path: str) -> bool:
+    return path in list(page_map.values())
