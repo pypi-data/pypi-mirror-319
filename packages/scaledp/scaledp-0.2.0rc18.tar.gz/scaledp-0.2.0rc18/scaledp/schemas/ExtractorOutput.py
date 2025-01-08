40fc1b7f6c6397740876900c93368ae10d74a4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 04:04:55 +0000

    Add package scaledp/0.2.0rc18/scaledp-0.2.0rc18.tar.gz

---
+++ packages/scaledp/scaledp-0.2.0rc18.tar.gz/scaledp-0.2.0rc18/scaledp/schemas/ExtractorOutput.py
@@ -0,0 +1,18 @@
+# Description: Schema Detector Output
+from scaledp.utils.dataclass import map_dataclass_to_struct, register_type
+from dataclasses import dataclass
+
+
+@dataclass(order=True)
+class ExtractorOutput:
+    path: str
+    data: str
+    type: str
+    exception: str = ""
+
+    @staticmethod
+    def get_schema():
+        return map_dataclass_to_struct(ExtractorOutput)
+
+
+register_type(ExtractorOutput, ExtractorOutput.get_schema)
