5951773caf88f415f748750fde80c0b378daa00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:46 +0000

    Add package ggb-distribution/1.9/ggb_distribution-1.9.tar.gz

---
+++ packages/ggb-distribution/ggb_distribution-1.9.tar.gz/ggb_distribution-1.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ggb_distribution',
+      version='1.9',
+      license='MIT',
+      long_description='Gaussian and Binomial distributions',
+      packages=['ggb_distribution'],
+      author = 'Rahul Unnikrishnan',
+      zip_safe=False)
