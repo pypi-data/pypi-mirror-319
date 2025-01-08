dd64e9d10f13717cd4c47f3f4e68c9f7873f2c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:05:17 +0000

    Add package aioautomower/2025.1.0/aioautomower-2025.1.0-py3-none-any.whl

---
+++ packages/aioautomower/aioautomower-2025.1.0-py3-none-any.whl/aioautomower/exceptions.py
@@ -0,0 +1,49 @@
+"""Library for exceptions using the Husqvarna Automower API."""
+
+
+class HusqvarnaAutomowerException(Exception):
+    """Base class for all client exceptions."""
+
+
+class ApiException(HusqvarnaAutomowerException):
+    """Raised during problems talking to the API."""
+
+
+class FeatureNotSupportedException(HusqvarnaAutomowerException):
+    """Raised when the feature is not supported by the mower."""
+
+
+class WorkAreasDifferentException(HusqvarnaAutomowerException):
+    """Raised when the work areas for setting the calendar are different."""
+
+
+class AuthException(HusqvarnaAutomowerException):
+    """Raised due to auth problems talking to API."""
+
+
+class InvalidSyncTokenException(HusqvarnaAutomowerException):
+    """Raised when the sync token is invalid."""
+
+
+class ApiBadRequestException(HusqvarnaAutomowerException):
+    """Raised due sending a Rest command resulting in a bad request."""
+
+
+class ApiForbiddenException(HusqvarnaAutomowerException):
+    """Raised due to permission errors talking to API."""
+
+
+class ApiUnauthorizedException(HusqvarnaAutomowerException):
+    """Raised occasionally, mustn't harm the connection."""
+
+
+class NoDataAvailableException(HusqvarnaAutomowerException):
+    """Raised due updating data, when no data is available."""
+
+
+class TimeoutException(HusqvarnaAutomowerException):
+    """Raised due connecting the websocket."""
+
+
+class HusqvarnaWSServerHandshakeError(HusqvarnaAutomowerException):
+    """Raised due connecting the websocket if server not available."""
