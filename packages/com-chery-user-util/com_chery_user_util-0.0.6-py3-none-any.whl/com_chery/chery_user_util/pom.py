dd1de88f6d74a29ef17dd0aa848414f8712808fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 15:05:22 +0000

    Add package com-chery-user-util/0.0.6/com_chery_user_util-0.0.6-py3-none-any.whl

---
+++ packages/com-chery-user-util/com_chery_user_util-0.0.6-py3-none-any.whl/com_chery/chery_user_util/pom.py
@@ -0,0 +1,20 @@
+PROJECT_PKG_VERSION = '0.0.6'
+
+INCLUDE_FILES = [
+    'user_util.py',
+]
+
+PROJECT_PKG_NAME = 'chery_user_util'
+PYPI_PKG_NAME = ('com_' + PROJECT_PKG_NAME).replace('_', '-')
+CHERY_POM_ROOT_REQUIRED_VERSION = '0.0.1'
+
+
+if __name__ == '__main__':
+    import chery_pom_tool
+    chery_pom_tool.build_and_upload(
+        PROJECT_PKG_NAME,
+        PYPI_PKG_NAME,
+        CHERY_POM_ROOT_REQUIRED_VERSION,
+        PROJECT_PKG_VERSION,
+        INCLUDE_FILES,
+    )
