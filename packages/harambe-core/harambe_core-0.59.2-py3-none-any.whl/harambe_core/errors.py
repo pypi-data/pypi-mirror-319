251841b12174de77ac7eb471257c8593567b2a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:52 +0000

    Add package harambe-core/0.59.2/harambe_core-0.59.2-py3-none-any.whl

---
+++ packages/harambe-core/harambe_core-0.59.2-py3-none-any.whl/harambe_core/errors.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+
+class HarambeException(Exception):
+    """Base exception for all custom exceptions in Harambe."""
+
+    pass
+
+
+class UnknownHTMLConverter(HarambeException):
+    def __init__(self, converter_type: Any) -> None:
+        super().__init__(f"Unknown HTML converter type: {converter_type}")
+
+
+class GotoError(HarambeException):
+    def __init__(self, url: str, status: int) -> None:
+        super().__init__(
+            f'Error calling goto("{url}"). Received unexpected status code: {status}'
+        )
+
+
+class SchemaValidationError(HarambeException):
+    def __init__(self, message: str = None):
+        super().__init__(message)
+
+
+class CaptchaError(HarambeException):
+    def __init__(self, message: str = "CAPTCHA was hit.") -> None:
+        super().__init__(message)
