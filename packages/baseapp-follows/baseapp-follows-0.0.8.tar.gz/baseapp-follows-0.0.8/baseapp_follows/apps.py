2c5d79c9201ecbe51831fb1b8d3de1503a3d8c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 07:05:48 +0000

    Add package baseapp-follows/0.0.8/baseapp-follows-0.0.8.tar.gz

---
+++ packages/baseapp-follows/baseapp-follows-0.0.8.tar.gz/baseapp-follows-0.0.8/baseapp_follows/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class FollowsConfig(AppConfig):
+    default = True
+    name = "baseapp_follows"
+    verbose_name = "BaseApp Follows"
+    default_auto_field = "django.db.models.AutoField"
