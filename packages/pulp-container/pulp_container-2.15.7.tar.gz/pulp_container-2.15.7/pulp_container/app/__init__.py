c26582d0779eba1fb3f0218b0b80ee6299eb6551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 05:04:24 +0000

    Add package pulp-container/2.15.7/pulp_container-2.15.7.tar.gz

---
+++ packages/pulp-container/pulp_container-2.15.7.tar.gz/pulp_container-2.15.7/pulp_container/app/__init__.py
@@ -0,0 +1,10 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpContainerPluginAppConfig(PulpPluginAppConfig):
+    """Entry point for the container plugin."""
+
+    name = "pulp_container.app"
+    label = "container"
+    version = "2.15.7"
+    python_package_name = "pulp-container"
