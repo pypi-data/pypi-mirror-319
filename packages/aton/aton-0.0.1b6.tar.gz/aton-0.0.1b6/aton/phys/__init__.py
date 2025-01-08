e2618ec84263b988e23fe765049deb9ba5eef47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:46 +0000

    Add package aton/0.0.1b6/aton-0.0.1b6.tar.gz

---
+++ packages/aton/aton-0.0.1b6.tar.gz/aton-0.0.1b6/aton/phys/__init__.py
@@ -0,0 +1,14 @@
+"""
+# Physico-chemical constants
+
+This subpackage contains:
+- `units`
+- `atoms`
+- `elements`
+
+"""
+
+from .units import *
+from .functions import *
+from .atoms import atoms
+
