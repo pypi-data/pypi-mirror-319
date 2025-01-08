8d65bb9edd3e0b5b0556eb2e32b69a68e41c0573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:58 +0000

    Add package tailwind-processor/1.2.2/tailwind_processor-1.2.2.tar.gz

---
+++ packages/tailwind-processor/tailwind_processor-1.2.2.tar.gz/tailwind_processor-1.2.2/test/test_tailwind_processor.py
@@ -0,0 +1,19 @@
+import unittest
+
+from tailwind_processor.tailwind_processor import TailwindProcessor
+
+
+class TestTailwindProcessor(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.tp = TailwindProcessor()
+
+    def test_text_processor(self):
+        tailwind_classes = [
+            "text-red-500",
+            "h-dvh",
+        ]
+        processed = self.tp.process(tailwind_classes)
+        self.assertIn(r".h-dvh{height:100dvh}.text-red-500{--tw-text-opacity:1;color:rgb(239 68 68/var(--tw-text-opacity,1))}", processed)
+
+
