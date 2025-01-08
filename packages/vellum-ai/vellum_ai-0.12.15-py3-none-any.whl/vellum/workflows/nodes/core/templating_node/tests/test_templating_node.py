c7335a8bbb1eedfdb881ff96c42c63b3b60ff883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 06:04:57 +0000

    Add package vellum-ai/0.12.15/vellum_ai-0.12.15-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-0.12.15-py3-none-any.whl/vellum/workflows/nodes/core/templating_node/tests/test_templating_node.py
@@ -0,0 +1,108 @@
+import json
+
+from vellum.workflows.nodes.bases.base import BaseNode
+from vellum.workflows.nodes.core.templating_node.node import TemplatingNode
+from vellum.workflows.state import BaseState
+from vellum.workflows.types.core import Json
+
+
+def test_templating_node__dict_output():
+    # GIVEN a templating node with a dict input that just returns it
+    class TemplateNode(TemplatingNode):
+        template = "{{ data }}"
+        inputs = {
+            "data": {
+                "key": "value",
+            }
+        }
+
+    # WHEN the node is run
+    node = TemplateNode()
+    outputs = node.run()
+
+    # THEN the output is json serializable
+    assert json.loads(outputs.result) == {"key": "value"}
+
+
+def test_templating_node__int_output():
+    # GIVEN a templating node that outputs an integer
+    class IntTemplateNode(TemplatingNode[BaseState, int]):
+        template = "{{ data }}"
+        inputs = {
+            "data": 42,
+        }
+
+    # WHEN the node is run
+    node = IntTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected integer
+    assert outputs.result == 42
+
+
+def test_templating_node__float_output():
+    # GIVEN a templating node that outputs a float
+    class FloatTemplateNode(TemplatingNode[BaseState, float]):
+        template = "{{ data }}"
+        inputs = {
+            "data": 42.5,
+        }
+
+    # WHEN the node is run
+    node = FloatTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected float
+    assert outputs.result == 42.5
+
+
+def test_templating_node__bool_output():
+    # GIVEN a templating node that outputs a bool
+    class BoolTemplateNode(TemplatingNode[BaseState, bool]):
+        template = "{{ data }}"
+        inputs = {
+            "data": True,
+        }
+
+    # WHEN the node is run
+    node = BoolTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected bool
+    assert outputs.result is True
+
+
+def test_templating_node__json_output():
+    # GIVEN a templating node that outputs JSON
+    class JSONTemplateNode(TemplatingNode[BaseState, Json]):
+        template = "{{ data }}"
+        inputs = {
+            "data": {"key": "value"},
+        }
+
+    # WHEN the node is run
+    node = JSONTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected JSON
+    assert outputs.result == {"key": "value"}
+
+
+def test_templating_node__execution_count_reference():
+    # GIVEN a random node
+    class OtherNode(BaseNode):
+        pass
+
+    # AND a templating node that references the execution count of the random node
+    class TemplateNode(TemplatingNode):
+        template = "{{ total }}"
+        inputs = {
+            "total": OtherNode.Execution.count,
+        }
+
+    # WHEN the node is run
+    node = TemplateNode()
+    outputs = node.run()
+
+    # THEN the output is just the total
+    assert outputs.result == "0"
