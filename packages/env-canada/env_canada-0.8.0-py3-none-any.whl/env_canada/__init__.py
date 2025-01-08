42358c12eba734450f741a91b4ffa925a38ce259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 14:05:33 +0000

    Add package env-canada/0.8.0/env_canada-0.8.0-py3-none-any.whl

---
+++ packages/env-canada/env_canada-0.8.0-py3-none-any.whl/env_canada/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "ECAirQuality",
+    "ECHistorical",
+    "ECHistoricalRange",
+    "ECHydro",
+    "ECRadar",
+    "ECWeather",
+]
+
+from .ec_aqhi import ECAirQuality
+from .ec_historical import ECHistorical, ECHistoricalRange
+from .ec_hydro import ECHydro
+from .ec_radar import ECRadar
+from .ec_weather import ECWeather
