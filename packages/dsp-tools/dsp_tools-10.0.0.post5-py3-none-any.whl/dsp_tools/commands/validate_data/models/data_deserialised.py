b81f6b4bb160de0c9b58ce048e5f1217068aa0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-08 16:05:07 +0000

    Add package dsp-tools/10.0.0.post5/dsp_tools-10.0.0.post5-py3-none-any.whl

---
+++ packages/dsp-tools/dsp_tools-10.0.0.post5-py3-none-any.whl/dsp_tools/commands/validate_data/models/data_deserialised.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from lxml import etree
+
+
+@dataclass
+class XMLProject:
+    shortcode: str
+    root: etree._Element
+    used_ontologies: set[str]
+
+
+@dataclass
+class ProjectDeserialised:
+    info: ProjectInformation
+    data: DataDeserialised
+
+
+@dataclass
+class ProjectInformation:
+    shortcode: str
+    default_onto: str
+
+
+@dataclass
+class DataDeserialised:
+    resources: list[ResourceDeserialised]
+    file_values: list[AbstractFileValueDeserialised]
+
+
+@dataclass
+class ResourceDeserialised:
+    res_id: str
+    label: str
+    res_class: str
+    values: list[ValueDeserialised]
+
+
+@dataclass
+class ValueDeserialised:
+    prop_name: str
+    object_value: str | None
+
+
+@dataclass
+class BooleanValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class ColorValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class DateValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class DecimalValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class GeonameValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class IntValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class LinkValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class ListValueDeserialised(ValueDeserialised):
+    list_name: str
+
+
+@dataclass
+class SimpleTextDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class RichtextDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class TimeValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class UriValueDeserialised(ValueDeserialised): ...
+
+
+@dataclass
+class AbstractFileValueDeserialised:
+    res_id: str
+    value: str | None
+
+
+@dataclass
+class BitstreamDeserialised(AbstractFileValueDeserialised): ...
+
+
+@dataclass
+class IIIFUriDeserialised(AbstractFileValueDeserialised): ...
