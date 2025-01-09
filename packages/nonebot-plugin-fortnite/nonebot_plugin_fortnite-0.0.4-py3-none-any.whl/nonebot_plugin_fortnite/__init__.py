36cb2b853b06cc24d7652ab4b3173eea2e1b8ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:15 +0000

    Add package nonebot-plugin-fortnite/0.0.4/nonebot_plugin_fortnite-0.0.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-fortnite/nonebot_plugin_fortnite-0.0.4-py3-none-any.whl/nonebot_plugin_fortnite/__init__.py
@@ -0,0 +1,25 @@
+from nonebot import (
+    require,
+    get_driver, # @get_driver().on_startup 装饰启动时运行函数
+    get_bots    # dict[str, BaseBot]
+)
+from nonebot.log import logger
+from nonebot.plugin import PluginMetadata
+
+
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
+
+from .config import Config
+from .matcher import *
+
+__plugin_meta__ = PluginMetadata(
+    name="堡垒之夜游戏插件",
+    description="堡垒之夜战绩，季卡，商城，vb图查询",
+    usage="略",
+    type="application",
+    config=Config,
+    homepage="https://github.com/fllesser/nonebot-plugin-fortnite",
+    supported_adapters={ "~onebot.v11" }
+)
+
