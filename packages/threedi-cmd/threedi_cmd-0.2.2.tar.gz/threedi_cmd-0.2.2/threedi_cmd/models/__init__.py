1542bcf4eb0b46d75d5c4aec033e35bbff71c1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 04:05:15 +0000

    Add package threedi-cmd/0.2.2/threedi_cmd-0.2.2.tar.gz

---
+++ packages/threedi-cmd/threedi_cmd-0.2.2.tar.gz/threedi_cmd-0.2.2/threedi_cmd/models/__init__.py
@@ -0,0 +1,46 @@
+from .actions import ActionWrapper
+from .simulation import SimulationWrapper
+from . import waitfor
+from . import savedstate
+from . import rain
+from . import lateral
+from . import sources_sinks
+from . import wind
+from . import breach
+from . import rasteredit
+from . import initial_waterlevels
+from . import structure_control
+from . import boundary_conditions
+from . import leakage
+from . import settings
+from . import revision
+from . import postprocessing
+
+# Define a list of WRAPPERS
+# A wrapper is an object that 'wraps' a certain
+# resource of the openapi by automatically mapping
+# all of it's openapi client methods.
+
+# For every step in a scenario a wrapper is generated
+
+WRAPPERS = (
+    [
+        ActionWrapper,
+        SimulationWrapper,
+    ]
+    + rain.WRAPPERS
+    + savedstate.WRAPPERS
+    + waitfor.WRAPPERS
+    + lateral.WRAPPERS
+    + sources_sinks.WRAPPERS
+    + wind.WRAPPERS
+    + breach.WRAPPERS
+    + rasteredit.WRAPPERS
+    + initial_waterlevels.WRAPPERS
+    + structure_control.WRAPPERS
+    + boundary_conditions.WRAPPERS
+    + leakage.WRAPPERS
+    + settings.WRAPPERS
+    + revision.WRAPPERS
+    + postprocessing.WRAPPERS
+)
