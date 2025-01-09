1281ddd5779dbe442dd2e608dd6533184186affc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 07:05:10 +0000

    Add package litestar/2.14.0/litestar-2.14.0.tar.gz

---
+++ packages/litestar/litestar-2.14.0.tar.gz/litestar-2.14.0/litestar/plugins/__init__.py
@@ -0,0 +1,23 @@
+from litestar.plugins.base import (
+    CLIPlugin,
+    CLIPluginProtocol,
+    DIPlugin,
+    InitPluginProtocol,
+    OpenAPISchemaPlugin,
+    OpenAPISchemaPluginProtocol,
+    PluginProtocol,
+    PluginRegistry,
+    SerializationPluginProtocol,
+)
+
+__all__ = (
+    "CLIPlugin",
+    "CLIPluginProtocol",
+    "DIPlugin",
+    "InitPluginProtocol",
+    "OpenAPISchemaPlugin",
+    "OpenAPISchemaPluginProtocol",
+    "PluginProtocol",
+    "PluginRegistry",
+    "SerializationPluginProtocol",
+)
