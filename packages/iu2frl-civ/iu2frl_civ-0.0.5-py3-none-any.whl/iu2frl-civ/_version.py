fa8871ba7bee5ff733a9eab433eaa8f0b078ec9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:38 +0000

    Add package iu2frl-civ/0.0.5/iu2frl_civ-0.0.5-py3-none-any.whl

---
+++ packages/iu2frl-civ/iu2frl_civ-0.0.5-py3-none-any.whl/iu2frl-civ/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides iu2frl-civ version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update iu2frl-civ` to change this file.
+
+from incremental import Version
+
+__version__ = Version("iu2frl-civ", 0, 0, 5)
+__all__ = ["__version__"]
