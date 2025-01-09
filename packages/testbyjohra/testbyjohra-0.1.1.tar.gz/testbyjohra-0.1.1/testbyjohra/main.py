36a75be91f65db63ffb2a173514ade2dd3179432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:05:15 +0000

    Add package testbyjohra/0.1.1/testbyjohra-0.1.1.tar.gz

---
+++ packages/testbyjohra/testbyjohra-0.1.1.tar.gz/testbyjohra-0.1.1/testbyjohra/main.py
@@ -0,0 +1,11 @@
+def add_signature(input_string):
+    """
+    Concatenates '- test by Johra Moosa' to the given input string.
+
+    Parameters:
+    input_string (str): The original string.
+
+    Returns:
+    str: The modified string with the signature.
+    """
+    return f"{input_string} - test by Johra Moosa tried again"
