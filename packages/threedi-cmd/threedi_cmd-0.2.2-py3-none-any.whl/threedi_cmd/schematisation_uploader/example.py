27c48a82f6e5aa22738379d001a6ff4883f81b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 04:05:15 +0000

    Add package threedi-cmd/0.2.2/threedi_cmd-0.2.2-py3-none-any.whl

---
+++ packages/threedi-cmd/threedi_cmd-0.2.2-py3-none-any.whl/threedi_cmd/schematisation_uploader/example.py
@@ -0,0 +1,14 @@
+from threedi_api_client.api import ThreediApi
+from threedi_cmd.schematisation_uploader.main import process
+from pathlib import Path
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+# TODO: Adjust path to correct path
+directory = Path("9_2D_FourCell_Rain/")
+
+with ThreediApi("./staging.env", version='v3-beta') as client:
+    schematisation, revision = process(client, directory)
+    print(schematisation)
+    print(revision)
