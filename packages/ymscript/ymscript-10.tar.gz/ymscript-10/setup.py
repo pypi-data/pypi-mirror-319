51cd95e6c37d30601868ac3e877df649bb47cd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:20:50 +0000

    Add package ymscript/10/ymscript-10.tar.gz

---
+++ packages/ymscript/ymscript-10.tar.gz/ymscript-10/setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+
+setuptools.setup(
+	name = "ymscript",
+	version = "10",
+	description = "Image Processing Scripts",
+	url="https://git.sr.ht/~coco/ymscript",
+	classifiers = [
+		"Operating System :: OS Independent",
+		"License :: OSI Approved :: GNU Affero General Public License v3",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Mathematics"
+		],
+	author = "Enric Meinhardt-Llopis",
+	author_email = "enric.meinhardt@fastmail.com",
+	py_modules = ["ymscript"]
+)
