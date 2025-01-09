6ef2f59652fb518938cb642fbc39f400df3f9438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:04:30 +0000

    Add package MdXLogseqTODOSync/0.1.0/mdxlogseqtodosync-0.1.0.tar.gz

---
+++ packages/MdXLogseqTODOSync/mdxlogseqtodosync-0.1.0.tar.gz/mdxlogseqtodosync-0.1.0/setup.py
@@ -0,0 +1,37 @@
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="MdXLogseqTODOSync",
+    version="0.1.0",
+    description="Script to always sync a Logseq TODO list with a markdown file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiswillbeyourgithub/MdXLogseqTODOSync",
+    packages=find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPLv3",
+    keywords=["logseq", "todo", "list", "markdown", "md", "organization", "productivity"],
+    python_requires=">=3.11",
+
+    entry_points={
+        'console_scripts': [
+            'MdXLogseqTODOSync=MdXLogseqTODOSync.__init__:cli_launcher',
+        ],
+    },
+
+    install_requires=[
+        'fire >= 0.6.0',
+        'beartype >= 0.18.5',
+        "LogseqMarkdownParser >= 3.3"
+    ],
+)
---
+++ packages/MdXLogseqTODOSync/mdxlogseqtodosync-0.1.0.tar.gz/mdxlogseqtodosync-0.1.0/test/TODO.md
@@ -0,0 +1,9 @@
+This is some text.
+To test the code, run the command "MdXLogseqTODOSync --input_file logseq_file.md --output_file TODO.md --required_pattern="\s+- TODO .*" --input_delim_start="Things I want to do" --input_delim_end="__END__"
+<!-- BEGIN_TODO -->
+qlkdjqsmdjk qsdjq
+dqd
+qd
+qlkdjqsmdjk
+<!-- END_TODO -->
+Some other text
