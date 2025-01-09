da809ebd07a8e6dc7f708f6764286a3417a464c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:05:29 +0000

    Add package nb-cli-plugin-bootstrap/0.5.0/nb_cli_plugin_bootstrap-0.5.0.tar.gz

---
+++ packages/nb-cli-plugin-bootstrap/nb_cli_plugin_bootstrap-0.5.0.tar.gz/nb_cli_plugin_bootstrap-0.5.0/nb_cli_plugin_bootstrap/templates/bootstrap/{{cookiecutter.nonebot.folder_name}}/#启动.bat
@@ -0,0 +1,2 @@
+{{ cookiecutter.nonebot.nb_command }} {% if cookiecutter.nonebot.use_web_ui %}webui {% endif %}run
+pause
---
+++ packages/nb-cli-plugin-bootstrap/nb_cli_plugin_bootstrap-0.5.0.tar.gz/nb_cli_plugin_bootstrap-0.5.0/nb_cli_plugin_bootstrap/templates/bootstrap/{{cookiecutter.nonebot.folder_name}}/#进入虚拟环境.bat
@@ -0,0 +1,1 @@
+start powershell -NoExit -Command "nb venv | Invoke-Expression"
