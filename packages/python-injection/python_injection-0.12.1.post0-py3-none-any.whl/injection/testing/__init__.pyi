63f4193518240a13995d48ecf6388d59204c8035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:40 +0000

    Add package python-injection/0.12.1.post0/python_injection-0.12.1.post0-py3-none-any.whl

---
+++ packages/python-injection/python_injection-0.12.1.post0-py3-none-any.whl/injection/testing/__init__.pyi
@@ -0,0 +1,14 @@
+from typing import ContextManager
+
+import injection as _
+
+set_test_constant = _.set_constant
+should_be_test_injectable = _.should_be_injectable
+test_constant = _.constant
+test_injectable = _.injectable
+test_singleton = _.singleton
+
+def load_test_profile(*names: str) -> ContextManager[None]:
+    """
+    Context manager or decorator for temporary use test module.
+    """
