deb27893e8a049ab985aaf865dac8f9048c2683c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:20 +0000

    Add package adf-lib/0.1.2/adf_lib-0.1.2.tar.gz

---
+++ packages/adf-lib/adf_lib-0.1.2.tar.gz/adf_lib-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+from setuptools import setup, find_packages
+
+setup(
+    name="adf-lib",
+    version="0.1.2",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    
+)
