4a62767f50859bc94999fba2b7f7a3a6483eea5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:20:52 +0000

    Add package ezKit/1.11.10/ezkit-1.11.10.tar.gz

---
+++ packages/ezKit/ezkit-1.11.10.tar.gz/ezkit-1.11.10/setup.py
@@ -0,0 +1,16 @@
+"""setup"""
+from setuptools import find_packages, setup
+
+setup(
+    name='ezKit',
+    version='1.11.10',
+    author='septvean',
+    author_email='septvean@gmail.com',
+    description='Easy Kit',
+    packages=find_packages(exclude=['documents', 'tests']),
+    include_package_data=True,
+    python_requires='>=3.11',
+    install_requires=[
+        "loguru>=0.7"
+    ]
+)
