bca09cc7c64c478504909c8de29c57b74894c6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:04:32 +0000

    Add package pymob/0.5.0a7/pymob-0.5.0a7.tar.gz

---
+++ packages/pymob/pymob-0.5.0a7.tar.gz/pymob-0.5.0a7/pymob/__init__.py
@@ -0,0 +1,10 @@
+from . import inference
+from . import sim
+from . import utils
+from . import solvers
+
+__version__ = "0.5.0a7"
+
+
+from .simulation import SimulationBase
+from .sim.config import Config
