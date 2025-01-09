c73a77b86bc596595e0b3453df60d19a536ce743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:05:43 +0000

    Add package harrix-test-package/0.8/harrix_test_package-0.8-py3-none-any.whl

---
+++ packages/harrix-test-package/harrix_test_package-0.8-py3-none-any.whl/harrix_test_package/functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def multiply_2(x):
+    return x * 2
+
+
+def multiply_10(x):
+    return x * 10
+
+
+def test_numpy():
+    return np.arange(-2, 2, 0.5)
+
+
+def multiply_20(x):
+    return x * 20
