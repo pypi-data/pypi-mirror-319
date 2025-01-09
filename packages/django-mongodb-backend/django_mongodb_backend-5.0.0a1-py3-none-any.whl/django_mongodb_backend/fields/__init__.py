420c6450e4bdd39cc935e5ff6ad213de1102cd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:02 +0000

    Add package django-mongodb-backend/5.0.0a1/django_mongodb_backend-5.0.0a1-py3-none-any.whl

---
+++ packages/django-mongodb-backend/django_mongodb_backend-5.0.0a1-py3-none-any.whl/django_mongodb_backend/fields/__init__.py
@@ -0,0 +1,11 @@
+from .auto import ObjectIdAutoField
+from .duration import register_duration_field
+from .json import register_json_field
+from .objectid import ObjectIdField
+
+__all__ = ["register_fields", "ObjectIdAutoField", "ObjectIdField"]
+
+
+def register_fields():
+    register_duration_field()
+    register_json_field()
