098af22a73a3eacd2fbd1df3feb946bf8ab1a520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:04:52 +0000

    Add package nystrom-ncut/0.0.6/nystrom_ncut-0.0.6.tar.gz

---
+++ packages/nystrom-ncut/nystrom_ncut-0.0.6.tar.gz/nystrom_ncut-0.0.6/src/nystrom_ncut/propagation_utils.py
@@ -0,0 +1,352 @@
+import logging
+from typing import Literal
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from .common import ceildiv, lazy_normalize
+
+
+@torch.no_grad()
+def run_subgraph_sampling(
+    features: torch.Tensor,
+    num_sample: int,
+    max_draw: int = 1000000,
+    sample_method: Literal["farthest", "random"] = "farthest",
+):
+    if num_sample >= features.shape[0]:
+        # if too many samples, use all samples and bypass Nystrom-like approximation
+        logging.info(
+            "num_sample is larger than total, bypass Nystrom-like approximation"
+        )
+        sampled_indices = torch.arange(features.shape[0])
+    else:
+        # sample subgraph
+        if sample_method == "farthest":  # default
+            if num_sample > max_draw:
+                logging.warning(
+                    f"num_sample is larger than max_draw, apply farthest point sampling on random sampled {max_draw} samples"
+                )
+                draw_indices = torch.randperm(features.shape[0])[:max_draw]
+                sampled_indices = farthest_point_sampling(
+                    features[draw_indices].detach(),
+                    num_sample=num_sample,
+                )
+                sampled_indices = draw_indices[sampled_indices]
+            else:
+                sampled_indices = farthest_point_sampling(
+                    features.detach(),
+                    num_sample=num_sample,
+                )
+        elif sample_method == "random":  # not recommended
+            sampled_indices = torch.randperm(features.shape[0])[:num_sample]
+        else:
+            raise ValueError("sample_method should be 'farthest' or 'random'")
+    return sampled_indices.to(features.device)
+
+
+def farthest_point_sampling(
+    features: torch.Tensor,
+    num_sample: int = 300,
+    h: int = 9,
+):
+    try:
+        import fpsample
+    except ImportError:
+        raise ImportError(
+            "fpsample import failed, please install `pip install fpsample`"
+        )
+
+    # PCA to reduce the dimension
+    if features.shape[1] > 8:
+        u, s, v = torch.pca_lowrank(features, q=8)
+        features = u @ torch.diag(s)
+
+    h = min(h, int(np.log2(features.shape[0])))
+
+    kdline_fps_samples_idx = fpsample.bucket_fps_kdline_sampling(
+        features.numpy(force=True), num_sample, h
+    ).astype(np.int64)
+    return torch.from_numpy(kdline_fps_samples_idx)
+
+
+def distance_from_features(
+    features: torch.Tensor,
+    features_B: torch.Tensor,
+    distance: Literal["cosine", "euclidean", "rbf"],
+):
+    """Compute affinity matrix from input features.
+    Args:
+        features (torch.Tensor): input features, shape (n_samples, n_features)
+        features_B (torch.Tensor, optional): optional, if not None, compute affinity between two features
+        distance (str): distance metric, 'cosine' (default) or 'euclidean', 'rbf'.
+    Returns:
+        (torch.Tensor): affinity matrix, shape (n_samples, n_samples)
+    """
+    # compute distance matrix from input features
+    if distance == "cosine":
+        features = lazy_normalize(features, dim=-1)
+        features_B = lazy_normalize(features_B, dim=-1)
+        D = 1 - features @ features_B.T
+    elif distance == "euclidean":
+        D = torch.cdist(features, features_B, p=2)
+    elif distance == "rbf":
+        D = torch.cdist(features, features_B, p=2) ** 2
+        D = D / (2 * features.var(dim=0).sum())
+    else:
+        raise ValueError("distance should be 'cosine' or 'euclidean', 'rbf'")
+    return D
+
+
+def affinity_from_features(
+    features: torch.Tensor,
+    features_B: torch.Tensor = None,
+    affinity_focal_gamma: float = 1.0,
+    distance: Literal["cosine", "euclidean", "rbf"] = "cosine",
+):
+    """Compute affinity matrix from input features.
+
+    Args:
+        features (torch.Tensor): input features, shape (n_samples, n_features)
+        features_B (torch.Tensor, optional): optional, if not None, compute affinity between two features
+        affinity_focal_gamma (float): affinity matrix parameter, lower t reduce the edge weights
+            on weak connections, default 1.0
+        distance (str): distance metric, 'cosine' (default) or 'euclidean', 'rbf'.
+    Returns:
+        (torch.Tensor): affinity matrix, shape (n_samples, n_samples)
+    """
+    # compute affinity matrix from input features
+
+    # if feature_B is not provided, compute affinity matrix on features x features
+    # if feature_B is provided, compute affinity matrix on features x feature_B
+    features_B = features if features_B is None else features_B
+
+    # compute distance matrix from input features
+    D = distance_from_features(features, features_B, distance)
+
+    # torch.exp make affinity matrix positive definite,
+    # lower affinity_focal_gamma reduce the weak edge weights
+    A = torch.exp(-D / affinity_focal_gamma)
+    return A
+
+
+def propagate_knn(
+    subgraph_output: torch.Tensor,
+    inp_features: torch.Tensor,
+    subgraph_features: torch.Tensor,
+    knn: int = 10,
+    distance: Literal["cosine", "euclidean", "rbf"] = "cosine",
+    affinity_focal_gamma: float = 1.0,
+    chunk_size: int = 8192,
+    device: str = None,
+    move_output_to_cpu: bool = False,
+):
+    """A generic function to propagate new nodes using KNN.
+
+    Args:
+        subgraph_output (torch.Tensor): output from subgraph, shape (num_sample, D)
+        inp_features (torch.Tensor): features from existing nodes, shape (new_num_samples, n_features)
+        subgraph_features (torch.Tensor): features from subgraph, shape (num_sample, n_features)
+        knn (int): number of KNN to propagate eige nvectors
+        distance (str): distance metric, 'cosine' (default) or 'euclidean', 'rbf'
+        chunk_size (int): chunk size for matrix multiplication
+        device (str): device to use for computation, if None, will not change device
+    Returns:
+        torch.Tensor: propagated eigenvectors, shape (new_num_samples, D)
+
+    Examples:
+        >>> old_eigenvectors = torch.randn(3000, 20)
+        >>> old_features = torch.randn(3000, 100)
+        >>> new_features = torch.randn(200, 100)
+        >>> new_eigenvectors = propagate_knn(old_eigenvectors, new_features, old_features, knn=3)
+        >>> # new_eigenvectors.shape = (200, 20)
+
+    """
+    device = subgraph_output.device if device is None else device
+
+    if knn == 1:
+        return propagate_nearest(
+            subgraph_output,
+            inp_features,
+            subgraph_features,
+            chunk_size=chunk_size,
+            device=device,
+            move_output_to_cpu=move_output_to_cpu,
+        )
+
+    # used in nystrom_ncut
+    # propagate eigen_vector from subgraph to full graph
+    subgraph_output = subgraph_output.to(device)
+
+    n_chunks = ceildiv(inp_features.shape[0], chunk_size)
+    V_list = []
+    for _v in torch.chunk(inp_features, n_chunks, dim=0):
+        _v = _v.to(device)
+
+        # _A = affinity_from_features(subgraph_features, _v, affinity_focal_gamma, distance).mT
+        # if knn is not None:
+        #     mask = torch.full_like(_A, True, dtype=torch.bool)
+        #     mask[torch.arange(len(_v))[:, None], _A.topk(knn, dim=-1, largest=True).indices] = False
+        #     _A[mask] = 0.0
+        # _A = F.normalize(_A, p=1, dim=-1)
+
+        if distance == 'cosine':
+            _A = _v @ subgraph_features.T
+        elif distance == 'euclidean':
+            _A = - torch.cdist(_v, subgraph_features, p=2)
+        elif distance == 'rbf':
+            _A = - torch.cdist(_v, subgraph_features, p=2) ** 2
+        else:
+            raise ValueError("distance should be 'cosine' or 'euclidean', 'rbf'")
+
+        # keep topk KNN for each row
+        topk_sim, topk_idx = _A.topk(knn, dim=-1, largest=True)
+        row_id = torch.arange(topk_idx.shape[0], device=_A.device)[:, None].expand(
+            -1, topk_idx.shape[1]
+        )
+        _A = torch.sparse_coo_tensor(
+            torch.stack([row_id, topk_idx], dim=-1).reshape(-1, 2).T,
+            topk_sim.reshape(-1),
+            size=(_A.shape[0], _A.shape[1]),
+            device=_A.device,
+        )
+        _A = _A.to_dense().to(dtype=subgraph_output.dtype)
+        _D = _A.sum(-1)
+        _A /= _D[:, None]
+
+        _V = _A @ subgraph_output
+        if move_output_to_cpu:
+            _V = _V.cpu()
+        V_list.append(_V)
+
+    subgraph_output = torch.cat(V_list, dim=0)
+    return subgraph_output
+
+
+def propagate_nearest(
+    subgraph_output: torch.Tensor,
+    inp_features: torch.Tensor,
+    subgraph_features: torch.Tensor,
+    distance: Literal["cosine", "euclidean", "rbf"] = "cosine",
+    chunk_size: int = 8192,
+    device: str = None,
+    move_output_to_cpu: bool = False,
+):
+    device = subgraph_output.device if device is None else device
+    if distance == 'cosine':
+        inp_features = lazy_normalize(inp_features, dim=-1)
+        subgraph_features = lazy_normalize(subgraph_features, dim=-1)
+
+    # used in nystrom_tsne, equivalent to propagate_by_knn with knn=1
+    # propagate tSNE from subgraph to full graph
+    V_list = []
+    subgraph_features = subgraph_features.to(device)
+    for i in range(0, inp_features.shape[0], chunk_size):
+        end = min(i + chunk_size, inp_features.shape[0])
+        _v = inp_features[i:end].to(device)
+        _A = -distance_from_features(subgraph_features, _v, distance).mT
+
+        # keep top1 for each row
+        top_idx = _A.argmax(dim=-1).cpu()
+        _V = subgraph_output[top_idx]
+        if move_output_to_cpu:
+            _V = _V.cpu()
+        V_list.append(_V)
+
+    subgraph_output = torch.cat(V_list, dim=0)
+    return subgraph_output
+
+
+# wrapper functions for adding new nodes to existing graph
+def propagate_eigenvectors(
+    eigenvectors: torch.Tensor,
+    features: torch.Tensor,
+    new_features: torch.Tensor,
+    knn: int,
+    num_sample: int,
+    sample_method: Literal["farthest", "random"],
+    chunk_size: int,
+    device: str,
+):
+    """Propagate eigenvectors to new nodes using KNN. Note: this is equivalent to the class API `NCUT.tranform(new_features)`, expect for the sampling is re-done in this function.
+    Args:
+        eigenvectors (torch.Tensor): eigenvectors from existing nodes, shape (num_sample, num_eig)
+        features (torch.Tensor): features from existing nodes, shape (n_samples, n_features)
+        new_features (torch.Tensor): features from new nodes, shape (n_new_samples, n_features)
+        knn (int): number of KNN to propagate eigenvectors, default 3
+        num_sample (int): number of samples for subgraph sampling, default 50000
+        sample_method (str): sample method, 'farthest' (default) or 'random'
+        chunk_size (int): chunk size for matrix multiplication, default 8192
+        device (str): device to use for computation, if None, will not change device
+    Returns:
+        torch.Tensor: propagated eigenvectors, shape (n_new_samples, num_eig)
+
+    Examples:
+        >>> old_eigenvectors = torch.randn(3000, 20)
+        >>> old_features = torch.randn(3000, 100)
+        >>> new_features = torch.randn(200, 100)
+        >>> new_eigenvectors = propagate_eigenvectors(old_eigenvectors, new_features, old_features, knn=3)
+        >>> # new_eigenvectors.shape = (200, 20)
+    """
+
+    device = eigenvectors.device if device is None else device
+
+    # sample subgraph
+    subgraph_indices = run_subgraph_sampling(
+        features,
+        num_sample,
+        sample_method=sample_method,
+    )
+
+    subgraph_eigenvectors = eigenvectors[subgraph_indices].to(device)
+    subgraph_features = features[subgraph_indices].to(device)
+    new_features = new_features.to(device)
+
+    # propagate eigenvectors from subgraph to new nodes
+    new_eigenvectors = propagate_knn(
+        subgraph_eigenvectors,
+        new_features,
+        subgraph_features,
+        knn=knn,
+        chunk_size=chunk_size,
+        device=device,
+    )
+    return new_eigenvectors
+
+
+def quantile_min_max(x, q1=0.01, q2=0.99, n_sample=10000):
+    if x.shape[0] > n_sample:
+        np.random.seed(0)
+        random_idx = np.random.choice(x.shape[0], n_sample, replace=False)
+        vmin, vmax = x[random_idx].quantile(q1), x[random_idx].quantile(q2)
+    else:
+        vmin, vmax = x.quantile(q1), x.quantile(q2)
+    return vmin, vmax
+
+
+def quantile_normalize(x, q=0.95):
+    """normalize each dimension of x to [0, 1], take 95-th percentage, this robust to outliers
+        </br> 1. sort x
+        </br> 2. take q-th quantile
+        </br>     min_value -> (1-q)-th quantile
+        </br>     max_value -> q-th quantile
+        </br> 3. normalize
+        </br> x = (x - min_value) / (max_value - min_value)
+
+    Args:
+        x (torch.Tensor): input tensor, shape (n_samples, n_features)
+            normalize each feature to 0-1 range
+        q (float): quantile, default 0.95
+
+    Returns:
+        torch.Tensor: quantile normalized tensor
+    """
+    # normalize x to 0-1 range, max value is q-th quantile
+    # quantile makes the normalization robust to outliers
+    if isinstance(x, np.ndarray):
+        x = torch.tensor(x)
+    vmax, vmin = quantile_min_max(x, q, 1 - q)
+    x = (x - vmin) / (vmax - vmin)
+    x = x.clamp(0, 1)
+    return x
---
+++ packages/nystrom-ncut/nystrom_ncut-0.0.6.tar.gz/nystrom_ncut-0.0.6/tests/test.py
@@ -0,0 +1,112 @@
+import numpy as np
+import torch
+import torch.nn.functional as Fn
+
+from src.nystrom_ncut.ncut_pytorch import NCUT, axis_align
+# from ncut_pytorch.src import rgb_from_umap_sphere
+# from ncut_pytorch.src.new_ncut_pytorch import NewNCUT
+
+# from ncut_pytorch.ncut_pytorch.backbone_text import load_text_model
+
+
+if __name__ == "__main__":
+    # torch.manual_seed(1212)
+    # M = torch.randn((7, 3))
+    # W = torch.nn.functional.cosine_similarity(M[:, None], M[None, :], dim=-1)
+    # A = torch.exp(W - 1)
+    # D_s2 = torch.sum(A, dim=-1, keepdim=True) ** -0.5
+    # # print(A)
+    # print(A * D_s2 * D_s2.mT)
+    #
+    # ncut = NCUT(num_eig=7, knn=1, eig_solver="svd")
+    # V, L = ncut.fit_transform(M)
+    # print(V @ torch.diag(L) @ V.mT)
+    # raise Exception()
+
+    # print(load_text_model("meta-llama/Meta-Llama-3.1-8B").cuda())
+    # print(AutoModelForCausalLM.from_pretrained(
+    #     "meta-llama/Meta-Llama-3.1-8B",
+    #     token="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",
+    # ))
+    # # print(transformers.pipeline(
+    # #     "text-generation",
+    # #     model="meta-llama/Meta-Llama-3.1-8B",
+    # #     model_kwargs={"torch_dtype": torch.bfloat16},
+    # #     token="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",
+    # #     device="cpu",
+    # # ))
+    # raise Exception(
+
+    torch.set_printoptions(precision=8, sci_mode=False, linewidth=400)
+    torch.set_default_dtype(torch.float32)
+    torch.manual_seed(1212)
+    np.random.seed(1212)
+
+    M = torch.rand((1200, 12))
+    NC = NCUT(n_components=30, num_sample=1000, sample_method="farthest", eig_solver="svd")
+
+    torch.manual_seed(1212)
+    np.random.seed(1212)
+    X, eigs = NC.fit_transform(M)
+    print(eigs)
+    # print(X.mT @ X)
+
+    normalized_M = Fn.normalize(M, p=2, dim=-1)
+    A = torch.exp(-(1 - normalized_M @ normalized_M.mT))
+    R = torch.diag(torch.sum(A, dim=-1) ** -0.5)
+    L = R @ A @ R
+    # print(L)
+    # print(X @ torch.diag(eigs) @ X.mT)
+    # print(L)
+    RE = torch.abs(X @ torch.diag(eigs) @ X.mT / L - 1)
+    print(RE.max().item(), RE.mean().item())
+
+    # torch.manual_seed(1212)
+    # np.random.seed(1212)
+    #
+    # aX, R = axis_align(X)
+    # print(aX[:3])
+    # print(R)
+    # print(R @ R.mT)
+    raise Exception()
+
+
+
+
+    # import time
+    # n_trials = 10
+    #
+    # with torch.no_grad():
+    #     start_t = time.perf_counter()
+    #     for _ in range(n_trials):
+    #         X, eigs = NC.fit_transform(M)
+    #     end_t = time.perf_counter()
+    #     print(X.min().item(), X.max().item(), eigs)
+    #     print(f"{1e3 * (end_t - start_t) / n_trials}ms")
+    #
+    #     start_t = time.perf_counter()
+    #     for _ in range(n_trials):
+    #         nX, neigs = nNC.fit_transform(M)
+    #     end_t = time.perf_counter()
+    #     print(nX.min().item(), nX.max().item(), neigs)
+    #     print(f"{1e3 * (end_t - start_t) / n_trials}ms")
+    # raise Exception()
+
+    # assert torch.all(torch.isclose(X, torch.Tensor([
+    #     [0.320216, 0.144101, -0.110744, -0.560543, -0.007982],
+    #     [0.297634, 0.662867, 0.146107, 0.277893, 0.553959],
+    #     [0.324994, -0.057295, 0.052916, 0.391666, -0.460911],
+    #     [0.301703, -0.460709, 0.528563, 0.222525, 0.325546],
+    #     [0.316614, 0.043475, -0.526899, 0.100665, -0.030259],
+    #     [0.325425, -0.127884, 0.294540, -0.012173, -0.303528],
+    #     [0.318136, -0.288952, -0.065148, -0.470192, 0.244805],
+    #     [0.309522, -0.352693, -0.473237, 0.234057, 0.276185],
+    #     [0.320464, 0.229301, 0.281134, -0.308938, -0.169746],
+    #     [0.326147, 0.213536, -0.112246, 0.155114, -0.341439]
+    # ]), atol=1e-6)), "Failed assertion"
+
+    torch.manual_seed(1212)
+    np.random.seed(1212)
+    X_2d, rgb = rgb_from_umap_sphere(X)
+    # X_3d, rgb = rgb_from_cosine_tsne_3d(X)
+    print(rgb)
