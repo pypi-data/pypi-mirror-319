0287c7e5c059a8d41864116bcef63ddcc8840b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 07:05:11 +0000

    Add package projectcompactor/0.6.1/projectcompactor-0.6.1.tar.gz

---
+++ packages/projectcompactor/projectcompactor-0.6.1.tar.gz/projectcompactor-0.6.1/setup.py
@@ -0,0 +1,34 @@
+# setup.py
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="projectcompactor",
+    version="0.6.1",
+    author="veppy",
+    author_email="vonepern@gmail.com",
+    description="A tool to generate a project tree and extract file contents.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/veppy1/projectcompactor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "tqdm>=4.0.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'projectcompactor=projectcompactor.generator:main',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
