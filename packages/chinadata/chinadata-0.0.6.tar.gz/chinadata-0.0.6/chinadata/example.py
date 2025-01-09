b454804fd66b6823156c5a409882a3421cf66874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:20:54 +0000

    Add package chinadata/0.0.6/chinadata-0.0.6.tar.gz

---
+++ packages/chinadata/chinadata-0.0.6.tar.gz/chinadata-0.0.6/chinadata/example.py
@@ -0,0 +1,19 @@
+#
+"""
+1、安装对应库
+pip install chinadata
+
+"""
+#2、
+import  chinadata.ca_data as ts
+pro = ts.pro_api('9e84ed87f29cf43f0a84c98cef6a69cc00')
+
+# #查询当前所有正常上市交易的股票列表
+data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+print(data)
+
+
+
+
+
+
