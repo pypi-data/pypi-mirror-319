c97e11fe128ba1f8e3cf81e59249a1735b11555a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 04:05:29 +0000

    Add package glj-interface/0.0.2/glj_interface-0.0.2.tar.gz

---
+++ packages/glj-interface/glj_interface-0.0.2.tar.gz/glj_interface-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Interface Communication'
+
+
+# Setting up
+setup(
+    name="glj_interface",
+    version=VERSION,
+    author="Jkrai44",
+    author_email="<jkraisinger@jlg.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
