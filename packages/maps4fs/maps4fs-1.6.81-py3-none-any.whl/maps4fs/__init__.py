28e55c693e954c198272840e49bf476e1b56ea26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:10 +0000

    Add package maps4fs/1.6.81/maps4fs-1.6.81-py3-none-any.whl

---
+++ packages/maps4fs/maps4fs-1.6.81-py3-none-any.whl/maps4fs/__init__.py
@@ -0,0 +1,16 @@
+# pylint: disable=missing-module-docstring
+from maps4fs.generator.dtm.dtm import DTMProvider
+from maps4fs.generator.dtm.srtm import SRTM30Provider
+from maps4fs.generator.dtm.usgs import USGS1mProvider
+from maps4fs.generator.game import Game
+from maps4fs.generator.map import Map
+from maps4fs.generator.settings import (
+    BackgroundSettings,
+    DEMSettings,
+    GRLESettings,
+    I3DSettings,
+    SettingsModel,
+    SplineSettings,
+    TextureSettings,
+)
+from maps4fs.logger import Logger
