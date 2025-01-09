f4651c53f28eafe8ad44e113552c2a72cfc6363b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:20:52 +0000

    Add package im8/0.2/im8-0.2.tar.gz

---
+++ packages/im8/im8-0.2.tar.gz/im8-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="im8",
+    version="0.2",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/your-package-name",
+    packages=find_packages(),
+    install_requires=["pyperclip==1.9.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
