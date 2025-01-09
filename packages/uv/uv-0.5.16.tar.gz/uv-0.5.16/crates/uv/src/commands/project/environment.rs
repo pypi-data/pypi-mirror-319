2cea2b67efc0ab0e50677ac30740be8dcd68d97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:05:22 +0000

    Add package uv/0.5.16/uv-0.5.16.tar.gz

---
+++ packages/uv/uv-0.5.16.tar.gz/uv-0.5.16/crates/uv/src/commands/project/environment.rs
@@ -0,0 +1,147 @@
+use tracing::debug;
+
+use crate::commands::pip::loggers::{InstallLogger, ResolveLogger};
+use crate::commands::project::{
+    resolve_environment, sync_environment, EnvironmentSpecification, ProjectError,
+};
+use crate::printer::Printer;
+use crate::settings::ResolverInstallerSettings;
+use uv_cache::{Cache, CacheBucket};
+use uv_cache_key::{cache_digest, hash_digest};
+use uv_client::Connectivity;
+use uv_configuration::{Concurrency, PreviewMode, TrustedHost};
+use uv_dispatch::SharedState;
+use uv_distribution_types::{Name, Resolution};
+use uv_python::{Interpreter, PythonEnvironment};
+
+/// A [`PythonEnvironment`] stored in the cache.
+#[derive(Debug)]
+pub(crate) struct CachedEnvironment(PythonEnvironment);
+
+impl From<CachedEnvironment> for PythonEnvironment {
+    fn from(environment: CachedEnvironment) -> Self {
+        environment.0
+    }
+}
+
+impl CachedEnvironment {
+    /// Get or create an [`CachedEnvironment`] based on a given set of requirements and a base
+    /// interpreter.
+    pub(crate) async fn get_or_create(
+        spec: EnvironmentSpecification<'_>,
+        interpreter: Interpreter,
+        settings: &ResolverInstallerSettings,
+        state: &SharedState,
+        resolve: Box<dyn ResolveLogger>,
+        install: Box<dyn InstallLogger>,
+        installer_metadata: bool,
+        connectivity: Connectivity,
+        concurrency: Concurrency,
+        native_tls: bool,
+        allow_insecure_host: &[TrustedHost],
+        cache: &Cache,
+        printer: Printer,
+        preview: PreviewMode,
+    ) -> Result<Self, ProjectError> {
+        // When caching, always use the base interpreter, rather than that of the virtual
+        // environment.
+        let interpreter = if let Some(interpreter) = interpreter.to_base_interpreter(cache)? {
+            debug!(
+                "Caching via base interpreter: `{}`",
+                interpreter.sys_executable().display()
+            );
+            interpreter
+        } else {
+            debug!(
+                "Caching via interpreter: `{}`",
+                interpreter.sys_executable().display()
+            );
+            interpreter
+        };
+
+        // Resolve the requirements with the interpreter.
+        let resolution = Resolution::from(
+            resolve_environment(
+                spec,
+                &interpreter,
+                settings.as_ref().into(),
+                state,
+                resolve,
+                connectivity,
+                concurrency,
+                native_tls,
+                allow_insecure_host,
+                cache,
+                printer,
+                preview,
+            )
+            .await?,
+        );
+
+        // Hash the resolution by hashing the generated lockfile.
+        // TODO(charlie): If the resolution contains any mutable metadata (like a path or URL
+        // dependency), skip this step.
+        let resolution_hash = {
+            let mut distributions = resolution.distributions().collect::<Vec<_>>();
+            distributions.sort_unstable_by_key(|dist| dist.name());
+            hash_digest(&distributions)
+        };
+
+        // Hash the interpreter based on its path.
+        // TODO(charlie): Come up with a robust hash for the interpreter.
+        let interpreter_hash = cache_digest(&interpreter.sys_executable());
+
+        // Search in the content-addressed cache.
+        let cache_entry = cache.entry(CacheBucket::Environments, interpreter_hash, resolution_hash);
+
+        if cache.refresh().is_none() {
+            if let Ok(root) = fs_err::read_link(cache_entry.path()) {
+                if let Ok(environment) = PythonEnvironment::from_root(root, cache) {
+                    return Ok(Self(environment));
+                }
+            }
+        }
+
+        // Create the environment in the cache, then relocate it to its content-addressed location.
+        let temp_dir = cache.venv_dir()?;
+        let venv = uv_virtualenv::create_venv(
+            temp_dir.path(),
+            interpreter,
+            uv_virtualenv::Prompt::None,
+            false,
+            false,
+            true,
+            false,
+        )?;
+
+        sync_environment(
+            venv,
+            &resolution,
+            settings.as_ref().into(),
+            state,
+            install,
+            installer_metadata,
+            connectivity,
+            concurrency,
+            native_tls,
+            allow_insecure_host,
+            cache,
+            printer,
+            preview,
+        )
+        .await?;
+
+        // Now that the environment is complete, sync it to its content-addressed location.
+        let id = cache
+            .persist(temp_dir.into_path(), cache_entry.path())
+            .await?;
+        let root = cache.archive(&id);
+
+        Ok(Self(PythonEnvironment::from_root(root, cache)?))
+    }
+
+    /// Convert the [`CachedEnvironment`] into an [`Interpreter`].
+    pub(crate) fn into_interpreter(self) -> Interpreter {
+        self.0.into_interpreter()
+    }
+}
---
+++ packages/uv/uv-0.5.16.tar.gz/uv-0.5.16/crates/uv/src/commands/project/remove.rs
@@ -0,0 +1,360 @@
+use std::fmt::Write;
+use std::path::Path;
+
+use anyhow::{Context, Result};
+use owo_colors::OwoColorize;
+
+use uv_cache::Cache;
+use uv_client::Connectivity;
+use uv_configuration::{
+    Concurrency, DevGroupsManifest, EditableMode, ExtrasSpecification, InstallOptions, LowerBound,
+    PreviewMode, TrustedHost,
+};
+use uv_dispatch::SharedState;
+use uv_fs::Simplified;
+use uv_normalize::DEV_DEPENDENCIES;
+use uv_pep508::PackageName;
+use uv_python::{PythonDownloads, PythonPreference, PythonRequest};
+use uv_scripts::Pep723Script;
+use uv_settings::PythonInstallMirrors;
+use uv_warnings::warn_user_once;
+use uv_workspace::pyproject::DependencyType;
+use uv_workspace::pyproject_mut::{DependencyTarget, PyProjectTomlMut};
+use uv_workspace::{DiscoveryOptions, VirtualProject, Workspace};
+
+use crate::commands::pip::loggers::{DefaultInstallLogger, DefaultResolveLogger};
+use crate::commands::pip::operations::Modifications;
+use crate::commands::project::install_target::InstallTarget;
+use crate::commands::project::lock::LockMode;
+use crate::commands::project::{default_dependency_groups, ProjectError};
+use crate::commands::{diagnostics, project, ExitStatus};
+use crate::printer::Printer;
+use crate::settings::ResolverInstallerSettings;
+
+/// Remove one or more packages from the project requirements.
+#[allow(clippy::fn_params_excessive_bools)]
+pub(crate) async fn remove(
+    project_dir: &Path,
+    locked: bool,
+    frozen: bool,
+    no_sync: bool,
+    packages: Vec<PackageName>,
+    dependency_type: DependencyType,
+    package: Option<PackageName>,
+    python: Option<String>,
+    install_mirrors: PythonInstallMirrors,
+    settings: ResolverInstallerSettings,
+    script: Option<Pep723Script>,
+    python_preference: PythonPreference,
+    python_downloads: PythonDownloads,
+    installer_metadata: bool,
+    connectivity: Connectivity,
+    concurrency: Concurrency,
+    native_tls: bool,
+    allow_insecure_host: &[TrustedHost],
+    no_config: bool,
+    cache: &Cache,
+    printer: Printer,
+    preview: PreviewMode,
+) -> Result<ExitStatus> {
+    let target = if let Some(script) = script {
+        // If we found a PEP 723 script and the user provided a project-only setting, warn.
+        if package.is_some() {
+            warn_user_once!(
+                "`--package` is a no-op for Python scripts with inline metadata, which always run in isolation"
+            );
+        }
+        if locked {
+            warn_user_once!(
+                "`--locked` is a no-op for Python scripts with inline metadata, which always run in isolation"
+            );
+        }
+        if frozen {
+            warn_user_once!(
+                "`--frozen` is a no-op for Python scripts with inline metadata, which always run in isolation"
+            );
+        }
+        if no_sync {
+            warn_user_once!(
+                "`--no-sync` is a no-op for Python scripts with inline metadata, which always run in isolation"
+            );
+        }
+        Target::Script(script)
+    } else {
+        // Find the project in the workspace.
+        let project = if let Some(package) = package {
+            VirtualProject::Project(
+                Workspace::discover(project_dir, &DiscoveryOptions::default())
+                    .await?
+                    .with_current_project(package.clone())
+                    .with_context(|| format!("Package `{package}` not found in workspace"))?,
+            )
+        } else {
+            VirtualProject::discover(project_dir, &DiscoveryOptions::default()).await?
+        };
+
+        Target::Project(project)
+    };
+
+    let mut toml = match &target {
+        Target::Script(script) => {
+            PyProjectTomlMut::from_toml(&script.metadata.raw, DependencyTarget::Script)
+        }
+        Target::Project(project) => PyProjectTomlMut::from_toml(
+            project.pyproject_toml().raw.as_ref(),
+            DependencyTarget::PyProjectToml,
+        ),
+    }?;
+
+    for package in packages {
+        match dependency_type {
+            DependencyType::Production => {
+                let deps = toml.remove_dependency(&package)?;
+                if deps.is_empty() {
+                    show_other_dependency_type_hint(printer, &package, &toml)?;
+                    anyhow::bail!(
+                        "The dependency `{package}` could not be found in `project.dependencies`"
+                    )
+                }
+            }
+            DependencyType::Dev => {
+                let dev_deps = toml.remove_dev_dependency(&package)?;
+                let group_deps =
+                    toml.remove_dependency_group_requirement(&package, &DEV_DEPENDENCIES)?;
+                if dev_deps.is_empty() && group_deps.is_empty() {
+                    show_other_dependency_type_hint(printer, &package, &toml)?;
+                    anyhow::bail!(
+                        "The dependency `{package}` could not be found in `tool.uv.dev-dependencies` or `tool.uv.dependency-groups.dev`"
+                    );
+                }
+            }
+            DependencyType::Optional(ref extra) => {
+                let deps = toml.remove_optional_dependency(&package, extra)?;
+                if deps.is_empty() {
+                    show_other_dependency_type_hint(printer, &package, &toml)?;
+                    anyhow::bail!(
+                        "The dependency `{package}` could not be found in `project.optional-dependencies.{extra}`"
+                    );
+                }
+            }
+            DependencyType::Group(ref group) => {
+                if group == &*DEV_DEPENDENCIES {
+                    let dev_deps = toml.remove_dev_dependency(&package)?;
+                    let group_deps =
+                        toml.remove_dependency_group_requirement(&package, &DEV_DEPENDENCIES)?;
+                    if dev_deps.is_empty() && group_deps.is_empty() {
+                        show_other_dependency_type_hint(printer, &package, &toml)?;
+                        anyhow::bail!(
+                            "The dependency `{package}` could not be found in `tool.uv.dev-dependencies` or `tool.uv.dependency-groups.dev`"
+                        );
+                    }
+                } else {
+                    let deps = toml.remove_dependency_group_requirement(&package, group)?;
+                    if deps.is_empty() {
+                        show_other_dependency_type_hint(printer, &package, &toml)?;
+                        anyhow::bail!(
+                            "The dependency `{package}` could not be found in `dependency-groups.{group}`"
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    // Save the modified dependencies.
+    match &target {
+        Target::Script(script) => {
+            script.write(&toml.to_string()).await?;
+        }
+        Target::Project(project) => {
+            let pyproject_path = project.root().join("pyproject.toml");
+            fs_err::write(pyproject_path, toml.to_string())?;
+        }
+    };
+
+    // If `--frozen`, exit early. There's no reason to lock and sync, and we don't need a `uv.lock`
+    // to exist at all.
+    if frozen {
+        return Ok(ExitStatus::Success);
+    }
+
+    let project = match target {
+        Target::Project(project) => project,
+        // If `--script`, exit early. There's no reason to lock and sync.
+        Target::Script(script) => {
+            writeln!(
+                printer.stderr(),
+                "Updated `{}`",
+                script.path.user_display().cyan()
+            )?;
+            return Ok(ExitStatus::Success);
+        }
+    };
+
+    // Discover or create the virtual environment.
+    let venv = project::get_or_init_environment(
+        project.workspace(),
+        python.as_deref().map(PythonRequest::parse),
+        &install_mirrors,
+        python_preference,
+        python_downloads,
+        connectivity,
+        native_tls,
+        allow_insecure_host,
+        no_config,
+        cache,
+        printer,
+    )
+    .await?;
+
+    // Determine the lock mode.
+    let mode = if frozen {
+        LockMode::Frozen
+    } else if locked {
+        LockMode::Locked(venv.interpreter())
+    } else {
+        LockMode::Write(venv.interpreter())
+    };
+
+    // Initialize any shared state.
+    let state = SharedState::default();
+
+    // Lock and sync the environment, if necessary.
+    let lock = match project::lock::do_safe_lock(
+        mode,
+        project.workspace().into(),
+        settings.as_ref().into(),
+        LowerBound::Allow,
+        &state,
+        Box::new(DefaultResolveLogger),
+        connectivity,
+        concurrency,
+        native_tls,
+        allow_insecure_host,
+        cache,
+        printer,
+        preview,
+    )
+    .await
+    {
+        Ok(result) => result.into_lock(),
+        Err(ProjectError::Operation(err)) => {
+            return diagnostics::OperationDiagnostic::default()
+                .report(err)
+                .map_or(Ok(ExitStatus::Failure), |err| Err(err.into()))
+        }
+        Err(err) => return Err(err.into()),
+    };
+
+    if no_sync {
+        return Ok(ExitStatus::Success);
+    }
+
+    // Perform a full sync, because we don't know what exactly is affected by the removal.
+    // TODO(ibraheem): Should we accept CLI overrides for this? Should we even sync here?
+    let extras = ExtrasSpecification::All;
+    let install_options = InstallOptions::default();
+
+    // Determine the default groups to include.
+    let defaults = default_dependency_groups(project.pyproject_toml())?;
+
+    // Identify the installation target.
+    let target = match &project {
+        VirtualProject::Project(project) => InstallTarget::Project {
+            workspace: project.workspace(),
+            name: project.project_name(),
+            lock: &lock,
+        },
+        VirtualProject::NonProject(workspace) => InstallTarget::NonProjectWorkspace {
+            workspace,
+            lock: &lock,
+        },
+    };
+
+    match project::sync::do_sync(
+        target,
+        &venv,
+        &extras,
+        &DevGroupsManifest::from_defaults(defaults),
+        EditableMode::Editable,
+        install_options,
+        Modifications::Exact,
+        settings.as_ref().into(),
+        Box::new(DefaultInstallLogger),
+        installer_metadata,
+        connectivity,
+        concurrency,
+        native_tls,
+        allow_insecure_host,
+        cache,
+        printer,
+        preview,
+    )
+    .await
+    {
+        Ok(()) => {}
+        Err(ProjectError::Operation(err)) => {
+            return diagnostics::OperationDiagnostic::default()
+                .report(err)
+                .map_or(Ok(ExitStatus::Failure), |err| Err(err.into()))
+        }
+        Err(err) => return Err(err.into()),
+    }
+
+    Ok(ExitStatus::Success)
+}
+
+/// Represents the destination where dependencies are added, either to a project or a script.
+#[derive(Debug)]
+enum Target {
+    /// A PEP 723 script, with inline metadata.
+    Project(VirtualProject),
+    /// A project with a `pyproject.toml`.
+    Script(Pep723Script),
+}
+
+/// Show a hint if a dependency with the given name is present as any dependency type.
+///
+/// This is useful when a dependency of the user-specified type was not found, but it may be present
+/// elsewhere.
+fn show_other_dependency_type_hint(
+    printer: Printer,
+    name: &PackageName,
+    pyproject: &PyProjectTomlMut,
+) -> Result<()> {
+    // TODO(zanieb): Attach these hints to the error so they render _after_ in accordance our
+    // typical styling
+    for dep_ty in pyproject.find_dependency(name, None) {
+        match dep_ty {
+            DependencyType::Production => writeln!(
+                printer.stderr(),
+                "{}{} `{name}` is a production dependency",
+                "hint".bold().cyan(),
+                ":".bold(),
+            )?,
+            DependencyType::Dev => writeln!(
+                printer.stderr(),
+                "{}{} `{name}` is a development dependency (try: `{}`)",
+                "hint".bold().cyan(),
+                ":".bold(),
+                format!("uv remove {name} --dev`").bold()
+            )?,
+            DependencyType::Optional(group) => writeln!(
+                printer.stderr(),
+                "{}{} `{name}` is an optional dependency (try: `{}`)",
+                "hint".bold().cyan(),
+                ":".bold(),
+                format!("uv remove {name} --optional {group}").bold()
+            )?,
+            DependencyType::Group(group) => writeln!(
+                printer.stderr(),
+                "{}{} `{name}` is in the `{group}` group (try: `{}`)",
+                "hint".bold().cyan(),
+                ":".bold(),
+                format!("uv remove {name} --group {group}").bold()
+            )?,
+        }
+    }
+
+    Ok(())
+}
