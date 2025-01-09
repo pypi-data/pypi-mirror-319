41a89d0675032ee6f0f9cf3308896473efb3e262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 04:06:01 +0000

    Add package foundationallm/0.9.1rc116/foundationallm-0.9.1rc116.tar.gz

---
+++ packages/foundationallm/foundationallm-0.9.1rc116.tar.gz/foundationallm-0.9.1rc116/foundationallm/models/orchestration/completion_response.py
@@ -0,0 +1,34 @@
+from typing import List, Optional, Union
+from pydantic import BaseModel
+
+from foundationallm.models.orchestration import (
+    AnalysisResult,
+    ContentArtifact,
+    OpenAIImageFileMessageContentItem,
+    OpenAITextMessageContentItem
+)
+
+class CompletionResponse(BaseModel):
+    """
+    Response from a language model.
+    """
+    id: Optional[str] = None
+    operation_id: str
+    user_prompt: str
+    full_prompt: Optional[str] = None
+    content: Optional[
+        List[
+            Union[
+                OpenAIImageFileMessageContentItem, 
+                OpenAITextMessageContentItem
+            ]
+        ]
+    ] = None
+    analysis_results: Optional[List[AnalysisResult]] = []
+    content_artifacts: Optional[List[ContentArtifact]] = []
+    user_prompt_embedding: Optional[List[float]] = []
+    prompt_tokens: int = 0
+    completion_tokens: int = 0
+    total_tokens: int = 0
+    total_cost: float = 0.0
+    errors: Optional[List[str]] = []
