52d210914c8a1553a4ba2507ce5e67ab15925b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:04 +0000

    Add package djc-heroicons/0.1.0/djc_heroicons-0.1.0.tar.gz

---
+++ packages/djc-heroicons/djc_heroicons-0.1.0.tar.gz/djc_heroicons-0.1.0/src/djc_heroicons/__init__.py
@@ -0,0 +1,14 @@
+# Public API
+# isort: off
+from djc_heroicons.app_settings import HeroIconsSettings
+from djc_heroicons.components.icon import Icon
+from djc_heroicons.icons import IconName
+
+# isort: on
+
+
+__all__ = [
+    "HeroIconsSettings",
+    "Icon",
+    "IconName",
+]
---
+++ packages/djc-heroicons/djc_heroicons-0.1.0.tar.gz/djc_heroicons-0.1.0/src/djc_heroicons/utils.py
@@ -0,0 +1,7 @@
+import sys
+
+# See https://peps.python.org/pep-0655/#usage-in-python-3-11
+if sys.version_info >= (3, 11):
+    from typing import NotRequired, TypedDict
+else:
+    from typing_extensions import NotRequired, TypedDict  # for Python <3.11 with (Not)Required
