1499a03cc36d50033bcfe73aa9fb9fbad60fe3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 04:05:27 +0000

    Add package spaceone-inventory/2.0.dev233/spaceone_inventory-2.0.dev233-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-2.0.dev233-py3-none-any.whl/spaceone/inventory/plugin/collector/conf/global_conf.py
@@ -0,0 +1,24 @@
+# Log Settings
+LOG = {
+    "filters": {
+        "masking": {
+            "rules": {
+                "Collector.verify": ["secret_data"],
+                "Collector.collect": ["secret_data"],
+                "Job.get_tasks": ["secret_data"],
+            }
+        }
+    }
+}
+
+# Cache Settings
+CACHES = {
+    "local": {
+        "backend": "spaceone.core.cache.local_cache.LocalCache",
+        "max_size": 128,
+        "ttl": 86400,
+    },
+}
+
+# gRPC Settings
+MAX_WORKERS = 4
