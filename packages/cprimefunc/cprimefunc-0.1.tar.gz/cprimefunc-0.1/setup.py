1c650f36e9190609d2107fdd6c2f15e0d70efb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:05:22 +0000

    Add package cprimefunc/0.1/cprimefunc-0.1.tar.gz

---
+++ packages/cprimefunc/cprimefunc-0.1.tar.gz/cprimefunc-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cprimefunc",
+    version="0.1",
+    packages=find_packages(),
+    description="A custom library for checking prime numbers",
+    author="Colourless-Chameleon",
+    license="MIT",
+)
