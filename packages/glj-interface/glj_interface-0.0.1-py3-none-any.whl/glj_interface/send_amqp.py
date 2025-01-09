585a8ef6414264e6ad7a1227a13e823bb325407d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 04:05:25 +0000

    Add package glj-interface/0.0.1/glj_interface-0.0.1-py3-none-any.whl

---
+++ packages/glj-interface/glj_interface-0.0.1-py3-none-any.whl/glj_interface/send_amqp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials('EndE,EndE')
+parameters = pika.ConnectionParameters('IP',
+                                    5672,
+                                    '/',
+                                    credentials)
+
+
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
