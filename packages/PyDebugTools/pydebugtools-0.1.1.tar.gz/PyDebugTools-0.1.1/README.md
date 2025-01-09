47bb9c732cec6c4c5d88fc65c830ee99b7a508aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:09 +0000

    Add package PyDebugTools/0.1.1/pydebugtools-0.1.1.tar.gz

---
+++ packages/PyDebugTools/pydebugtools-0.1.1.tar.gz/PyDebugTools-0.1.1/README.md
@@ -0,0 +1,9 @@
+# Notebook Debugger
+
+A Python library for debugging Jupyter notebooks using LangChain and Rich.
+
+## Installation
+
+```bash
+pip install PyFix
+
---
+++ packages/PyDebugTools/pydebugtools-0.1.1.tar.gz/PyDebugTools-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyDebugTools',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'ipython',
+        'rich',
+        'langchain_groq',
+    ],
+    entry_points={
+        'console_scripts': [
+            'notebook-debugger=PyDebugTool.debug:main',
+        ],
+    },
+    license='MIT',
+    author='Sambit Mallick',
+    author_email='sambitmallick.pro@gmail.com',
+    description='A Python library for debugging python files and notebooks using GROQ',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/notebook-debugger',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
