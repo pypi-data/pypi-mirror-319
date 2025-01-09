ce02888881717abce1752cfb9579e61bbe28cae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:09 +0000

    Add package RelativeAbundance/1.0.1/relativeabundance-1.0.1.tar.gz

---
+++ packages/RelativeAbundance/relativeabundance-1.0.1.tar.gz/relativeabundance-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RelativeAbundance',
+    version='1.0.1',
+    author='Lillian Tatka',
+    description="""A package for assessing target engagement via relative abundance""",
+    packages=find_packages(), 
+    install_requires=[
+        'numpy',  
+        'pandas',
+        'matplotlib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+)
