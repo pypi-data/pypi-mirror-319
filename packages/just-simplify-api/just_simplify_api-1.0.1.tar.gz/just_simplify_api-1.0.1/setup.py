adaf89cd572ac0089d5e3587d9d1f3258b990821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:09 +0000

    Add package just-simplify-api/1.0.1/just_simplify_api-1.0.1.tar.gz

---
+++ packages/just-simplify-api/just_simplify_api-1.0.1.tar.gz/just_simplify_api-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Read the README file
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name="just_simplify_api",
+    version="1.0.1",
+    packages=find_packages(),
+    description="Mesh API tools for consistent response formatting and validation",
+    author="Preston Hollis",
+    author_email="thomas.preston.hollis@gmail.com",
+    url="https://github.com/tphollis/api-tools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
