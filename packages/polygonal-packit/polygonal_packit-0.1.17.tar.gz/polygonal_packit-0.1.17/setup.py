62f58bb2f931e81b6206092b58a509934a306297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:02 +0000

    Add package polygonal-packit/0.1.17/polygonal_packit-0.1.17.tar.gz

---
+++ packages/polygonal-packit/polygonal_packit-0.1.17.tar.gz/polygonal_packit-0.1.17/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='polygonal_packit',
+    version='0.1.17',
+    author='Michał Matejczuk, Piotr Kosakowski',
+    description='Hexagonal and triangular version of game Pack It! along with Alpha Zero General-based RL algorithms.',
+    long_description=open('README.md').read(),
+    url='https://github.com/matejczukm/alpha-zero-general',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    license='MIT',
+    python_requires='>=3.8',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
