1ed004154032789ec8f27e3929e4b16341382c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:04:48 +0000

    Add package templateengine/2.0.3/templateengine-2.0.3.tar.gz

---
+++ packages/templateengine/templateengine-2.0.3.tar.gz/templateengine-2.0.3/templateengine/main.py
@@ -0,0 +1,11 @@
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# from templateengine.container.TemplateEngineProcess import TemplateEngineProcess
+
+from .container.TemplateEngineProcess import TemplateEngineProcess
+
+if __name__ == "__main__":
+    proc = TemplateEngineProcess()
+    proc.run()
