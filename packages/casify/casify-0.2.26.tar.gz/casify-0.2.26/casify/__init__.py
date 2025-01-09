d1bafe4cd5a33d32338ce245fb7bea037a98ae66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:06:19 +0000

    Add package casify/0.2.26/casify-0.2.26.tar.gz

---
+++ packages/casify/casify-0.2.26.tar.gz/casify-0.2.26/casify/__init__.py
@@ -0,0 +1,47 @@
+from .equation import solve, Solve
+from .likning import løs, Løs
+from .function import function, Function
+from .funksjon import funksjon, Funksjon
+from .algebra import (
+    expand,
+    factor,
+    utvid,
+    Utvid,
+    faktoriser,
+    Faktoriser,
+    div,
+    Div,
+    polynomdivisjon,
+    Polynomdivisjon,
+)
+
+from .vector import vector, Vector2d
+
+from .vektor import vektor, Vektor2d, vinkel
+
+
+__all__ = [
+    "solve",
+    "løs",
+    "Solve",
+    "Løs",
+    "function",
+    "Function",
+    "funksjon",
+    "Funksjon",
+    "expand",
+    "factor",
+    "utvid",
+    "Utvid",
+    "faktoriser",
+    "Faktoriser",
+    "div",
+    "Div",
+    "polynomdivisjon",
+    "Polynomdivisjon",
+    "vector",
+    "Vector2d",
+    "vektor",
+    "Vektor2d",
+    "vinkel",
+]
