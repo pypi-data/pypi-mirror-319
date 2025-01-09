9019fcf1b0c3bbe52a7365e2c5ee787d70bc76e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:04:54 +0000

    Add package slack-bot-openai/0.1.3/slack_bot_openai-0.1.3.tar.gz

---
+++ packages/slack-bot-openai/slack_bot_openai-0.1.3.tar.gz/slack_bot_openai-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="slack_bot_openai",
+    version="0.1.3",
+    description="Common code for Peakmojo Slack bots",
+    author="Peakmojo",
+    author_email="dev@peakmojo.com",
+    packages=find_packages(),
+    install_requires=[
+        "openai==1.53.0",
+        "slack-bolt>=1.18.0",
+        "boto3==1.34.49"
+    ],
+    python_requires=">=3.11",
+) 
