9a2caa1c77e40aee6dac48b659b1b0d8b16d081f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:04:33 +0000

    Add package casino-of-life/0.1.0/casino_of_life-0.1.0-py3-none-any.whl

---
+++ packages/casino-of-life/casino_of_life-0.1.0-py3-none-any.whl/casino_of_life/src/vast_config.py
@@ -0,0 +1,8 @@
+VAST_INSTANCE = {
+    'host': '70.69.205.56',
+    'port': 57258,
+    'local_port': 5000,
+    'remote_port': 5000,
+    'workspace': '/workspace/casino-of-life',
+    'ssh_key': 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCjA0iP0tL6hFQb+goBb7qSyi3XVgXGZQ+VYPhoq5XqDG4p7zGCcCr4abs7dygZnZClxaGsjc3IeP/moA/kpn+POhYkAeIrGbIDBcAwiV3FySXIHL8IOiXQbAgtWakkrw95/6ramC8H07vCmIDIwM0cMKfevVvl8E5rLnVLqymrlg3hvF5pImH1I9u1lwKDhUrmaBX3vNkubZgD6+lqzfB3ttjnbnaZ0K7nD9hBx/hfHPeHCyEyvzmhe7+yc4GiEicumA6/IvLrC4NDBkYBG2uMeZkQZs0dNnFT4VtL9hmODpzAweMRgDtqykl8PesnS+yUvFKwJVftVQz31oWHs3JBWTrgBQJgUKTrVl0ONiAIAWWD6RGu9pBcetV8QxTgaxiLumppibAsF8Tous3cH6WSSTnUOP+wNlWflT7Z3P6qocQbkeJArEqrDVxl2OdegNs5yfCeupaUABPmm+iR+ciwVbMkxszxUNSt0L4VXL2UVOw0wcWmNSgWjwy3j3h7jDTU9C4dwMneQJWPvLzsyKxIWbDDXXOKlrQS+nFz2Xhp+62pPoCYoUaP/luVK/egPXR458rZz+oH5IXhbx3+5g6I+4IGNXreHC9ItU3aq55K6xBHDLSU3e0jwgLw4D9VookXgnqv5H5gmKvuKopC0uL6sUtz6vPm7FukgjZGIyxcw== root@d29334980230'
+}
