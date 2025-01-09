5467696102a4756dab6ef08277d383d04315994c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:33 +0000

    Add package linopy/0.4.2/linopy-0.4.2-2-py3-none-any.whl

---
+++ packages/linopy/linopy-0.4.2-2-py3-none-any.whl/linopy/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Created on Wed Mar 10 11:03:06 2021.
+
+@author: fabulous
+"""
+
+from importlib.metadata import version
+
+raise ImportError(
+    "This version of linopy has a major bug that can lead to incorrect results. "
+    "Please update to the latest version of linopy (at least v0.4.4)."
+)
+
+
+__version__ = version("linopy")
+
+# Note: For intercepting multiplications between xarray dataarrays, Variables and Expressions
+# we need to extend their __mul__ functions with a quick special case
+import linopy.monkey_patch_xarray  # noqa: F401
+from linopy import model, remote, types
+from linopy.config import options
+from linopy.constants import EQUAL, GREATER_EQUAL, LESS_EQUAL
+from linopy.constraints import Constraint
+from linopy.expressions import LinearExpression, QuadraticExpression, merge
+from linopy.io import read_netcdf
+from linopy.model import Model, Variable, available_solvers
+from linopy.objective import Objective
+from linopy.remote import RemoteHandler
+
+__all__ = (
+    "Constraint",
+    "EQUAL",
+    "GREATER_EQUAL",
+    "LESS_EQUAL",
+    "LinearExpression",
+    "Model",
+    "Objective",
+    "QuadraticExpression",
+    "RemoteHandler",
+    "Variable",
+    "available_solvers",
+    "merge",
+    "model",
+    "options",
+    "read_netcdf",
+    "remote",
+    "types",
+)
