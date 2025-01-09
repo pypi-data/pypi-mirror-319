fa6ed0284eacf017dae7227c50b52f4b8eb400d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:40 +0000

    Add package python-injection/0.12.1.post0/python_injection-0.12.1.post0.tar.gz

---
+++ packages/python-injection/python_injection-0.12.1.post0.tar.gz/python_injection-0.12.1.post0/injection/_core/common/type.py
@@ -0,0 +1,60 @@
+from collections.abc import Callable, Iterable, Iterator
+from inspect import isfunction
+from types import GenericAlias, UnionType
+from typing import (
+    Annotated,
+    Any,
+    TypeAliasType,
+    Union,
+    get_args,
+    get_origin,
+    get_type_hints,
+)
+
+type TypeDef[T] = type[T] | TypeAliasType | GenericAlias
+type InputType[T] = TypeDef[T] | UnionType
+type TypeInfo[T] = InputType[T] | Callable[..., T] | Iterable[TypeInfo[T]]
+
+
+def get_return_types(*args: TypeInfo[Any]) -> Iterator[InputType[Any]]:
+    for arg in args:
+        if isinstance(arg, Iterable) and not (
+            isinstance(arg, type | str) or isinstance(get_origin(arg), type)
+        ):
+            inner_args = arg
+
+        elif isfunction(arg) and (return_type := get_type_hints(arg).get("return")):
+            inner_args = (return_type,)
+
+        else:
+            yield arg  # type: ignore[misc]
+            continue
+
+        yield from get_return_types(*inner_args)
+
+
+def standardize_types(
+    *types: InputType[Any],
+    with_origin: bool = False,
+) -> Iterator[TypeDef[Any]]:
+    for tp in types:
+        if tp is None:
+            continue
+
+        origin = get_origin(tp)
+
+        if origin is Union or isinstance(tp, UnionType):
+            inner_types = get_args(tp)
+
+        elif origin is Annotated:
+            inner_types = get_args(tp)[:1]
+
+        else:
+            yield tp
+
+            if with_origin and origin is not None:
+                yield origin
+
+            continue
+
+        yield from standardize_types(*inner_types, with_origin=with_origin)
