5d203284d6bd0b466294445e4361bda5cb22599d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 03:21:04 +0000

    Add package asterrdetection/1.1.0/asterrdetection-1.1.0-py3-none-any.whl

---
+++ packages/asterrdetection/asterrdetection-1.1.0-py3-none-any.whl/ast_error_detection/__init__.py
@@ -0,0 +1,17 @@
+# This file is part of ast_error_detection.
+# Copyright (C) 2025 Badmavasan.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or any later version.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+from .error_diagnosis import get_code_errors
+
+__all__ = [
+    "get_code_errors"
+]
+
+__version__ = "0.1.0"
