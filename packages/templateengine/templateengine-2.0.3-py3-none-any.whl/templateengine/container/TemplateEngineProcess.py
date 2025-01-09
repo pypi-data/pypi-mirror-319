d2b8b1bdd09cddb737738d8bd9ef560d704e4cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 05:04:48 +0000

    Add package templateengine/2.0.3/templateengine-2.0.3-py3-none-any.whl

---
+++ packages/templateengine/templateengine-2.0.3-py3-none-any.whl/templateengine/container/TemplateEngineProcess.py
@@ -0,0 +1,14 @@
+from pip_services4_container import ProcessContainer
+from ..build import TemplateEngineFactory, StrategiesFactory, PlannersFactory, ToolsFactory, AIModelsFactory, ExecutorsFactory
+
+class TemplateEngineProcess(ProcessContainer):
+    def __init__(self):
+        super(TemplateEngineProcess, self).__init__('templateengine-v2', 'Template engine process')
+        self._config_path = "./config/config.yml"
+
+        self._factories.add(TemplateEngineFactory())
+        self._factories.add(StrategiesFactory())
+        self._factories.add(PlannersFactory())
+        self._factories.add(ToolsFactory())
+        self._factories.add(AIModelsFactory())
+        self._factories.add(ExecutorsFactory())
