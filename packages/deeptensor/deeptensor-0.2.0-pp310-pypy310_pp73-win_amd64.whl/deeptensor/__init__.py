6df40d036df01981da10f9dac895f01a69afe5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 07:05:22 +0000

    Add package deeptensor/0.2.0/deeptensor-0.2.0-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/deeptensor/deeptensor-0.2.0-pp310-pypy310_pp73-win_amd64.whl/deeptensor/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from ._core import (
+    FeedForwardLayer,
+    GeLu,
+    LeakyReLu,
+    Model,
+    ReLu,
+    Sigmoid,
+    SoftMax,
+    Tanh,
+    Tensor,
+    Value,
+    __doc__,
+)
+
+__all__ = [
+    "FeedForwardLayer",
+    "GeLu",
+    "LeakyReLu",
+    "Model",
+    "ReLu",
+    "Sigmoid",
+    "SoftMax",
+    "Tanh",
+    "Tensor",
+    "Value",
+    "__doc__",
+]
