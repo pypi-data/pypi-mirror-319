ad47b7c4458b52d64f700195e37480940a719a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-09 06:05:09 +0000

    Add package fabric-user-data-functions/0.2.8rc0/fabric_user_data_functions-0.2.8rc0.tar.gz

---
+++ packages/fabric-user-data-functions/fabric_user_data_functions-0.2.8rc0.tar.gz/fabric_user_data_functions-0.2.8rc0/fabric/functions/internal/__init__.py
@@ -0,0 +1,12 @@
+"""You can ignore this module and any files within it. It is used to help set up your User Data Functions.
+"""
+
+# flake8: noqa: F401
+from .custom_token_credential import *
+from .fabric_item import *
+from .fabric_lakehouse_files_client import *
+from .item_binding import *
+from .invoke_response import *
+from .udf_binding import *
+from .udf_string import *
+from .user_data_function_context_binding import *
