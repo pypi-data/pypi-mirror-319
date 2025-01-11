72bca8ec57de8e03077c2198576c2afe1cbaa47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:14 +0000

    Add package terralab-cli/0.0.21/terralab_cli-0.0.21-py3-none-any.whl

---
+++ packages/terralab-cli/terralab_cli-0.0.21-py3-none-any.whl/terralab/constants.py
@@ -0,0 +1,9 @@
+# status key values as defined by the Teaspoons service
+FAILED_KEY = "FAILED"
+SUCCEEDED_KEY = "SUCCEEDED"
+RUNNING_KEY = "RUNNING"
+PREPARING_KEY = "PREPARING"
+
+# support text
+SUPPORT_EMAIL = "scientific-services-support@broadinstitute.org"
+SUPPORT_EMAIL_TEXT = f"For help troubleshooting, email {SUPPORT_EMAIL}"
