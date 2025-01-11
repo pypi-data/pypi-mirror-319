9f9266c81d3baec738bfcf34179b4eae2f4ba626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:06:14 +0000

    Add package dpowers/0.1.7rc2/dpowers-0.1.7rc2.tar.gz

---
+++ packages/dpowers/dpowers-0.1.7rc2.tar.gz/dpowers-0.1.7rc2/Dlib/Dpowers/Dfuncs.py
@@ -0,0 +1,262 @@
+#
+#
+# Copyright (c) 2020-2025 DPS, dps@my.mail.de
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+import time, os, sys, threading, traceback, warnings, functools
+from Dhelpers.container import container
+from Dhelpers.builtin_manipulation import execute_after_print,\
+    execute_after_warnings, execute_after_error, always_print_traceback
+#the last two print functions should be imported via the Dfuncs module
+from Dhelpers.arghandling import PositiveInt
+from .winapps import pythoneditor
+
+from .events import KeyboardAdaptor, MouseAdaptor, KeyWaiter, HookAdaptor, CombinedSender
+from . import dlg, ntfy, clip, Win, launch
+from .windowpower import WindowObject
+
+# =======================================================================
+# SENDWAIT function
+# =======================================================================
+
+keyb = KeyboardAdaptor(group="Dfuncs",_primary_name="keyb")
+mouse = MouseAdaptor(group="Dfuncs", _primary_name="mouse")
+event_sender = CombinedSender(keyb,mouse)
+
+class SendwaitError(Exception):
+    pass
+        
+
+def nfsendwait(*args, **kwargs):
+    """function shortcut to enable notifications"""
+    return sendwait(*args, nfOn=True, notifytime=20, **kwargs)
+
+
+def sendwait(mandatory, *optional, winwaittime=10, notifytime=5,
+        continueOnError=False, nfOn=False, delay=None):
+    """A multi-purpose function to quickly pass a chain of commands:
+    The default symbol "→" is used to escape special command tags.
+    Functions see code.
+    """
+    
+    def report(a, b):
+        if nfOn:
+            ntfy(a, notifytime, b)
+    
+    args = (mandatory,) + optional
+    
+    for i in range(len(args)):
+        ai = args[i]
+        # notify(str(ai))
+        error = None
+        
+        if isinstance(ai, (int, float)):
+            report("sleeping", ai)
+            time.sleep(ai)
+        
+        elif isinstance(ai, str):
+            report("Sending keys:", ai)
+            event_sender.send(ai)
+        
+        elif isinstance(ai, WindowObject):
+            report("Waiting for Window to exist and activate:", ai)
+            found_win = ai.wait_exist_activate(timeout=winwaittime)
+            if found_win:
+                report("Found active window:", ai)
+            else:
+                error = "Window did not exist after winwaittime"
+        else:
+            try:
+                event_sender.send_event(ai)
+            except TypeError:
+                pass
+            else:
+                report(f"Sending event:", ai)
+            try:
+                pid = ai.pid
+            except AttributeError:
+                error = "Wrong argument type for startwait command."
+            else:
+                if isinstance(pid, PositiveInt):
+                    report("Waiting for Window to exist and activate:",
+                            "pid={pid}\nProcessObject='{ai}'".format_map(
+                                    locals()))
+                    found_win = Win(pid=pid).wait_exist(timeout=winwaittime)
+                    if found_win:
+                        report(f"Found active window: pid"
+                            f"={pid}\nProcessObject='{ai}'")
+                    else:
+                        error = "Window did not exist after winwaittime"
+                else:
+                    error = "Wrong argument type for startwait command."
+        
+        # checking if there was an error:
+        if error is not None:
+            if continueOnError:
+                # Change the behavior if startwait encounters an error.
+                # Default= Notify and Exit
+                return error
+            else:
+                raise SendwaitError(f"Argument {i} gave the following error: "
+                    f"{error}")
+        if delay: time.sleep(delay)
+    # after looping over all arguments:
+    return True
+
+
+
+# ========================================================
+### Catching messages to additionally show a notification
+# ========================================================
+
+def notify_about_error(hotkey=None, timeout=10):
+    @execute_after_error
+    def error_notification(error_type, error_message, error_traceback):
+        source_msg = ""
+        if hotkey: source_msg += f"Press {hotkey} to see."
+        ntfy(f"{error_type.__name__}: {error_message}", timeout, source_msg)
+        def analyze_error():
+            options = traceback.format_tb(error_traceback)
+            text = f"{error_type}\n{error_message}\n\nTraeback:\n"
+            result = dlg.choose(options, options[-1], text=text, width=1000,
+                    height=1000)
+            if result:
+                i = options.index(result)
+                fs = traceback.extract_tb(error_traceback, i + 1)[i]
+                pythoneditor.jump_to_line(fs[0], fs[1])
+        container.set_temp_store_key("exec_func",analyze_error, timeout)
+
+
+
+def notify_about_warnings(hotkey=None,timeout=10):
+    @execute_after_warnings
+    def post_notifcation(msg,file,line):
+        source_msg = f"Printed on line {line} in: {file}."
+        if hotkey: source_msg+= f" Press {hotkey} to see."
+        ntfy(f"Warning: {msg}", timeout, source_msg)
+        container.set_temp_store_key("exec_func", functools.partial(
+                pythoneditor.jump_to_line,file, line), timeout)
+        
+
+def notify_about_print(hotkey=None, timeout=5):
+    @execute_after_print
+    def ntfy_after_print(*args, sep=" ", end="\n", notify=True, **ignore):
+        if not notify: return
+        file, line, func, text = traceback.extract_stack(limit=3)[-3]
+        # there must be at least three frames: this one, _custom_print,
+        # call of print func
+        # we want to access the call of the print_func, so we use [-3]
+        print_message = sep.join([str(arg) for arg in args])
+        if end != "\n": print_message += end
+        source_msg = f"Printed on line {line} in: {file}."
+        if hotkey: source_msg += f" Press {hotkey} to see."
+        ntfy(print_message, timeout, source_msg)
+        container.set_temp_store_key("exec_func", functools.partial(
+                pythoneditor.jump_to_line, file, line), timeout*2)
+
+
+
+# ===============================================================
+
+def display_sorted_dict(dic, name=""):
+    f = ""
+    for i in sorted(dic):
+        #    notify(i)
+        f += i + "\t"*(3 + (i[0].upper() == "L") - len(i)//4) + "-> " + dic[
+            i] + "\n"
+        # notify(i)
+    launch.get("echo", f, "|", "zenity", "--text-info")
+
+extdict = {"jpeg": "jpg", "tiff": "tif"}
+
+
+class FilePathParts:
+    def __init__(self, file_path):
+        self.path = file_path.rstrip("/")
+        self.dir, self.base = os.path.split(self.path)
+        self.name, self.ext = os.path.splitext(self.base)
+        if self.ext == "":
+            if os.path.isdir(self.path):
+                self.ext = "dir"
+        a = self.ext.lower()
+        self.ext_norm = extdict.get(a,
+                a)  # replace extension by version in extdict if present
+
+
+# =============================================================================
+#     Displaying window information and putting it to clipboard
+# =============================================================================
+
+def display_win_info():
+    ntfy("Click on a window", 3)
+    
+    x = Win(loc="SELECT").all_info()
+    winprops = x[:3] + ((x[1], x[2]),) + x[3:]
+    
+    show = [str(winprops[0]) + " [ID]", str(winprops[1]) + " [TITLE]",
+        str(winprops[2]) + " [CLASS]", str(winprops[3]),
+        str(winprops[4]) + " [PID]",
+        str(winprops[5]) + " [GEOMETRY] (x,y,width,height)"]
+    
+    ret = dlg.choose(show, default=3, title="Window information",
+            text="Save to clipboard:", width=700)
+    
+    if ret is not None:
+        for i in range(len(show)):
+            # dlg.msg(str(ret)+"\n"+str(show[i]))
+            if ret == show[i]:
+                clip.fill(winprops[i], notify=True)
+                break
+
+
+# =============================================================================
+#     Display key names
+# =============================================================================
+def display_key_names():
+    y = KeyWaiter.get1key()
+    x = tuple(y.named_instance.names)
+    time.sleep(0.1)
+    if dlg.quest("Dpowers key name(s):\n{x}\n\nSave first(=standard) name to "
+                 "clipboard?".format_map(locals()),title="Get key names"):
+        clip.fill(x[0], notify=True)
+        
+def monitor_input_events(timeout=10, implementation=None,**hookkwargs):
+    if implementation:
+        this_hook=HookAdaptor(implementation)
+    else:
+        this_hook=HookAdaptor(group="keywait")
+    ntfy("Showing all input events for %s seconds." % timeout)
+    with this_hook.keyboard_mouse(functools.partial(ntfy,timeout=1), **hookkwargs):
+        time.sleep(timeout)
+    ntfy("Done showing input events.")
+
+
+saved_patterns = []
+    
+def save_key_replay():
+    k = KeyWaiter(release=True, buttons = True, maxtime=600,endevents=("Esc",))
+    ntfy("Saving pressed keys",10, "Abort by pressing Esc")
+    saved_patterns.append(k)
+    k.start()
+    ntfy("Stop saving pressed keys")
+
+def replay_pressed_keys(delay=100):
+    k = saved_patterns[-1]
+    ntfy("Press ShiftL to start")
+    if KeyWaiter.wait_for_key("ShiftL"):
+        ntfy("reinjecting")
+        k.reinject(delay=delay)
+        ntfy("reinjecting done")
---
+++ packages/dpowers/dpowers-0.1.7rc2.tar.gz/dpowers-0.1.7rc2/Dlib/Dpowers/events/hookpower/adapt_evdev.py
@@ -0,0 +1,195 @@
+#
+#
+# Copyright (c) 2020-2025 DPS, dps@my.mail.de
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+
+
+from .baseclasses import (InputEventHandler, KeyhookBase, ButtonhookBase,
+    CursorhookBase, PressReleaseHook)
+from ..event_classes import PressReleaseEvent
+
+from .. import DependencyManager
+
+with DependencyManager(__name__) as manager:
+    device_control = manager.import_module("evdev_prepared.device_control")
+    uinput = manager.import_module("evdev_prepared.uinput")
+
+
+# the following import will not be performed if the DependencyManager is only
+# checking the import capability. if evdev is not installed, an error will
+# be raised when trying to import evdev_prepared already.
+from evdev.ecodes import (EV_KEY, EV_ABS, EV_SYN, EV_MSC, KEY, BTN,
+    EV_LED, EV_REL, ABS_MT_POSITION_X, ABS_MT_POSITION_Y, ABS_X, ABS_Y,
+    REL_X, REL_Y, bytype)
+
+class EvdevHandler(device_control.DeviceHandler, InputEventHandler):
+    
+   
+    devupdater = device_control.DeviceUpdater()
+    # this class will also use the CollactableInputDevice class and the
+    # EvdevInputLooper class in the background
+    uinput = uinput.global_uinput
+    uinput.start()
+    
+    def __init__(self, hook_cls=None, **selection_kwargs):
+        InputEventHandler.__init__(self,hook_cls)
+        device_control.DeviceHandler.__init__(self,devupdater= self.devupdater,
+                **selection_kwargs)
+    
+    def process_event(self, ty, co ,val, dev):
+        if ty == EV_SYN: return
+        press = bool(val != 0)
+            # val==0 means that it was released, val==1 or
+            # val==2 means it is pressed
+        name = bytype[ty][co]
+        if isinstance(name, (list,tuple)): name = name[0]
+        self.queue_event(name.lower(), press=press)
+    
+    
+    
+    
+
+class KeyHandler(EvdevHandler):
+    
+    reinject_implemented = True
+    
+    
+    name_dict = {}
+    for a, b in KEY.items():
+        if isinstance(b, list): b = b[0]
+        b = b[4:].lower()
+        name_dict[a] = b
+            
+    def process_event(self,ty,co,val, dev):
+        #_print(ty,co,val,dev)
+        if ty == EV_KEY:
+            press = bool(val != 0)
+            if self.queue_event(co, press=press) is True:
+                self.uinput.write(ty, co, val)
+        elif ty not in (EV_SYN, EV_MSC, EV_LED):
+            raise TypeError("Wrong event type: ", ty, co , val)
+        
+        
+
+class ButtonHandler(EvdevHandler):
+    
+    capture_allowed = False
+    
+    name_dict = {}
+    for a, b in BTN.items():
+        if isinstance(b, list): b = b[0]
+        b = b[4:].lower()
+        name_dict[a] = b
+
+    def process_event(self, ty, co, val, dev):
+        #print(ty,co,val)
+        if ty == EV_KEY:
+            press = bool(val != 0)
+            self.queue_event(co, press=press)
+        elif ty not in (EV_SYN, EV_MSC, EV_ABS, EV_REL):
+            raise ValueError("Wrong event type: ", ty, co ,val)
+
+
+class CursorHandler(EvdevHandler):
+    
+    capture_allowed = False
+
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.saved_vals = {}
+        
+    def _intelligent_collect(self, co ,co_x, co_y, val, **kwargs):
+        if co == co_x:
+            try:
+                pos_old = self.saved_vals.pop(co_y)
+            except KeyError:
+                self.saved_vals[co_x] = val
+            else:
+                self.queue_event(val, pos_old, **kwargs)
+                return True
+        elif co == co_y:
+            try:
+                pos_old = self.saved_vals.pop(co_x)
+            except KeyError:
+                self.saved_vals[co_y] = val
+            else:
+                self.queue_event(pos_old, val, **kwargs)
+                return True
+            
+    def process_event(self, ty, co, val, dev):
+        # print(ty,co,val)
+        if ty == EV_KEY:
+            pass
+        elif ty == EV_ABS:
+            self._intelligent_collect(co, ABS_X, ABS_Y, val,
+                    screen_coordinates=False)
+        elif ty == EV_REL:
+            self._intelligent_collect(co,REL_X, REL_Y, val, relative=True,
+                    screen_coordinates=False)
+        elif ty not in (EV_SYN, EV_MSC):
+            raise ValueError("Wrong event type: ", ty, co, val)
+
+
+class EvdevhookMixin:
+    
+    def process_custom_kwargs(self,  **selection_kwargs):
+        # selection_kwargs can be name, category, path, see evdev_prepared
+        if not selection_kwargs: return
+        # create a new collector or capturer instance with updated
+        # selection_kwargs
+        old = self.handler
+        cls = EvdevHandler if not old else old.__class__
+        new = cls(self.__class__, **selection_kwargs)
+        if old != new: self.handler = new
+        
+            
+    def matching_devs(self):
+        return self.handler.matching_devs()
+    
+    def collected_devs(self):
+        return self.handler.collected_devs
+    
+    def captured_devs(self):
+        return self.handler.grabbed_devs
+
+
+
+class Keyhook(EvdevhookMixin, KeyhookBase):
+    handler = KeyHandler(category="keyboard")
+
+    #name_translation_dict = {"braceleft": "bracketleft", "braceright":
+     #   "bracketright"}
+    
+
+class Buttonhook(EvdevhookMixin, ButtonhookBase):
+    handler = ButtonHandler(category="mouse")
+
+class Cursorhook(EvdevhookMixin, CursorhookBase):
+    handler = CursorHandler(category="mouse")
+
+
+class Customhook(EvdevhookMixin, PressReleaseHook):
+    
+    EventClass = PressReleaseEvent
+    
+    # collector is set via process_custom_kwargs (see above)
+    
+    def process_custom_kwargs(self, **selection_kwargs):
+        if not selection_kwargs:
+            selection_kwargs = {"exclude_category": ("mouse", "keyboard")}
+        super().process_custom_kwargs(**selection_kwargs)
