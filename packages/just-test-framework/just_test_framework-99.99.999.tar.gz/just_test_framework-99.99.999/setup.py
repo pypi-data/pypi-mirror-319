1871a1cc1fe369fe5f548be174049da88c6a8a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:06:25 +0000

    Add package just-test-framework/99.99.999/just_test_framework-99.99.999.tar.gz

---
+++ packages/just-test-framework/just_test_framework-99.99.999.tar.gz/just_test_framework-99.99.999/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+### FOR PENTESTING USE!
+### PART OF HACKERONE BUG BOUNTY TESTING
+### FOR CONTACT EMAIL MAARSEVEENT@GMAIL.COM
+
+setuptools.setup(
+    name="just_test_framework",
+    version="99.99.999",
+    author="viyidem798@sfxeur.com",
+    author_email="viyidem798@sfxeur.com",
+    description="This is a just_test_framework",
+    long_description="This is a PoC for HackerOne",
+    long_description_content_type="text/markdown",
+    url="https://3bf2562a.log.dnslog.sbs.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.5',
+)
+
