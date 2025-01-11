0ebf88559c7d4ec4cec05b5f203471fd48120d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:10:09 +0000

    Add package freenit/0.3.6/freenit-0.3.6.tar.gz

---
+++ packages/freenit/freenit-0.3.6.tar.gz/freenit-0.3.6/tests/test_auth.py
@@ -0,0 +1,30 @@
+import pytest
+
+from freenit.models.user import User
+
+from . import factories
+
+
+@pytest.mark.asyncio
+class TestAuth:
+    async def test_login(self, client):
+        user: User = factories.User()
+        await user.save()
+        response = client.login(user=user)
+
+        assert response.status_code == 200
+
+    async def test_register(self, client):
+        data = {
+            "email": "user3@example.com",
+            "password": "Sekrit",
+        }
+        response = client.post("/auth/register", data=data)
+        assert response.status_code == 200
+
+    async def test_refresh(self, client):
+        user: User = factories.User()
+        await user.save()
+        client.login(user=user)
+        response = client.post("/auth/refresh")
+        assert response.status_code == 200
