663c39030d410e070d40da8a66ac8bc6dc5665d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:06:32 +0000

    Add package iaptoolkit/0.3.3/iaptoolkit-0.3.3-py3-none-any.whl

---
+++ packages/iaptoolkit/iaptoolkit-0.3.3-py3-none-any.whl/iaptoolkit/constants.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version
+
+IAPTOOLKIT_VERSION = version("iaptoolkit")
+IAPTOOLKIT_CONFIG_VERSION = 1
+
+# https://cloud.google.com/iap/docs/authentication-howto#authenticating_from_proxy-authorization_header
+# Default auth header used for IAP-aware requests. Can clash with other uses of that header key.
+GOOGLE_IAP_AUTH_HEADER = "Authorization"
+# Alternative auth header used for IAP-aware requests when 'Authorization' clashes. Stripped by IAP if consumed.
+GOOGLE_IAP_AUTH_HEADER_PROXY = "Proxy-Authorization"
