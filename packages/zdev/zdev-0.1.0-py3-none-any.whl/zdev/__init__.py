2934557333fa6483745478e6fe80791966363e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:02 +0000

    Add package zdev/0.1.0/zdev-0.1.0-py3-none-any.whl

---
+++ packages/zdev/zdev-0.1.0-py3-none-any.whl/zdev/__init__.py
@@ -0,0 +1,32 @@
+""" Versatile collection of routines & tools for common development tasks """
+
+__author__ = 'Dr. Marcus Zeller (dsp4444@gmail.com)'
+__version__ = '0.1.0'
+__all__ = []
+
+# simplify (whole) package import (i.e. make all modules accessible)
+# import zdev.base
+# import zdev.ccnx
+# import zdev.colors
+# import zdev.core
+# import zdev.guitils
+# import zdev.indexing
+# import zdev.libdsp
+# import zdev.parallel
+# import zdev.plot
+# import zdev.searchstr
+# import zdev.sigproc
+# import zdev.testing
+# import zdev.validio
+# import zdev.xlcmp
+
+# #-------------- testing ----------------
+
+# def main(verbose=False):
+#     if (verbose):
+#          print("Welcome to the 'zdev' package!")
+#          print("Initialising Python...")
+#     zdev.base.init_session(zdev.base._BASE, zdev.base._ENVIRONMENT, verbose)
+#     if (verbose): print("...done - have phun! ;)")
+
+# main(True) # call 'init_session' with internal defaults if only 'import zdev'!
