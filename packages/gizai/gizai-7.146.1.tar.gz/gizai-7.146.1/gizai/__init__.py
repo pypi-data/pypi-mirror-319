b7c9aea81805c194cdb1f9a794a683b046b73224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:06:20 +0000

    Add package gizai/7.146.1/gizai-7.146.1.tar.gz

---
+++ packages/gizai/gizai-7.146.1.tar.gz/gizai-7.146.1/gizai/__init__.py
@@ -0,0 +1,13 @@
+NAME = "gizai"
+
+ICON = "🔻"
+
+DESCRIPTION = f"{ICON} Access, Automation, Analytics, AI"
+
+VERSION = "7.146.1"
+
+REPO_NAME = "giza"
+
+MARQUEE = "https://github.com/kamangir/giza/raw/main/assets/giza.png"
+
+ALIAS = "giza"
