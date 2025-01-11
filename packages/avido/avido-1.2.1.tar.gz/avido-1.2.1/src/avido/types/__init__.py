cab238c1b8777aee87ac7d8a69956a24e4db07ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:56 +0000

    Add package avido/1.2.1/avido-1.2.1.tar.gz

---
+++ packages/avido/avido-1.2.1.tar.gz/avido-1.2.1/src/avido/types/__init__.py
@@ -0,0 +1,31 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .test import Test as Test
+from .thread import Thread as Thread
+from .application import Application as Application
+from .thread_list import ThreadList as ThreadList
+from .paginated_test import PaginatedTest as PaginatedTest
+from .evaluation_case import EvaluationCase as EvaluationCase
+from .test_run_params import TestRunParams as TestRunParams
+from .evaluation_topic import EvaluationTopic as EvaluationTopic
+from .test_list_params import TestListParams as TestListParams
+from .topic_list_params import TopicListParams as TopicListParams
+from .test_list_response import TestListResponse as TestListResponse
+from .thread_list_params import ThreadListParams as ThreadListParams
+from .topic_create_params import TopicCreateParams as TopicCreateParams
+from .topic_list_response import TopicListResponse as TopicListResponse
+from .ingest_create_params import IngestCreateParams as IngestCreateParams
+from .paginated_application import PaginatedApplication as PaginatedApplication
+from .evaluation_list_params import EvaluationListParams as EvaluationListParams
+from .ingest_create_response import IngestCreateResponse as IngestCreateResponse
+from .application_list_params import ApplicationListParams as ApplicationListParams
+from .webhook_validate_params import WebhookValidateParams as WebhookValidateParams
+from .evaluation_create_params import EvaluationCreateParams as EvaluationCreateParams
+from .evaluation_list_response import EvaluationListResponse as EvaluationListResponse
+from .application_create_params import ApplicationCreateParams as ApplicationCreateParams
+from .application_list_response import ApplicationListResponse as ApplicationListResponse
+from .paginated_evaluation_case import PaginatedEvaluationCase as PaginatedEvaluationCase
+from .webhook_validate_response import WebhookValidateResponse as WebhookValidateResponse
+from .paginated_evaluation_topic import PaginatedEvaluationTopic as PaginatedEvaluationTopic
