85370f1a045d56c9e6409956210d141dc2191d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:11:45 +0000

    Add package ob-metaflow-stubs/6.0.3.131/ob-metaflow-stubs-6.0.3.131.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.131.tar.gz/ob-metaflow-stubs-6.0.3.131/metaflow-stubs/profilers/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.13.2.1+obcheckpoint(0.1.6);ob(v1)                                                    #
+# Generated on 2025-01-08T23:52:28.235585                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ..mf_extensions.outerbounds.profilers import gpu as gpu
+from ..mf_extensions.outerbounds.profilers.gpu import gpu_profile as gpu_profile
+
