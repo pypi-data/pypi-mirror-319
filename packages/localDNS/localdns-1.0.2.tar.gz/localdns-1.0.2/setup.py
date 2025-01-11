0f75e9cd6aa2f4d810b0b0fb4253e52ebb7b0e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:06:17 +0000

    Add package localDNS/1.0.2/localdns-1.0.2.tar.gz

---
+++ packages/localDNS/localdns-1.0.2.tar.gz/localdns-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="localDNS", 
+    version="1.0.2", 
+    description="A Python library for DNS service registration using Zeroconf",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Cld338",
+    author_email="jihuno291@gmail.com",
+    url="https://github.com/cld338/local-dns-service",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "zeroconf>=0.39.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
