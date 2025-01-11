a07b1fa4dfdacec9becf359ccca0346c51fc0de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:15 +0000

    Add package selva/0.18.0/selva-0.18.0.tar.gz

---
+++ packages/selva/selva-0.18.0.tar.gz/selva-0.18.0/tests/web/application/test_middleware.py
@@ -0,0 +1,32 @@
+from asgikit.requests import Request
+from httpx import ASGITransport, AsyncClient
+
+from selva.configuration import Settings
+from selva.configuration.defaults import default_settings
+from selva.web.application import Selva
+
+
+async def my_middleware(callnext, request: Request):
+    async def send(f, event: dict):
+        if event["type"] == "http.response.body":
+            event["body"] = b"Middleware Ok"
+        await f(event)
+
+    request.wrap_asgi(send=send)
+    await callnext(request)
+
+
+async def test_middleware():
+    settings = Settings(
+        default_settings
+        | {
+            "application": f"{__package__}.application",
+            "middleware": [f"{__package__}.test_middleware:my_middleware"],
+        }
+    )
+    app = Selva(settings)
+    await app._lifespan_startup()
+
+    client = AsyncClient(transport=ASGITransport(app=app))
+    response = await client.get("http://localhost:8000/")
+    assert response.text == "Middleware Ok"
