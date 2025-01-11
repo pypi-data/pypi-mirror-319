2429e576ab9174b457ff009cab82994160dbd8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:05:00 +0000

    Add package fmu-dataio/2.9.0/fmu_dataio-2.9.0-py3-none-any.whl

---
+++ packages/fmu-dataio/fmu_dataio-2.9.0-py3-none-any.whl/fmu/dataio/_metadata.py
@@ -0,0 +1,119 @@
+"""Module for DataIO metadata.
+
+This contains the _MetaData class which collects and holds all relevant metadata
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Final
+
+from ._logging import null_logger
+from ._model import fields, schema
+from ._model.global_configuration import GlobalConfiguration
+from ._model.product import Product
+from .exceptions import InvalidMetadataError
+from .providers._filedata import FileDataProvider
+from .providers.objectdata._provider import objectdata_provider_factory
+
+if TYPE_CHECKING:
+    from . import types
+    from .dataio import ExportData
+    from .providers._fmu import FmuProvider
+    from .providers.objectdata._base import ObjectDataProvider
+
+logger: Final = null_logger(__name__)
+
+
+def _get_meta_filedata(
+    dataio: ExportData,
+    obj: types.Inferrable,
+    objdata: ObjectDataProvider,
+    fmudata: FmuProvider | None,
+) -> fields.File:
+    """Derive metadata for the file."""
+    return FileDataProvider(
+        dataio=dataio,
+        objdata=objdata,
+        runpath=fmudata.get_runpath() if fmudata else None,
+        obj=obj,
+    ).get_metadata()
+
+
+def _get_meta_fmu(fmudata: FmuProvider) -> fields.FMU | None:
+    try:
+        return fmudata.get_metadata()
+    except InvalidMetadataError:
+        return None
+
+
+def _get_meta_access(dataio: ExportData) -> fields.SsdlAccess:
+    return fields.SsdlAccess(
+        asset=(
+            dataio.config.access.asset
+            if isinstance(dataio.config, GlobalConfiguration)
+            else fields.Asset(name="")
+        ),
+        classification=dataio._classification,
+        ssdl=fields.Ssdl(
+            access_level=dataio._classification,
+            rep_include=dataio._rep_include,
+        ),
+    )
+
+
+def _get_meta_display(
+    dataio: ExportData, objdata: ObjectDataProvider
+) -> fields.Display:
+    return fields.Display(name=dataio.display_name or objdata.name)
+
+
+def generate_export_metadata(
+    obj: types.Inferrable,
+    dataio: ExportData,
+    fmudata: FmuProvider | None = None,
+    product: Product | None = None,
+) -> schema.InternalObjectMetadata:
+    """
+    Main function to generate the full metadata
+
+    Metadata has basically these different providers:
+
+    * The FmuProvider, which is typically ERT, which provides a kind of an
+      environment. The FmuProvider may also be legally missing, e.g. when
+      running interactive RMS project
+
+    * The User data, which is a mix of global variables and user set keys
+
+    * The data object itself ie. ObjectDataProvider
+
+    Then, metadata are stored as:
+
+    * meta_dollars: holding 'system' state: $schema, version, source
+    * meta_class: a simple string
+    * meta_tracklog: dict of events (datdtime, user)
+    * meta_fmu: nested dict of model, case, etc (complex)
+    * meta_file: dict of paths and checksums
+    * meta_masterdata: dict of (currently) smda masterdata
+    * meta_access: dict with name of field + access rules
+    * meta_objectdata: the data block, may be complex
+    * meta_display: dict of default display settings (experimental)
+
+    """
+
+    objdata = objectdata_provider_factory(obj, dataio, product)
+
+    return schema.InternalObjectMetadata(  # type: ignore[call-arg]
+        class_=objdata.classname,
+        fmu=_get_meta_fmu(fmudata) if fmudata else None,
+        masterdata=(
+            dataio.config.masterdata
+            if isinstance(dataio.config, GlobalConfiguration)
+            else None
+        ),
+        access=_get_meta_access(dataio),
+        data=objdata.get_metadata(),
+        file=_get_meta_filedata(dataio, obj, objdata, fmudata),
+        tracklog=fields.Tracklog.initialize(),
+        display=_get_meta_display(dataio, objdata),
+        preprocessed=dataio.preprocessed,
+    )
---
+++ packages/fmu-dataio/fmu_dataio-2.9.0-py3-none-any.whl/fmu/dataio/_model/root.py
@@ -0,0 +1,295 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Dict, List, Literal, TypeVar, Union
+
+from pydantic import (
+    BaseModel,
+    Field,
+    GetJsonSchemaHandler,
+    RootModel,
+    model_validator,
+)
+from pydantic.json_schema import GenerateJsonSchema
+from typing_extensions import Annotated
+
+from fmu.dataio._definitions import FmuSchemas, SchemaBase
+
+from .data import AnyData
+from .enums import FMUClass
+from .fields import (
+    FMU,
+    Access,
+    Display,
+    File,
+    FMUBase,
+    FMUIteration,
+    FMURealization,
+    Masterdata,
+    SsdlAccess,
+    Tracklog,
+)
+
+if TYPE_CHECKING:
+    from typing import Any, Final, Mapping
+
+    from pydantic_core import CoreSchema
+
+T = TypeVar("T", Dict, List, object)
+
+
+class MetadataBase(BaseModel):
+    """Base model for all root metadata models generated."""
+
+    class_: FMUClass = Field(alias="class", title="metadata_class")
+    """The class of this metadata object. Functions as the discriminating field."""
+
+    masterdata: Masterdata
+    """The ``masterdata`` block contains information related to masterdata.
+    See :class:`Masterdata`."""
+
+    tracklog: Tracklog
+    """The ``tracklog`` block contains a record of events recorded on these data.
+    See :class:`Tracklog`."""
+
+    source: Literal["fmu"]
+    """The source of this data. Defaults to 'fmu'."""
+
+    version: Literal["0.8.0"]
+    """The version of the schema that generated this data."""
+
+
+class CaseMetadata(MetadataBase):
+    """The FMU metadata model for an FMU case.
+
+    A case represent a set of iterations that belong together, either by being part of
+    the same run (i.e. history matching) or by being placed together by the user,
+    corresponding to /scratch/<asset>/<user>/<my case name>/.
+    """
+
+    class_: Literal[FMUClass.case] = Field(alias="class", title="metadata_class")
+    """The class of this metadata object. In this case, always an FMU case."""
+
+    fmu: FMUBase
+    """The ``fmu`` block contains all attributes specific to FMU.
+    See :class:`FMUBase`."""
+
+    access: Access
+    """The ``access`` block contains information related to access control for
+    this data object. See :class:`Access`."""
+
+
+class IterationMetadata(MetadataBase):
+    """The FMU metadata model for an FMU Iteration.
+
+    An object representing a single Iteration of a specific case.
+    """
+
+    class_: Literal[FMUClass.iteration] = Field(alias="class", title="metadata_class")
+    """The class of this metadata object. In this case, always an FMU iteration."""
+
+    fmu: FMUIteration
+    """The ``fmu`` block contains all attributes specific to FMU.
+    See :class:`FMU`."""
+
+    access: Access
+    """The ``access`` block contains information related to access control for
+    this data object. See :class:`Access`."""
+
+
+class RealizationMetadata(MetadataBase):
+    """The FMU metadata model for an FMU Realization.
+
+    An object representing a single Realization of a specific Iteration.
+    """
+
+    class_: Literal[FMUClass.realization] = Field(alias="class", title="metadata_class")
+    """The class of this metadata object. In this case, always an FMU realization."""
+
+    fmu: FMURealization
+    """The ``fmu`` block contains all attributes specific to FMU.
+    See :class:`FMU`."""
+
+    access: Access
+    """The ``access`` block contains information related to access control for
+    this data object. See :class:`Access`."""
+
+
+class ObjectMetadata(MetadataBase):
+    """The FMU metadata model for a given data object."""
+
+    class_: Literal[
+        FMUClass.surface,
+        FMUClass.table,
+        FMUClass.cpgrid,
+        FMUClass.cpgrid_property,
+        FMUClass.polygons,
+        FMUClass.cube,
+        FMUClass.well,
+        FMUClass.points,
+        FMUClass.dictionary,
+    ] = Field(
+        alias="class",
+        title="metadata_class",
+    )
+    """The class of the data object being exported and described by the metadata
+    contained herein."""
+
+    fmu: FMU
+    """The ``fmu`` block contains all attributes specific to FMU.
+    See :class:`FMU`."""
+
+    access: SsdlAccess
+    """The ``access`` block contains information related to access control for
+    this data object. See :class:`SsdlAccess`."""
+
+    data: AnyData
+    """The ``data`` block contains information about the data contained in this
+    object. See :class:`data.AnyData`."""
+
+    file: File
+    """ The ``file`` block contains references to this data object as a file on a disk.
+    See :class:`File`."""
+
+    display: Display
+    """ The ``display`` block contains information related to how this data object
+    should/could be displayed. See :class:`Display`."""
+
+
+class Root(
+    RootModel[
+        Annotated[
+            Union[
+                CaseMetadata,
+                ObjectMetadata,
+                RealizationMetadata,
+                IterationMetadata,
+            ],
+            Field(discriminator="class_"),
+        ]
+    ]
+):
+    @model_validator(mode="after")
+    def _check_class_data_spec(self) -> Root:
+        if (
+            self.root.class_ in (FMUClass.table, FMUClass.surface)
+            and hasattr(self.root, "data")
+            and self.root.data.root.spec is None
+        ):
+            raise ValueError(
+                "When 'class' is 'table' or 'surface', "
+                "'data' must contain the 'spec' field."
+            )
+        return self
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls,
+        core_schema: CoreSchema,
+        handler: GetJsonSchemaHandler,
+    ) -> dict[str, object]:
+        json_schema = super().__get_pydantic_json_schema__(core_schema, handler)
+        json_schema = handler.resolve_ref_schema(json_schema)
+        json_schema.update(
+            {
+                "if": {"properties": {"class": {"enum": ["table", "surface"]}}},
+                "then": {"properties": {"data": {"required": ["spec"]}}},
+            }
+        )
+        return json_schema
+
+
+class FmuResultsSchema(SchemaBase):
+    """The main metadata export describing the results."""
+
+    VERSION: str = "0.8.0"
+    FILENAME: str = "fmu_results.json"
+    PATH: Path = FmuSchemas.PATH / VERSION / FILENAME
+
+    class FmuResultsGenerateJsonSchema(GenerateJsonSchema):
+        contractual: Final[list[str]] = [
+            "access",
+            "class",
+            "data.alias",
+            "data.bbox",
+            "data.content",
+            "data.format",
+            "data.geometry",
+            "data.grid_model",
+            "data.is_observation",
+            "data.is_prediction",
+            "data.name",
+            "data.offset",
+            "data.product.name",
+            "data.seismic.attribute",
+            "data.spec.columns",
+            "data.stratigraphic",
+            "data.stratigraphic_alias",
+            "data.tagname",
+            "data.time",
+            "data.vertical_domain",
+            "file.checksum_md5",
+            "file.relative_path",
+            "file.size_bytes",
+            "fmu.aggregation.operation",
+            "fmu.aggregation.realization_ids",
+            "fmu.case",
+            "fmu.context.stage",
+            "fmu.iteration.name",
+            "fmu.iteration.uuid",
+            "fmu.model",
+            "fmu.realization.id",
+            "fmu.realization.is_reference",
+            "fmu.realization.name",
+            "fmu.realization.uuid",
+            "fmu.workflow",
+            "masterdata",
+            "source",
+            "tracklog.datetime",
+            "tracklog.event",
+            "tracklog.user.id",
+            "version",
+        ]
+
+        def _remove_format_path(self, obj: T) -> T:
+            """
+            Removes entries with key "format" and value "path" from dictionaries. This
+            adjustment is necessary because JSON Schema does not recognize the "format":
+            "path", while OpenAPI does. This function is used in contexts where OpenAPI
+            specifications are not applicable.
+            """
+
+            if isinstance(obj, dict):
+                return {
+                    k: self._remove_format_path(v)
+                    for k, v in obj.items()
+                    if not (k == "format" and v == "path")
+                }
+
+            if isinstance(obj, list):
+                return [self._remove_format_path(element) for element in obj]
+
+            return obj
+
+        def generate(
+            self,
+            schema: Mapping[str, Any],
+            mode: Literal["validation", "serialization"] = "validation",
+        ) -> dict[str, Any]:
+            json_schema = super().generate(schema, mode=mode)
+            json_schema["$schema"] = self.schema_dialect
+            json_schema["$id"] = FmuResultsSchema.url()
+            json_schema["$contractual"] = self.contractual
+
+            # sumo-core's validator does not recognize these.
+            del json_schema["discriminator"]["mapping"]
+            del json_schema["$defs"]["AnyData"]["discriminator"]["mapping"]
+            del json_schema["$defs"]["AnyProduct"]["discriminator"]["mapping"]
+
+            return self._remove_format_path(json_schema)
+
+    @staticmethod
+    def dump() -> dict[str, Any]:
+        return Root.model_json_schema(
+            schema_generator=FmuResultsSchema.FmuResultsGenerateJsonSchema
+        )
