903c2fb3918a3d06034257842f1d125be7e2a76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:06:03 +0000

    Add package jinko-sdk/0.4.7/jinko_sdk-0.4.7-py3-none-any.whl

---
+++ packages/jinko-sdk/jinko_sdk-0.4.7-py3-none-any.whl/jinko_stats/__init__.py
@@ -0,0 +1,9 @@
+"""
+jinko_stats package initialization.
+
+This package provides statistics features for interacting with the Jinko API.
+"""
+
+from .calibration import *
+
+__all__ = ["INNCalibrator", "INN", "Subloss"]
