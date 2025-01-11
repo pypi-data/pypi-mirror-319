48df022f19a32601f946d7302b223ee2bfaa770f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:07:21 +0000

    Add package start-watching-case-list/0.1.5/start_watching_case_list-0.1.5-py3-none-any.whl

---
+++ packages/start-watching-case-list/start_watching_case_list-0.1.5-py3-none-any.whl/start_watching_case_list/main.py
@@ -0,0 +1,19 @@
+import argparse
+
+from start_watching_case_list.funcs.auth import login
+
+
+parser = argparse.ArgumentParser(description="Get data from case services")
+
+
+def main():
+    parser.add_argument("-t", "--task", type=str, help="[login]")
+    args = parser.parse_args()
+    if args.task == "login":
+        login()
+    else:
+        print("Please enter valid task")
+
+
+if __name__ == "__main__":
+    main()
