8d27062b01a21b83f8134097ac7001c835621d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:05:08 +0000

    Add package jupiter-python-sdk-public/0.0.2.2/jupiter_python_sdk_public-0.0.2.2.tar.gz

---
+++ packages/jupiter-python-sdk-public/jupiter_python_sdk_public-0.0.2.2.tar.gz/jupiter_python_sdk_public-0.0.2.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2.2'
+DESCRIPTION = 'Jupiter Python SDK'
+LONG_DESCRIPTION = 'This package allows the use of Jupiter decentralized exchange features on Solana using Python.'
+
+# Setting up
+setup(
+    name="jupiter-python-sdk-public",
+    version=VERSION,
+    author="Samuel Sallee",
+    author_email="sam@tenxor.sh",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    license=('LICENSE.txt'),
+    install_requires=[
+    'base58',
+    'solders',
+    'solana',
+    'httpx',
+    'anchorpy',
+    ],
+    keywords=['python', 'solana', 'jupiter', 'dex', 'trading', 'sdk'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
