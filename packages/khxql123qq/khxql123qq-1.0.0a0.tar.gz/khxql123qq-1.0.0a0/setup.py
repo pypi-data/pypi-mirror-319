c9d87493db9eca7922de8e6dcc323fa26cf5127d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:05:14 +0000

    Add package khxql123qq/1.0.0a0/khxql123qq-1.0.0a0.tar.gz

---
+++ packages/khxql123qq/khxql123qq-1.0.0a0.tar.gz/khxql123qq-1.0.0a0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="khxql123qq",
+    version="1.0.0-alpha",
+    author="Khaled Mahmoud Al Jamous",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/khxql/Scopium",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+        "Development Status :: 3 - Alpha"
+    ]    
+)
+
