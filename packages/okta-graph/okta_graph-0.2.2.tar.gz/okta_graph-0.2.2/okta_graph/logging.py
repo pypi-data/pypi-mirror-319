d3f573435ca51d6263e05569e090cf3388d24d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 11:05:46 +0000

    Add package okta-graph/0.2.2/okta_graph-0.2.2.tar.gz

---
+++ packages/okta-graph/okta_graph-0.2.2.tar.gz/okta_graph-0.2.2/okta_graph/logging.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import logging
+import os
+import sys
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+os.environ["LOGURU_LEVEL"] = LOG_LEVEL
+
+from loguru import logger
+
+
+JSON_LOGS = True if os.environ.get("JSON_LOGS", "0") == "1" else False
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # find caller from where originated the logged message
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+def setup_logging():
+    # intercept everything at the root logger
+    logging.root.handlers = [InterceptHandler()]
+    logging.root.setLevel(LOG_LEVEL)
+
+    # remove every other logger's handlers
+    # and propagate to root logger
+    for name in logging.root.manager.loggerDict.keys():
+        logging.getLogger(name).handlers = []
+        logging.getLogger(name).propagate = True
+
+    # configure loguru
+    logger.configure(handlers=[{"sink": sys.stdout, "serialize": JSON_LOGS}])
+    return logger
+
+
+LOGGER = setup_logging()
