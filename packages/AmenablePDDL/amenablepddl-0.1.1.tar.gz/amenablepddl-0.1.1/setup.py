28205c6ad9ce4f6bf5d18a3908e2d1befd281ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:10:13 +0000

    Add package AmenablePDDL/0.1.1/amenablepddl-0.1.1.tar.gz

---
+++ packages/AmenablePDDL/amenablepddl-0.1.1.tar.gz/amenablepddl-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AmenablePDDL",
+    version="0.1.1",
+    author="Julius Arolovitch",
+    author_email="jarolovi@andrew.cmu.edu",
+    description="A high-level PDDL parsing and planning interface for implementing common classical planning algorithms. ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/juliusarolovitch/Amenable-PDDL",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "pddl",
+        "lark-parser",
+        "typing-extensions",
+    ],
+)
