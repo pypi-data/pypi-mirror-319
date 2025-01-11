a62b4d740ff75a4bef3d00e4941cc67fb87d18c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:55 +0000

    Add package guara/0.0.6/guara-0.0.6.tar.gz

---
+++ packages/guara/guara-0.0.6.tar.gz/guara-0.0.6/examples/rest_api/test_test_api.py
@@ -0,0 +1,16 @@
+from examples.rest_api import echo_api
+from guara.transaction import Application
+from guara import it
+
+
+class TestEchoApi:
+    def setup_method(self, method):
+        self._app = Application(None)
+
+    def test_get(self):
+        expected = {"foo1": "bar1", "foo2": "bar2"}
+        self._app.at(echo_api.Get, path=expected).asserts(it.IsEqualTo, expected)
+
+    def test_post(self):
+        expected = "This is expected to be sent back as part of response body."
+        self._app.at(echo_api.Post, data=expected).asserts(it.IsEqualTo, expected)
---
+++ packages/guara/guara-0.0.6.tar.gz/guara-0.0.6/examples/unit_test/README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+Even being possible to use this framweork to unit tests it is overkill. 
