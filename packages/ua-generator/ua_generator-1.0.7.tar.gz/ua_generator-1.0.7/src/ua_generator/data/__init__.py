7a6c9740e9a6f422bbe3ec716e07031c5fb74749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:05:27 +0000

    Add package ua-generator/1.0.7/ua_generator-1.0.7.tar.gz

---
+++ packages/ua-generator/ua_generator-1.0.7.tar.gz/ua_generator-1.0.7/src/ua_generator/data/__init__.py
@@ -0,0 +1,14 @@
+"""
+Random User-Agent
+Copyright: 2022-2024 Ekin Karadeniz (github.com/iamdual)
+License: Apache License 2.0 
+"""
+
+DEVICES = ('desktop', 'mobile')
+
+PLATFORMS = ('windows', 'macos', 'ios', 'linux', 'android')
+PLATFORMS_DESKTOP = ('windows', 'macos', 'linux')  # Platforms on desktop devices
+PLATFORMS_MOBILE = ('ios', 'android')  # Platforms on mobile devices
+
+BROWSERS = ('chrome', 'edge', 'firefox', 'safari')
+BROWSERS_SUPPORT_CH = ('chrome', 'edge')  # Browsers that support Client Hints
