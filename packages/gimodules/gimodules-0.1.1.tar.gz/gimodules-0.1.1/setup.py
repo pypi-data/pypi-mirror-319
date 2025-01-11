e04124851126b1fab3956a4ec888dd311425b725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:56 +0000

    Add package gimodules/0.1.1/gimodules-0.1.1.tar.gz

---
+++ packages/gimodules/gimodules-0.1.1.tar.gz/gimodules-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Python package to deliver a Gantner cloud interface'
+LONG_DESCRIPTION = ''
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    
+# Setting up
+setup(
+    name="gimodules",
+    version=VERSION,
+    author="gimodules devs",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=required,
+    keywords=['python'],
+    python_requires='>=3.8',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
