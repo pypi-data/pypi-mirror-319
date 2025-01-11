ba5c7eff76193781683d02f954a5724d957e08ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:05:17 +0000

    Add package ajent/0.1/ajent-0.1.tar.gz

---
+++ packages/ajent/ajent-0.1.tar.gz/ajent-0.1/ajent/openai/openai_client.py
@@ -0,0 +1,22 @@
+from ajent import LLMClient
+from openai import OpenAI
+from typing import Any, Dict, List
+from ..response_serializer import ResponseSerializer
+
+
+class OpenAIClient(LLMClient):
+    def __init__(self, token: str):
+        self._client = OpenAI(api_key=token)
+        self._response_serializer = ResponseSerializer()
+
+    def send(self, messages: List[Dict], tools: List[Dict], model: str) -> Any:
+        response = self._client.chat.completions.create(
+            model=model,
+            messages=messages,
+            tools=tools
+        )
+        message = response.choices[0].message
+        return self.serialize_response(message)
+    
+    def serialize_response(self, response: Any) -> Dict:
+        return self._response_serializer.serialize(response)
