5e62507c299ef76a1bb9203488506861f9a0ea0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:06:33 +0000

    Add package just-framework/1.3.3/just_framework-1.3.3.tar.gz

---
+++ packages/just-framework/just_framework-1.3.3.tar.gz/just_framework-1.3.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+### FOR PENTESTING USE!
+### PART OF HACKERONE BUG BOUNTY TESTING
+### FOR CONTACT EMAIL MAARSEVEENT@GMAIL.COM
+
+setuptools.setup(
+    name="just_framework",
+    version="1.3.3",
+    author="viyidem7918@sfxeur.com",
+    author_email="viyidem7918@sfxeur.com",
+    description="This is a just_framework",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://3bf2562dasdaa.log.dn1slog.sbs.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.5',
+)
+
