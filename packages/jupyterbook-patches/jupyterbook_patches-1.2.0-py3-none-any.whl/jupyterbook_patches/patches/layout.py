707bf8c60231cde4cc8fa5bdb7db11de11519172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:32 +0000

    Add package jupyterbook-patches/1.2.0/jupyterbook_patches-1.2.0-py3-none-any.whl

---
+++ packages/jupyterbook-patches/jupyterbook_patches-1.2.0-py3-none-any.whl/jupyterbook_patches/patches/layout.py
@@ -0,0 +1,10 @@
+from jupyterbook_patches.patches import BasePatch, logger
+
+
+class DarkModePatch(BasePatch):
+    name = "layout"
+
+    def initialize(self, app):
+        logger.info("Initializing layout patch")
+        app.add_css_file(filename="fix_admonition_style.css")
+        app.add_css_file(filename="fix_dropdown_style.css")
