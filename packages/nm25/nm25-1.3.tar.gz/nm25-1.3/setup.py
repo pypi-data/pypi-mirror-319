3f8be94630272758f07fb308acdd1dd16a866a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:33 +0000

    Add package nm25/1.3/nm25-1.3.tar.gz

---
+++ packages/nm25/nm25-1.3.tar.gz/nm25-1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+libs = open("requirements.txt").read().splitlines()
+setup(
+    name="nm25",
+    version="1.3",
+    url="",
+    license="MIT",
+    author="",
+    author_email="",
+    platforms=["any"],
+    package_data={
+        "nm25": ["data/th/*.md"],
+    },
+)
