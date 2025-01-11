59a180df7f41c5e04836830876da71cf33c66cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:51 +0000

    Add package python-project-gama/1.0.0/python_project_gama-1.0.0-py3-none-any.whl

---
+++ packages/python-project-gama/python_project_gama-1.0.0-py3-none-any.whl/python_project_gama/example.py
@@ -0,0 +1,22 @@
+"""Example of code."""
+
+
+def hello(name: str) -> str:
+    """Just an greetings example.
+
+    Parameters
+    ----------
+    name : str
+        Name to greet.
+
+    Returns
+    -------
+    str
+        Greeting message.
+
+    Examples
+    --------
+    >>> hello("Manoel")
+    'Hello Manoel!'
+    """
+    return f"Hello {name}!"
