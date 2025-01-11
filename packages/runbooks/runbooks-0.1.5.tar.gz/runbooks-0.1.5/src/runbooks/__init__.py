3e25d3458c8ec3abb7f983f9caefe27f103277c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:08:48 +0000

    Add package runbooks/0.1.5/runbooks-0.1.5.tar.gz

---
+++ packages/runbooks/runbooks-0.1.5.tar.gz/runbooks-0.1.5/src/runbooks/__init__.py
@@ -0,0 +1,10 @@
+"""
+runbooks
+
+Provides utility functions for math operations, AWS S3 interactions, and data management.
+"""
+
+# __version__ = "0.1.5"
+
+# from .toolkit import add, divide, multiply, subtract
+# from .s3 import S3Client
