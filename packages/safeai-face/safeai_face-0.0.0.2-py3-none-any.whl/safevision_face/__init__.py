0e6a50536a2aa28b716d5cccb124ab217f81d4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:05:27 +0000

    Add package safeai-face/0.0.0.2/safeai_face-0.0.0.2-py3-none-any.whl

---
+++ packages/safeai-face/safeai_face-0.0.0.2-py3-none-any.whl/safevision_face/__init__.py
@@ -0,0 +1,8 @@
+from safevision_face import face_detection, face_extraction, db_set, db_insert, db_search
+
+from .database import db_set, db_insert, db_search
+from .detection import face_detection
+from .extraction import face_extraction
+
+__version__ = '0.0.0.2'
+
