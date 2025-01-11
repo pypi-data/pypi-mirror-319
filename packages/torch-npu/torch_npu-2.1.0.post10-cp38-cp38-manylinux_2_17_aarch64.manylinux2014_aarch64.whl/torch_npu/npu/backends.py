434f93a34c6c1bbf9d29fab1bb702e3af531138b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:07:05 +0000

    Add package torch-npu/2.1.0.post10/torch_npu-2.1.0.post10-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/torch-npu/torch_npu-2.1.0.post10-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/npu/backends.py
@@ -0,0 +1,8 @@
+"""
+This file will be discarded.
+"""
+
+from torch_npu.npu._backends import flash_sdp_enabled, enable_flash_sdp, mem_efficient_sdp_enabled, \
+    enable_mem_efficient_sdp, math_sdp_enabled, enable_math_sdp, sdp_kernel, preferred_linalg_library
+
+__all__ = []
