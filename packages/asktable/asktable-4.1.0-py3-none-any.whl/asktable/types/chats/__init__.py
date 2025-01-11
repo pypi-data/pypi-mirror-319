e2d7ce7fff03f12023c2508f49e80e3b333d955d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:05:28 +0000

    Add package asktable/4.1.0/asktable-4.1.0-py3-none-any.whl

---
+++ packages/asktable/asktable-4.1.0-py3-none-any.whl/asktable/types/chats/__init__.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .message_list_params import MessageListParams as MessageListParams
+from .message_list_response import MessageListResponse as MessageListResponse
+from .message_retrieve_response import MessageRetrieveResponse as MessageRetrieveResponse
+from .message_send_message_params import MessageSendMessageParams as MessageSendMessageParams
+from .message_send_message_response import MessageSendMessageResponse as MessageSendMessageResponse
