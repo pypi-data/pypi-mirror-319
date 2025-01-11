a9fb5203508d67dee89aeecee691ff69bda51e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:05:07 +0000

    Add package smartwordcloudgenerator/0.3/smartwordcloudgenerator-0.3.tar.gz

---
+++ packages/smartwordcloudgenerator/smartwordcloudgenerator-0.3.tar.gz/smartwordcloudgenerator-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="smartwordcloudgenerator",
+    version="0.3",
+    description="Generate smart word cloud.",
+    author="刘晔玮  Yewei Liu (Lewis)",
+    author_email="liuyeweilewis@gmail.com",
+    url="https://github.com/Yewei-Liu/smart_word_cloud.git",
+    license="MIT",
+    install_requires=[
+        'nltk',
+        'Pillow',
+    ]
+)
