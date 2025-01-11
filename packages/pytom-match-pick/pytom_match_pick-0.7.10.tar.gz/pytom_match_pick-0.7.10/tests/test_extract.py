a425490f44d480230e495aefa0f3dbefefa09b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:05:03 +0000

    Add package pytom-match-pick/0.7.10/pytom_match_pick-0.7.10.tar.gz

---
+++ packages/pytom-match-pick/pytom_match_pick-0.7.10.tar.gz/pytom_match_pick-0.7.10/tests/test_extract.py
@@ -0,0 +1,22 @@
+import unittest
+import numpy as np
+from pytom_tm.extract import predict_tophat_mask
+
+
+class TestExtract(unittest.TestCase):
+    def test_predict_tophat_mask(self):
+        rng = np.random.default_rng(0)
+        volume = rng.normal(loc=0, scale=0.1, size=(50,) * 3)  # generate random peaks
+        tophat_mask = predict_tophat_mask(volume)
+        self.assertEqual(
+            tophat_mask.shape,
+            volume.shape,
+            msg="tophat mask should have same size as input",
+        )
+        self.assertEqual(
+            tophat_mask.dtype, bool, msg="predicted tophat mask should be boolean"
+        )
+
+    # part of the extraction test in test_tmjob.py
+    # def test_extract_job_with_tomogram_mask(self):
+    #    pass
