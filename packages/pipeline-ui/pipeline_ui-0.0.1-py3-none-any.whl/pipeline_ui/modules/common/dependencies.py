c9c545ae3bbddaa3451e02011c71325f8ae0ac8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 11:05:27 +0000

    Add package pipeline-ui/0.0.1/pipeline_ui-0.0.1-py3-none-any.whl

---
+++ packages/pipeline-ui/pipeline_ui-0.0.1-py3-none-any.whl/pipeline_ui/modules/common/dependencies.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development") #TODO to change to production when launch
+if ENVIRONMENT == "development":
+    INTERNAL_SERVER_URL = "http://localhost:8014"
+elif ENVIRONMENT == "production":
+    INTERNAL_SERVER_URL = "https://api.pipeline-ui.com"
+else:
+    raise ValueError(f"Invalid environment: {ENVIRONMENT}")
