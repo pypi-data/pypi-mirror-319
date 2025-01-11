52aa671b4e624ab75d7fe855318fdf02988af3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:40 +0000

    Add package SqrtRange/0.0.2/sqrtrange-0.0.2.tar.gz

---
+++ packages/SqrtRange/sqrtrange-0.0.2.tar.gz/sqrtrange-0.0.2/tests/test_SqrtRange.py
@@ -0,0 +1,9 @@
+import SqrtRange
+
+testfirstnumber = -10
+testsecondnumber = 10
+logFile = "log.log"
+clearLog = True
+
+def test():
+    SqrtRange.Calculate(testfirstnumber, testsecondnumber, logFile, clearLog)
