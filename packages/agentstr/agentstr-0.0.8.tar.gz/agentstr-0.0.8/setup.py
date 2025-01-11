6c4309d3b93ff25f4ebd8d7e426e6d2407be5a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:05:22 +0000

    Add package agentstr/0.0.8/agentstr-0.0.8.tar.gz

---
+++ packages/agentstr/agentstr-0.0.8.tar.gz/agentstr-0.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agentstr",
+    version="0.0.8",
+    author="Alejandro Gil",
+    description="A library for collaborative AI agents",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/synvya/agentstr",
+    packages=find_packages(where="."),
+    install_requires=[
+        "phi>=2.7.0", "openai>=1.50.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
