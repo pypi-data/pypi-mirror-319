fa12304297b39a4240593c18296d4875a4aee93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:05:18 +0000

    Add package causal-testing-framework/9.0.2/causal_testing_framework-9.0.2-py3-none-any.whl

---
+++ packages/causal-testing-framework/causal_testing_framework-9.0.2-py3-none-any.whl/causal_testing/__init__.py
@@ -0,0 +1,14 @@
+"""
+This is the CausalTestingFramework Module
+It contains 5 subpackages:
+data_collection
+generation
+json_front
+specification
+testing
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
