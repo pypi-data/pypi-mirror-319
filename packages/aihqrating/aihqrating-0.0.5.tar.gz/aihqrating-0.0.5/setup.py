269cfeaeea33f9f7d442a656aaf60ae1f1f5a067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:12:44 +0000

    Add package aihqrating/0.0.5/aihqrating-0.0.5.tar.gz

---
+++ packages/aihqrating/aihqrating-0.0.5.tar.gz/aihqrating-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aihqrating',  
+    version='0.0.5',         
+    author='evans.zhu',
+    author_email='evanszhu2001@gmail.com',
+    description='TBD',
+    long_description=' TBD',
+    long_description_content_type='text/markdown',
+    packages=['aihqrating'],
+    package_data={'aihqrating': ['images/*']},
+    install_requires=['numpy', 'matplotlib', 'scipy','pandas','scikit-learn', 'Werkzeug==2.3.8','Flask==2.2.2','Flask-SocketIO==5.3.6','sentencepiece==0.2.0','torch==2.0.1','transformers==4.29.2','openai==1.18.0'],  
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
