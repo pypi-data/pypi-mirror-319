27d1b703b07287b2e855c0fa4d6396de1c1b47bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:57 +0000

    Add package python-web3-wallet/0.0.13/python_web3_wallet-0.0.13.tar.gz

---
+++ packages/python-web3-wallet/python_web3_wallet-0.0.13.tar.gz/python_web3_wallet-0.0.13/python_web3_wallet/frontend/src/index.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { Chain, gnosis, localhost } from "wagmi/chains";
+import PythonWeb3Wallet from "./PythonWeb3Wallet";
+
+const queryClient = new QueryClient();
+
+// for using with forked-Gnosis chain
+// const gnosisFoundryLocalhost = {
+//   id: 99,
+//   name: 'Gnosis-Fork',
+//   nativeCurrency: { name: 'Ether', symbol: 'xDAI', decimals: 18 },
+//   rpcUrls: {
+//     default: { http: ['http://127.0.0.1:8545'] },
+//   },
+// } as const satisfies Chain;
+
+const config = getDefaultConfig({
+  appName: 'app',
+  projectId: process.env.REACT_APP_RAINBOW_PROJECT_ID!,
+  chains: [
+    gnosis,
+    //gnosisFoundryLocalhost
+  ],
+});
+
+ReactDOM.render(
+  <WagmiProvider config={config}>
+  <QueryClientProvider client={queryClient}>
+    <RainbowKitProvider>
+      <PythonWeb3Wallet />
+    </RainbowKitProvider>
+  </QueryClientProvider>
+</WagmiProvider>,
+  document.getElementById("root")
+)
