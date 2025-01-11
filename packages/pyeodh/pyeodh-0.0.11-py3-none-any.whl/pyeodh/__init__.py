55d026913f73d4476772dd9321054f7a9b39a57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:40 +0000

    Add package pyeodh/0.0.11/pyeodh-0.0.11-py3-none-any.whl

---
+++ packages/pyeodh/pyeodh-0.0.11-py3-none-any.whl/pyeodh/__init__.py
@@ -0,0 +1,8 @@
+from pyeodh.client import Client
+from pyeodh.logger import set_log_level
+from pyeodh.logger import setup as setup_logs
+
+setup_logs()
+
+__version__ = "0.0.11"
+__all__ = ["Client", "set_log_level"]
