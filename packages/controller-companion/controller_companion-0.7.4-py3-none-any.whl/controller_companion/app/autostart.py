f9628b602f1c826606eac8bb41478ffa387e892b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:19 +0000

    Add package controller-companion/0.7.4/controller_companion-0.7.4-py3-none-any.whl

---
+++ packages/controller-companion/controller_companion-0.7.4-py3-none-any.whl/controller_companion/app/autostart.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+from typing import Any
+import winreg
+from controller_companion.app import resources
+from controller_companion.app.utils import OperatingSystem, get_os
+from controller_companion.logs import logger
+
+REG_PATH = r"Software\Microsoft\Windows\CurrentVersion\Run"
+REG_ITEM_NAME = f"ControllerCompanion"
+EXECUTABLE_PATH = resources.get_executable_path()
+OS = get_os()
+
+
+def autostart_supported() -> bool:
+    return OS in [OperatingSystem.WINDOWS]
+
+
+def set_auto_start(enable: bool):
+    if OS == OperatingSystem.WINDOWS:
+        if enable:
+            result = __set_reg_item(
+                REG_PATH, REG_ITEM_NAME, f'"{EXECUTABLE_PATH.absolute()}" "-m"'
+            )
+            logger.debug(f"set_reg_item {REG_PATH}/{REG_ITEM_NAME}: {result}")
+        else:
+            result = __delete_reg_item(REG_PATH, REG_ITEM_NAME)
+            logger.debug(f"delete_reg_item {REG_PATH}/{REG_ITEM_NAME}: {result}")
+
+    else:
+        logger.warning(f"Autostart not supported for {OS}.")
+
+
+def get_autostart_enabled() -> bool:
+    if OS == OperatingSystem.WINDOWS:
+        result: str = __get_reg_item(REG_PATH, REG_ITEM_NAME)
+        return (
+            # a value is set
+            result != None
+            # the file exists (it might have been moved by the user)
+            and Path(result.replace('"-m"', "").replace('"', "")).is_file()
+        )
+    else:
+        logger.warning(f"{OS} is not supported (get_autostart_enabled)")
+        return False
+
+
+# ---------------------------------- WINDOWS --------------------------------- #
+def __set_reg_item(path: str, name: str, value: str) -> bool:
+    try:
+        with winreg.CreateKey(winreg.HKEY_CURRENT_USER, path) as registry_key:
+            winreg.SetValueEx(registry_key, name, 0, winreg.REG_SZ, value)
+            return True
+    except WindowsError:
+        return False
+
+
+def __get_reg_item(path: str, name: str) -> Any:
+    try:
+        with winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_READ
+        ) as registry_key:
+            value, _ = winreg.QueryValueEx(registry_key, name)
+            return value
+    except WindowsError:
+        return None
+
+
+def __delete_reg_item(path, name):
+    try:
+        with winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_ALL_ACCESS
+        ) as registry_key:
+            winreg.DeleteValue(registry_key, name)
+            return True
+    except WindowsError as e:
+        logger.debug(e)
+        return False
