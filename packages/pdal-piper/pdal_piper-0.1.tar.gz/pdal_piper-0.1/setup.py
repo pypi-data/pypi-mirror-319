70ffcbcb4c088337108737c59b114f354643e0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:10:14 +0000

    Add package pdal-piper/0.1/pdal_piper-0.1.tar.gz

---
+++ packages/pdal-piper/pdal_piper-0.1.tar.gz/pdal_piper-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pdal-piper',
+    version='0.1',
+    packages=['pdal_piper'],
+    package_data={'pdal_piper': ['data/usgs_3dep_resources.geojson']},
+    include_package_data=True,
+    url='',
+    license='',
+    author='Johnathan Tenny (j-tenny)',
+    author_email='',
+    description=''
+)
