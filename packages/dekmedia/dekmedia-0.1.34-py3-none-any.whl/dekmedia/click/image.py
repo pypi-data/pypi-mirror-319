ed91895ca2af8d53e3455e22d2baaef6d5b1d5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:05:42 +0000

    Add package dekmedia/0.1.34/dekmedia-0.1.34-py3-none-any.whl

---
+++ packages/dekmedia/dekmedia-0.1.34-py3-none-any.whl/dekmedia/click/image.py
@@ -0,0 +1,50 @@
+import os
+import asyncio
+import typer
+from typing import List
+from typing_extensions import Annotated
+from dektools.file import read_file, normal_path
+from . import svg as svg_command
+
+app = typer.Typer(add_completion=False)
+app.add_typer(svg_command.app, name='svg')
+
+
+@app.command()
+def trans(src, outputs: Annotated[List[str], typer.Argument()] = None, sizes=''):
+    if not outputs:
+        return
+    if os.path.splitext(src)[-1].lower() == '.svg':
+        from ..image.svg import load_svg
+        image = load_svg(read_file(src))
+    else:
+        from PIL import Image
+        image = Image.open(src)
+    from ..image.core import resize_image
+    resize_image(image, outputs, parse_sizes(sizes))
+
+
+def parse_sizes(sizes):
+    if sizes:
+        ss = [int(n) for n in sizes.split(',')]
+        return list(zip(ss[::2], ss[1::2]))
+
+
+@app.command()
+def psd(src, dest: Annotated[str, typer.Argument()] = ""):
+    from ..image.psd import PsdCanvas
+    src = normal_path(src)
+    dest = dest if dest else os.path.join(src + '.dump')
+    pc = PsdCanvas.load(src)
+    pc.dump(dest)
+
+
+@app.command()
+def design(src, dest: Annotated[str, typer.Argument()] = ""):
+    src = normal_path(src)
+    if os.path.isfile(src):
+        src = os.path.dirname(src)
+    from ..image.design import ImageSvgSetManager
+    manager = ImageSvgSetManager()
+    manager.load_path(src)
+    asyncio.run(manager.dump(dest or os.path.join(src, 'dist')))
