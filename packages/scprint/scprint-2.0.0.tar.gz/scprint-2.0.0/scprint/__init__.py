9e0f877bce67e550dc7e25649dbd7f15d212e0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:30 +0000

    Add package scprint/2.0.0/scprint-2.0.0.tar.gz

---
+++ packages/scprint/scprint-2.0.0.tar.gz/scprint-2.0.0/scprint/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "1.6.4"
+
+from .model.model import scPrint
+
+# from .data_collator import DataCollator
+# from .data_sampler import SubsetsBatchSampler
+# from .trainer import (
+#    prepare_data,
+#    prepare_dataloader,
+#    train,
+#    define_wandb_metrcis,
+#    evaluate,
+#    eval_testdata,
+#    test,
+# )#
