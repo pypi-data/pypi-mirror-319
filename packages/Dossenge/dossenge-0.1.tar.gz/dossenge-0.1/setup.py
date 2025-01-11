81f2364dfdf3e6dbcbfc3dd02f504890847674d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:59 +0000

    Add package Dossenge/0.1/dossenge-0.1.tar.gz

---
+++ packages/Dossenge/dossenge-0.1.tar.gz/dossenge-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name="Dossenge",
+    version="0.1",
+    description="My First Package",
+    packages=find_packages(),
+    python_requires='>=3.0',
+)
