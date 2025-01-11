2be84aa84d169e12b999ff0ddf2e41acf64b879b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:08 +0000

    Add package oxgram/0.0.2/oxgram-0.0.2.tar.gz

---
+++ packages/oxgram/oxgram-0.0.2.tar.gz/oxgram-0.0.2/Oxgram/functions/function01.py
@@ -0,0 +1,16 @@
+from pyrogram.file_id import FileId
+#=========================================================================================================
+
+class Pyrogram:
+
+    async def get01(update):
+        medias = await Pyrogram.get01(update)
+        filexs = medias.file_id
+        fileud = FileId.decode(filexs)
+        moonus = fileud.dc_id
+        return moonus
+
+    async def get20(update):
+        return update.video or update.audio or update.voice or update.document or update.video_note
+
+#=========================================================================================================
---
+++ packages/oxgram/oxgram-0.0.2.tar.gz/oxgram-0.0.2/README.md
@@ -0,0 +1,14 @@
+<p align="center">
+ 📦 <a href="https://pypi.org/project/oxgram" style="text-decoration:none;">Oxgram</a>
+</p>
+
+
+```python
+
+from Oxgram.functions import Pyrogram
+
+file_dc = await Pyrogram.get01(update)
+
+print(file_dc)
+
+```
