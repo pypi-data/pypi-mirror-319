f5652d038a18e2413e28a9b3d79abc2a0fa99e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:10:03 +0000

    Add package icloudpd-web/0.1.1/icloudpd_web-0.1.1.tar.gz

---
+++ packages/icloudpd-web/icloudpd_web-0.1.1.tar.gz/icloudpd_web-0.1.1/src/icloudpd_web/webapp/_next/static/chunks/3d47b92a-c34d319c49004317.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[873],{6084:(h,t,a)=>{a.d(t,{Dwj:()=>c});var e=a(3435);function c(h){return(0,e.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M7 20h2V8h3L8 4 4 8h3zm13-4h-3V4h-2v12h-3l4 4z"},child:[]}]})(h)}}}]);
