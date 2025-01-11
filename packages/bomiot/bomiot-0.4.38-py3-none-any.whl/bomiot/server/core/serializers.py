b338ff2c3f585d0f3952f56345d264bf00ba6886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 11:05:47 +0000

    Add package bomiot/0.4.38/bomiot-0.4.38-py3-none-any.whl

---
+++ packages/bomiot/bomiot-0.4.38-py3-none-any.whl/bomiot/server/core/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True, required=False)
+    email = serializers.CharField(read_only=True, required=False)
+    phone = serializers.CharField(read_only=True, required=False)
+    permission = serializers.JSONField(read_only=True, required=False)
+    locked = serializers.BooleanField(read_only=True, required=False)
+    request_limit = serializers.IntegerField(read_only=True, required=False)
+    is_active = serializers.BooleanField(read_only=True, required=False)
+    date_joined = serializers.DateTimeField(read_only=True, required=False, format='%Y-%m-%d %H:%M:%S')
+    last_login = serializers.DateTimeField(read_only=True, required=False, format='%Y-%m-%d %H:%M:%S')
+    created_time = serializers.DateTimeField(read_only=True, required=False, format='%Y-%m-%d %H:%M:%S')
+    updated_time = serializers.DateTimeField(read_only=True, required=False, format='%Y-%m-%d %H:%M:%S')
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'phone', 'is_active', 'date_joined', 'last_login', 'updated_time']
+        read_only_fields = ['id']
