3b01cff006c801848a51a4632dff329c9ca415b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:05:35 +0000

    Add package tomtoolkit/2.22.5/tomtoolkit-2.22.5-py3-none-any.whl

---
+++ packages/tomtoolkit/tomtoolkit-2.22.5-py3-none-any.whl/tom_common/templatetags/tom_common_extras.py
@@ -0,0 +1,146 @@
+from django import template
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
+from django.conf import settings
+from django_comments.models import Comment
+from django.apps import apps
+from django.core.exceptions import FieldDoesNotExist
+from guardian.shortcuts import get_objects_for_user
+
+from tom_targets.models import Target
+
+register = template.Library()
+
+
+@register.inclusion_tag('tom_common/partials/navbar_login.html', takes_context=True)
+def navbar_login(context):
+    """
+    Renders the username as a link to the user page, as well as the login button. Can be overridden to render additional
+    account-related buttons.
+    """
+    return {'user': context['user']}
+
+
+@register.inclusion_tag('tom_common/partials/navbar_app_addons.html', takes_context=True)
+def navbar_app_addons(context):
+    """
+    Imports the navbar content from appropriate apps
+    This should be a list of partials containing an <li> element that you would like displayed in the navbar with the
+    following format:
+     `<li class="nav-item"> <a class="nav-link" href="{% url 'namespace:view_name' %}">Link Text</a> </li>`
+    """
+    nav_item_list = []
+    for app in apps.get_app_configs():
+        try:
+            nav_items = app.nav_items()
+            if nav_items:
+                for item in nav_items:
+                    nav_item_list.append(item)
+        except AttributeError:
+            pass
+    return {'nav_item_list': nav_item_list}
+
+
+@register.simple_tag
+def comments_enabled():
+    """
+    Returns the TOM setting specifying whether or not comments are enabled
+    """
+    try:
+        return settings.COMMENTS_ENABLED
+    except AttributeError:
+        return True
+
+
+@register.simple_tag
+def verbose_name(instance, field_name):
+    """
+    Displays the more descriptive field name from a Django model field
+    """
+    try:
+        return instance._meta.get_field(field_name).verbose_name.title()
+    except (FieldDoesNotExist, AttributeError):
+        return field_name.title()
+
+
+@register.simple_tag
+def help_text(instance, field_name):
+    """
+    Displays the help text from a Django model field
+    """
+    return instance._meta.get_field(field_name).help_text
+
+
+@register.inclusion_tag('comments/list.html', takes_context=True)
+def recent_comments(context, limit=10):
+    """
+    Displays a list of the most recent comments in the TOM up to the given limit, or 10 if not specified.
+
+    Comments will only be displayed for targets which the logged-in user has permission to view.
+    """
+    user = context['request'].user
+    targets_for_user = get_objects_for_user(user, f'{Target._meta.app_label}.view_target')
+
+    # In django-contrib-comments, the Comment model has a field ``object_pk`` which refers to the primary key
+    # of the object it is related to, i.e., a comment on a ``Target`` has an ``object_pk`` corresponding with the
+    # ``Target`` id. The ``object_pk`` is stored as a TextField.
+
+    # In order to filter on ``object_pk`` with an iterable of ``IntegerFields`` using the ``in`` comparator,
+    # we have to cast the ``object_pk`` to an int and annotate it as ``object_pk_as_int``.
+    return {
+        'comment_list': Comment.objects.annotate(
+            object_pk_as_int=Cast('object_pk', output_field=IntegerField())
+        ).filter(
+            object_pk_as_int__in=targets_for_user
+        ).order_by('-submit_date')[:limit]
+    }
+
+
+@register.filter
+def truncate_number(value):
+    """
+    Truncates a numerical value to four decimal places for display purposes. Etienne Bachelet advised that three
+    decimal places was insufficient precision, but that four would be more acceptable.
+    """
+    try:
+        return '%.4f' % value
+    except Exception:
+        return value
+
+
+@register.filter
+def multiplyby(value, arg):
+    """
+    Multiply the value by a number and return a float.
+    `{% value|multiplyby:"x.y" %}`
+    """
+    return float(value) * float(arg)
+
+
+@register.filter
+def addstr(arg1, arg2):
+    """
+    Concatenate strings arg1 & arg2.
+    This filter is necessary when adding strings because the default `|add:` will try to add the args as INTs first
+    rather than concatenating them.
+    Example:
+    ``` {% some_var|addstr:'string' %} ```
+    """
+    return str(arg1) + str(arg2)
+
+
+@register.simple_tag
+def tom_name():
+    return getattr(settings, 'TOM_NAME', 'TOM Toolkit')
+
+
+@register.inclusion_tag('tom_common/partials/copy_button.html')
+def copy_button(text_to_copy='', help_text='Copy'):
+    """Uses the `copy_button.html` partial to copy the `text_to_copy` to the clipboard.
+    The `help_text` is displayed as a tooltip when hovering over the button.
+    Use this tag to include a low-profile copy button in your template.
+    Example:
+    ```{% copy_button 'text to be copied' 'Help Text' %}```
+    """
+    return {'copy_text': str(text_to_copy),
+            'copy_help': str(help_text)}
---
+++ packages/tomtoolkit/tomtoolkit-2.22.5-py3-none-any.whl/tom_common/tests.py
@@ -0,0 +1,272 @@
+from http import HTTPStatus
+import tempfile
+
+from django.test import TestCase, override_settings
+
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django_comments.models import Comment
+
+from tom_targets.tests.factories import SiderealTargetFactory
+from tom_common.templatetags.tom_common_extras import verbose_name, multiplyby
+
+
+class TestCommonViews(TestCase):
+    def setUp(self):
+        pass
+
+    def test_index(self):
+        self.admin = User.objects.create_superuser(username='admin', password='admin', email='test@example.com')
+        self.client.force_login(self.admin)
+
+        response = self.client.get(reverse('home'))
+        # TODO: Use python http status enumerator in place of magic number everywhere
+        # from http import HTTPStatus
+        # assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestCommonExtras(TestCase):
+    def setUp(self):
+        pass
+
+    def test_verbose_name(self):
+        # Check that the verbose name for a model field is returned correctly
+        self.assertEqual(verbose_name(User, 'email'), 'Email Address')
+        # Check that the verbose name for a non-existent field is returned correctly
+        self.assertEqual(verbose_name(User, 'definitely_not_a_field'), 'Definitely_Not_A_Field')
+
+    def test_multiplyby(self):
+        # Check that the multiplyby template filter works correctly
+        self.assertEqual(multiplyby(2, 3), 6)
+        self.assertEqual(multiplyby(-3, 4), -12)
+        self.assertEqual(multiplyby(0.5, 5), 2.5)
+
+
+class TestUserManagement(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create_superuser(username='admin', password='admin', email='test@example.com')
+        self.client.force_login(self.admin)
+
+    def test_user_list(self):
+        user = User.objects.create(username='addme', password='addme')
+        response = self.client.get(reverse('user-list'))
+        self.assertContains(response, user.username)
+
+    def test_user_create(self):
+        user_data = {
+            'profile-TOTAL_FORMS': '1',
+            'profile-INITIAL_FORMS': '0',
+            'username': 'testuser',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'testuser@example.com',
+            'password1': 'suchsecure543',
+            'password2': 'suchsecure543',
+        }
+        response = self.client.post(reverse('user-create'), data=user_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_user_change_password(self):
+        user = User.objects.create(username='changemypass', email='changeme@example.com', password='unchanged')
+        response = self.client.post(
+            reverse('admin-user-change-password', kwargs={'pk': user.id}), data={'password': 'changed'}
+        )
+        self.assertEqual(response.status_code, 302)
+        user.refresh_from_db()
+        self.assertTrue(user.check_password('changed'))
+
+    def test_user_delete(self):
+        user = User.objects.create(username='deleteme', email='deleteme@example.com', password='deleted')
+        response = self.client.post(
+            reverse('user-delete', kwargs={'pk': user.id})
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(User.objects.filter(pk=user.id).exists())
+
+    def test_non_superuser_cannot_delete_other_user(self):
+        user = User.objects.create(username='deleteme', email='deleteme@example.com', password='deleted')
+        other_user = User.objects.create_user(username='other', email='other@example.com', password='other')
+        self.client.force_login(user)
+        response = self.client.post(reverse('user-delete', kwargs={'pk': other_user.id}))
+        self.assertRedirects(response, reverse('user-delete', kwargs={'pk': user.id}))
+
+    def test_must_be_superuser(self):
+        user = User.objects.create_user(username='notallowed', email='notallowed@example.com', password='notallowed')
+        self.client.force_login(user)
+        response = self.client.get(reverse('admin-user-change-password', kwargs={'pk': user.id}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_can_update_self(self):
+        user = User.objects.create(username='luke', password='forc3')
+        self.client.force_login(user)
+        user_data = {
+            'profile-TOTAL_FORMS': '1',
+            'profile-INITIAL_FORMS': '0',
+            'username': 'luke',
+            'first_name': 'Luke',
+            'last_name': 'Skywalker',
+            'email': 'luke@example.com',
+            'password1': 'forc34eva!',
+            'password2': 'forc34eva!',
+        }
+        response = self.client.post(reverse('user-update', kwargs={'pk': user.id}), data=user_data, follow=True)
+        user.refresh_from_db()
+        self.assertEqual(user.first_name, 'Luke')
+        self.assertContains(response, 'Profile updated')
+
+    def test_user_cannot_update_other(self):
+        user = User.objects.create(username='luke', password='forc3')
+        self.client.force_login(user)
+        user_data = {
+            'profile-TOTAL_FORMS': '1',
+            'profile-INITIAL_FORMS': '0',
+            'username': 'luke',
+            'first_name': 'Luke',
+            'last_name': 'Skywalker',
+            'email': 'luke@example.com',
+            'password1': 'forc34eva!',
+            'password2': 'forc34eva!',
+        }
+        response = self.client.post(reverse('user-update', kwargs={'pk': self.admin.id}), data=user_data)
+        self.admin.refresh_from_db()
+        self.assertRedirects(response, reverse('user-update', kwargs={'pk': user.id}))
+        self.assertNotEqual(self.admin.username, user_data['username'])
+
+    def test_user_can_delete_self(self):
+        user = User.objects.create(username='luke', password='forc3')
+        self.client.force_login(user)
+        self.assertTrue(User.objects.filter(username='luke').exists())
+        response = self.client.post(reverse('user-delete', kwargs={'pk': user.id}), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username='luke').exists())
+
+
+class TestUserProfile(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create_superuser(username='admin', password='admin', email='test@example.com')
+        self.client.force_login(self.admin)
+
+    def test_user_profile(self):
+        user_data = {
+            'profile-TOTAL_FORMS': '1',
+            'profile-INITIAL_FORMS': '0',
+            'username': 'testuser',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'testuser@example.com',
+            'password1': 'suchsecure543',
+            'password2': 'suchsecure543',
+            'profile-0-affiliation': 'Test University',
+        }
+        response = self.client.post(reverse('user-create'), data=user_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        user = User.objects.get(username='testuser')
+        self.assertEqual(user.profile.affiliation, 'Test University')
+
+
+class TestAuthScheme(TestCase):
+    @override_settings(AUTH_STRATEGY='LOCKED')
+    def test_user_cannot_access_view(self):
+        response = self.client.get(reverse('tom_targets:list'))
+        self.assertRedirects(
+            response, reverse('login') + '?next=' + reverse('tom_targets:list'), status_code=302
+        )
+
+    @override_settings(AUTH_STRATEGY='READ_ONLY')
+    def test_user_can_access_view(self):
+        response = self.client.get(reverse('tom_targets:list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Create Targets')
+
+
+class CommentDeleteViewTest(TestCase):
+    def setUp(self):
+        self.site = Site.objects.get_current()
+        self.user = User.objects.create_user(username='user', password='password')
+        self.superuser = User.objects.create_superuser(username='admin', password='admin')
+
+        # Create a content object and a comment linked to that object.
+        self.content_object = SiderealTargetFactory.create(ra=123.456, dec=-32.1)
+        self.comment = Comment.objects.create(user=self.user, content_object=self.content_object,
+                                              comment="Test Comment", site=self.site)
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('comment-delete', kwargs={'pk': self.comment.pk}))
+        self.assertRedirects(response, f'/accounts/login/?next=/comment/{self.comment.pk}/delete')
+
+    def test_logged_in_but_not_author_or_superuser(self):
+        another_user = User.objects.create_user(username='another_user', password='password')
+        self.client.force_login(another_user)
+        response = self.client.post(reverse('comment-delete', kwargs={'pk': self.comment.pk}))
+        # Somewhere in TOM redirects the user to login as a user with correct permissions rather than
+        # send to 403 page.
+        self.assertEqual(response.status_code, 302)
+
+    def test_superuser_can_delete(self):
+        self.client.force_login(self.superuser)
+        response = self.client.post(reverse('comment-delete', kwargs={'pk': self.comment.pk}))
+        self.assertRedirects(response, self.content_object.get_absolute_url())
+        self.assertFalse(Comment.objects.filter(pk=self.comment.pk).exists())
+
+    def test_author_can_delete(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('comment-delete', kwargs={'pk': self.comment.pk}), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(Comment.objects.filter(pk=self.comment.pk).exists())
+
+
+class TestRobotsDotTxt(TestCase):
+    def test_get(self):
+        """Test that the robots.txt file is served correctly.
+        """
+        response = self.client.get("/robots.txt")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response["content-type"] == "text/plain"
+        assert response.content.startswith(b"User-Agent: *\n")  # known a priori from default robots.txt
+
+    def test_post_disallowed(self):
+        """Test that a User-Agent can not POST to the /robots.txt endpoint.
+        """
+        response = self.client.post("/robots.txt")
+
+        assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_custom_robots_txt(self):
+        """Test that a custom robots.txt file is served if it exists at the
+        path specified in the settings as `ROBOTS_TXT_PATH`.
+        """
+        file_content = b'User-Agent: *\nDisallow: /\n'
+
+        # create a temporary file with the custom robots.txt content
+        with tempfile.NamedTemporaryFile() as fp:
+            fp.write(file_content)
+            fp.flush()
+            # set the settings.ROBOTS_TXT_PATH to the whatever the path of the temporary file is
+            with self.settings(ROBOTS_TXT_PATH=fp.name):
+                response = self.client.get("/robots.txt")
+
+                assert response.status_code == HTTPStatus.OK
+                assert response["content-type"] == "text/plain"
+                assert response.content.startswith(file_content)
+
+    def test_nonexistent_custom_robots_txt(self):
+        """Test that (1) the default robots.txt is served if the file specified in
+        the settings as `ROBOTS_TXT_PATH` does not exist, and (2) a warning is logged.
+        """
+        # set the settings.ROBOTS_TXT_PATH to a nonexistent file
+        with self.settings(ROBOTS_TXT_PATH="/nonexistent/file"):
+            # create a context manager to capture the warning logs
+            with self.assertLogs(logger="tom_common.views", level="WARNING") as logs:
+                response = self.client.get("/robots.txt")  # make the request; a warning should be logged...
+                # and check that the warning was logged
+                self.assertIn('Default robots.txt served', logs.output[0])
+
+            # now check that the default robots.txt was served
+            assert response.status_code == HTTPStatus.OK
+            assert response["content-type"] == "text/plain"
+            # check for default content
+            assert response.content.startswith(b"User-Agent: *\n")  # known a priori from default robots.txt
