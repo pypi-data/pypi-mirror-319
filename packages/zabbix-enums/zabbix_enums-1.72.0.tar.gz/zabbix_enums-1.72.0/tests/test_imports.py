5a847c8ca3d076cc8ed4ee15e8cb45c546de0b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:58 +0000

    Add package zabbix-enums/1.72.0/zabbix_enums-1.72.0.tar.gz

---
+++ packages/zabbix-enums/zabbix_enums-1.72.0.tar.gz/zabbix_enums-1.72.0/tests/test_imports.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from zabbix_enums.z50 import *
+from zabbix_enums.z52 import *
+from zabbix_enums.z54 import *
+from zabbix_enums.z60 import *
+from zabbix_enums.z62 import *
+from zabbix_enums.z64 import *
+from zabbix_enums.z70 import *
+from zabbix_enums.z72 import *
