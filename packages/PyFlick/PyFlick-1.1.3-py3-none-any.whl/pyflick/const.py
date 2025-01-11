59df8568994bbd2a7681de61acbd3cccf4892bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:11:29 +0000

    Add package PyFlick/1.1.3/PyFlick-1.1.3-py3-none-any.whl

---
+++ packages/PyFlick/PyFlick-1.1.3-py3-none-any.whl/pyflick/const.py
@@ -0,0 +1,5 @@
+DEFAULT_CLIENT_ID = "le37iwi3qctbduh39fvnpevt1m2uuvz"
+DEFAULT_CLIENT_SECRET = "ignwy9ztnst3azswww66y9vd9zt6qnt"
+
+DEFAULT_API_HOST = "https://api.flick.energy"
+DEFAULT_AUTH_ENDPOINT = "/identity/oauth/token"
