8ae123ed49c2a4925437e5c9b8fbfe99e418c03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:57 +0000

    Add package neurospeed/2.2.9/neurospeed-2.2.9-py3-none-any.whl

---
+++ packages/neurospeed/neurospeed-2.2.9-py3-none-any.whl/neurospeed/utils/api_config_service.py
@@ -0,0 +1,37 @@
+
+
+from neurospeed.utils.helper_service import UtilService as utils
+from pathlib import Path
+import os
+
+class ApiConfig:
+
+    def __init__(self, api_config = {
+            "api_address": "https://api.neurospeed.io",
+            "pipeline_address": "wss://api.neurospeed.io"
+        }):
+        self._contex = "ApiConfig - "
+        
+        '''
+        precedence of config path options: 
+        
+        if C:/neurobrave/hia_config.json exists, it's used
+        otherwise, if /config/api_config.json exists under the runtime folder, its' used
+        it none of these exist, defauilt that comes with the neurospeed library is used.
+            
+        '''
+        
+        self._api_config = api_config
+        self._api_http_url = api_config["api_address"]
+        self._pipeline_url = api_config["pipeline_address"]
+        
+        # print("api config path: " + self._config_path)
+        print("{} NeuroSpeed API HTTP URL: {} ".format(self._contex, self._api_http_url))
+        print("{} NeuroSpeed API SOCKET URL: {} ".format(self._contex, self._pipeline_url))
+        
+    def get_http_url(self):
+        return self._api_http_url
+    
+    def get_socket_url(self):
+        return self._pipeline_url
+        
