d0da47311b03dc33b55b2023828444956bb580c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:46 +0000

    Add package javascan/0.2/javascan-0.2.tar.gz

---
+++ packages/javascan/javascan-0.2.tar.gz/javascan-0.2/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'javascan',         
+  packages = ['javascan'],   
+  version = '0.2', 
+  license='MIT',       
+  description = 'Java Code Linter',   
+  author = 'Akshay',                   
+  author_email = '',     
+  url = 'https://github.com/akshay-kumar-91/javascan',   
+  download_url = 'https://github.com/akshay-kumar-91/javascan/archive/v_01.tar.gz',    
+  keywords = [],   
+  install_requires=[
+      'cryptography',
+      'requests',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+
+# Check dependecies were installed
+
+import os
+os.makedirs('/tmp/dependencies', exist_ok=True)
