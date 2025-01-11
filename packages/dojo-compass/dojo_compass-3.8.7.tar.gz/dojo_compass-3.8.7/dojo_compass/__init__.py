902ed08847e3923ef176ed0071ee33e1c085df7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:07:19 +0000

    Add package dojo-compass/3.8.7/dojo_compass-3.8.7.tar.gz

---
+++ packages/dojo-compass/dojo_compass-3.8.7.tar.gz/dojo_compass-3.8.7/dojo_compass/__init__.py
@@ -0,0 +1,7 @@
+"""This folder tricks poetry into publishing the package under dojo_compass in PyPi.
+
+It is automatically overwritten when releasing to contain the github SHA of the source repository.
+"""
+
+version = None
+version="01732ec017dd6d4ea14151661e1f49a509f5a721"
