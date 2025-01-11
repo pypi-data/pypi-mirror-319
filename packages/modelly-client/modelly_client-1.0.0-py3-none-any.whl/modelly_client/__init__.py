9212c93142ac450fcca7b18d3d456608e6ec115c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:10:51 +0000

    Add package modelly-client/1.0.0/modelly_client-1.0.0-py3-none-any.whl

---
+++ packages/modelly-client/modelly_client-1.0.0-py3-none-any.whl/modelly_client/__init__.py
@@ -0,0 +1,11 @@
+from modelly_client.client import Client
+from modelly_client.data_classes import FileData
+from modelly_client.utils import __version__, file, handle_file
+
+__all__ = [
+    "Client",
+    "file",
+    "handle_file",
+    "FileData",
+    "__version__",
+]
---
+++ packages/modelly-client/modelly_client-1.0.0-py3-none-any.whl/modelly_client/cli/__init__.py
@@ -0,0 +1,3 @@
+from modelly_client.cli import deploy_discord
+
+__all__ = ["deploy_discord"]
