d77ca7689164601e398f74cf5264d3abcd797d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:05:41 +0000

    Add package dinamis-sdk/0.3.8/dinamis_sdk-0.3.8-py3-none-any.whl

---
+++ packages/dinamis-sdk/dinamis_sdk-0.3.8-py3-none-any.whl/dinamis_sdk/settings.py
@@ -0,0 +1,15 @@
+"""Settings from environment variables."""
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Holds settings."""
+
+    dinamis_sdk_ttl_margin: int = 1800
+    dinamis_sdk_url_duration: int = 0
+    dinamis_sdk_bypass_api: str = ""
+    dinamis_sdk_token_server: str = ""
+    dinamis_sdk_settings_dir: str = ""
+    dinamis_sdk_access_key: str = ""
+    dinamis_sdk_secret_key: str = ""
