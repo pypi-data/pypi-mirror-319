df3a7fc97e9c8769af8af805febab03816c139bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:11:03 +0000

    Add package basedmypy/2.9.1/basedmypy-2.9.1-cp312-cp312-win_amd64.whl

---
+++ packages/basedmypy/basedmypy-2.9.1-cp312-cp312-win_amd64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "1.14.1"
+based_version_info = VersionInfo(major=2, minor=9, patch=1, release_level='final', iteration=0, mypy_version='1.14.1', mypy_release_level='final')
+__based_version__ = "2.9.1"
