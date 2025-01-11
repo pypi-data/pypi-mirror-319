aee5f1f43e360a07598a1f27e6e5acbc092340d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:05:51 +0000

    Add package usethis/0.3.0/usethis-0.3.0-py3-none-any.whl

---
+++ packages/usethis/usethis-0.3.0-py3-none-any.whl/usethis/__main__.py
@@ -0,0 +1,20 @@
+import typer
+
+import usethis._interface.browse
+import usethis._interface.ci
+import usethis._interface.show
+import usethis._interface.tool
+
+app = typer.Typer(
+    help=(
+        "Automate Python package and project setup tasks that are otherwise "
+        "performed manually."
+    )
+)
+app.add_typer(usethis._interface.tool.app, name="tool")
+app.add_typer(usethis._interface.browse.app, name="browse")
+app.add_typer(usethis._interface.ci.app, name="ci")
+app.add_typer(usethis._interface.show.app, name="show")
+app(prog_name="usethis")
+
+__all__ = ["app"]
