d3fed908113c9d6cb0362cf26cf016f74dd525bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:09:01 +0000

    Add package squiid-parser/0.1.1/squiid_parser-0.1.1-cp310-cp310-win32.whl

---
+++ packages/squiid-parser/squiid_parser-0.1.1-cp310-cp310-win32.whl/squiid_parser/__init__.py
@@ -0,0 +1,9 @@
+"""Squiid parser bindings.
+
+This module includes basic bindings to the Squiid parser that allow you to
+parse a string into a list of tokens with the `parse` function
+"""
+
+from squiid_parser._backend import SquiidParser
+
+__all__ = ["SquiidParser"]
